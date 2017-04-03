72aa60616db9abf475aa97221b79f95569018f4d
Flynn <flynn@datawire.io>
2017-04-03 14:59:26 -0400

    Bump version: 0.1.6 → 0.1.7

--- ambassador/ambassador.py
+++ ambassador/ambassador.py
@@ -23,12 +23,12 @@ MyHostName = socket.gethostname()
 MyResolvedName = socket.gethostbyname(socket.gethostname())
 
 # Don't change this line without also changing .bumpversion.cfg
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 logging.basicConfig(
     # filename=logPath,
     level=logging.DEBUG, # if appDebug else logging.INFO,
-    format="%(asctime)s ambassador 0.1.6 %(levelname)s: %(message)s",
+    format="%(asctime)s ambassador 0.1.7 %(levelname)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
