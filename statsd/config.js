1a50bf32f64a3677d46d58308e0489c9c40f5299
Abhay Saxena <ark3@datawire.io>
2017-04-24 16:28:17 -0400

    Clean up statsd config a bit; increase flush rate for more fun in dev

--- statsd/config.js
+++ statsd/config.js
@@ -1,10 +1,16 @@
 (function () {
     "use strict";
     return {
+        // Production configuration
+        //"backends": ["./backends/repeater"],
+        //"flushInterval": 10000,
+
+        // Development configuration
         "debug": true,
         "backends": ["./backends/repeater", "./backends/console"],
+        "flushInterval": 1000,
+
         "repeater": [ { "host": "statsd-sink", "port": 8125 } ],
-        //"repeaterProtocol": "tcp"  // Default: "udp4"
         "repeaterProtocol": "upd4"
     };
 })();
