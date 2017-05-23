2795dc93230dbd47f17d2aac49d1b2436539bb07
Flynn <flynn@datawire.io>
2017-05-23 17:21:50 -0400

    Fix istio/ambassador.yaml and template. Update version in some examples.

--- templates/ambassador-istio.yaml.sh
+++ templates/ambassador-istio.yaml.sh
@@ -62,7 +62,7 @@ spec:
     spec:
       containers:
       - name: ambassador
-        image: ${AMREG}ambassador:0.8.6
+        image: ${AMREG}ambassador:0.8.7
         imagePullPolicy: Always
         env:
         - name: AMBASSADOR_DB_HOST
@@ -80,7 +80,7 @@ spec:
         - mountPath: /etc/cacert
           name: cacert-data
       - name: statsd
-        image: ${STREG}statsd:0.8.6
+        image: ${STREG}statsd:0.8.7
       volumes:
       - name: cert-data
         secret:
