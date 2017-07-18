694b531ab875c65996006f22ee5d558026ddbfba
Abhay Saxena <ark3@datawire.io>
2017-07-18 14:50:38 -0400

    Fix sed command for Mac and GNU hopefully

--- build-website.sh
+++ build-website.sh
@@ -15,7 +15,8 @@ npm run build
 # which are the ones marked with data-level="1.1". This causes the GitBook
 # scripts to redirect to the index page rather fetching and replacing just
 # the content area, as they do for proper GitBook-generated pages.
-sed -i"" 's,<li class="chapter " data-level="1.1" data-path="[^"]*">,<li class="chapter " data-level="1.1">,' $(fgrep -rl 'data-level="1.1"' _book)
+sed -i.maclinuxincompat 's,<li class="chapter " data-level="1.1" data-path="[^"]*">,<li class="chapter " data-level="1.1">,' $(fgrep -rl 'data-level="1.1"' _book)
+find . -type f -name "*.maclinuxincompat" -print0 | xargs -0 rm -f
 
 # Replace index.html with our hand-crafted landing page
 cp index.html _book/
