8f99e6e9934869b561c889da88ce3625971df194
Flynn <flynn@datawire.io>
2017-09-15 16:18:15 -0400

    Force draft website to refer to dwflynn registry.

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -72,6 +72,7 @@ if [ $doc_changes -gt 0 ]; then
     else
         NETLIFY_DRAFT=--draft
         HRDRAFT=" (draft)"
+        DOCKER_REGISTRY=dwflynn
     fi
 
     echo "==== BUILDING DOCS FOR ${VERSION}${HRDRAFT}"
