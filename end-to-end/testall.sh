cceb48b831d313ce3a09d59edfe321aab91d92fc
Flynn <flynn@datawire.io>
2017-11-20 15:31:00 -0500

    Make the demo-service common across tests.

--- end-to-end/testall.sh
+++ end-to-end/testall.sh
@@ -6,6 +6,17 @@ HERE=$(cd $(dirname $0); pwd)
 
 cd "$HERE"
 
+echo "Building images"
+
+set -x
+docker build -q -t dwflynn/demo:1.0.0 --build-arg VERSION=1.0.0 demo-service
+docker build -q -t dwflynn/demo:2.0.0 --build-arg VERSION=2.0.0 demo-service
+
+# seriously? there's no docker push --quiet???
+docker push dwflynn/demo:1.0.0
+docker push dwflynn/demo:2.0.0
+set +x
+
 for dir in 0*; do
     echo "========"
     echo "${dir}..."
