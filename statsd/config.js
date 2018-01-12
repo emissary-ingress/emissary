74d7c9551431bb900258373bee20693c851164bc
Flynn <flynn@datawire.io>
2018-01-12 13:48:53 -0500

    Let's just do this the more graceful way, shall we?

--- statsd/config.js
+++ statsd/config.js
@@ -2,14 +2,13 @@
     "use strict";
     return {
         // Production configuration
-        //"backends": ["./backends/repeater"],
-        //"flushInterval": 10000,
+        "backends": ["./backends/repeater"],
+        "flushInterval": 10000,
 
         // Development configuration
-        "debug": false,
-        "backends": [ "./backends/repeater" ],
-        // , "./backends/console"],
-        "flushInterval": 10000,
+        // "debug": true,
+        // "backends": ["./backends/repeater", "./backends/console"],
+        // "flushInterval": 1000,
 
         "repeater": [ { "host": "statsd-sink", "port": 8125 } ],
         "repeaterProtocol": "upd4"
