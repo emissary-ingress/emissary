ff7cca1f7115fd67a5d9907f2be4fe3a46200760
Flynn <flynn@datawire.io>
2018-02-06 15:07:05 -0500

    Make Envoy-restarting parameters tunable via environment variables.

--- ambassador/start-envoy.sh
+++ ambassador/start-envoy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+DRAIN_TIME=${AMBASSADOR_DRAIN_TIME:-2}
+SHUTDOWN_TIME=${AMBASSADOR_SHUTDOWN_TIME:-3}
+
 LATEST=$(ls -1v /etc/envoy*.json | tail -1)
-exec /usr/local/bin/envoy -c ${LATEST} --restart-epoch $RESTART_EPOCH --drain-time-s 2 --parent-shutdown-time-s 3
+exec /usr/local/bin/envoy -c ${LATEST} --restart-epoch $RESTART_EPOCH --drain-time-s "${DRAIN_TIME}" --parent-shutdown-time-s "${SHUTDOWN_TIME}"
