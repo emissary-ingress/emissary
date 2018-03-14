b570673ee6e5437d87c7e252924b91f9c229b8cc
Flynn <flynn@datawire.io>
2018-03-14 15:35:14 -0400

    Extend default restart times and initial liveness/readiness check delays.

--- ambassador/start-envoy.sh
+++ ambassador/start-envoy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-DRAIN_TIME=${AMBASSADOR_DRAIN_TIME:-2}
-SHUTDOWN_TIME=${AMBASSADOR_SHUTDOWN_TIME:-3}
+DRAIN_TIME=${AMBASSADOR_DRAIN_TIME:-5}
+SHUTDOWN_TIME=${AMBASSADOR_SHUTDOWN_TIME:-10}
 
 LATEST=$(ls -1v /etc/envoy*.json | tail -1)
 exec /usr/local/bin/envoy -c ${LATEST} --restart-epoch $RESTART_EPOCH --drain-time-s "${DRAIN_TIME}" --parent-shutdown-time-s "${SHUTDOWN_TIME}"
