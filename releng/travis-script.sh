a4d4225f453d0f36fcbfcf3f1d6f9af61d3cb8a7
Philip Lombardi <plombardi@datawire.io>
2018-04-12 16:22:37 -0400

    run tests; build docker image with commit hash tag

--- releng/travis-script.sh
+++ releng/travis-script.sh
@@ -7,4 +7,25 @@ git_commit="$2"
 
 printf "== Begin: travis-script.sh (branch: $git_branch, commit: $git_commit) ==\n"
 
+make deps-check
+make clean
+
+printf "== Begin: execute tests\n"
+
+make setup-develop
+make test
+
+printf "== End:   execute tests\n"
+
+printf "== Begin: build docker image\n"
+
+docker build -t quay.io/datawire/ambassador-gh369:${git_commit} ambassador
+docker push quay.io/datawire/ambassador-gh369:${git_commit}
+
+printf "== End:   build docker image\n"
+
+printf "== Begin: create documentation draft\n"
+
+printf "== End:   create documentation draft\n"
+
 printf "== End:   travis-script.sh ==\n"
