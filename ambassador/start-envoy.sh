7f553254370d3ea36e9f11458d312c1986fde6ac
Shubham <shubham@linux.com>
2018-06-11 17:23:51 +0530

    Allow running ambassador as a non-privileged user
    This commit lets ambassador to be run as a non-root user and
    moves all ambassador related configurations to /ambassador
    inside the container.
    
    Fix #457

--- ambassador/start-envoy.sh
+++ ambassador/start-envoy.sh
@@ -2,6 +2,7 @@
 
 DRAIN_TIME=${AMBASSADOR_DRAIN_TIME:-5}
 SHUTDOWN_TIME=${AMBASSADOR_SHUTDOWN_TIME:-10}
+AMBASSADOR_ROOT="/ambassador"
 
-LATEST=$(ls -1v /etc/envoy*.json | tail -1)
+LATEST=$(ls -1v "$AMBASSADOR_ROOT"/envoy*.json | tail -1)
 exec /usr/local/bin/envoy -c ${LATEST} --restart-epoch $RESTART_EPOCH --drain-time-s "${DRAIN_TIME}" --parent-shutdown-time-s "${SHUTDOWN_TIME}"
