ac300f87b19f1f04b04c97d4c60941156a2e9557
Flynn <flynn@datawire.io>
2018-06-22 00:40:08 -0400

    Switch to cluster instead of weighted_cluster for websockets, and allow only one upstream. Includes basic (positive) E2E test.

---
+++ end-to-end/011-websocket/test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+HERE=$(cd $(dirname $0); pwd)
+
+cd "$HERE"
+
+ROOT=$(cd ..; pwd)
+PATH="${ROOT}:${PATH}"
+
+source ${ROOT}/utils.sh
+
+initialize_namespace "ambassador-sbx0"
+
+# Make sure cluster-wide RBAC is set up.
+kubectl apply -f ${ROOT}/rbac.yaml
+
+kubectl cluster-info
+
+set +e +o pipefail
+
+cd ambassador
+forge --profile=sandbox0 deploy
+cd ../web-basic
+forge --profile=sandbox0 deploy
+cd ..
+
+wait_for_pods ambassador-sbx0
+
+CLUSTER=$(cluster_ip)
+APORT=$(service_port ambassador ambassador-sbx0)
+
+BASEURL="http://${CLUSTER}:${APORT}"
+
+echo "Base URL $BASEURL"
+echo "Diag URL $BASEURL/ambassador/v0/diag/"
+
+wait_for_ready "$BASEURL"
+
+if ! check_diag "$BASEURL" 1 "No annotated services"; then
+    exit 1
+fi
+
+python web-basic/wscat.py ws://${CLUSTER}:${APORT}/ws
+
+# kubernaut discard
