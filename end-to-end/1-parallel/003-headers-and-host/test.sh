108e9501b4eb46e0c406f9af62e508eea96053f6
Shubham <shubham@linux.com>
2018-07-12 21:20:35 +0530

    Parallelize 003-headers-and-host e2e test

--- end-to-end/1-parallel/003-headers-and-host/test.sh
+++ end-to-end/1-parallel/003-headers-and-host/test.sh
@@ -16,33 +16,34 @@
 
 set -e -o pipefail
 
-HERE=$(cd $(dirname $0); pwd)
+NAMESPACE="003-headers-and-host"
 
-cd "$HERE"
-
+cd $(dirname $0)
 ROOT=$(cd ../..; pwd)
-PATH="${ROOT}:${PATH}"
-
 source ${ROOT}/utils.sh
+bootstrap ${NAMESPACE} ${ROOT}
 
-initialize_cluster
+python ${ROOT}/yfix.py ${ROOT}/fixes/ambassador-id.yfix \
+    ${ROOT}/ambassador-deployment.yaml \
+    k8s/ambassador-deployment.yaml \
+    ${NAMESPACE} \
+    ${NAMESPACE}
 
-kubectl cluster-info
-
+kubectl apply -f k8s/rbac.yaml
 kubectl apply -f k8s/ambassador.yaml
 kubectl apply -f k8s/cors.yaml
 kubectl apply -f k8s/demo1.yaml
 kubectl apply -f k8s/demo2.yaml
-kubectl apply -f ${ROOT}/ambassador-deployment.yaml
+kubectl apply -f k8s/ambassador-deployment.yaml
 kubectl run demotest --image=dwflynn/demotest:0.0.1 -- /bin/sh -c "sleep 3600"
 
 set +e +o pipefail
 
-wait_for_pods
+wait_for_pods ${NAMESPACE}
 
 CLUSTER=$(cluster_ip)
-APORT=$(service_port ambassador)
-DEMOTEST_POD=$(demotest_pod)
+APORT=$(service_port ambassador ${NAMESPACE})
+DEMOTEST_POD=$(demotest_pod ${NAMESPACE})
 
 BASEURL="http://${CLUSTER}:${APORT}"
 
@@ -64,7 +65,7 @@ if ! kubectl exec -i $DEMOTEST_POD -- python3 demotest.py "$BASEURL" /dev/fd/0 <
 fi
 
 kubectl apply -f k8s/canary-50.yaml
-wait_for_pods
+wait_for_pods ${NAMESPACE}
 wait_for_demo_weights "$BASEURL" x-demo-mode=canary 50 50
 
 if ! check_diag "$BASEURL" 2 "Canary 50/50"; then
@@ -78,7 +79,7 @@ if ! kubectl exec -i $DEMOTEST_POD -- python3 demotest.py "$BASEURL" /dev/fd/0 <
 fi
 
 kubectl apply -f k8s/canary-100.yaml
-wait_for_pods
+wait_for_pods ${NAMESPACE}
 wait_for_demo_weights "$BASEURL" x-demo-mode=canary 100
 
 if ! check_diag "$BASEURL" 3 "Canary 100"; then
@@ -91,3 +92,6 @@ if ! kubectl exec -i "$DEMOTEST_POD" -- python3 demotest.py "$BASEURL" /dev/fd/0
     exit 1
 fi
 
+if [ -n "$CLEAN_ON_SUCCESS" ]; then
+    drop_namespace ${NAMESPACE}
+fi
