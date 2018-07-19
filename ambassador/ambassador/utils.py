cliveseldon/ambassador

Allow arbitrary secrets for TLS config
Before this commit, hardcoded secret names "ambassador-certs" and
"ambassador-cacert" were required to be configured for TLS
termination and TLS client authentication.

This commit lets users configure both of these secrets in the TLS
module annotation.

This change is backwards compatible, and if a user has the
hardcoded secrets set, then it will continue to work as usual and
hardcoded secrets will take precedence over manually configured
secrets.

Some refactor that has been done includes taking some Kubernetes
specific helper functions from kubewatch.py and moving those to
ambassador/utils.py.

ambassador/ambassador/utils.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
-
-import sys
-
+import binascii
 import socket
 import threading
 import time
+import os
+import logging
+
+from kubernetes import client, config
+from enum import Enum
 
 from .VERSION import Version
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+
+
+class TLSPaths(Enum):
+    cert_dir = "/etc/certs"
+    tls_crt = os.path.join(cert_dir, "tls.crt")
+    tls_key = os.path.join(cert_dir, "tls.key")
+    client_cert_dir = "/etc/cacert"
+    client_tls_crt = os.path.join(client_cert_dir, "tls.crt")
+
 
 class SystemInfo (object):
     MyHostName = socket.gethostname()
@@ -130,3 +145,84 @@ class PeriodicTrigger(threading.Thread):
         while True:
             time.sleep(self.period)
             self.onfired()
+
+
+def read_cert_secret(k8s_api, secret_name, namespace):
+    cert_data = None
+    cert = None
+    key = None
+
+    try:
+        cert_data = k8s_api.read_namespaced_secret(secret_name, namespace)
+    except client.rest.ApiException as e:
+        if e.reason == "Not Found":
+            pass
+        else:
+            logger.info("secret %s/%s could not be read: %s" % (namespace, secret_name, e))
+
+    if cert_data and cert_data.data:
+        cert_data = cert_data.data
+        cert = cert_data.get('tls.crt', None)
+
+        if cert:
+            cert = binascii.a2b_base64(cert)
+
+        key = cert_data.get('tls.key', None)
+
+        if key:
+            key = binascii.a2b_base64(key)
+
+    return (cert, key, cert_data)
+
+
+def save_cert(cert, key, dir):
+    try:
+        os.makedirs(dir)
+    except FileExistsError:
+        pass
+
+    open(os.path.join(dir, "tls.crt"), "w").write(cert.decode("utf-8"))
+
+    if key:
+        open(os.path.join(dir, "tls.key"), "w").write(key.decode("utf-8"))
+
+
+def kube_v1():
+    # Assume we got nothin'.
+    k8s_api = None
+
+    # XXX: is there a better way to check if we are inside a cluster or not?
+    if "KUBERNETES_SERVICE_HOST" in os.environ:
+        # If this goes horribly wrong and raises an exception (it shouldn't),
+        # we'll crash, and Kubernetes will kill the pod. That's probably not an
+        # unreasonable response.
+        config.load_incluster_config()
+        k8s_api = client.CoreV1Api()
+    else:
+        # Here, we might be running in docker, in which case we'll likely not
+        # have any Kube secrets, and that's OK.
+        try:
+            config.load_kube_config()
+            k8s_api = client.CoreV1Api()
+        except FileNotFoundError:
+            # Meh, just ride through.
+            logger.info("No K8s")
+            pass
+
+    return k8s_api
+
+
+def check_cert_file(path):
+    readable = False
+
+    try:
+        data = open(path, "r").read()
+
+        if data and (len(data) > 0):
+            readable = True
+    except OSError:
+        pass
+    except IOError:
+        pass
+
+    return readable

emissary-ingress/emissary

ambassador/ambassador/utils.py
@@ -14,13 +14,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
-import sys
-
+import binascii
 import socket
 import threading
 import time
+import os
+import logging
+
+from kubernetes import client, config
+from enum import Enum
 
 from .VERSION import Version
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+
+
+class TLSPaths(Enum):
+    mount_cert_dir = "/etc/certs"
+    mount_tls_crt = os.path.join(mount_cert_dir, "tls.crt")
+    mount_tls_key = os.path.join(mount_cert_dir, "tls.key")
+
+    client_mount_dir = "/etc/cacert"
+    client_mount_crt = os.path.join(client_mount_dir, "tls.crt")
+
+    cert_dir = "/ambassador/certs"
+    tls_crt = os.path.join(cert_dir, "tls.crt")
+    tls_key = os.path.join(cert_dir, "tls.key")
+
+    client_cert_dir = "/ambassador/cacert"
+    client_tls_crt = os.path.join(client_cert_dir, "tls.crt")
+
 
 class SystemInfo (object):
     MyHostName = socket.gethostname()
@@ -144,3 +168,84 @@ class PeriodicTrigger(threading.Thread):
         while True:
             time.sleep(self.period)
             self.onfired()
+
+
+def read_cert_secret(k8s_api, secret_name, namespace):
+    cert_data = None
+    cert = None
+    key = None
+
+    try:
+        cert_data = k8s_api.read_namespaced_secret(secret_name, namespace)
+    except client.rest.ApiException as e:
+        if e.reason == "Not Found":
+            pass
+        else:
+            logger.info("secret %s/%s could not be read: %s" % (namespace, secret_name, e))
+
+    if cert_data and cert_data.data:
+        cert_data = cert_data.data
+        cert = cert_data.get('tls.crt', None)
+
+        if cert:
+            cert = binascii.a2b_base64(cert)
+
+        key = cert_data.get('tls.key', None)
+
+        if key:
+            key = binascii.a2b_base64(key)
+
+    return (cert, key, cert_data)
+
+
+def save_cert(cert, key, dir):
+    try:
+        os.makedirs(dir)
+    except FileExistsError:
+        pass
+
+    open(os.path.join(dir, "tls.crt"), "w").write(cert.decode("utf-8"))
+
+    if key:
+        open(os.path.join(dir, "tls.key"), "w").write(key.decode("utf-8"))
+
+
+def kube_v1():
+    # Assume we got nothin'.
+    k8s_api = None
+
+    # XXX: is there a better way to check if we are inside a cluster or not?
+    if "KUBERNETES_SERVICE_HOST" in os.environ:
+        # If this goes horribly wrong and raises an exception (it shouldn't),
+        # we'll crash, and Kubernetes will kill the pod. That's probably not an
+        # unreasonable response.
+        config.load_incluster_config()
+        k8s_api = client.CoreV1Api()
+    else:
+        # Here, we might be running in docker, in which case we'll likely not
+        # have any Kube secrets, and that's OK.
+        try:
+            config.load_kube_config()
+            k8s_api = client.CoreV1Api()
+        except FileNotFoundError:
+            # Meh, just ride through.
+            logger.info("No K8s")
+            pass
+
+    return k8s_api
+
+
+def check_cert_file(path):
+    readable = False
+
+    try:
+        data = open(path, "r").read()
+
+        if data and (len(data) > 0):
+            readable = True
+    except OSError:
+        pass
+    except IOError:
+        pass
+
+    return readable