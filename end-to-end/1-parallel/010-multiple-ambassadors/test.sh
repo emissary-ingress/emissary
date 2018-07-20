6cfa4f0774a4314bbd7a3caf56cdf4cece8974cd
Shubham <shubham@linux.com>
2018-07-20 19:43:54 +0530

    parallelize 010-multiple-ambassadors

--- end-to-end/1-parallel/010-multiple-ambassadors/test.sh
+++ end-to-end/1-parallel/010-multiple-ambassadors/test.sh
@@ -16,50 +16,49 @@
 
 set -e -o pipefail
 
-HERE=$(cd $(dirname $0); pwd)
+NAMESPACE="010-multiple-ambassadors"
+NAMESPACE_1="010-multiple-ambassadors-1"
+NAMESPACE_2="010-multiple-ambassadors-2"
+NAMESPACE_3="010-multiple-ambassadors-3"
 
-cd "$HERE"
-
+cd $(dirname $0)
 ROOT=$(cd ../..; pwd)
-PATH="${ROOT}:${PATH}"
-
 source ${ROOT}/utils.sh
-
-initialize_cluster
-
-kubectl cluster-info
+bootstrap ${NAMESPACE} ${ROOT}
 
 python ${ROOT}/yfix.py ${ROOT}/fixes/test-dep.yfix \
     ${ROOT}/ambassador-deployment.yaml \
     k8s/ambassador-deployment-1.yaml \
-    test-010-1 \
-    ambassador-1
+    ${NAMESPACE_1} \
+    ${NAMESPACE_1}
 
 python ${ROOT}/yfix.py ${ROOT}/fixes/test-dep.yfix \
     ${ROOT}/ambassador-deployment.yaml \
     k8s/ambassador-deployment-2.yaml \
-    test-010-2 \
-    ambassador-2
+    ${NAMESPACE_2} \
+    ${NAMESPACE_2}
 
-kubectl create namespace test-010-1
-kubectl create namespace test-010-2
-kubectl create namespace test-010-svc
-kubectl apply -f k8s/rbac.yaml
-kubectl apply -f k8s/ambassador-1.yaml
-kubectl apply -f k8s/ambassador-2.yaml
-kubectl apply -f k8s/ambassador-deployment-1.yaml
-kubectl apply -f k8s/ambassador-deployment-2.yaml
+kubectl create namespace ${NAMESPACE_1}
+kubectl create namespace ${NAMESPACE_2}
+kubectl create namespace ${NAMESPACE_3}
+\kubectl apply -f k8s/rbac.yaml
+\kubectl apply -f k8s/ambassador-1.yaml
+\kubectl apply -f k8s/ambassador-2.yaml
+\kubectl apply -f k8s/ambassador-deployment-1.yaml
+\kubectl apply -f k8s/ambassador-deployment-2.yaml
 
 kubectl run demotest --image=dwflynn/demotest:0.0.1 -- /bin/sh -c "sleep 3600"
 
 set +e +o pipefail
 
-wait_for_pods
+wait_for_pods ${NAMESPACE}
+wait_for_pods ${NAMESPACE_1}
+wait_for_pods ${NAMESPACE_2}
 
 CLUSTER=$(cluster_ip)
-APORT1=$(service_port ambassador test-010-1)
-APORT2=$(service_port ambassador test-010-2)
-DEMOTEST_POD=$(demotest_pod)
+APORT1=$(service_port ambassador ${NAMESPACE_1})
+APORT2=$(service_port ambassador ${NAMESPACE_2})
+DEMOTEST_POD=$(demotest_pod ${NAMESPACE})
 
 BASEURL1="http://${CLUSTER}:${APORT1}"
 BASEURL2="http://${CLUSTER}:${APORT2}"
@@ -80,10 +79,12 @@ if ! check_diag "$BASEURL2" 1-2 "No annotated services"; then
     exit 1
 fi
 
-kubectl apply -f k8s/demo-1.yaml
-kubectl apply -f k8s/demo-2.yaml
+\kubectl apply -f k8s/demo-1.yaml
+\kubectl apply -f k8s/demo-2.yaml
 
-wait_for_pods
+wait_for_pods ${NAMESPACE}
+wait_for_pods ${NAMESPACE_1}
+wait_for_pods ${NAMESPACE_2}
 
 wait_for_demo_weights "$BASEURL1" 100
 wait_for_demo_weights "$BASEURL2" 100
@@ -105,4 +106,6 @@ if ! kubectl exec -i $DEMOTEST_POD -- python3 demotest.py "$BASEURL2" /dev/fd/0 
     exit 1
 fi
 
-# kubernaut discard
+if [ -n "$CLEAN_ON_SUCCESS" ]; then
+    drop_namespace ${NAMESPACE} ${NAMESPACE_1} ${NAMESPACE_2} ${NAMESPACE_3}
+fi
