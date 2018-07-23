90092a5549cc79b24572767a2dc0e4238cd4aa41
Shubham <shubham@linux.com>
2018-07-23 18:47:44 +0530

    parallelize 007-rate-limit

--- end-to-end/1-parallel/007-rate-limit/test.sh
+++ end-to-end/1-parallel/007-rate-limit/test.sh
@@ -16,30 +16,31 @@
 
 set -e -o pipefail
 
-HERE=$(cd $(dirname $0); pwd)
+NAMESPACE="007-rate-limit"
 
-cd "$HERE"
-
+cd $(dirname $0)
 ROOT=$(cd ../..; pwd)
-PATH="${ROOT}:${PATH}"
-
 source ${ROOT}/utils.sh
+bootstrap ${NAMESPACE} ${ROOT}
 
-initialize_cluster
+python ${ROOT}/yfix.py ${ROOT}/fixes/ambassador-id.yfix \
+    ${ROOT}/ambassador-deployment.yaml \
+    k8s/ambassador-deployment.yaml \
+    ${NAMESPACE} \
+    ${NAMESPACE}
 
-kubectl cluster-info
-
+kubectl apply -f k8s/rbac.yaml
 kubectl apply -f k8s/ambassador.yaml
-kubectl apply -f ${ROOT}/ambassador-deployment.yaml
+kubectl apply -f k8s/ambassador-deployment.yaml
 kubectl apply -f k8s/qotm.yaml
 kubectl apply -f k8s/rate-limit.yaml
 
 set +e +o pipefail
 
-wait_for_pods
+wait_for_pods ${NAMESPACE}
 
 CLUSTER=$(cluster_ip)
-APORT=$(service_port ambassador)
+APORT=$(service_port ambassador ${NAMESPACE})
 
 BASEURL="http://${CLUSTER}:${APORT}"
 
@@ -47,10 +48,12 @@ echo "Base URL $BASEURL"
 echo "Diag URL $BASEURL/ambassador/v0/diag/"
 
 wait_for_ready "$BASEURL"
-wait_for_pods
+wait_for_pods ${NAMESPACE}
 
-if ! python rate-limit-test.py $CLUSTER:$APORT; then
+if ! python rate-limit-test.py ${CLUSTER}:${APORT}; then
     exit 1
 fi
 
-# kubernaut discard
+if [ -n "$CLEAN_ON_SUCCESS" ]; then
+    drop_namespace ${NAMESPACE}
+fi
