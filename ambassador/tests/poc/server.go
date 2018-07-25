5e0cec34870f490b083cc46e634b4b31356ffca7
Rafael Schloming <rhs@alum.mit.edu>
2018-07-25 16:05:46 -0400

    Added backend server implementation.

---
+++ ambassador/tests/poc/server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"strconv"
+)
+
+func requestLogger(w http.ResponseWriter, r *http.Request) {
+	var request = make(map[string]interface{})
+	request["URL"] = r.URL
+	request["Method"] = r.Method
+	request["Header"] = r.Header
+
+	// respond with the requested status
+	status := r.Header.Get("Requested-Status")
+	if status == "" {
+		status = "200"
+	}
+	
+	statusCode, err := strconv.Atoi(status)
+	if err != nil {
+		log.Print(err)
+		statusCode = 500
+	}
+
+	// copy the requested headers into the response
+	headers, ok := r.Header["Requested-Header"]
+	if ok {
+		for _, header := range headers {
+			canonical := http.CanonicalHeaderKey(header)
+			value, ok := r.Header[canonical]
+			if ok {
+				w.Header()[canonical] = value
+			}
+		}
+	}
+
+	w.WriteHeader(statusCode)
+
+	// Write out all request/response information
+	var response = make(map[string]interface{})
+	response["Header"] = w.Header()
+
+	var body = make(map[string]interface{})
+	body["Request"] = request
+	body["Response"] = response
+
+	b, err := json.MarshalIndent(body, "", "  ")
+	if err != nil {
+		b = []byte(fmt.Sprintf("Error: %v", err))
+	}
+
+	w.Write(b)
+}
+
+func main() {
+	http.HandleFunc("/", requestLogger)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
