c57938b5bc44193dca630246ff0b22aec5fdc8b8
Gabriel <gsagula@gmail.com>
2018-10-02 18:24:37 -0700

    --allow-empty
    Signed-off-by: Gabriel <gsagula@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 TRAVIS_TAG=ok
 
-# k8s generated scripts
-PROFILE=prod
-
 # This is the namespace you want to use for development in quay,
 # probably your quay.io username:
 REGISTRY_NAMESPACE=datawire
@@ -26,11 +23,19 @@ K8S_CLUSTER=ambassador-oauth-testing
 K8S_ZONE=us-central1-a
 K8S_ACCOUNT_NAME=ambassador-oauth@datawireio.iam.gserviceaccount.com
 
+# Kubectl
+KUBECTL_VERSION=v1.12.0
+
 if [ -n "$TRAVIS_TAG" ]; then
   openssl aes-256-cbc -K $encrypted_3c8a53ca0ead_key -iv $encrypted_3c8a53ca0ead_iv -in key-file.json.enc -out key-file.json -d
   if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
-    rm -rf $HOME/google-cloud-sdk; curl https://sdk.cloud.google.com | bash;
+    rm -rf $HOME/google-cloud-sdk; curl https://sdk.cloud.google.com | bash;  
   fi
+
+  curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl
+  chmod +x ./kubectl
+  sudo mv $HOME/google-cloud-sdk/bin/kubectl /usr/local/bin/kubectl
+ 
   source /home/travis/google-cloud-sdk/path.bash.inc;
   gcloud version;
   gcloud auth activate-service-account $K8S_ACCOUNT_NAME --key-file=./key-file.json;
@@ -39,14 +44,11 @@ if [ -n "$TRAVIS_TAG" ]; then
   gcloud --quiet config set container/cluster $K8S_CLUSTER;
   gcloud --quiet config set compute/zone $K8S_ZONE;
   gcloud --quiet container clusters get-credentials $K8S_CLUSTER --zone=$K8S_ZONE;
-  gcloud --quiet components install kubectl;
-  kubectl config current-context;
 
+  kubectl config current-context;
   if [[ -z $(kubectl get clusterrolebinding --field-selector metadata.name=cluster-admin) && -n $TRAVIS_TAG ]]; then
     kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user=$K8S_ACCOUNT_NAME --namespace=datawire;
   fi
-
-  make deploy
 
   echo "INFO: Waiting for authorizarion service deployment..."
   typeset -i cnt=60
