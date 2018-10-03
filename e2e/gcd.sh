1761619763da26821f9a1a10d4e7e596e2d103c9
Gabriel <gsagula@gmail.com>
2018-10-03 00:40:27 -0700

    --allow-empty
    Signed-off-by: Gabriel <gsagula@gmail.com>

--- e2e/gcd.sh
+++ e2e/gcd.sh
@@ -3,26 +3,25 @@
 set -e
 set -u
 
-if [ -n $(TRAVIS_TAG) ]; then
-  if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
-    rm -rf $HOME/google-cloud-sdk; curl https://sdk.cloud.google.com | bash;  
-  fi
+cd $HOME
 
-  curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl
-  chmod +x ./kubectl
-  sudo mv $HOME/google-cloud-sdk/bin/kubectl /usr/local/bin/kubectl
- 
-  source /home/travis/google-cloud-sdk/path.bash.inc;
-  gcloud version;
-  gcloud auth activate-service-account $K8S_ACCOUNT_NAME --key-file=./key-file.json;
-  gcloud --quiet config set container/use_client_certificate False;
-  gcloud --quiet config set project $K8S_PROJECT;
-  gcloud --quiet config set container/cluster $K8S_CLUSTER;
-  gcloud --quiet config set compute/zone $K8S_ZONE;
-  gcloud --quiet container clusters get-credentials $K8S_CLUSTER --zone=$K8S_ZONE;
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
+  rm -rf $HOME/google-cloud-sdk; curl https://sdk.cloud.google.com | bash;
+fi
 
-  kubectl config current-context;
-  if [[ -z $(kubectl get clusterrolebinding --field-selector metadata.name=cluster-admin) && -n $TRAVIS_TAG ]]; then
-    kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user=$K8S_ACCOUNT_NAME --namespace=datawire;
-  fi  
+curl -o $HOME/kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl
+chmod +x $HOME/kubectl
+
+source /home/travis/google-cloud-sdk/path.bash.inc
+gcloud version
+gcloud auth activate-service-account $K8S_ACCOUNT_NAME --key-file=$HOME/key-file.json
+gcloud --quiet config set container/use_client_certificate False
+gcloud --quiet config set project $K8S_PROJECT
+gcloud --quiet config set container/cluster $K8S_CLUSTER
+gcloud --quiet config set compute/zone $K8S_ZONE
+gcloud --quiet container clusters get-credentials $K8S_CLUSTER --zone=$K8S_ZONE
+
+./kubectl config current-context;
+if [[ -z $(kubectl get clusterrolebinding --field-selector metadata.name=cluster-admin) && -n $TRAVIS_TAG ]]; then
+  ./kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user=$K8S_ACCOUNT_NAME --namespace=datawire;
 fi
--- k8s.make
+++ k8s.make
@@ -93,6 +93,6 @@ e2e-check:
 
 .PHONY: docker-login
 docker-login:
-	@if [ -z "$DOCKER_USERNAME" ]; then echo 'DOCKER_USERNAME not defined'; exit 1; fi
-	@if [ -z "$DOCKER_PASSWORD" ]; then echo 'DOCKER_PASSWORD not defined'; exit 1; fi
-	@printf "$DOCKER_PASSWORD" | docker login -u="$DOCKER_USERNAME" --password-stdin "$DOCKER_REGISTRY"
+	@if [ -z "$$DOCKER_USERNAME" ]; then echo 'DOCKER_USERNAME not defined'; exit 1; fi
+	@if [ -z "$$DOCKER_PASSWORD" ]; then echo 'DOCKER_PASSWORD not defined'; exit 1; fi
+	@printf "$$DOCKER_PASSWORD" | docker login -u="$$DOCKER_USERNAME" --password-stdin "$$DOCKER_REGISTRY"
