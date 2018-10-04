953398bc76fabf850b8c6faa95eea26001f7f367
Rafael Schloming <rhs@alum.mit.edu>
2018-10-03 23:40:33 -0400

    Cleaned up and simplified tests; enabled/added ambassador_id tests.

--- ambassador/tests/kat/test_ambassador.py
+++ ambassador/tests/kat/test_ambassador.py
@@ -3,14 +3,15 @@ import pytest
 
 from typing import ClassVar, Dict, List, Sequence, Tuple, Union
 
-from kat.harness import abstract_test, _instantiate, sanitize, variant, variants, Query, Runner
+from kat.harness import sanitize, variants, Query, Runner
 from kat import manifests
 
-from abstract_tests import AmbassadorBaseTest, AmbassadorMixin, AmbassadorTest, HTTP
-from abstract_tests import MappingBaseTest, MappingTest, OptionTest, ServiceType, Node, Test
+from abstract_tests import AmbassadorTest, HTTP
+from abstract_tests import MappingTest, OptionTest, ServiceType, Node, Test
 
-class TLS(AmbassadorMixin, AmbassadorTest):
-    VARIES_BY = MappingTest
+# XXX: should test empty ambassador config
+
+class TLS(AmbassadorTest):
 
     def config(self):
         yield self, """
@@ -28,17 +29,12 @@ config:
     def scheme(self) -> str:
         return "http"
 
-#class Empty(ConfigTest):
 
-#    def yaml(self):
-#        return ""
+class Plain(AmbassadorTest):
 
-#    def scheme(self) -> str:
-#        return "http"
-
-
-class Plain(AmbassadorMixin, AmbassadorTest):
-    VARIES_BY = MappingTest
+    @classmethod
+    def variants(cls):
+        yield cls(variants(MappingTest))
 
     def config(self) -> Union[str, Tuple[Node, str]]:
         yield self, """
@@ -49,18 +45,14 @@ name:  ambassador
 config: {}
 """
 
-    def scheme(self) -> str:
-        return "http"
 
-
-# XXX: should put this somewhere better
-def unique(variants):
+def unique(options):
     added = set()
     result = []
-    for v in variants:
-        if v.cls not in added:
-            added.add(v.cls)
-            result.append(v)
+    for o in options:
+        if o.__class__ not in added:
+            added.add(o.__class__)
+            result.append(o)
     return tuple(result)
 
 class SimpleMapping(MappingTest):
@@ -68,10 +60,10 @@ class SimpleMapping(MappingTest):
     @classmethod
     def variants(cls):
         for st in variants(ServiceType):
-            yield variant(st, name="{self.target.name}")
+            yield cls(st, name="{self.target.name}")
             for mot in variants(OptionTest):
-                yield variant(st, (mot,), name="{self.target.name}-{self.options[0].name}")
-            yield variant(st, unique(variants(OptionTest)), name="{self.target.name}-all")
+                yield cls(st, (mot,), name="{self.target.name}-{self.options[0].name}")
+            yield cls(st, unique(variants(OptionTest)), name="{self.target.name}-all")
 
     def config(self):
         yield self, self.format("""
@@ -91,6 +83,7 @@ service: http://{self.target.path.k8s}
             if r.backend:
                 assert r.backend.name == self.target.path.k8s, (r.backend.name, self.target.path.k8s)
 
+
 class AddRequestHeaders(OptionTest):
 
     VALUES: ClassVar[Sequence[Dict[str, str]]] = (
@@ -107,6 +100,7 @@ class AddRequestHeaders(OptionTest):
                 actual = r.backend.request.headers.get(k.lower())
                 assert actual == [v], (actual, [v])
 
+
 class CaseSensitive(OptionTest):
 
     def config(self):
@@ -119,6 +113,7 @@ class CaseSensitive(OptionTest):
             assert upped != q.url
             yield Query(upped)
 
+
 class AutoHostRewrite(OptionTest):
 
     def config(self):
@@ -128,6 +123,7 @@ class AutoHostRewrite(OptionTest):
         for r in self.parent.results:
             host = r.backend.request.host
             assert r.backend.name == host, (r.backend.name, host)
+
 
 class Rewrite(OptionTest):
 
@@ -147,6 +143,7 @@ class Rewrite(OptionTest):
             pytest.xfail("this is broken")
         for r in self.parent.results:
             assert r.backend.request.url.path == self.value
+
 
 class TLSOrigination(MappingTest):
 
@@ -173,10 +170,10 @@ tls: true
     def variants(cls):
         for v in variants(ServiceType):
             for name, dfn in ("IMPLICIT", cls.IMPLICIT), ("EXPLICIT", cls.EXPLICIT):
-                yield variant(v, dfn, name="{self.target.name}-%s" % name)
+                yield cls(v, dfn, name="{self.target.name}-%s" % name)
 
-    def __init__(self, target, definition):
-        self.target = target
+    def init(self, target, definition):
+        MappingTest.init(self, target)
         self.definition = definition
 
     def config(self):
@@ -189,16 +186,17 @@ tls: true
         for r in self.results:
             assert r.backend.request.tls.enabled
 
+
 class CanaryMapping(MappingTest):
 
     @classmethod
     def variants(cls):
         for v in variants(ServiceType):
             for w in (10, 50):
-                yield variant(v, v.clone("canary"), w, name="{self.target.name}-{self.weight}")
+                yield cls(v, v.clone("canary"), w, name="{self.target.name}-{self.weight}")
 
-    def __init__(self, target, canary, weight):
-        MappingTest.__init__(self, target)
+    def init(self, target, canary, weight):
+        MappingTest.init(self, target)
         self.canary = canary
         self.weight = weight
 
@@ -234,25 +232,11 @@ weight: {self.weight}
         assert abs(self.weight - canary) < 25, (self.weight, canary)
 
 
-class AmbassadorIDTest (Test):
+class AmbassadorIDTest(AmbassadorTest):
 
-    @classmethod
-    def variants(cls):
-        yield variant(variants(AmbassadorIDInnerTest))
+    def init(self):
+        self.target = HTTP()
 
-    def config(self):
-        if False: yield
-
-    def manifests(self):
-        m = self.format(manifests.BACKEND)
-        print("AmbassadorIDTest: %s" % m)
-        return m
-
-    def requirements(self):
-        yield ("pod", self.path.k8s)
-
-
-class AmbassadorIDInnerTest (AmbassadorMixin, AmbassadorBaseTest):
     def config(self) -> Union[str, Tuple[Node, str]]:
         yield self, """
 ---
@@ -261,110 +245,58 @@ kind:  Module
 name:  ambassador
 config: {}
 """
-
-    # # You MUST define scheme in order for this class not to be considered abstract.
-    def scheme(self) -> str:
-        return "http"
-
-    @classmethod
-    def variants(cls):
-        c = 0
-        for v_idopts in variants(AmbassadorIDOptions):
-            yield variant(v_idopts, variants(AmbassadorIDMappings), name=str(c))
-            c += 1
-
-    def __init__(self, idoptions: 'AmbassadorIDOptions', mappings=List['AmbassadorIDMappings']):
-        print("idoptions %s" % repr(idoptions))
-
-        super().__init__(mappings=mappings, ambassador_id=idoptions.ambassador_runs_as)
-        self.ambassador_id_findme = idoptions.ambassador_id_findme
-        self.ambassador_id_missme = idoptions.ambassador_id_missme
-
-    #     ambid = idoptions.value
-    #     print("AmbassadorIDInnerTest ambid %s" % ambid)
-    #
-    #     self.plain = Plain(mappings=(), ambassador_id=ambid)
-    #     self.plain.name = "{self.name}-%s" % sanitize(ambid)
-    #
-    #     self.config = self.plain.config
-    #     self.manifests = self.plain.manifests
-    #     self.requirements = self.plain.requirements
-
-class AmbassadorIDMappings (Test):
-    CTR: ClassVar[int] = 0
-
-    @classmethod
-    def variants(cls):
-        for prefix in [ 'findme', 'missme' ]:
-            yield variant(prefix, name=prefix)
-
-    def config(self):
-        print("AmbassadorIDMapping trying to add to %s" % self.parent.parent.name)
-        yield self.parent.parent, self.format("""
+        yield self.target, self.format("""
 ---
 apiVersion: ambassador/v0
 kind:  Mapping
-name:  {self.mapping_name}
-prefix: /{self.prefix}/
-rewrite: /{self.prefix}/ 
-service: http://{self.target.name}
+name:  {self.path.k8s}-findme
+prefix: /findme/
+service: {self.target.path.k8s}
+ambassador_id: {self.ambassador_id}
+""")
+        yield self.target, self.format("""
+---
+apiVersion: ambassador/v0
+kind:  Mapping
+name:  {self.path.k8s}-findme-array
+prefix: /findme-array/
+service: {self.target.path.k8s}
+ambassador_id: [{self.ambassador_id}, missme]
+""")
+        yield self.target, self.format("""
+---
+apiVersion: ambassador/v0
+kind:  Mapping
+name:  {self.path.k8s}-findme-array2
+prefix: /findme-array2/
+service: {self.target.path.k8s}
+ambassador_id: [missme, {self.ambassador_id}]
+""")
+        yield self.target, self.format("""
+---
+apiVersion: ambassador/v0
+kind:  Mapping
+name:  {self.path.k8s}-missme
+prefix: /missme/
+service: {self.target.path.k8s}
+ambassador_id: missme
+""")
+        yield self.target, self.format("""
+---
+apiVersion: ambassador/v0
+kind:  Mapping
+name:  {self.path.k8s}-missme
+prefix: /missme-array/
+service: {self.target.path.k8s}
+ambassador_id: [missme1, missme2]
 """)
 
-    @property
-    def ambassador_id(self) -> str:
-        return getattr(self.parent, "ambassador_id_%s" % self.prefix)
-
     def queries(self):
-        yield Query(self.parent.url(self.prefix + "/"))
-
-    def check(self):
-        print("%s: WTFO results %s" % (self.name, self.results))
-
-        for r in self.results:
-            print("%s: result %s" % (self.name, repr(r)))
-
-            if r.backend:
-                assert r.backend.name == self.target.path.k8s, (r.backend.name, self.target.path.k8s)
-
-    def __init__(self, prefix: str):
-        self.prefix = prefix
-        self.mapping_name = "idmapping-%d" % AmbassadorIDMappings.CTR
-
-        # What a crock.
-        self.target = Node()
-        self.target.name = "ambassadoridtest"
-
-        AmbassadorIDMappings.CTR += 1
-
-
-class AmbassadorIDOptions (Test):
-
-    @classmethod
-    def variants(cls):
-        for val in [
-            {
-                "name": "scalars",
-                "ambassador_runs_as": "id_test_one",
-                "ambassador_id_findme": "id_test_one",
-                "ambassador_id_missme": "id_test_two"
-            },
-            {
-                "name": "arrays",
-                "ambassador_runs_as": "id_test_one",
-                "ambassador_id_findme": [ "id_test_one", "id_test_two" ],
-                "ambassador_id_missme": [ "id_test_two", "id_test_three" ]
-            }
-        ]:
-            yield variant(val, name=sanitize(val["name"]))
-
-    def __init__(self, idoptions = None):
-        for k, v in idoptions.items():
-            setattr(self, k, v)
-
-    # def check(self):
-    #     print("%s CHECK HO" % self.name)
-    #     assert False, "kaboom"
-
+        yield Query(self.url("findme/"))
+        yield Query(self.url("findme-array/"))
+        yield Query(self.url("findme-array2/"))
+        yield Query(self.url("missme/"), expected=404)
+        yield Query(self.url("missme-array/"), expected=404)
 
 # pytest will find this because Runner is a toplevel callable object in a file
 # that pytest is willing to look inside.
@@ -373,6 +305,4 @@ class AmbassadorIDOptions (Test):
 # - Runner(cls) will look for variants of _every subclass_ of cls.
 # - Any class you pass to Runner needs to be standalone (it must have its
 #   own manifests and be able to set up its own world).
-main = Runner(AmbassadorTest
-              # , AmbassadorIDTest
-             )
+main = Runner(AmbassadorTest)
