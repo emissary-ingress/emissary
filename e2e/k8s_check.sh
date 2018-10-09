486bec95ca53c4c4444b4113b73a5c078c71d38b
Gabriel <gsagula@gmail.com>
2018-10-09 16:27:13 -0700

    added release stage
    Signed-off-by: Gabriel <gsagula@gmail.com>

--- e2e/k8s_check.sh
+++ e2e/k8s_check.sh
@@ -18,6 +18,7 @@ until kubectl rollout status deployment/ambassador -n datawire | grep "successfu
   sleep 1; 
 done
 
+echo "Checking container image..."
 SERVICE_IMAGE=$(kubectl get deployment auth0-service -n datawire -o=jsonpath='{$$.spec.template.spec.containers[:1].image}')
 MANIFEST_IMAGE=$(cat k8s_build/DEV/authorization-srv.yaml | grep ${SERVICE_IMAGE})
 if [ -z "$MANIFEST_IMAGE" ]; then echo "INCORRECT IMAGE"; exit 1; fi
--- k8s.make
+++ k8s.make
@@ -103,3 +103,7 @@ docker-login:
 	@if [ -z "$$DOCKER_USERNAME" ]; then echo 'DOCKER_USERNAME not defined'; exit 1; fi
 	@if [ -z "$$DOCKER_PASSWORD" ]; then echo 'DOCKER_PASSWORD not defined'; exit 1; fi
 	@printf "$$DOCKER_PASSWORD" | docker login -u="$$DOCKER_USERNAME" --password-stdin "$$DOCKER_REGISTRY"
+
+.PHONY: get-image
+get-image: $(HASH_FILE)
+	@echo $(IMAGE)
