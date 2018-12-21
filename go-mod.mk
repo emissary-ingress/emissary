4b527d90ad5ecf9138165792dac76b51f6ab1d88
Luke Shumaker <lukeshu@datawire.io>
2018-12-20 20:15:03 -0500

    (ambassador-ratelimit) build-aux: Expand common.mk, factor out common code between go-{mod,workspace}.mk
    This makes some (BREAKING!) changes, and simplifies things for Makefile.
    
     - $(pkg) is now $(go.module), and is no longer necessary to manually set;
       go-{mod,workspace}.mk are both smart enough to set it automatically!
     - $(bins) is now $(notdir $(go.bins)), and is no longer nescessary to
       manually set; go-{mod,workspace}.mk are both smart enough to set it
       automatically!
     - Makefile doesn't need to export GOPATH or anything like that, the .mk
       snippets now export things
     - It's no longer necessary to use `$(GO)`, just write `go`--but don't
       don't use it inside of $(shell …) if you can avoid it, exported
       variables don't affect $(shell …).
     - It's no longer necessary to clean up bin_*/, in your `clean:` rule,
       common.mk takes care of that for you.
     - common.mk goes ahead and declares several common targets as .PHONY
     - Go binaries now go in ./bin_$(GOOS)_$(GOARCH)/ by default, instead of
       the old default of ./

--- go-mod.mk
+++ go-mod.mk
@@ -1,78 +1,64 @@
-# This requires Go 1.11 or newer
+# Copyright 2018 Datawire. All rights reserved.
+#
+# Makefile snippet to build Go programs using Go 1.11 modules
+#
+## Inputs ##
+#  - File: ./go.mod
+## Outputs ##
+#  - Variable: go.module = EXAMPLE.COM/YOU/YOURREPO
+#  - Variable: go.bins = List of "main" Go packages
+#  - .PHONY Target: go-get
+#  - .PHONY Target: go-build
+#  - .PHONY Target: check-go-fmt
+#  - .PHONY Target: go-vet
+#  - .PHONY Target: go-fmt
+#  - .PHONY Target: go-test
+## common.mk targets ##
+#  - build
+#  - lint
+#  - check
 
-# Note that the `export` directive *only* affects recipes, and does
-# *not* affect $(shell …).  Because of this, you shoud not call `go`
-# inside of $(shell …).
-export GOBIN ?= $(CURDIR)
-export GO111MODULE = on
-# We only set GOPATH and GOCACHE to control where the module cache
-# (`$GOPATH/pkg/mod`) and the build cache are; it isn't strictly
-# nescessary to set them.  We care about where the caches are because
-# - we'd like to be able to copy them in to Docker images.
-# - we'd like `make clean` to actually clean build caches
-# - we'd like to not vomit all over the user's global caches
-export GOPATH = $(CURDIR)/.gocache/workspace
-export GOCACHE = $(CURDIR)/.gocache/go-build
+#
 
-# We could more easily set $(go.module) and $(go.bins) using Go's
-# built-in module handling facilities, but that would cause the module
-# system go ahead and download dependencies.  We don't want Go to do
-# that at Makefile-parse-time; what if we're running `make clean`?
-
+# Why use this complex `sed` expression to parse go.mod, instead of
+# just having `go list -m` do it?  Because `go list -m` will go ahead
+# and download dependencies.  We don't want Go to do that at
+# Makefile-parse-time; what if we're running `make clean`?
+#
 # See: cmd/go/internal/modfile/read.go:ModulePath()
 go.module := $(strip $(shell sed -n -e 's,//.*,,' -e '/^\s*module/{s/^\s*module//;p;q}' go.mod))
 #go.module := $(shell $(GO) list -m)
+ifeq ($(go.module),)
+  # Print a helpful message
+  ifeq ($(wildcard go.mod),)
+    $(info go-mod.mk: File `./go.mod` does not exist.)
+    ifeq ($(wildcard .go-workspace/),)
+      $(info go-mod.mk: Initalize it with `go mod init github.com/YOU/REPONAME`)
+    else
+      $(info go-mod.mk: But `./go-workspace/` does.  Did you mean to use go-workspace.mk?)
+    endif
+  else
+    $(info go-mod.mk: File `./go.mod` seems to be malformed; could not parse.)
+  endif
+  # And then error out
+  $(error Could not extract $$(go.module) from ./go.mod)
+endif
 
-go.bins := $(notdir $(shell GO111MODULE=off GOCACHE=off go list -f='{{if eq .Name "main"}}{{.ImportPath}}{{end}}' ./...))
-#go.bins := $(notdir $(shell $(GO) list -f='{{if eq .Name "main"}}{{.ImportPath}}{{end}}' $(go.module)/...))
+export GO111MODULE = on
 
-# Add the Go "main" packages to the "build" target.
-build: $(addprefix $(GOBIN)/,$(go.bins))
-.PHONY: build
-
-# This is a little awkward.  We can't specify each binary as a normal
-# separate target, because Make will call `go install` separately for
-# each of them, and if we have multiple `go install`s going at once,
-# they could corrupt $GOCACHE.  We could just say .NOTPARALLEL:, but
-# we can do better.  Use a multi-target pattern rule, where the
-# pattern matches $GOBIN.
-#
-# https://www.gnu.org/software/make/manual/html_node/Pattern-Examples.html#Pattern-Examples
-$(addprefix %/,$(go.bins)): %/. FORCE
-	GOBIN=$(abspath $(@D)) go install $(go.module)/...
-
-# Make trims the leading `./` before doing pattern-matching, so if
-# GOBIN=$(CURDIR), then `./teleproxy` becomes `teleproxy`, and doesn't
-# match `%/teleproxy`.
-$(go.bins): %: $(CURDIR)/%
-
-clean: clean-go
-clean-go:
-	rm -f -- $(go.bins)
-	find .gocache/workspace -exec chmod +w -- {} + || true
-	rm -rf .gocache
-.PHONY: clean clean-go
-
-# Some utility targets:
 go-get:
-	go get -t -u -d $(go.module)/...
-.PHONY: go-get
+	go list ./...
 
-go-fmt:
-	go fmt $(go.module)/...
-.PHONY: go-fmt
+# .NOTPARALLEL is important, because (as of Go 1.11) `go` commands
+# that write to the module cache are not safe to invoke concurrently
+# (this should be fixed in Go 1.12, scheduled for February 2019[1]).
+# We could try working around that with a multi-target pattern rule[2]
+# and using `GOBIN=$(@D) go install` instead of `go build -o`, but you
+# can't use GOBIN when cross-compiling.  So, until we can depend on Go
+# 1.12, just disable parallel builds.
+#
+# [1]: https://tip.golang.org/doc/go1.12
+# [2]: https://www.gnu.org/software/make/manual/html_node/Pattern-Examples.html#Pattern-Examples
+.NOTPARALLEL:
 
-go-mod-tidy:
-	go mod tidy
-.PHONY: go-mod-tidy
-
-go-mod-vendor:
-	go mod vendor
-.PHONY: go-mod-vendor
-
-# The $(go.bins) aren't .PHONY--they're real files that will exist,
-# but we should try to update them every run, and let `go` decide if
-# they're up-to-date or not, rather than trying to teach Make to do
-# it.  So instead, have them depend on a .PHONY target so that they'll
-# always be considered out-of-date.
-.PHONY: FORCE
+include $(dir $(lastword $(MAKEFILE_LIST)))/_go-common.mk
--- go-workspace.mk
+++ go-workspace.mk
@@ -1,19 +1,57 @@
+# Copyright 2018 Datawire. All rights reserved.
+#
+# Makefile snippet to build Go programs using Go workspaces
+# ("GOPATH").  Go workspaces are scheduled for deprecation in Go 1.13,
+# which is scheduled for August 2019.
+#
+## Inputs ##
+#  - Symlink: ./.go-workspace/src/EXAMPLE.COM/YOU/YOURREPO -> (git topdir)
+## Outputs ##
+#  - Variable: go.module = EXAMPLE.COM/YOU/YOURREPO
+#  - Variable: go.bins = List of "main" Go packages
+#  - Target: vendor/ (if `./glade.yaml` is present)
+#  - .PHONY Target: go-get
+#  - .PHONY Target: go-build
+#  - .PHONY Target: check-go-fmt
+#  - .PHONY Target: go-vet
+#  - .PHONY Target: go-fmt
+#  - .PHONY Target: go-test
+## common.mk targets ##
+#  - build
+#  - lint
+#  - check
+#  - clobber
+
+go.module := $(patsubst src/%,%,$(shell cd .go-workspace && find src \( -name '.*' -prune \) -o -type l -print))
+ifneq ($(words $(go.module)),1)
+  # Print a helpful message
+  ifeq ($(wildcard .go-workspace/),)
+    $(info go-worksapce.mk: Directory `./go-workspace/` does not exist.)
+    ifeq ($(wildcard go.mod),)
+      $(info go-workspace.mk: Initialize it with:)
+      $(info go-workspace.mk:     $$ mkdir -p .go-workspace/src/github.com/YOU)
+      $(info go-workspace.mk:     $$ ln -srT . .go-workspace/src/github.com/YOU/REPONAME)
+    else
+      $(info go-workspace.mk: But `./go.mod` does.  Did you mean to use go-mod.mk?)
+    endif
+  else
+    $(info go-workspace.mk: Did not find exactly 1 symlink under `./go-workspace/`)
+  endif
+  # And then error out
+  $(error Could not extract $$(go.module) from ./.go-workspace/src/)
+endif
+
 export GO111MODULE = off
 export GOPATH = $(CURDIR)/.go-workspace
-GOBIN=$(CURDIR)
-GO = GOPATH=$(GOPATH) GOBIN=$(GOBIN) go
 
-IMAGE_GOBIN=$(CURDIR)/image
-IMAGE_GO = CGO_ENABLED=0 GOOS=linux GOPATH=$(GOPATH) GOBIN=$(IMAGE_GOBIN) go
+go-get:
+	go get -d $(go.bins)
 
-build: $(bins)
-.PHONY: build
-
-vendor::
 ifneq ($(wildcard glide.yaml),)
-vendor:: glide.yaml $(wildcard glide.lock)
+vendor: glide.yaml $(wildcard glide.lock)
 	rm -rf $@
-	glide install
+	glide install || { r=$?; rm -rf $@; exit $?; }
+go-get: vendor
 
 _go-clobber-vendor:
 	rm -rf vendor
@@ -21,17 +59,11 @@ _go-clobber-vendor:
 clobber: _go-clobber-vendor
 endif
 
-$(bins): %: FORCE vendor
-	$(GO) install $(pkg)/cmd/$@
-
-$(bins:%=image/%): %: FORCE vendor
-	$(IMAGE_GO) build -o ${@} $(pkg)/cmd/${@:image/%=%}
-
 _go-clobber:
 	find .go-workspace -exec chmod +w {} +
 	rm -rf .go-workspace
-	mkdir -p $(dir .go-workspace/src/$(pkg))
-	ln -s $(call joinlist,$(patsubst %,..,$(subst /, ,$(dir .go-workspace/src/$(pkg)))),/) .go-workspace/src/$(pkg)
+	mkdir -p $(dir .go-workspace/src/$(go.module))
+	ln -s $(call joinlist,$(patsubst %,..,$(subst /, ,$(dir .go-workspace/src/$(go.module)))),/) .go-workspace/src/$(go.module)
 .PHONY: _go-clobber
 clobber: _go-clobber
 
@@ -39,3 +71,5 @@ clobber: _go-clobber
 # once can corrupt `$(GOPATH)/pkg`.  Setting .NOTPARALLEL is simpler
 # than mucking with multi-target pattern rules.
 .NOTPARALLEL:
+
+include $(dir $(lastword $(MAKEFILE_LIST)))/_go-common.mk
