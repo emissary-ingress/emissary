047e3f3634c3299c9943e1f945305ac1e4dac49f
Luke Shumaker <lukeshu@datawire.io>
2019-01-07 13:02:20 -0500

    (ambassador-oauth) kubernaut-ui.mk: Don't include $(CURDIR) in the claim name

--- kubernaut-ui.mk
+++ kubernaut-ui.mk
@@ -17,7 +17,7 @@ include $(dir $(lastword $(MAKEFILE_LIST)))kubernaut.mk
 # We do $(or $(NAME),cluster) instead of setting NAME?=cluster, so
 # that a .mk file providing automatic NAME can set `NAME?=` without
 # having to force the Makefile to worry about inclusion order.
-_KUBECONFIG = $(CURDIR)/$(or $(NAME),cluster).knaut
+_KUBECONFIG = $(or $(NAME),cluster).knaut
 export KUBECONFIG = $(_KUBECONFIG)
 
 claim: ## Obtain an ephemeral k8s cluster from kubernaut.io
