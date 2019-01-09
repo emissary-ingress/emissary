cb331967b5552fd21ed38c0f877a98c7ca0df866
Abhay Saxena <ark3@datawire.io>
2019-01-09 16:42:11 -0500

    Add version command to apictl-key

--- cmd/apictl-key/main.go
+++ cmd/apictl-key/main.go
@@ -8,6 +8,9 @@ var apictl_key = &cobra.Command{
 	Use: "apictl-key [command]",
 }
 
+// Version is inserted at build using --ldflags -X
+var Version = "(unknown version)"
+
 func main() {
 	apictl_key.Execute()
 }
