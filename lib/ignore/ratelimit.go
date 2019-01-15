dbe58275469b3f35b7fcdd194f806d59afa7d948
Luke Shumaker <lukeshu@datawire.io>
2019-01-15 16:37:31 -0500

    Pin github.com/lyft/ratelimit in go.mod by doing a bogus import

---
+++ lib/ignore/ratelimit.go
@@ -0,0 +1,7 @@
+// Package ignore can be ignored.  It exists to pin the ratelimit
+// repo, so `go mod tidy` knows about it.
+package ignore
+
+import (
+	_ "github.com/lyft/ratelimit/src/settings"
+)
