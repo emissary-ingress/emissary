c7d270c91d3fdc076eb129e1186055519ed60f2f
Luke Shumaker <lukeshu@datawire.io>
2019-01-15 10:53:38 -0500

    e2e-oauth/env.sh: Organize

--- e2e-oauth/env.sh
+++ e2e-oauth/env.sh
@@ -1,20 +1,20 @@
 #!/hint/sh
 
-# This is the external ip address of the ambassador you are using to
-# route to the auth service under development:
-EXTERNAL_IP=ambassador.datawire.svc.cluster.local
-
+# 03-ambassador-pro-oauth.yaml
 IMAGE=$(cat docker/ambassador-pro.docker.knaut-push)
-
-# Auth0
-AUTH_TENANT_URL=https://ambassador.datawire.svc.cluster.local
-AUTH_CALLBACK_URL=https://ambassador.datawire.svc.cluster.local/callback
 AUTH_PROVIDER_URL=https://ambassador-oauth-e2e.auth0.com
+
+# 04-tenants.yaml
+AUTH_TENANT_URL=https://ambassador.datawire.svc.cluster.local
 AUTH_AUDIENCE=https://ambassador-oauth-e2e.auth0.com/api/v2/
 AUTH_CLIENT_ID=DOzF9q7U2OrvB7QniW9ikczS1onJgyiC
 AUTH_CLIENT_SECRET=MkpnAmzX-EEzV708qD_giNd9CF_R-owNau94QZVgOfna9FYf-SdTvATuNkrEDBk-
-AUTH_SCOPE=email
 
-# E2e test data:
+# acceptance_test.js
+EXTERNAL_IP=ambassador.datawire.svc.cluster.local
 TESTUSER_EMAIL=testuser@datawire.com
 TESTUSER_PASSWORD=TestUser321
+
+# Unused?
+AUTH_CALLBACK_URL=https://ambassador.datawire.svc.cluster.local/callback
+AUTH_SCOPE=email
