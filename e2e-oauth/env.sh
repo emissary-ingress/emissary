2d3ce29f965685fed429f1f4d4a2f90321206670
Luke Shumaker <lukeshu@datawire.io>
2019-01-15 11:01:04 -0500

    e2e-oauth/env.sh: Clean up

--- e2e-oauth/env.sh
+++ e2e-oauth/env.sh
@@ -1,20 +1,26 @@
 #!/hint/sh
 
-# 03-ambassador-pro-oauth.yaml
-IMAGE=$(cat docker/ambassador-pro.docker.knaut-push)
-AUTH_PROVIDER_URL=https://ambassador-oauth-e2e.auth0.com
-
-# 04-tenants.yaml
-AUTH_TENANT_URL=https://ambassador.datawire.svc.cluster.local
-AUTH_AUDIENCE=https://ambassador-oauth-e2e.auth0.com/api/v2/
-AUTH_CLIENT_ID=DOzF9q7U2OrvB7QniW9ikczS1onJgyiC
-AUTH_CLIENT_SECRET=MkpnAmzX-EEzV708qD_giNd9CF_R-owNau94QZVgOfna9FYf-SdTvATuNkrEDBk-
-
 # acceptance_test.js
 EXTERNAL_IP=ambassador.datawire.svc.cluster.local
 TESTUSER_EMAIL=testuser@datawire.com
 TESTUSER_PASSWORD=TestUser321
 
-# Unused?
-AUTH_CALLBACK_URL=https://ambassador.datawire.svc.cluster.local/callback
-AUTH_SCOPE=email
+# These come directly from
+# https://manage.auth0.com/#/applications/DOzF9q7U2OrvB7QniW9ikczS1onJgyiC/settings
+_Auth0_Domain=ambassador-oauth-e2e.auth0.com
+_Auth0_Client_ID=DOzF9q7U2OrvB7QniW9ikczS1onJgyiC
+_Auth0_Client_Secret=MkpnAmzX-EEzV708qD_giNd9CF_R-owNau94QZVgOfna9FYf-SdTvATuNkrEDBk-
+# Make sure that:
+#  - "https://${EXTERNAL_IP}/callback" is in the "Allowed Callback URLs" textbox
+#  - "https://${EXTERNAL_IP}" is in the "Allowed Web Origins" textbox
+#  - The TESTUSER_EMAIL/TESTUSER_PASSWORD account is set up
+
+# 03-ambassador-pro-oauth.yaml
+IMAGE=$(cat docker/ambassador-pro.docker.knaut-push)
+AUTH_PROVIDER_URL=https://${_Auth0_Domain}
+
+# 04-tenants.yaml
+AUTH_TENANT_URL=https://${EXTERNAL_IP}
+AUTH_AUDIENCE=https://${_Auth0_Domain}/api/v2/
+AUTH_CLIENT_ID=${_Auth0_Client_ID}
+AUTH_CLIENT_SECRET=${_Auth0_Client_Secret}
