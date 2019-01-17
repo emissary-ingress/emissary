b1d8cdaab374d8843298c8bec34f9f906ee3d03c
Luke Shumaker <lukeshu@datawire.io>
2019-01-16 19:54:13 -0500

    apictl: Look in ~/.config/ for the license key
    I have enough stuff in my home directory!
    
    To avoid breaking existing users, if ~/.config/ambassador/license-key does
    not exist, but ~/.ambassador.key does, we'll use that.  This is the same
    strategy used by Arch Linux's `makepkg` tool to migrate from
    ~/.makepkg.conf to to ~/.config/pacman/makepkg.conf.
    
    Oh, it also obeys macOS filesystem layout guidelines, and puts it in
    `~/Library/Application Support/ambassador/`, same as kubernaut.

--- cmd/apictl/main.go
+++ cmd/apictl/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"os/user"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"time"
 
@@ -33,16 +34,69 @@ var (
 	}
 )
 
+// userConfigDir returns the default directory to use for
+// user-specific config data.  It is similar to os.UserCacheDir().
+func userConfigDir() (string, error) {
+	var dir string
+
+	switch runtime.GOOS {
+	case "darwin":
+		// https://developer.apple.com/library/archive/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/FileSystemOverview/FileSystemOverview.html
+		dir = os.Getenv("HOME")
+		if dir == "" {
+			return "", errors.New("$HOME is not defined")
+		}
+		dir += "/Library/Application Support"
+
+	case "linux":
+		// http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+		dir = os.Getenv("XDG_CONFIG_HOME")
+		if dir == "" {
+			dir = os.Getenv("HOME")
+			if dir == "" {
+				return "", errors.New("neither $XDG_CACHE_HOME nor $HOME are defined")
+			}
+			dir += "/.config"
+		}
+
+	default:
+		return "", errors.New(`Only the "darwin" and "linux" GOOS are supported at this time`)
+	}
+
+	return dir, nil
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
+func defaultLicenseFile() (string, error) {
+	filename := os.Getenv("AMBASSADOR_LICENSE_FILE")
+	if filename != "" {
+		return filename, nil
+	}
+	cfgDir, err := userConfigDir()
+	if err != nil {
+		return "", err
+	}
+	filename = filepath.Join(cfgDir, "ambassador", "license-key")
+	if !fileExists(filename) {
+		// for compatibility with < 0.1.1
+		if home := os.Getenv("HOME"); home != "" {
+			legacyFile := filepath.Join(home, ".ambassador.key")
+			if fileExists(legacyFile) {
+				filename = legacyFile
+			}
+		}
+	}
+	return filename, nil
+}
+
 func init() {
 	apictl.PersistentFlags().StringVarP(&LICENSE_KEY, "license-key", "", os.Getenv("AMBASSADOR_LICENSE_KEY"),
 		"ambassador license key")
-	keyfile := os.Getenv("AMBASSADOR_LICENSE_FILE")
-	if keyfile == "" {
-		usr, err := user.Current()
-		if err == nil {
-			keyfile = filepath.Join(usr.HomeDir, ".ambassador.key")
-		}
-	}
+	keyfile, _ := defaultLicenseFile()
 	apictl.PersistentFlags().StringVarP(&LICENSE_FILE, "license-file", "", keyfile, "ambassador license file")
 }
 
