4e15f5620cd12c99f5a239952c630796915df397
Luke Shumaker <lukeshu@datawire.io>
2019-01-17 16:18:51 -0500

    docker/amb-sidecar-ratelimit: Put runtime files under /run
    And set the appropriate environment variables in entrypoint.sh instead of
    Dockerfile.

--- docker/amb-sidecar-ratelimit/entrypoint.sh
+++ docker/amb-sidecar-ratelimit/entrypoint.sh
@@ -3,8 +3,9 @@ set -x
 
 trap 'jobs -p | xargs -r kill --' INT
 
-./ratelimit &
-./apictl rls watch -o config &
+mkdir -p /run/amb
+USE_STATSD=false RUNTIME_ROOT=/run/amb/config RUNTIME_SUBDIRECTORY=config ./ratelimit &
+./apictl rls watch -o /run/amb/config &
 
 while test -n "$(jobs -p)"; do
 	wait -n
