243506a55b166ca6b8b9130ba537fac96f12611e
Luke Shumaker <lukeshu@datawire.io>
2019-01-24 10:05:49 -0500

    docker/app-sidecar/entrypoint.sh: Deal robustly with crashes
    This is important because if ./app-sidecar quits because it doesn't have
    a license key, then the container will keep running, but mysteriously not
    working (and the huge amount of logging from envoy(?) means that the
    license message will probably get buried).

--- docker/app-sidecar/entrypoint.sh
+++ docker/app-sidecar/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z "${APPPORT}" ]; then
     echo "ERROR: APPPORT env var not configured."
@@ -23,6 +23,27 @@ cat >> bootstrap-ads.yaml <<EOF
                 port_value: ${APPPORT}
 EOF
 
-./ambex -watch data > /dev/null 2>&1 &
-./app-sidecar &
-envoy -l debug -c bootstrap-ads.yaml
+# Initialize job management
+trap 'jobs -p | xargs -r kill --' INT
+launch() {
+	(
+		trap 'echo "Exited with $?: $*"' EXIT
+		env "$@"
+	) &
+}
+
+# Launch each of the worker processes
+launch sh -c 'exec ./ambex -watch data >/dev/null 2>&1'
+launch ./app-sidecar
+launch envoy -l debug -c bootstrap-ads.yaml
+
+# Wait for one of them to quit, then kill the others
+wait -n
+r=$?
+echo ' ==> One of the worker processes exited; shutting down the others <=='
+while test -n "$(jobs -p)"; do
+	jobs -p | xargs -r kill --
+	wait -n
+done
+echo 'Finished shutting down'
+exit $r
