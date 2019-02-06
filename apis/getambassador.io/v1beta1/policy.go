efcedcfe29d0dc485911975e58ab8af77acbbcff
Luke Shumaker <lukeshu@datawire.io>
2019-02-06 10:01:34 -0500

    Revert "oauth: Use ericchiang/k8s"
    This reverts commit 7d0618f6665115525cc0e11d7ad962700a284a16.

--- apis/getambassador.io/v1beta1/policy.go
+++ apis/getambassador.io/v1beta1/policy.go
@@ -1,15 +1,8 @@
 package v1
 
 import (
-	"github.com/ericchiang/k8s"
-	metav1 "github.com/ericchiang/k8s/apis/meta/v1"
 	"github.com/gobwas/glob"
 )
-
-type Policy struct {
-	Metadata *metav1.ObjectMeta `json:"metadata"`
-	Spec     *PolicySpec        `json:"spec"`
-}
 
 type PolicySpec struct {
 	Rules []Rule `json:"rules"`
@@ -22,28 +15,6 @@ type Rule struct {
 	Public bool            `json:"public"`
 	Scope  string          `json:"scope"`
 	Scopes map[string]bool `json:"-"` // is calculated from Scope
-}
-
-// register //////////////////////////////////////////////////////////
-
-// Required to implement k8s.Resource
-func (crd *Policy) GetMetadata() *metav1.ObjectMeta {
-	return crd.Metadata
-}
-
-type PolicyList struct {
-	Metadata *metav1.ListMeta `json:"metadata"`
-	Items    []*Policy        `json:"items"`
-}
-
-// Require for PolicyList to implement k8s.ResourceList
-func (crdl *PolicyList) GetMetadata() *metav1.ListMeta {
-	return crdl.Metadata
-}
-
-func init() {
-	k8s.Register("getambassador.io", "v1beta1", "policies", true, &Policy{})
-	k8s.RegisterList("getambassador.io", "v1beta1", "policies", true, &PolicyList{})
 }
 
 //////////////////////////////////////////////////////////////////////
