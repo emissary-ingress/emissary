ae163d43912179eca8c3ada97db74dbea737c54b
Luke Shumaker <lukeshu@datawire.io>
2019-02-08 11:36:00 -0500

    Factor as much as I can out from cmd_auth.go in to util.go

--- cmd/amb-sidecar/cmd_auth.go
+++ cmd/amb-sidecar/cmd_auth.go
@@ -3,14 +3,9 @@ package main
 import (
 	"context"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 
-	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"golang.org/x/sync/errgroup"
 
 	"github.com/datawire/apro/cmd/amb-sidecar/oauth/app"
 	"github.com/datawire/apro/cmd/amb-sidecar/oauth/controller"
@@ -50,37 +45,11 @@ func init() {
 			l.SetLevel(logrus.InfoLevel)
 		}
 
-		// Initialize hardCtx/softCtx/group to manage
-		// our goroutines and graceful shutdown.
-		sigs := make(chan os.Signal, 1)
-		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-		hardCtx, hardCancel := context.WithCancel(context.Background())
-		group, softCtx := errgroup.WithContext(hardCtx)
-		group.Go(func() error {
-			defer func() {
-				// If we receive another signal after
-				// graceful-shutdown, we should trigger a
-				// not-so-graceful shutdown.
-				go func() {
-					sig := <-sigs
-					l.Errorln(errors.Errorf("received signal %v", sig))
-					hardCancel()
-					// keep logging signals
-					for sig := range sigs {
-						l.Errorln(errors.Errorf("received signal %v", sig))
-					}
-				}()
-			}()
-
-			select {
-			case sig := <-sigs:
-				return errors.Errorf("received signal %v", sig)
-			case <-softCtx.Done():
-				return nil
-			}
+		group := NewGroup(context.Background(), cfg, func(name string) types.Logger {
+			return types.WrapLogrus(l)
 		})
-
-		return cmdAuth(hardCtx, softCtx, group, cfg, types.WrapLogrus(l))
+		cmdAuth(group)
+		return group.Wait()
 	}
 
 	argparser.AddCommand(cmd)
@@ -91,29 +60,24 @@ func init() {
 //  - `softCtx` being canceled triggers a graceful shutdown
 //  - `hardCtx` being canceled triggers a not-so-graceful shutdown
 //  - register goroutines with `group`
-func cmdAuth(
-	hardCtx, softCtx context.Context, group *errgroup.Group, // for keeping track of goroutines
-	authCfg types.Config, // config, tells us what to do
-	l types.Logger, // where to log to
-) error {
+func cmdAuth(group *Group) {
 	// The gist here is that we have 2 main goroutines:
 	// - the k8s controller, witch watches for CRD changes
 	// - the HTTP server than handles auth requests
 	// The HTTP server queries k8s controller for the current CRD
 	// state.
 
-	ct := &controller.Controller{
-		Config: authCfg,
-		Logger: l.WithField("MAIN", "auth-k8s"),
-	}
+	ct := &controller.Controller{}
 
-	group.Go(func() error {
+	group.Go("auth_http", func(hardCtx, softCtx context.Context, cfg types.Config, l types.Logger) error {
+		ct.Config = cfg
+		ct.Logger = l.WithField("MAIN", "auth-k8s")
 		ct.Watch(softCtx)
 		return nil
 	})
 
-	group.Go(func() error {
-		httpHandler, err := app.NewHandler(authCfg, l, ct)
+	group.Go("auth_http", func(hardCtx, softCtx context.Context, cfg types.Config, l types.Logger) error {
+		httpHandler, err := app.NewHandler(cfg, l, ct)
 		if err != nil {
 			return err
 		}
@@ -124,27 +88,4 @@ func cmdAuth(
 		}
 		return listenAndServeWithContext(hardCtx, softCtx, server)
 	})
-
-	return group.Wait()
-}
-
-// listenAndServeWithContext runs server.ListenAndServer() on an
-// http.Server(), but properly calls server.Shutdown when the context
-// is canceled.
-//
-// softCtx should be a child context of hardCtx.  softCtx being
-// canceled triggers server.Shutdown().  If hardCtx being cacneled
-// triggers that .Shutdown() to kill any live requests and return,
-// instead of waiting for them to be completed gracefully.
-func listenAndServeWithContext(hardCtx, softCtx context.Context, server *http.Server) error {
-	serverCh := make(chan error)
-	go func() {
-		serverCh <- server.ListenAndServe()
-	}()
-	select {
-	case err := <-serverCh:
-		return err
-	case <-softCtx.Done():
-		return server.Shutdown(hardCtx)
-	}
 }
