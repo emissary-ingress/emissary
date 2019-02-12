fd98d234491f902637801be42bc05597f37a2cb4
Gabriel <gsagula@gmail.com>
2019-02-11 19:58:57 -0800

    re-imported the http auth tests
    Signed-off-by: Gabriel <gsagula@gmail.com>

--- ambassador/tests/kat/test_ambassador.py
+++ ambassador/tests/kat/test_ambassador.py
@@ -14,7 +14,15 @@ from abstract_tests import MappingTest, OptionTest, ServiceType, Node
 from t_ratelimit import RateLimitTest
 from t_tracing import TracingTest
 from t_shadow import ShadowTest
-from t_extauth import AuthenticationGRPCTest
+from t_extauth import (
+    AuthenticationTest,	
+    AuthenticationTestV1,	
+    AuthenticationHTTPBufferedTest,	
+    AuthenticationWebsocketTest,	
+    AuthenticationWebsocketTest,	
+    AuthenticationWebsocketTimeoutTest,
+    AuthenticationGRPCTest
+)
 
 # XXX: should test empty ambassador config
 
