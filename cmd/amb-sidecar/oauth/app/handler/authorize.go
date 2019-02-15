c05ec9f0627d8f2d6ed17d3aa21b578b3b52d48d
Luke Shumaker <lukeshu@datawire.io>
2019-02-15 09:57:30 -0500

    amb-sidecar: oauth: Move authorize.go and callback.go in to the same file

--- cmd/amb-sidecar/oauth/app/handler/authorize.go
+++ cmd/amb-sidecar/oauth/app/handler/authorize.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 	"net/url"
 	"time"
@@ -8,11 +10,19 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 	uuid "github.com/satori/go.uuid"
 
+	"github.com/datawire/apro/cmd/amb-sidecar/oauth/app/client"
 	"github.com/datawire/apro/cmd/amb-sidecar/oauth/app/discovery"
 	"github.com/datawire/apro/cmd/amb-sidecar/oauth/app/secret"
 	"github.com/datawire/apro/cmd/amb-sidecar/oauth/controller"
 	"github.com/datawire/apro/cmd/amb-sidecar/types"
 	"github.com/datawire/apro/lib/util"
+)
+
+const (
+	// AccessTokenCookie cookie's name
+	AccessTokenCookie = "access_token"
+	// Code is the default grant used in for this handler.
+	Code = "authorization_code"
 )
 
 // Authorize is the last handler in the chain of the authorization server.
@@ -22,6 +32,73 @@ type Authorize struct {
 	Ctrl      *controller.Controller
 	Secret    *secret.Secret
 	Discovery *discovery.Discovery
+}
+
+// Callback validates IDP requests and handles code exchange flow.
+type Callback struct {
+	Logger types.Logger
+	Secret *secret.Secret
+	Ctrl   *controller.Controller
+	Rest   *client.Rest
+}
+
+// ServeHTTP inspects if the request contains code and signed states...
+func (c *Callback) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c.Logger.Debug("request received")
+	if err := r.URL.Query().Get("error"); err != "" {
+		util.ToJSONResponse(w, http.StatusUnauthorized, &util.Error{Message: "unauthorized"})
+		return
+	}
+
+	tenant := controller.GetTenantFromContext(r.Context())
+	if tenant == nil {
+		c.Logger.Errorf("app request context cannot be nil")
+		util.ToJSONResponse(w, http.StatusUnauthorized, &util.Error{Message: "unauthorized"})
+		return
+	}
+
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		c.Logger.Error("check code failed")
+		util.ToJSONResponse(w, http.StatusUnauthorized, &util.Error{Message: "unauthorized"})
+		return
+	}
+
+	rURL, err := c.checkState(r)
+	if err != nil {
+		c.Logger.Errorf("check state failed: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var res *client.AuthorizationResponse
+	res, err = c.Rest.Authorize(&client.AuthorizationRequest{
+		GrantType:    Code,
+		ClientID:     tenant.ClientID,
+		Code:         code,
+		RedirectURL:  tenant.CallbackURL,
+		ClientSecret: tenant.Secret,
+	})
+	if err != nil {
+		c.Logger.Errorf("authorization request failed: %v", err)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	c.Logger.Debug("setting authorization cookie")
+	http.SetCookie(w, &http.Cookie{
+		Name:     AccessTokenCookie,
+		Value:    res.AccessToken,
+		HttpOnly: true,
+		Secure:   tenant.TLS,
+		Expires:  time.Now().Add(time.Duration(res.ExpiresIn) * time.Second),
+	})
+
+	// If the user-agent request was a POST or PUT, 307 will preserve the body
+	// and just follow the location header.
+	// https://tools.ietf.org/html/rfc7231#section-6.4.7
+	c.Logger.Debugf("redirecting user-agent to: %s", rURL)
+	http.Redirect(w, r, rURL, http.StatusTemporaryRedirect)
 }
 
 // ServeHTTP is a handler function that inspects the request by looking for the presence of
@@ -71,4 +148,29 @@ func (h *Authorize) signState(r *http.Request) string {
 	}
 
 	return k
+}
+
+func (c *Callback) checkState(r *http.Request) (string, error) {
+	state := r.URL.Query().Get("state")
+	if state == "" {
+		return "", errors.New("empty state param")
+	}
+
+	token, err := jwt.Parse(state, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
+			return "", fmt.Errorf("unexpected signing method %v", t.Header["redirect_url"])
+		}
+		return c.Secret.GetPublicKey(), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !(ok && token.Valid) {
+		return "", errors.New("state token validation failed")
+	}
+
+	return claims["redirect_url"].(string), nil
 }
