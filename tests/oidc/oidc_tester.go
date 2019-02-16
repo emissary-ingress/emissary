0bf7d65e514c8f0d8b1d67611a3861f912061f1e
plombardi <plombardi89@gmail.com>
2019-02-15 23:03:24 -0500

    Cleanup and move IDP specific logic into IDP specific funcs

--- tests/oidc/oidc_tester.go
+++ tests/oidc/oidc_tester.go
@@ -5,17 +5,35 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	"github.com/davecgh/go-spew/spew"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httputil"
 	"net/url"
+	"os"
 	"strings"
 	"time"
 )
 
 type oidcIntegration interface {
 	createLoginRequest() (*http.Request, error)
+	doLogin() (string, error)
+}
+
+type RequestResponsePair struct {
+	Request  *http.Request
+	Response *http.Response
+}
+
+type History struct {
+	entries map[string]RequestResponsePair
+}
+
+func (h *History) registerRequest(name string, pair RequestResponsePair) {
+
+}
+
+func (h *History) getUnauthorizedPair() *RequestResponsePair {
+	return nil
 }
 
 // =====================================
@@ -23,14 +41,15 @@ type oidcIntegration interface {
 // =====================================
 
 type auth0 struct {
-	username string
-	password string
-	tenant   string
-	audience string
-	clientID string
-	scopes   []string
-	csrf     string
-	state    string
+	username   string
+	password   string
+	tenant     string
+	audience   string
+	clientID   string
+	scopes     []string
+	csrf       string
+	state      string
+	httpClient *http.Client
 }
 
 // auth0Login is the payload sent to the Auth0 Username and Password login endpoint.
@@ -50,6 +69,75 @@ type auth0UsernameAndPasswordLogin struct {
 	State        string                 `json:"state"`
 	Tenant       string                 `json:"tenant"`
 	Username     string                 `json:"username"`
+}
+
+func (auth0 *auth0) Authenticate(loginForm *http.Response, state string) (string, error) {
+	loginRequest, err := auth0.createLoginRequest(loginForm, state)
+	CheckIfError(err)
+
+	loginRequest.Header.Add(
+		"User-Agent",
+		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36")
+
+	loginResponse, err := auth0.httpClient.Do(loginRequest)
+	CheckIfError(err)
+	CheckIfStatus(loginResponse, 200)
+
+	htmlDoc, err := goquery.NewDocumentFromReader(loginResponse.Body)
+	CheckIfError(err)
+
+	form := htmlDoc.Find("form[name=hiddenform]")
+	loginCallbackURL, err := url.Parse(form.AttrOr("action", ""))
+	CheckIfError(err)
+
+	loginCallbackToken := htmlDoc.Find("input[name=wresult]").AttrOr("value", "")
+	loginCallbackCtx := htmlDoc.Find("input[name=wctx]").AttrOr("value", "")
+
+	formData := url.Values{}
+	formData.Add("wresult", loginCallbackToken)
+	formData.Add("wctx", loginCallbackCtx)
+
+	loginCallbackRequest, err := http.NewRequest("POST", loginCallbackURL.String(), strings.NewReader(formData.Encode()))
+	// this is all stuff the browser sends so add it as well in case its needed
+	loginCallbackRequest.Header.Set("accept", "*/*")
+	loginCallbackRequest.Header.Set("accept-language", "en-US,en;q=0.9")
+	loginCallbackRequest.Header.Set("content-type", "application/x-www-form-urlencoded")
+	loginCallbackRequest.Header.Set("dnt", "1")
+	loginCallbackRequest.Header.Set("origin", fmt.Sprintf("https://%s.auth0.com/", auth0.tenant))
+	loginCallbackRequest.Header.Set("referrer", "https://ambassador-oauth-e2e.auth0.com/login")
+	loginCallbackRequest.Header.Set("cookie", fmt.Sprintf("_csrf=%s", auth0.csrf)) // VERY IMPORTANT. 403 if excluded
+
+	loginCallbackResponse, err := auth0.httpClient.Do(loginCallbackRequest)
+	CheckIfError(err)
+
+	//data, err := httputil.DumpResponse(loginCallbackResponse, true)
+	//fmt.Println(string(data))
+
+	// back to our callback...
+	redirectURL, err := url.Parse(loginCallbackResponse.Header.Get("Location"))
+	callbackRequest, err := http.NewRequest("POST", redirectURL.String(), strings.NewReader(formData.Encode()))
+	callbackRequest.Header.Set("accept", "*/*")
+	callbackRequest.Header.Set("accept-language", "en-US,en;q=0.9")
+	callbackRequest.Header.Set("content-type", "application/x-www-form-urlencoded")
+	callbackRequest.Header.Set("dnt", "1")
+	callbackRequest.Header.Set("origin", fmt.Sprintf("https://%s.auth0.com/", auth0.tenant))
+	callbackRequest.Header.Set("referrer", "https://ambassador-oauth-e2e.auth0.com/login")
+	callbackRequest.Header.Set("cookie", fmt.Sprintf("_csrf=%s", auth0.csrf)) // VERY IMPORTANT. 403 if excluded
+
+	callbackResponse, err := auth0.httpClient.Do(callbackRequest)
+	if err != nil {
+		panic(err)
+	}
+
+	accessToken := ""
+	for _, c := range callbackResponse.Cookies() {
+		if c.Name == "access_token" {
+			accessToken = c.Value
+			break
+		}
+	}
+
+	return accessToken, nil
 }
 
 func (idp *auth0) createLoginRequest(r *http.Response, state string) (*http.Request, error) {
@@ -90,8 +178,6 @@ func (idp *auth0) createLoginRequest(r *http.Response, state string) (*http.Requ
 	}
 
 	loginParamsString := string(loginParamsBytes)
-	fmt.Println(loginParamsString)
-
 	request, err := http.NewRequest("POST", loginEndpoint.String(), strings.NewReader(loginParamsString))
 	if err != nil {
 		return nil, err
@@ -112,20 +198,6 @@ func (idp *auth0) createLoginRequest(r *http.Response, state string) (*http.Requ
 	return request, nil
 }
 
-//func (c *Rest) request(method string, url fmt.Stringer, params url.Values, body interface{}) (*http.Request, error) {
-//	rq, err := http.NewRequest(method, url.String(), strings.NewReader(params.Encode()))
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	rq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-//	if c.token != "" {
-//		rq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.token))
-//	}
-//
-//	return rq, nil
-//}
-
 // =====================================
 // Keycloak IdP
 // =====================================
@@ -136,11 +208,11 @@ func (idp *keycloak) fmtLoginURL() url.URL {
 	return url.URL{}
 }
 
-func main() {
-	var err error
-
+// returns an http client that is configured for use in OpenID Connect authentication tests. The client is configured
+// to ignore self-signed TLS certificates and to not follow redirects automatically.
+func newHTTPClient(timeout time.Duration) http.Client {
 	cookieJar, _ := cookiejar.New(nil)
-	httpClient := http.Client{
+	return http.Client{
 		// DO NOT FOLLOW REDIRECTS: https://stackoverflow.com/a/38150816
 		//
 		// This is test code. We do not want to follow any redirects automatically because we may want to write
@@ -156,80 +228,32 @@ func main() {
 		},
 
 		Jar:     cookieJar,
-		Timeout: 5 * time.Second,
+		Timeout: timeout,
+	}
+}
+
+func CheckIfError(err error) {
+	if err == nil {
+		return
 	}
 
-	// 1. Initiate an HTTP GET request to our protected service
-	unauthorizedRequestURL := url.URL{
-		Scheme: "https",
-		Host:   "ambassador.localdev.svc.cluster.local",
-		Path:   "/httpbin/headers",
+	fmt.Printf("Error %s", err)
+	os.Exit(1)
+}
+
+func CheckIfStatus(r *http.Response, expectedStatus int) {
+	if r.StatusCode != expectedStatus {
+		fmt.Printf("Error expected HTTP status %d but was %d\n", expectedStatus, r.StatusCode)
+
+		data, err := httputil.DumpResponse(r, true)
+		CheckIfError(err)
+		fmt.Println(string(data))
+
+		os.Exit(1)
 	}
+}
 
-	unauthorizedRequest, err := createHTTPRequest("GET", unauthorizedRequestURL)
-	if err != nil {
-		panic(err)
-	}
-
-	unauthorizedResponse, err := httpClient.Do(unauthorizedRequest)
-	if err != nil {
-		panic(err)
-	}
-
-	// 2. Check that we have been given a Redirect
-	if unauthorizedResponse.StatusCode != 303 {
-		panic(fmt.Errorf("unexpected HTTP status code for initial redirect: %d", unauthorizedResponse.StatusCode))
-	}
-
-	// 3. Construct an HTTP request to the IDP Authorization URL.
-	redirectURL, err := url.Parse(unauthorizedResponse.Header.Get("Location"))
-
-	//// this will be needed because future requests will give us relative URLs
-	//redirectURLBase := url.URL{
-	//	Scheme: redirectURL.Scheme,
-	//	Host: redirectURL.Host,
-	//}
-
-	loginUIRequest, err := createHTTPRequest("GET", *redirectURL)
-	if err != nil {
-		panic(err)
-	}
-
-	// 4. Since we're not Authn/z with the IdP yet we *SHOULD* be redirected to a Login UI
-	loginUIResponse, err := httpClient.Do(loginUIRequest)
-	if err != nil {
-		panic(err)
-	}
-
-	if loginUIResponse.StatusCode != 302 {
-		spew.Dump(loginUIResponse)
-		panic(fmt.Errorf("unexpected HTTP status code for login UI redirect %d", unauthorizedResponse.StatusCode))
-	}
-
-	// 5. Construct a request to the IdP login UI based
-	redirectURL, err = url.Parse(loginUIResponse.Header.Get("Location"))
-	redirectURL.Scheme = "https"
-	redirectURL.Host = loginUIRequest.Host
-
-	loginUIRequest, err = createHTTPRequest("GET", *redirectURL)
-	if err != nil {
-		panic(err)
-	}
-
-	loginUIResponse, err = httpClient.Do(loginUIRequest)
-	if err != nil {
-		panic(err)
-	}
-
-	if loginUIResponse.StatusCode != 200 {
-		spew.Dump(loginUIResponse)
-		panic(fmt.Errorf("unexpected HTTP status code after login UI redirect %d", unauthorizedResponse.StatusCode))
-	}
-
-	// We should be at a login form by this point. Unfortunately the login form is rendered is IDP specific, for
-	// example, Auth0 renders it with some JavaScript magic. This form has important attributes on it like the form
-	// field names to send during the login request.
-
+func main() {
 	auth0 := auth0{
 		username: "testuser@datawire.com",
 		password: "TestUser321",
@@ -239,95 +263,67 @@ func main() {
 		clientID: "DOzF9q7U2OrvB7QniW9ikczS1onJgyiC",
 	}
 
-	loginRequest, err := auth0.createLoginRequest(loginUIResponse, redirectURL.Query().Get("state"))
+	httpClient := newHTTPClient(5 * time.Second)
+	auth0.httpClient = &httpClient
 
-	// Set the User-Agent to be Chrome just in case the IDPs do something like inspect the User Agent
-	loginRequest.Header.Add(
-		"User-Agent",
-		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36")
-
-	if err != nil {
-		panic(err)
+	// 1. Initiate an HTTP GET request to our protected service
+	unauthorizedRequestURL := url.URL{
+		Scheme: "https",
+		Host:   "ambassador.localdev.svc.cluster.local",
+		Path:   "/httpbin/headers",
 	}
 
-	loginResponse, err := httpClient.Do(loginRequest)
-	if loginResponse.StatusCode != 200 {
-		data, _ := httputil.DumpResponse(loginResponse, true)
-		fmt.Println(string(data))
-		panic(fmt.Errorf("unexpected HTTP status code after login %d", loginResponse.StatusCode))
+	unauthorizedRequest, err := createHTTPRequest("GET", unauthorizedRequestURL)
+	CheckIfError(err)
+
+	unauthorizedResponse, err := httpClient.Do(unauthorizedRequest)
+	CheckIfError(err)
+	CheckIfStatus(unauthorizedResponse, http.StatusSeeOther)
+
+	// 2. Construct a redirect to the Identity Providers Authorization endpoint. Since we do not have an access token
+	// at this point we will end up being redirected.
+	redirectURL, err := url.Parse(unauthorizedResponse.Header.Get("Location"))
+	CheckIfError(err)
+
+	authRequest, err := createHTTPRequest("GET", *redirectURL)
+	CheckIfError(err)
+
+	authResponse, err := httpClient.Do(authRequest)
+	CheckIfError(err)
+	CheckIfStatus(authResponse, http.StatusFound)
+
+	// 3. Handle the Redirect and goto the IdP Login URL
+	loginUIRedirectURL, err := url.Parse(authResponse.Header.Get("Location"))
+
+	// Auth0 hands back relative (path-only) redirects which are useless for subsequent requests. We are talking to the
+	// same endpoint as "authRequest" so just use the scheme, host and port info from that.
+	loginUIRedirectURL.Scheme = authRequest.URL.Scheme
+	loginUIRedirectURL.Host = authRequest.URL.Host
+	loginUIRequest, err := createHTTPRequest("GET", *loginUIRedirectURL)
+	CheckIfError(err)
+
+	loginUIResponse, err := httpClient.Do(loginUIRequest)
+	CheckIfError(err)
+	CheckIfStatus(loginUIResponse, http.StatusOK)
+
+	// We should be at a login form by this point. Unfortunately the login form rendered is IDP specific, for
+	// example, Auth0 renders it with some JavaScript magic. This form has important attributes on it like the form
+	// field names to send during the login request.
+	accessToken, err := auth0.Authenticate(loginUIResponse, loginUIRedirectURL.Query().Get("state"))
+	CheckIfError(err)
+
+	if accessToken == "" {
+		fmt.Println("Access Token was not returned")
+		os.Exit(1)
 	}
-
-	htmlDoc, err := goquery.NewDocumentFromReader(loginResponse.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	form := htmlDoc.Find("form[name=hiddenform]")
-	loginCallbackURL, err := url.Parse(form.AttrOr("action", ""))
-	if err != nil {
-		panic(err)
-	}
-
-	loginCallbackToken := htmlDoc.Find("input[name=wresult]").AttrOr("value", "")
-	loginCallbackCtx := htmlDoc.Find("input[name=wctx]").AttrOr("value", "")
-
-	fmt.Printf("Callback URL: %s\n", loginCallbackURL)
-	fmt.Printf("Callback Token: %s\n", loginCallbackToken)
-	fmt.Printf("Callback Context: %s\n", loginCallbackCtx)
-
-	formData := url.Values{}
-	formData.Add("wresult", loginCallbackToken)
-	formData.Add("wctx", loginCallbackCtx)
-
-	loginCallbackRequest, err := http.NewRequest("POST", loginCallbackURL.String(), strings.NewReader(formData.Encode()))
-	// this is all stuff the browser sends so add it as well in case its needed
-	loginCallbackRequest.Header.Set("accept", "*/*")
-	loginCallbackRequest.Header.Set("accept-language", "en-US,en;q=0.9")
-	loginCallbackRequest.Header.Set("content-type", "application/x-www-form-urlencoded")
-	loginCallbackRequest.Header.Set("dnt", "1")
-	loginCallbackRequest.Header.Set("origin", fmt.Sprintf("https://%s.auth0.com/", auth0.tenant))
-	loginCallbackRequest.Header.Set("referrer", "https://ambassador-oauth-e2e.auth0.com/login")
-	loginCallbackRequest.Header.Set("cookie", fmt.Sprintf("_csrf=%s", auth0.csrf)) // VERY IMPORTANT. 403 if excluded
-
-	loginCallbackResponse, err := httpClient.Do(loginCallbackRequest)
-	if err != nil {
-		panic(err)
-	}
-
-	data, err := httputil.DumpResponse(loginCallbackResponse, true)
-	fmt.Println(string(data))
-
-	// back to our callback...
-	redirectURL, err = url.Parse(loginCallbackResponse.Header.Get("Location"))
-	callbackRequest, err := http.NewRequest("POST", redirectURL.String(), strings.NewReader(formData.Encode()))
-	callbackRequest.Header.Set("accept", "*/*")
-	callbackRequest.Header.Set("accept-language", "en-US,en;q=0.9")
-	callbackRequest.Header.Set("content-type", "application/x-www-form-urlencoded")
-	callbackRequest.Header.Set("dnt", "1")
-	callbackRequest.Header.Set("origin", fmt.Sprintf("https://%s.auth0.com/", auth0.tenant))
-	callbackRequest.Header.Set("referrer", "https://ambassador-oauth-e2e.auth0.com/login")
-	callbackRequest.Header.Set("cookie", fmt.Sprintf("_csrf=%s", auth0.csrf)) // VERY IMPORTANT. 403 if excluded
-
-	callbackResponse, err := httpClient.Do(callbackRequest)
-	if err != nil {
-		panic(err)
-	}
-
-	accessToken := ""
-	for _, c := range callbackResponse.Cookies() {
-		if c.Name == "access_token" {
-			accessToken = c.Value
-			break
-		}
-	}
-
-	data, err = httputil.DumpResponse(callbackResponse, true)
-	fmt.Println(string(data))
 
 	unauthorizedRequest.Header.Add("Authorization", fmt.Sprintf("Bearer %s", accessToken))
 	finalDestination, err := httpClient.Do(unauthorizedRequest)
 
-	data, err = httputil.DumpResponse(finalDestination, true)
+	data, err := httputil.DumpResponse(finalDestination, true)
+	CheckIfError(err)
+	CheckIfStatus(finalDestination, 200)
+
 	fmt.Println(string(data))
 }
 
@@ -336,11 +332,6 @@ func createHTTPRequest(method string, url url.URL) (*http.Request, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	//rq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	//if c.token != "" {
-	//	rq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.token))
-	//}
 
 	return request, nil
 }
