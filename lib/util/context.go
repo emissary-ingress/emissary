15dbcab66aec3e47218c9a8e5fbc244ce485c856
Luke Shumaker <lukeshu@datawire.io>
2019-03-04 17:38:51 -0500

    lib/util.ListenAndServeHTTPWithContext: Proof-read docs

--- lib/util/context.go
+++ lib/util/context.go
@@ -6,13 +6,14 @@ import (
 )
 
 // ListenAndServeHTTPWithContext runs server.ListenAndServe() on an
-// http.Server(), but properly calls server.Shutdown when the context
-// is canceled.
+// http.Server, but properly calls server.Shutdown when the context is
+// canceled.
 //
 // softCtx should be a child context of hardCtx.  softCtx being
-// canceled triggers server.Shutdown().  If hardCtx being cacneled
-// triggers that .Shutdown() to kill any live requests and return,
-// instead of waiting for them to be completed gracefully.
+// canceled triggers server.Shutdown().  hardCtx also being canceled
+// causes softCtx's .Shutdown() to hurry along and kill any live
+// requests and return, instead of waiting for them to be completed
+// gracefully.
 func ListenAndServeHTTPWithContext(hardCtx, softCtx context.Context, server *http.Server) error {
 	serverCh := make(chan error)
 	go func() {
