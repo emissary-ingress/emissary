3cb54ba4eceb3fa23e67883c3f9ab5d2733e9b24
Itamar Turner-Trauring <itamar@itamarst.org>
2019-04-04 10:21:53 -0400

    Service that exposes OpenAPI docs.

---
+++ docs/dev-portal/example-service/server.py
@@ -0,0 +1,13 @@
+import json
+import time
+from flask import Flask, make_response
+app = Flask(__name__)
+
+@app.route('/.well-known/openapi-docs')
+def openapi_docs():
+    with open("openapi.json") as f:
+        docs = json.load(f)
+    docs["info"]["title"] += " generated @ " + time.asctime()
+    result = make_response(json.dumps(docs))
+    result.headers["content-type"] = "text/json"
+    return result
