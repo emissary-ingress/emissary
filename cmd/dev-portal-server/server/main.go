2c8fdfb8af31094f3b92972130789079c2f2115d
Itamar Turner-Trauring <itamar@itamarst.org>
2019-04-04 15:03:14 -0400

    In theory the overall code is now done.

--- cmd/dev-portal-server/server/main.go
+++ cmd/dev-portal-server/server/main.go
@@ -1,8 +1,24 @@
 package server
 
-import ()
+import (
+	"github.com/datawire/apro/cmd/dev-portal-server/kubernetes"
+	"time"
+)
 
 func Main(version string) {
 	s := NewServer()
+
+	serviceMap := s.K8sStore.List()
+	knownServices := make([]kubernetes.Service, len(serviceMap))
+	i := 0
+	for k := range serviceMap {
+		knownServices[i] = k
+		i++
+	}
+	fetcher := NewFetcher(
+		s.getServiceAdd(), s.getServiceDelete(), knownServices,
+		"http://192.168.39.80:31320", "http://192.168.39.80:31320",
+		time.Second*5, "https://myapi.woot.example.com")
+	defer fetcher.Stop()
 	s.ServeHTTP()
 }
