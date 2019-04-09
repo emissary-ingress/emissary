5b96f637c280d491bf19cc9081e5c8da82ff2440
Itamar Turner-Trauring <itamar@itamarst.org>
2019-04-09 11:03:41 -0400

    More TODOs.

--- cmd/dev-portal-server/openapi/doc.go
+++ cmd/dev-portal-server/openapi/doc.go
@@ -9,14 +9,15 @@ type OpenAPIDoc struct {
 	JSON *gabs.Container
 }
 
-// Create OpenAPI 3.0 JSON spec with URL based on routing information:
+// Create OpenAPI 3.0 JSON spec with URL based on routing information.
+// TODO maybe need to support Swagger 2.0.
 func NewOpenAPI(jsonDoc []byte, base_url string, prefix string) *OpenAPIDoc {
+	// TODO need to support YAML.
 	result, err := gabs.ParseJSON(jsonDoc)
 	if err != nil {
 		log.Print(err)
 		return nil
 	}
-
 	// TODO need to handle case where there's a prefix on the existing
 	// server URL, e.g. /v1.
 	result.Delete("servers")
