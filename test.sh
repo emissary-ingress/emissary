4c577642f50eea6f554a37f76344ca5dc49aaea9
Luke Shumaker <lukeshu@datawire.io>
2019-04-10 18:12:49 +0000

    test http-auth baseline

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-#make apply
+make apply
 export KUBECONFIG=$PWD/build-aux/ambassador-pro.knaut
-#./bin_linux_amd64/max-load --csv-file=00-base.csv --step-rps=50 nodeport+https://ambassador.no-pro/load-testing/base/
-#./bin_linux_amd64/max-load --csv-file=01-grpc.csv --step-rps=50 nodeport+https://ambassador/load-testing/base/
-./bin_linux_amd64/max-load --csv-file=02-pro.csv --step-rps=50 nodeport+https://ambassador/load-testing/base/
+./bin_linux_amd64/max-load --csv-file=01-baseline.csv  --step-rps=50 nodeport+https://ambassador-baseline.no-pro/load-testing/base/
+./bin_linux_amd64/max-load --csv-file=02-grpc-auth.csv --step-rps=50 nodeport+https://ambassador-grpc.no-pro/load-testing/base/
+./bin_linux_amd64/max-load --csv-file=03-http-auth.csv --step-rps=50 nodeport+https://ambassador-http.no-pro/load-testing/base/
+./bin_linux_amd64/max-load --csv-file=03-pro-auth.csv  --step-rps=50 nodeport+https://ambassador.default/load-testing/base/
