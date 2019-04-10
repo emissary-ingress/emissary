1fe752d065539b6fc7164abc261565d793ae7613
Luke Shumaker <lukeshu@datawire.io>
2019-04-10 17:19:59 +0000

    test

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-make apply
+#make apply
 export KUBECONFIG=$PWD/build-aux/ambassador-pro.knaut
-./bin_linux_amd64/max-load --csv-file=00-base.csv --step-rps=50 nodeport+https://ambassador.no-pro/load-testing/base/
-./bin_linux_amd64/max-load --csv-file=01-grpc.csv --step-rps=50 nodeport+https://ambassador/load-testing/base/
+#./bin_linux_amd64/max-load --csv-file=00-base.csv --step-rps=50 nodeport+https://ambassador.no-pro/load-testing/base/
+#./bin_linux_amd64/max-load --csv-file=01-grpc.csv --step-rps=50 nodeport+https://ambassador/load-testing/base/
+./bin_linux_amd64/max-load --csv-file=02-pro.csv --step-rps=50 nodeport+https://ambassador/load-testing/base/
