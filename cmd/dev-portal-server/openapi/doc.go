c4b3349cd765c6143ff49fb92eac353bc41db340
Itamar Turner-Trauring <itamar@itamarst.org>
2019-04-11 16:03:41 -0400

    Logging for OpenAPI.

--- cmd/dev-portal-server/openapi/doc.go
+++ cmd/dev-portal-server/openapi/doc.go
@@ -1,10 +1,10 @@
 package openapi
 
 import (
-	"log"
 	"net/url"
 
 	"github.com/Jeffail/gabs"
+	log "github.com/sirupsen/logrus"
 )
 
 type OpenAPIDoc struct {
@@ -14,10 +14,13 @@ type OpenAPIDoc struct {
 // Create OpenAPI 3.0 JSON spec with URL based on routing information.
 // TODO maybe need to support Swagger 2.0.
 func NewOpenAPI(jsonDoc []byte, base_url string, prefix string) *OpenAPIDoc {
+	logger := log.WithFields(log.Fields{"subsytem": "openapi"})
+	logger.WithFields(log.Fields{"base_url": base_url, "prefix": prefix}).Info(
+		"Trying to create new OpenAPI doc")
 	// TODO need to support YAML.
 	result, err := gabs.ParseJSON(jsonDoc)
 	if err != nil {
-		log.Print(err)
+		logger.WithError(err).Error("Failed to parse OpenAPI JSON")
 		return nil
 	}
 
@@ -26,16 +29,24 @@ func NewOpenAPI(jsonDoc []byte, base_url string, prefix string) *OpenAPIDoc {
 	// prefix.
 	existingPrefix := ""
 	currentServer := result.S("servers").Index(0).S("url").Data()
+	log.WithFields(log.Fields{"url": currentServer}).Info(
+		"Checking first server's URL (if any)")
 	if currentServer != nil {
 		existingUrl, err := url.Parse(currentServer.(string))
 		if err == nil {
 			existingPrefix = existingUrl.Path
+		} else {
+			logger.WithFields(
+				log.Fields{"error": err, "url": currentServer}).Error(
+				"Failed to parse 'servers' URL")
 		}
 	}
 	result.Delete("servers")
 	result.Array("servers")
 	result.ArrayAppend(0, "servers")
 	server, _ := result.S("servers").ObjectI(0)
-	server.Set(base_url+existingPrefix+prefix, "url")
+	serverURL := base_url + existingPrefix + prefix
+	logger.WithFields(log.Fields{"url": serverURL}).Info("Creating OpenAPI doc with public URL")
+	server.Set(serverURL, "url")
 	return &OpenAPIDoc{JSON: result}
 }
