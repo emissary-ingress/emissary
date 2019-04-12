5977068a24279516281dedc4f92fec5b141e158d
Flynn <flynn@datawire.io>
2019-04-12 19:05:34 -0400

    Should've been with the previous commits. Arrgh.

---
+++ ambassador/ambassador/ir/irserviceresolver.py
@@ -0,0 +1,225 @@
+from typing import Any, ClassVar, Dict, List, Optional, Union, TYPE_CHECKING
+from typing import cast as typecast
+
+import json
+import logging
+import re
+import urllib.parse
+
+from ..config import Config
+from ..utils import RichStatus
+
+from .irresource import IRResource
+from .irtlscontext import IRTLSContext
+
+if TYPE_CHECKING:
+    from .ir import IR
+    from .ircluster import IRCluster
+
+#############################################################################
+## irserviceresolver.py -- resolve endpoints for services
+##
+## IRServiceResolver does the work of looking into Service data structures.
+## There are, naturally, some weirdnesses.
+##
+## Here's the way this goes:
+##
+## When you create an AConf, you must hand in Service objects and Resolver
+## objects. (This will generally happen by virtue of the ResourceFetcher
+## finding them someplace.) There can be multiple kinds of Resolver objects
+## (e.g. ConsulResolver, KubernetesEndpointResolver, etc.).
+##
+## When you create an IR from that AConf, the various kinds of Resolvers
+## all get turned into IRServiceResolvers, and the IR uses those to handle
+## the mechanics of finding the upstream endpoints for a service.
+
+IREndpointSet = Any
+
+
+class IRServiceResolver(IRResource):
+    def __init__(self, ir: 'IR', aconf: Config,
+                 rkey: str = "ir.resolver",
+                 kind: str = "IRServiceResolver",
+                 name: str = "ir.resolver",
+                 location: str = "--internal--",
+                 **kwargs) -> None:
+        super().__init__(
+            ir=ir, aconf=aconf, rkey=rkey, kind=kind, name=name,
+            location=location,
+            **kwargs)
+
+    def setup(self, ir: 'IR', aconf: Config) -> bool:
+        if self.kind == 'ConsulResolver':
+            self.resolve_with = 'consul'
+
+            if not self.datacenter:
+                self.post_error("ConsulResolver is required to have a datacenter")
+                return False
+        elif self.kind == 'KubernetesServiceResolver':
+            self.resolve_with = 'k8s'
+        elif self.kind == 'KubernetesEndpointResolver':
+            self.resolve_with = 'k8s'
+        else:
+            self.post_error(f"Resolver kind {self.kind} unknown")
+            return False
+
+        return True
+
+    def resolve(self, ir: 'IR', cluster: 'IRCluster', svc_name: str, port: int, lb: str) -> IREndpointSet:
+        keyfields = [ self.resolve_with ]
+
+        if self.resolve_with == 'k8s':
+            # K8s service names can be 'svc' or 'svc.namespace'. Which does this look like?
+
+            svc = svc_name
+            namespace = Config.ambassador_namespace
+
+            if '.' in svc:
+                # OK, cool.
+                ( svc, namespace ) = svc.split(".", 1)
+
+            keyfields.append(svc)
+            keyfields.append(namespace)
+        elif self.resolve_with == 'consul':
+            keyfields.append(svc_name)
+            keyfields.append(self.datacenter)
+        else:
+            # "Impossible."
+            self.post_error(f'resolver {self.name} is neither Kubernetes nor Consul?')
+            return None
+
+        # OK. Do we have a Service by this key?
+        key = "-".join(keyfields)
+
+        service = ir.services.get(key)
+
+        if not service:
+            self.logger.debug(f'Resolver {self.name}: {key} matches no Service')
+            return None
+
+        self.logger.debug(f'Resolver {self.name}: {key} matches %s' % service.as_json())
+
+        endpoints = service.get('endpoints')
+
+        if not endpoints:
+            self.logger.debug(f'Resolver {self.name}: {key} has no endpoints')
+            return None
+
+        # Do we have a match for the port they're asking for?
+
+        targets = endpoints.get(port)
+
+        if targets:
+            # Yes!
+            tstr = ", ".join([ f'{x["ip"]}:{x["port"]}' for x in targets ])
+
+            self.logger.debug(f'Resolver {self.name}: {key}:{port} matches {tstr}')
+
+            return targets
+        else:
+            self.logger.debug(f'Resolver {self.name}: {key}:{port} matches no endpoints')
+
+            return None
+
+
+class IRServiceResolverFactory:
+    @classmethod
+    def load_all(cls, ir: 'IR', aconf: Config) -> None:
+        config_info = aconf.get_config('resolvers')
+
+        if config_info:
+            assert(len(config_info) > 0)    # really rank paranoia on my part...
+
+            for config in config_info.values():
+                cdict = config.as_dict()
+                cdict['rkey'] = config.rkey
+                cdict['location'] = config.location
+
+                ir.add_resolver(IRServiceResolver(ir, aconf, **cdict))
+
+        if not ir.get_resolver('kubernetes-service'):
+            # Default the K8s service resolver.
+            config = {
+                'apiVersion': 'getambassador.io/v2',
+                'kind': 'KubernetesServiceResolver',
+                'name': 'kubernetes-service'
+            }
+
+            if Config.single_namespace:
+                config['namespace'] = Config.ambassador_namespace
+
+            ir.add_resolver(IRServiceResolver(ir, aconf, **config))
+
+        # Ugh, the aliasing for the K8s and Consul endpoint resolvers is annoying.
+        res_e = ir.get_resolver('endpoint')
+        res_k_e = ir.get_resolver('kubernetes-endpoint')
+
+        if not res_e and not res_k_e:
+            # Neither exists. Create them from scratch.
+
+            config = {
+                'apiVersion': 'getambassador.io/v2',
+                'kind': 'KubernetesEndpointResolver',
+                'name': 'kubernetes-endpoint'
+            }
+
+            if Config.single_namespace:
+                config['namespace'] = Config.ambassador_namespace
+
+            ir.add_resolver(IRServiceResolver(ir, aconf, **config))
+
+            config['name'] = 'endpoint'
+
+            ir.add_resolver(IRServiceResolver(ir, aconf, **config))
+        else:
+            cls.check_aliases(ir, aconf, 'endpoint', res_e, 'kubernetes-endpoint', res_k_e)
+
+        res_c = ir.get_resolver('consul')
+        res_c_e = ir.get_resolver('consul-endpoint')
+
+        if not res_c and not res_c_e:
+            # Neither exists. Create them from scratch.
+
+            config = {
+                'apiVersion': 'getambassador.io/v2',
+                'kind': 'ConsulResolver',
+                'name': 'consul-endpoint',
+                'datacenter': 'dc1'
+            }
+
+            ir.add_resolver(IRServiceResolver(ir, aconf, **config))
+
+            config['name'] = 'consul'
+
+            ir.add_resolver(IRServiceResolver(ir, aconf, **config))
+        else:
+            cls.check_aliases(ir, aconf, 'consul', res_c, 'consul-endpoint', res_c_e)
+
+    @classmethod
+    def check_aliases(cls, ir: 'IR', aconf: Config,
+                      n1: str, r1: Optional[IRServiceResolver],
+                      n2: str, r2: Optional[IRServiceResolver]) -> None:
+        source = None
+        name = None
+
+        if not r1:
+            # r2 must exist to be here.
+            source = r2
+            name = n1
+        elif not r2:
+            # r1 must exist to be here.
+            source = r1
+            name = n2
+
+        if source:
+            config = dict(**source.as_dict())
+
+            # Fix up this dict. Sigh.
+            config['rkey'] = config.pop('_rkey', config.get('rkey', None))  # Kludge, I know...
+            config.pop('_errored', None)
+            config.pop('_active', None)
+            config.pop('resolve_with', None)
+
+            config['name'] = name
+
+            ir.add_resolver(IRServiceResolver(ir, aconf, **config))
