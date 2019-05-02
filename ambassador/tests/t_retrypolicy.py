2497dd1f31416eaf7b39ab1eb19eb81373c4c5c7
Shubham <shubham@linux.com>
2019-05-02 18:53:57 +0530

    Add global test for retry policy

--- ambassador/tests/t_retrypolicy.py
+++ ambassador/tests/t_retrypolicy.py
@@ -2,18 +2,16 @@ from email.utils import parsedate_to_datetime
 from datetime import datetime
 from kat.harness import Query
 
-from abstract_tests import AmbassadorTest, HTTP, MappingTest, ServiceType
+from abstract_tests import AmbassadorTest, HTTP, ServiceType
 
-
-class RetryPolicyTest(MappingTest):
-    parent: AmbassadorTest
+class RetryPolicyTest(AmbassadorTest):
     target: ServiceType
 
     def init(self) -> None:
         self.target = HTTP()
 
     def config(self):
-        yield self.target, self.format("""
+        yield self, self.format("""
 ---
 apiVersion: ambassador/v1
 kind:  Mapping
@@ -21,9 +19,10 @@ name:  {self.name}-normal
 prefix: /{self.name}-normal/
 service: httpstat.us:80
 host_rewrite: httpstat.us
+timeout_ms: 10000
 """)
 
-        yield self.target, self.format("""
+        yield self, self.format("""
 ---
 apiVersion: ambassador/v1
 kind:  Mapping
@@ -31,55 +30,42 @@ name:  {self.name}-target
 prefix: /{self.name}-retry/
 service: httpstat.us:80
 host_rewrite: httpstat.us
+timeout_ms: 10000
 retry_policy:
   retry_on: "5xx"
-  num_retries: 3
-  per_try_timeout: "0.5s"
+  num_retries: 10
+""")
+
+        yield self, self.format("""
+---
+apiVersion: ambassador/v0
+kind:  Module
+name:  ambassador
+config:
+  retry_policy:
+    retry_on: "retriable-4xx"
+    num_retries: 10
 """)
 
     def queries(self):
-        for i in range(5):
-            yield Query(self.parent.url(self.name + '-normal/200'), expected=200)
-
-        for i in range(5):
-            yield Query(self.parent.url(self.name + '-normal/500'), expected=500)
-
-        for i in range(5):
-            yield Query(self.parent.url(self.name + '-retry/500'), expected=500)
-
-    @staticmethod
-    def assert_time_difference(results):
-        previous_time = None
-        for result in results:
-            current_time = parsedate_to_datetime(result.headers['Date'][0]).timestamp()
-            if previous_time is None:
-                previous_time = current_time
-            else:
-                assert abs(previous_time - current_time) <= 1, \
-                    "Difference between previous time {} and current time {} is greater than 1".format(
-                        previous_time, current_time)
-
-    @staticmethod
-    def get_average_time(results):
-        average_time = 0.0
-        for result in results:
-            average_time += parsedate_to_datetime(result.headers['Date'][0]).timestamp()
-        return average_time/len(results)
+        yield Query(self.url(self.name + '-normal/200'), expected=200)
+        yield Query(self.url(self.name + '-normal/500'), expected=500)
+        yield Query(self.url(self.name + '-retry/500'), expected=500)
+        yield Query(self.url(self.name + '-normal/409'), expected=409)
 
     def check(self):
-        assert len(self.results) == 15
+        ok_result = self.results[0]
+        normal_result = self.results[1]
+        retry_result = self.results[2]
+        conflict_result = self.results[3]
 
-        # we are going to use synchronization_results to sync clocks between envoy and system
-        synchronization_results = self.results[0:5]
-        normal_results = self.results[5:10]
-        retry_results = self.results[10:15]
+        ok_time = parsedate_to_datetime(ok_result.headers['Date'][0]).timestamp()
+        normal_time = parsedate_to_datetime(normal_result.headers['Date'][0]).timestamp()
+        retry_time = parsedate_to_datetime(retry_result.headers['Date'][0]).timestamp()
+        conflict_time = parsedate_to_datetime(conflict_result.headers['Date'][0]).timestamp()
 
-        self.assert_time_difference(normal_results)
-        self.assert_time_difference(retry_results)
-
-        normal_time = self.get_average_time(normal_results)
-        retry_time = self.get_average_time(retry_results)
-
-        assert retry_time > normal_time, "retry time {} is not greater than normal time {}".format(retry_time, normal_time)
+        assert abs(ok_time - normal_time) <= 1, "time to get 200 OK {} deviates more than a second from time to get a 500 {}".format(ok_time, normal_time)
+        assert retry_time - normal_time >= 3, "retry time {} should be at least 3 seconds slower than normal time {}".format(retry_time, normal_time)
+        assert conflict_time - ok_time >= 3, "conflict time {} should be at least 3 seconds slower than 200 OK time {}".format(conflict_time, ok_time)
 
 # main = Runner(AmbassadorTest)
