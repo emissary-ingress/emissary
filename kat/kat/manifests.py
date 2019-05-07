0231aa38362724066157b367fca69a2203f2d995
Flynn <flynn@datawire.io>
2019-05-07 12:17:54 -0400

    Drop initial delays for Kube health checks from 120 to 30.
    I raised the delays awhile ago while we were wrestling performance issues, but now they mostly force even a manual test run to take at least two minutes, even if you're only running one test.

--- kat/kat/manifests.py
+++ kat/kat/manifests.py
@@ -312,13 +312,13 @@ spec:
       httpGet:
         path: /ambassador/v0/check_alive
         port: 8877
-      initialDelaySeconds: 120
+      initialDelaySeconds: 30
       periodSeconds: 3
     readinessProbe:
       httpGet:
         path: /ambassador/v0/check_ready
         port: 8877
-      initialDelaySeconds: 120
+      initialDelaySeconds: 30
       periodSeconds: 3
   restartPolicy: Always
 """
