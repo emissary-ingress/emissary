7c10291a183fd31f2874065bc9afa548ee422e9a
Luke Shumaker <lukeshu@datawire.io>
2019-05-20 12:38:01 -0400

    Implement §4.3

---
+++ rfc6749client/sec4_3_resource_owner_password_credentials_grant.go
@@ -0,0 +1,73 @@
+package rfc6749client
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// A ResourceOwnerPasswordCredentialsClient is a Client that utilizes
+// the "Resource Owner Password Credentials" Grant-type, as defined by
+// §4.3.
+type ResourceOwnerPasswordCredentialsClient struct {
+	tokenEndpoint        *url.URL
+	clientAuthentication ClientAuthenticationMethod
+}
+
+// NewResourceOwnerPasswordCredentialsClient creates a new
+// ResourceOwnerPasswordCredentialsClient as defined by §4.3.
+func NewResourceOwnerPasswordCredentialsClient(
+	tokenEndpoint *url.URL,
+	clientAuthentication ClientAuthenticationMethod,
+) (*ResourceOwnerPasswordCredentialsClient, error) {
+	if err := validateTokenEndpointURI(tokenEndpoint); err != nil {
+		return nil, err
+	}
+	ret := &ResourceOwnerPasswordCredentialsClient{
+		tokenEndpoint:        tokenEndpoint,
+		clientAuthentication: clientAuthentication,
+	}
+	return ret, nil
+}
+
+// AccessToken talks to the Authorization Server to exchange a
+// username and password for an Access Token (and maybe a Refresh
+// Token); submitting the request per §4.3.2, and handling the
+// response per §4.3.3.
+//
+// The scopes argument is optional.
+//
+// The returned response is either a TokenSuccessResponse or a
+// TokenErrorResponse.
+func (client *ResourceOwnerPasswordCredentialsClient) AccessToken(httpClient *http.Client, username string, password string, scope Scope) (TokenResponse, error) {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+
+	parameters := url.Values{
+		"grant_type": {"password"},
+		"username":   {"username"},
+		"password":   {"password"},
+	}
+	if len(scope) != 0 {
+		parameters.Set("scope", scope.String())
+	}
+
+	req, err := http.NewRequest("POST", client.tokenEndpoint.String(), strings.NewReader(parameters.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	if client.clientAuthentication != nil {
+		client.clientAuthentication(req)
+	}
+
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	return parseTokenResponse(res)
+}
