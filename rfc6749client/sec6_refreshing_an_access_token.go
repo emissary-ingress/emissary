b965653dd8592a13fe27c927ca63627e75ba735d
Luke Shumaker <lukeshu@datawire.io>
2019-05-20 14:07:54 -0400

    Round out the refresh API

--- rfc6749client/sec6_refreshing_an_access_token.go
+++ rfc6749client/sec6_refreshing_an_access_token.go
@@ -3,25 +3,49 @@ package rfc6749client
 import (
 	"net/http"
 	"net/url"
+
+	"github.com/pkg/errors"
 )
 
 // RefreshToken talks to the Authorization Server to exchange a
 // Refresh Token for an Access Token (and maybe a new Refresh Token);
 // per §6.
 //
+// oldtoken.RefreshToken must not be nil.
+//
 // The scope argument is optional, and may be used to obtain a token
 // with _reduced_ scope.  It is not valid to list a scope that is not
 // present in the original Token.
-func (client *explicitClient) RefreshToken(httpClient *http.Client, refreshToken string, scope Scope) (TokenResponse, error) {
+//
+// The returned response is either a TokenSuccessResponse or a
+// TokenErrorResponse.  If it is a TokenSuccessResponse, then you
+// should replace your store of the oldtoken with it.
+func (client *explicitClient) RefreshToken(httpClient *http.Client, oldtoken TokenSuccessResponse, scope Scope) (TokenResponse, error) {
+	if oldtoken.RefreshToken == nil {
+		return nil, errors.New("RefreshToken(): oldtoken.RefreshToken must not be nil")
+	}
+
 	parameters := url.Values{
 		"grant_type":    {"refresh_token"},
-		"refresh_token": {refreshToken},
+		"refresh_token": {*oldtoken.RefreshToken},
 	}
 	if len(scope) != 0 {
 		parameters.Set("scope", scope.String())
 	}
 
-	return client.postForm(httpClient, parameters)
+	res, err := client.postForm(httpClient, parameters)
+	if err != nil {
+		return nil, err
+	}
 
-	// TODO: store the new Refresh Token
+	if res, ok := res.(TokenSuccessResponse); ok {
+		if res.RefreshToken == nil && oldtoken.RefreshToken != nil {
+			res.RefreshToken = oldtoken.RefreshToken
+		}
+		if res.Scope == nil && oldtoken.Scope != nil {
+			res.Scope = oldtoken.Scope
+		}
+	}
+
+	return res, nil
 }
