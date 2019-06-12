0e7f68bc28a1e56b1ecb4ad9abb389a2b8bd1530
Luke Shumaker <lukeshu@datawire.io>
2019-06-12 14:28:55 -0400

    lib/util/panic.go: fix

--- lib/util/panic.go
+++ lib/util/panic.go
@@ -35,9 +35,16 @@ func (pe panicError) Cause() error                  { return pe.err }
 func (pe panicError) StackTrace() errors.StackTrace { return pe.err.StackTrace()[1:] }
 func (pe panicError) Format(s fmt.State, verb rune) {
 	switch verb {
-	case 'v', 's':
+	case 'v':
 		io.WriteString(s, "PANIC: ")
-		pe.err.Format(s, verb)
+		if s.Flag('+') {
+			fmt.Fprintf(s, "%v", pe.err)
+			pe.StackTrace().Format(s, verb)
+			return
+		}
+		io.WriteString(s, pe.err.Error())
+	case 's':
+		io.WriteString(s, pe.Error())
 	case 'q':
 		fmt.Fprintf(s, "%q", pe.Error())
 	}
