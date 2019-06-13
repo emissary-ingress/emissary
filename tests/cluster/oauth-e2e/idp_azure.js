b66b99d727e4057ec13c5e68fffba34c65bd0f1e
Luke Shumaker <lukeshu@datawire.io>
2019-06-13 17:35:52 -0400

    fix whitespace

--- tests/cluster/oauth-e2e/idp_azure.js
+++ tests/cluster/oauth-e2e/idp_azure.js
@@ -7,18 +7,18 @@ module.exports.testcases = {
 };
 
 module.exports.authenticate = async function(browsertab, username, password) {
-  // page 1: Username
+	// page 1: Username
 	await browsertab.waitForSelector('input[type="email"]', { visible: true });
 	await browsertab.waitForSelector('input[type="submit"][value="Next"]', { visible: true });
 	await browsertab.type('input[type="email"]', username);
 	await browsertab.click('input[type="submit"][value="Next"]');
-  // page 2: Password
+	// page 2: Password
 	await browsertab.waitForSelector('input[type="password"]', { visible: true });
-  await browsertab.waitForSelector('input[type="submit"][value="Sign in"]', { visible: true });
+	await browsertab.waitForSelector('input[type="submit"][value="Sign in"]', { visible: true });
 	await browsertab.type('input[type="password"]', password);
 	await browsertab.click('input[type="submit"][value="Sign in"]');
-  // page 3: "Stay signed in?"
-  await browsertab.waitForSelector('input[type="submit"][value="Yes"]', { visible: true });
+	// page 3: "Stay signed in?"
+	await browsertab.waitForSelector('input[type="submit"][value="Yes"]', { visible: true });
 	const done = browsertab.waitForNavigation();
 	await browsertab.click('input[type="submit"][value="Yes"]');
 	await done;
