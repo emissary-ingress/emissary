1ca11e8fbf4ac09c36af9c6062d640a209eb75fb
Luke Shumaker <lukeshu@datawire.io>
2019-06-14 11:28:20 -0400

    client/rfc6750: Use the newer non-global type registry

--- client/rfc6750/rfc6750.go
+++ client/rfc6750/rfc6750.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"net/url"
 
-	"github.com/datawire/liboauth2/common/rfc6749"
+	"github.com/datawire/liboauth2/client/rfc6749"
 
 	// Register error codes shared between client and resourceserver.
 	_ "github.com/datawire/liboauth2/common/rfc6750"
@@ -24,19 +24,22 @@ func AddToBody(token string, body url.Values) {
 	body.Set("access_token", token)
 }
 
-// §6.1.
-func init() {
-	rfc6749.AccessTokenType{
-		Name:                              "Bearer",
-		AdditionalTokenEndpointParameters: nil,
-		ChangeController:                  "IETF",
-		SpecificationDocuments:            []string{"RFC 6750"},
+// OAuthProtocolExtension contains the information to register Bearer token support with an OAuth
+// 2.0 Client, per §6.
+var OAuthProtocolExtension = rfc6749.ProtocolExtension{
+	AccessTokenTypes: []rfc6749.AccessTokenType{
+		{
+			Name:                              "Bearer",
+			AdditionalTokenEndpointParameters: nil,
+			ChangeController:                  "IETF",
+			SpecificationDocuments:            []string{"RFC 6750"},
 
-		ClientNeedsBody: false,
-		ClientAuthorizationForResourceRequest: func(token string, _ io.Reader) (http.Header, error) {
-			ret := make(http.Header)
-			AddToHeader(token, ret)
-			return ret, nil
+			NeedsBody: false,
+			AuthorizationForResourceRequest: func(token string, _ io.Reader) (http.Header, error) {
+				ret := make(http.Header)
+				AddToHeader(token, ret)
+				return ret, nil
+			},
 		},
-	}.Register()
+	},
 }
