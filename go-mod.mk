00d59c7fc5945e17dcbdf5c31a440345bd73b9a7
Luke Shumaker <lukeshu@datawire.io>
2019-06-20 10:11:08 -0400

    (apro) build-aux: go-mod.mk: Clean up how go.module is set, add dependency on `jq`

--- go-mod.mk
+++ go-mod.mk
@@ -54,27 +54,8 @@ endif
 #
 # 1. Set go.module
 
-# Why use this complex `sed` expression to parse go.mod, instead of
-# just having `go list -m` do it?  Because `go list -m` will go ahead
-# and download dependencies.  We don't want Go to do that at
-# Makefile-parse-time; what if we're running `make clean`?
-#
-# See: cmd/go/internal/modfile/read.go:ModulePath()
-go.module := $(strip $(shell sed -n -e 's,//.*,,' -e '/^\s*module/{s/^\s*module//;p;q;}' go.mod))
-#go.module := $(shell $(GO) list -m)
+go.module := $(shell GO111MODULE=on go mod edit -json | jq -r .Module.Path)
 ifneq ($(words $(go.module)),1)
-  # Print a helpful message
-  ifeq ($(wildcard go.mod),)
-    $(info go-mod.mk: File `./go.mod` does not exist.)
-    ifeq ($(wildcard .go-workspace/),)
-      $(info go-mod.mk: Initalize it with `go mod init github.com/YOU/REPONAME`)
-    else
-      $(info go-mod.mk: But `./go-workspace/` does.  Did you mean to use go-workspace.mk?)
-    endif
-  else
-    $(info go-mod.mk: File `./go.mod` seems to be malformed; could not parse.)
-  endif
-  # And then error out
   $(error Could not extract $$(go.module) from ./go.mod)
 endif
 
