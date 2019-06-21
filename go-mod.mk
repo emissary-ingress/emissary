cc38557de306f1a0858ee1469a6b0340342bd44b
Luke Shumaker <lukeshu@datawire.io>
2019-06-20 22:04:13 -0400

    (apro) build-aux: go-mod.mk: Clean up in light of no go-workspace.mk

--- go-mod.mk
+++ go-mod.mk
@@ -1,29 +1,36 @@
 # Copyright 2018 Datawire. All rights reserved.
 #
-# Makefile snippet to build Go programs using Go 1.11 modules
+# Makefile snippet to build Go programs using Go 1.11 modules.
 #
 ## Eager inputs ##
 #  - File: ./go.mod
 #  - Variable: go.DISABLE_GO_TEST ?=
 #  - Variable: go.PLATFORMS ?= $(GOOS)_$(GOARCH)
+#
 ## Lazy inputs ##
 #  - Variable: go.GOBUILD ?= go build
 #  - Variable: go.LDFLAGS ?=
 #  - Variable: go.GOLANG_LINT_VERSION ?= …
 #  - Variable: go.GOLANG_LINT_FLAGS ?= …$(wildcard .golangci.yml .golangci.toml .golangci.json)…
-#  - Variable: CI
+#  - Variable: CI ?=
+#
 ## Outputs ##
+#  - Variable: export GO111MODULE = on
 #  - Variable: NAME ?= $(notdir $(go.module))
+#
 #  - Variable: go.module = EXAMPLE.COM/YOU/YOURREPO
 #  - Variable: go.bins = List of "main" Go packages
 #  - Variable: go.pkgs = ./...
-#  - Function: go.list = $(shell go list $1), but ignores submodules and doesn't download things
+#
+#  - Function: go.list = like $(shell go list $1), but ignores nested Go modules and doesn't download things
+#
 #  - Targets: bin_$(OS)_$(ARCH)/$(CMD)
 #  - .PHONY Target: go-get
 #  - .PHONY Target: go-build
 #  - .PHONY Target: go-lint
 #  - .PHONY Target: go-fmt
 #  - .PHONY Target: go-test
+#
 ## common.mk targets ##
 #  - build
 #  - lint
@@ -36,13 +43,11 @@
 # platforms `make build` should compile for.  Unlike most variables,
 # it must be specified *before* including go-workspace.mk.
 ifeq ($(words $(filter $(abspath $(lastword $(MAKEFILE_LIST))),$(abspath $(MAKEFILE_LIST)))),1)
-_go-common.mk := $(lastword $(MAKEFILE_LIST))
-ifneq ($(go.module),)
-$(error Only include one of go-mod.mk or go-workspace.mk)
-endif
+_go-mod.mk := $(lastword $(MAKEFILE_LIST))
+include $(dir $(_go-mod.mk))common.mk
 
 #
-# 0. configure the `go` command
+# Configure the `go` command
 
 export GO111MODULE = on
 
@@ -53,30 +58,7 @@ ifneq ($(filter go1.11.%,$(shell go version)),)
 endif
 
 #
-# 1. Set go.module
-
-go.module := $(shell GO111MODULE=on go mod edit -json | jq -r .Module.Path)
-ifneq ($(words $(go.module)),1)
-  $(error Could not extract $$(go.module) from ./go.mod)
-endif
-
-#
-# 2. Set go.pkgs
-
-go.pkgs := ./...
-
-#
-# 3. Recipe for go-get
-
-go-get:
-	go mod download
-
-#
-# Include _go-common.mk
-
-include $(dir $(_go-common.mk))common.mk
-
-NAME ?= $(notdir $(go.module))
+# Set default values for input variables
 
 go.GOBUILD ?= go build
 go.DISABLE_GO_TEST ?=
@@ -84,18 +66,28 @@ go.LDFLAGS ?=
 go.PLATFORMS ?= $(GOOS)_$(GOARCH)
 go.GOLANG_LINT_VERSION ?= 1.15.0
 go.GOLANG_LINT_FLAGS ?= $(if $(wildcard .golangci.yml .golangci.toml .golangci.json),,--disable-all --enable=gofmt --enable=govet)
+CI ?=
 
-# It would be simpler to create this list if we could use Go modules:
+#
+# Set output variables and functions
+
+NAME ?= $(notdir $(go.module))
+
+go.module := $(shell GO111MODULE=on go mod edit -json | jq -r .Module.Path)
+ifneq ($(words $(go.module)),1)
+  $(error Could not extract $$(go.module) from ./go.mod)
+endif
+
+# It would be simpler to create this list if we could use module-aware
+# `go list`:
 #
-#     go.bins := $(shell $(GO) list -f='{{if eq .Name "main"}}{{.ImportPath}}{{end}}' ./...)
+#     go.bins := $(shell GO111MODULE=on go list -f='{{if eq .Name "main"}}{{.ImportPath}}{{end}}' ./...)
 #
-# But alas, we can't do that *even if* the build is using Go modules,
-# as that would cause the module system go ahead and download
-# dependencies.  We don't want Go to do that at Makefile-parse-time;
-# what if we're running `make clean`?
+# But alas, we can't do that, as that would cause the module system go
+# ahead and download dependencies.  We don't want Go to do that at
+# Makefile-parse-time; what if we're running `make clean`?
 #
-# So instead, we must deal with this abomination.  At least that means
-# we can share it between go-mod.mk and go-workspace.mk.
+# So instead, we must deal with this abomination.
 _go.submods := $(patsubst %/go.mod,%,$(shell git ls-files '*/go.mod'))
 go.list = $(call path.addprefix,$(go.module),\
                                 $(filter-out $(foreach d,$(_go.submods),$d $d/%),\
@@ -103,11 +95,13 @@ go.list = $(call path.addprefix,$(go.module),\
                                                                     $(shell GOPATH=/bogus GO111MODULE=off go list $1))))
 go.bins := $(call go.list,-f='{{if eq .Name "main"}}{{.ImportPath}}{{end}}' ./...)
 
-#
-# (_go-common.mk) Rules
+go.pkgs := ./...
 
-# go-FOO.mk is responsible for implementing go-get
+#
+# Rules
+
 go-get: ## (Go) Download Go dependencies
+	go mod download
 .PHONY: go-get
 
 define _go.bin.rule
@@ -125,14 +119,16 @@ endef
 $(foreach go.bin,$(go.bins),$(eval $(_go.bin.rule)))
 
 go-build: $(foreach _go.PLATFORM,$(go.PLATFORMS),$(addprefix bin_$(_go.PLATFORM)/,$(notdir $(go.bins))))
+
+go-build: ## (Go) Build the code with `go build`
 .PHONY: go-build
 
-$(dir $(_go-common.mk))golangci-lint: $(_go-common.mk)
+$(dir $(_go-mod.mk))golangci-lint: $(_go-mod.mk)
 	curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(@D) v$(go.GOLANG_LINT_VERSION)
 
 go-lint: ## (Go) Check the code with `golangci-lint`
-go-lint: $(dir $(_go-common.mk))golangci-lint go-get
-	$(dir $(_go-common.mk))golangci-lint run $(go.GOLANG_LINT_FLAGS) $(go.pkgs)
+go-lint: $(dir $(_go-mod.mk))golangci-lint go-get
+	$(dir $(_go-mod.mk))golangci-lint run $(go.GOLANG_LINT_FLAGS) $(go.pkgs)
 .PHONY: go-lint
 
 go-fmt: ## (Go) Fixup the code with `go fmt`
@@ -143,34 +139,34 @@ go-fmt: go-get
 go-test: ## (Go) Check the code with `go test`
 go-test: go-build
 ifeq ($(go.DISABLE_GO_TEST),)
-	$(MAKE) $(dir $(_go-common.mk))go-test.tap.summary
+	$(MAKE) $(dir $(_go-mod.mk))go-test.tap.summary
 endif
 
-$(dir $(_go-common.mk))go-test.tap: FORCE
-	@go test -json $(go.pkgs) 2>&1 | GO111MODULE=off go run $(dir $(_go-common.mk))gotest2tap.go | tee $@ | $(dir $(_go-common.mk))tap-driver stream -n go-test
+$(dir $(_go-mod.mk))go-test.tap: FORCE
+	@go test -json $(go.pkgs) 2>&1 | GO111MODULE=off go run $(dir $(_go-mod.mk))gotest2tap.go | tee $@ | $(dir $(_go-mod.mk))tap-driver stream -n go-test
 
 #
-# (_go-common.mk) Hook in to common.mk
+# Hook in to common.mk
 
 build: go-build
 lint: go-lint
 format: go-fmt
-test-suite.tap: $(if $(go.DISABLE_GO_TEST),,$(dir $(_go-common.mk))go-test.tap)
+test-suite.tap: $(if $(go.DISABLE_GO_TEST),,$(dir $(_go-mod.mk))go-test.tap)
 
-clean: _clean-go-common
-_clean-go-common:
-	rm -f $(dir $(_go-common.mk))go-test.tap
+clean: _go-clean
+_go-clean:
+	rm -f $(dir $(_go-mod.mk))go-test.tap
 # Files made by older versions.  Remove the tail of this list when the
 # commit making the change gets far enough in to the past.
 #
 # 2019-02-06
-	rm -f $(dir $(_go-common.mk))patter.go $(dir $(_go-common.mk))patter.go.tmp
-.PHONY: _clean-go-common
+	rm -f $(dir $(_go-mod.mk))patter.go $(dir $(_go-mod.mk))patter.go.tmp
+.PHONY: _go-clean
 
-clobber: _clobber-go-common
-_clobber-go-common:
-	rm -f $(dir $(_go-common.mk))golangci-lint
-.PHONY: _clobber-go-common
+clobber: _go-clobber
+_go-clobber:
+	rm -f $(dir $(_go-mod.mk))golangci-lint
+.PHONY: _go-clobber
 
 #
 endif
