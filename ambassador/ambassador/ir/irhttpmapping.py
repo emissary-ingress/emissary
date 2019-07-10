05a274ee3085d4e7e0616059d2e612f6184cc7cd
John Esmet <jesmet@appnexus.com>
2019-07-10 01:16:07 +0000

    Per-mapping cluster_idle_timeout_ms

--- ambassador/ambassador/ir/irhttpmapping.py
+++ ambassador/ambassador/ir/irhttpmapping.py
@@ -87,6 +87,7 @@ class IRHTTPMapping (IRBaseMapping):
         "service": True,
         "shadow": True,
         "connect_timeout_ms": True,
+        "cluster_idle_timeout_ms": True,
         "timeout_ms": True,
         "idle_timeout_ms": True,
         "tls": True,
