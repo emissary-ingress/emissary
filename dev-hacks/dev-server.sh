a243ac247bdacb22e4f62d9cd9d3c152c7c38b24
Bozo Dragojevic <bozzo@digiverse.si>
2019-07-17 23:54:57 +0200

    remove more osxisms

--- dev-hacks/dev-server.sh
+++ dev-hacks/dev-server.sh
@@ -6,10 +6,10 @@ if [ ! -d .venv ]; then
 fi
 . ../k8s-env.sh
 BIN=../bin_$(go env GOHOSTOS)_$(go env GOHOSTARCH)
-AMBASSADOR_KEYLOC=~/"Library/Application Support/ambassador"
-test -d "$AMBASSADOR_KEYLOC" || mkdir "$AMBASSADOR_KEYLOC"
-echo "$AMBASSADOR_LICENSE_KEY" > "$AMBASSADOR_KEYLOC/license-key"
-export SHARED_SECRET_PATH="$AMBASSADOR_KEYLOC/shared-secret"
+AMBASSADOR_KEYLOC=$(pwd)
+export AMBASSADOR_LICENSE_FILE="$AMBASSADOR_KEYLOC/.license-key"
+echo "$AMBASSADOR_LICENSE_KEY" > "$AMBASSADOR_LICENSE_FILE"
+export SHARED_SECRET_PATH="$AMBASSADOR_KEYLOC/.shared-secret"
 echo tbd > "$SHARED_SECRET_PATH"
 export AMBASSADOR_URL=http://localhost:8877
 export POLL_EVERY_SECS=20
