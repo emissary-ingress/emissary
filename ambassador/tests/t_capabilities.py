55ac50173b1240ffbc17972fea5044f7f8c49c5f
Sean Walberg <sean@ertw.com>
2019-08-22 11:41:25 -0500

    Removing extra test but where are ports?

--- ambassador/tests/t_capabilities.py
+++ ambassador/tests/t_capabilities.py
@@ -1,38 +1,10 @@
+import os
 from typing import Tuple, Union
 
 from kat.harness import Query
+from kat.manifests import AMBASSADOR, RBAC_CLUSTER_SCOPE
 
 from abstract_tests import AmbassadorTest, assert_default_errors, HTTP, Node, ServiceType
-
-
-class Empty(AmbassadorTest):
-    single_namespace = True
-    namespace = "empty-namespace"
-
-    @classmethod
-    def variants(cls):
-        yield cls()
-
-    def manifests(self) -> str:
-        return """
----
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: empty-namespace
-""" + super().manifests()
-
-    def config(self) -> Union[str, Tuple[Node, str]]:
-        yield from ()
-
-    def queries(self):
-        yield Query(self.url("ambassador/v0/diag/?json=true&filter=errors"), phase=2)
-
-    def check(self):
-        # XXX Ew. If self.results[0].json is empty, the harness won't convert it to a response.
-        errors = self.results[0].json
-        assert_default_errors(errors)
-
 
 class LowPortTest(AmbassadorTest):
 
@@ -42,14 +14,21 @@ class LowPortTest(AmbassadorTest):
         self.target = HTTP()
 
     def manifests(self) -> str:
-       capabilities_block = f"""
-  capabilities:
-    add: ["NET_BIND_SERVICE"]
+
+        eports = f"""
+  - name: extra-80
+    protocol: TCP
+    port: 82
+    targetPort: 80
 """
-       return self.format(RBAC_CLUSTER_SCOPE + AMBASSADOR,
+        capabilities_block = f"""
+      capabilities:
+        add: ["NET_BIND_SERVICE"]
+"""
+        return self.format(RBAC_CLUSTER_SCOPE + AMBASSADOR,
                            image=os.environ["AMBASSADOR_DOCKER_IMAGE"],
-                           envs=envs,
-                           extra_ports="",
+                           envs="",
+                           extra_ports=eports,
                            capabilities_block=capabilities_block)
 
     def config(self):
@@ -59,11 +38,11 @@ apiVersion: ambassador/v0
 kind:  Module
 name:  ambassador
 config:
-  service_port: 82
+  service_port: 80
 """)
 
     def queries(self):
-        yield Query(self.url("server-name/", "http", 8099), expected=399)
+        yield Query(self.url("server-name/", "http", 80), expected=399)
 
     def check(self):
         assert self.results[0].headers["Server"] == [ "test-server" ]
