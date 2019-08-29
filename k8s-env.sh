ff4f60b1e8d89f4b21bef46b03245a182046eb09
Luke Shumaker <lukeshu@datawire.io>
2019-08-29 18:13:17 -0400

    build-system: Only use 1 ambassador.git checkout (& improvements that come with it)
    This is made possible by (cherry-picking) recent improvements in the way
    ambassador.git handles caching Envoy binaries
    (<https://github.com/datawire/ambassador/pull/1778>).
    
    Funnily enough, I did not have Pro in mind when I wrote that PR, but I
    noticed after the fact that it would help things in Pro.
    
    Impact:
    
     1. In the v0.7.0 release, the released amb-core did not work with v0
        license keys (despite the code saying it would), because it used a
        cached certified-envoy binary from
        quay.io/datawire/ambassador_pro-buildcache, which was older, from
        before I adjusted certified-envoy to accept the v0 license keys.
    
        We fixed the v0.7.0 release after-the-fact by deleting the images from
        quay.io/datawire/ambassador_pro-buildcache and re-running the release
        CI job.
    
        This should avoid this happening in the future, because it is now the
        case that the "final" Envoy binary (in OSS "final"=stripped, here
        "final"=stripped+license-keyed) Envoy binary ONLY exists in the final
        ambassador image, not any of the cache images.
    
     2. This also means that if a customer sends us a stack trace from a
        certified Envoy, we now also have the saved debug symbols for that
        binary, in order to be able to help them.  This was my intent in OSS
        with that original PR.

--- k8s-env.sh
+++ k8s-env.sh
@@ -1,7 +1,7 @@
 #!/hint/sh
 
 # "Releasable" images
-AMBASSADOR_IMAGE=$(                  sed -n 2p ambassador-withlicense/ambassador.docker.push.cluster)
+AMBASSADOR_IMAGE=$(                  sed -n 2p ambassador/ambassador.docker.push.cluster)
 AMB_SIDECAR_IMAGE=$(                 sed -n 2p docker/model-cluster-amb-sidecar-plugins.docker.push.cluster) # XXX: not releasable because plugins
 CONSUL_CONNECT_INTEGRATION_IMAGE=$(  sed -n 2p docker/consul_connect_integration.docker.push.cluster)
 DEV_PORTAL_IMAGE=$(                  sed -n 2p docker/dev-portal-server.docker.push.cluster)
