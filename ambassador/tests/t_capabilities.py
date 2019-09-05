d73bca1f42153800e37288c554c446631f85b9e3
Sean Walberg <sean@ertw.com>
2019-09-05 10:36:41 -0400

    Modify the manifest to use the new port
    Not working on my mac but does on a real cluster. Trying it out in CI

--- ambassador/tests/t_capabilities.py
+++ ambassador/tests/t_capabilities.py
@@ -1,4 +1,5 @@
 import os
+import re
 from typing import Tuple, Union
 
 from kat.harness import Query
@@ -15,20 +16,21 @@ class LowPortTest(AmbassadorTest):
 
     def manifests(self) -> str:
 
-        eports = f"""
-  - name: extra-80
-    protocol: TCP
-    port: 82
-    targetPort: 80
+        port_block = f"""
+    ports:
+      - containerPort: 81
 """
         capabilities_block = f"""
       capabilities:
         add: ["NET_BIND_SERVICE"]
 """
-        return self.format(RBAC_CLUSTER_SCOPE + AMBASSADOR,
+
+        ambassador_new_ports = re.sub(r'targetPort: 8080\b', r'targetPort: 81', AMBASSADOR)
+        ambassador_new_ports = re.sub(r'(image: .*)', r'\1' + port_block, ambassador_new_ports)
+        return self.format(RBAC_CLUSTER_SCOPE + ambassador_new_ports,
                            image=os.environ["AMBASSADOR_DOCKER_IMAGE"],
                            envs="",
-                           extra_ports=eports,
+                           extra_ports="",
                            capabilities_block=capabilities_block)
 
     def config(self):
@@ -38,7 +40,7 @@ apiVersion: ambassador/v0
 kind:  Module
 name:  ambassador
 config:
-  service_port: 80
+  service_port: 81
 """)
 
     def queries(self):
