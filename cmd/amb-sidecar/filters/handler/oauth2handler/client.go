5b9136915c50ee5b3c3aca6a08200d396c783ff7
Luke Shumaker <lukeshu@datawire.io>
2019-10-03 19:47:05 -0400

    oauth2handler.go: Split in to multiple files

--- cmd/amb-sidecar/filters/handler/oauth2handler/client.go
+++ cmd/amb-sidecar/filters/handler/oauth2handler/client.go
@@ -13,17 +13,13 @@ import (
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
-	"github.com/mediocregopher/radix.v2/pool"
 	"github.com/mediocregopher/radix.v2/redis"
 	"github.com/pkg/errors"
 	uuid "github.com/satori/go.uuid"
 
 	rfc6749client "github.com/datawire/liboauth2/client/rfc6749"
 	rfc6750client "github.com/datawire/liboauth2/client/rfc6750"
-	rfc6750resourceserver "github.com/datawire/liboauth2/resourceserver/rfc6750"
 
-	crd "github.com/datawire/apro/apis/getambassador.io/v1beta2"
-	"github.com/datawire/apro/cmd/amb-sidecar/filters/handler/httpclient"
 	"github.com/datawire/apro/cmd/amb-sidecar/filters/handler/middleware"
 	"github.com/datawire/apro/cmd/amb-sidecar/types"
 	"github.com/datawire/apro/lib/filterapi"
@@ -36,67 +32,8 @@ const (
 	sessionExpiry = 365 * 24 * time.Hour
 )
 
-// OAuth2Filter looks up the appropriate Tenant and Rule objects from
-// the CRD Controller, and validates the signed JWT tokens when
-// present in the request.  If the request Path is "/callback", it
-// validates IDP requests and handles code exchange flow.
-type OAuth2Filter struct {
-	PrivateKey *rsa.PrivateKey
-	PublicKey  *rsa.PublicKey
-	RedisPool  *pool.Pool
-	QName      string
-	Spec       crd.FilterOAuth2
-	Arguments  crd.FilterOAuth2Arguments
-}
-
 func (c *OAuth2Filter) sessionCookieName() string {
 	return "ambassador_session." + c.QName
-}
-
-func (c *OAuth2Filter) Filter(ctx context.Context, request *filterapi.FilterRequest) (filterapi.FilterResponse, error) {
-	logger := middleware.GetLogger(ctx)
-	httpClient := httpclient.NewHTTPClient(logger, c.Spec.MaxStale, c.Spec.InsecureTLS)
-
-	discovered, err := Discover(httpClient, c.Spec, logger)
-	if err != nil {
-		return middleware.NewErrorResponse(ctx, http.StatusBadGateway,
-			errors.Wrap(err, "OIDC-discovery"), nil), nil
-	}
-
-	clientResponse := c.filterClient(ctx, logger, httpClient, discovered, request)
-	switch clientResponse := clientResponse.(type) {
-	case *filterapi.HTTPResponse:
-		return clientResponse, nil
-	case *filterapi.HTTPRequestModification:
-		filterutil.ApplyRequestModification(request, clientResponse)
-	default:
-		panic(errors.Errorf("unexpexted filter response type %T", clientResponse))
-	}
-
-	resourceResponse := c.filterResourceServer(ctx, logger, httpClient, discovered, request)
-	switch resourceResponse := resourceResponse.(type) {
-	case *filterapi.HTTPResponse:
-		if resourceResponse.StatusCode == http.StatusUnauthorized {
-			// The upstream Resource Server returns 401 Unauthorized to the Client--the Client does NOT pass
-			// 401 along to the User Agent; the User Agent is NOT using an RFC 7235-compatible
-			// authentication scheme to talk to the Client; 401 would be inappropriate.
-			//
-			// Instead, wrap the 401 response in a 403 Forbidden response.
-			return middleware.NewErrorResponse(ctx, http.StatusForbidden,
-				errors.New("authorization rejected"),
-				map[string]interface{}{
-					"synthesized_upstream_response": resourceResponse,
-				},
-			), nil
-		}
-		return resourceResponse, nil
-	case nil:
-		// do nothing
-	default:
-		panic(errors.Errorf("unexpexted filter response type %T", resourceResponse))
-	}
-
-	return clientResponse, nil
 }
 
 // filterClient implements the OAuth Client part of the Filter.
@@ -299,155 +236,6 @@ func (c *OAuth2Filter) filterClient(ctx context.Context, logger types.Logger, ht
 			Body: "",
 		}
 	}
-}
-
-// filterResourceServer implements the OAuth Resource Server part of the Filter.
-func (c *OAuth2Filter) filterResourceServer(ctx context.Context, logger types.Logger, httpClient *http.Client, discovered *Discovered, request *filterapi.FilterRequest) filterapi.FilterResponse {
-	token := rfc6750resourceserver.GetFromHeader(filterutil.GetHeader(request))
-	if err := c.validateAccessToken(token, discovered, httpClient, logger); err != nil {
-		return middleware.NewErrorResponse(ctx, http.StatusBadRequest, err, nil)
-	}
-	return nil
-}
-
-func (j *OAuth2Filter) validateAccessToken(token string, discovered *Discovered, httpClient *http.Client, logger types.Logger) error {
-	switch j.Spec.AccessTokenValidation {
-	case "auto":
-		claims, err := j.parseJWT(token, discovered)
-		if err == nil {
-			return j.validateJWT(claims, discovered, logger)
-		}
-		logger.Debugln("rejecting JWT validation; falling back to UserInfo Endpoint validation:", err)
-		fallthrough
-	case "userinfo":
-		return j.validateAccessTokenUserinfo(token, discovered, httpClient, logger)
-	case "jwt":
-		claims, err := j.parseJWT(token, discovered)
-		if err != nil {
-			return err
-		}
-		return j.validateJWT(claims, discovered, logger)
-	}
-	panic("not reached")
-}
-
-func (j *OAuth2Filter) validateAccessTokenUserinfo(token string, discovered *Discovered, httpClient *http.Client, logger types.Logger) error {
-	req, err := http.NewRequest("GET", discovered.UserInfoEndpoint.String(), nil)
-	if err != nil {
-		return err
-	}
-	rfc6750client.AddToHeader(token, req.Header)
-	res, err := httpClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		return errors.Errorf("token validation through userinfo endpoint failed: HTTP %d", res.StatusCode)
-	}
-	return nil
-}
-
-func (j *OAuth2Filter) parseJWT(token string, discovered *Discovered) (jwt.MapClaims, error) {
-	jwtParser := jwt.Parser{
-		ValidMethods: []string{
-			// Any of the RSA algs supported by jwt-go
-			"RS256",
-			"RS384",
-			"RS512",
-		},
-		SkipClaimsValidation: true,
-	}
-
-	var claims jwt.MapClaims
-	_, err := jwtsupport.SanitizeParse(jwtParser.ParseWithClaims(token, &claims, func(t *jwt.Token) (interface{}, error) {
-		// Validates key id header.
-		if t.Header["kid"] == nil {
-			return nil, errors.New("missing kid")
-		}
-
-		kid, ok := t.Header["kid"].(string)
-		if !ok {
-			return nil, errors.New("kid is not a string")
-		}
-
-		// Get RSA public key
-		return discovered.JSONWebKeySet.GetKey(kid)
-	}))
-	if err != nil {
-		return nil, err
-	}
-	return claims, nil
-}
-
-func (j *OAuth2Filter) validateScope(actual rfc6749client.Scope) error {
-	desired := make(rfc6749client.Scope, len(j.Arguments.Scopes))
-	for _, s := range j.Arguments.Scopes {
-		desired[s] = struct{}{}
-	}
-	var missing []string
-	for scopeValue := range desired {
-		if scopeValue == "offline_access" {
-			continue
-		}
-		if _, ok := actual[scopeValue]; !ok {
-			missing = append(missing, scopeValue)
-		}
-	}
-	switch len(missing) {
-	case 0:
-		return nil
-	case 1:
-		return errors.Errorf("missing required scope value: %q", missing[0])
-	default:
-		return errors.Errorf("missing required scope values: %q", missing)
-	}
-}
-
-func (j *OAuth2Filter) validateJWT(claims jwt.MapClaims, discovered *Discovered, logger types.Logger) error {
-	// Validate 'exp', 'iat', and 'nbf' claims.
-	if err := claims.Valid(); err != nil {
-		return err
-	}
-
-	// Validate 'aud' claim.
-	//if !claims.VerifyAudience(j.Spec.Audience, false) {
-	//	return errors.Errorf("token has wrong audience: token=%#v expected=%q", claims["aud"], j.Spec.Audience)
-	//}
-
-	// Validate 'iss' claim.
-	if !claims.VerifyIssuer(discovered.Issuer, false) {
-		return errors.Errorf("token has wrong issuer: token=%#v expected=%q", claims["iss"], discovered.Issuer)
-	}
-
-	// Validate 'scopes' claim (draft standard).
-	// https://www.iana.org/assignments/jwt/jwt.xhtml
-	// https://tools.ietf.org/html/draft-ietf-oauth-token-exchange-16#section-4.2
-	switch scopeClaim := claims["scope"].(type) {
-	case nil:
-		logger.Debugf("No scope to verify")
-	case string: // proposed standard; most Authorization Servers do this
-		if err := j.validateScope(rfc6749client.ParseScope(scopeClaim)); err != nil {
-			return errors.Wrap(err, "token has wrong scope")
-		}
-	case []interface{}: // UAA does this
-		actual := make(rfc6749client.Scope, len(scopeClaim))
-		for _, scopeValue := range scopeClaim {
-			switch scopeValue := scopeValue.(type) {
-			case string:
-				actual[scopeValue] = struct{}{}
-			default:
-				logger.Warningf("Unexpected scope[n] type: %T", scopeValue)
-			}
-		}
-		if err := j.validateScope(actual); err != nil {
-			return errors.Wrap(err, "token has wrong scope")
-		}
-	default:
-		logger.Warningf("Unexpected scope type: %T", scopeClaim)
-	}
-
-	return nil
 }
 
 func randomString(bits int) (string, error) {
