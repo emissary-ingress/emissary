baa7da2e30621436266548dd2ec7d4823d0d353a
Luke Shumaker <lukeshu@datawire.io>
2019-10-03 21:44:27 -0400

    oauth2handler/client: Pull things from .Filter() in to separate methods
    And also invent a SessionInfo struct to make passing around pointers sane.
    This also means that the type-checker now handles the
    "// NB: Do NOT re-declare" stuff, so I can drop those comments.

--- cmd/amb-sidecar/filters/handler/oauth2handler/client/client.go
+++ cmd/amb-sidecar/filters/handler/oauth2handler/client/client.go
@@ -65,10 +65,12 @@ func (c *OAuth2Client) Filter(ctx context.Context, logger types.Logger, httpClie
 	}
 	oauthClient.RegisterProtocolExtensions(rfc6750client.OAuthProtocolExtension)
 
-	sessionID, sessionData, sessionErr := c.loadSession(redisClient, request)
+	sessionInfo := &SessionInfo{c: c}
+	var sessionErr error
+	sessionInfo.sessionID, sessionInfo.sessionData, sessionErr = c.loadSession(redisClient, request)
 	defer func() {
-		if sessionData != nil {
-			err := c.saveSession(redisClient, sessionID, sessionData)
+		if sessionInfo.sessionData != nil {
+			err := c.saveSession(redisClient, sessionInfo.sessionID, sessionInfo.sessionData)
 			if err != nil {
 				// TODO(lukeshu): Letting FilterMux recover() this panic() and generate an error message
 				// isn't the *worst* way of handling this error.
@@ -76,50 +78,19 @@ func (c *OAuth2Client) Filter(ctx context.Context, logger types.Logger, httpClie
 			}
 		}
 	}()
-	logger.Debugf("session data: %#v", sessionData)
+	logger.Debugf("session data: %#v", sessionInfo.sessionData)
 	switch {
 	case sessionErr != nil:
 		logger.Debugln("session status:", errors.Wrap(sessionErr, "no session"))
-	case sessionData.CurrentAccessToken == nil:
+	case sessionInfo.sessionData.CurrentAccessToken == nil:
 		logger.Debugln("session status:", "non-authenticated session")
 	default:
 		logger.Debugln("session status:", "authenticated session")
-		authorization, err := oauthClient.AuthorizationForResourceRequest(sessionData, func() io.Reader {
+		authorization, err := oauthClient.AuthorizationForResourceRequest(sessionInfo.sessionData, func() io.Reader {
 			return strings.NewReader(request.GetRequest().GetHttp().GetBody())
 		})
 		if err == nil {
-			addAuthorization := &filterapi.HTTPRequestModification{}
-			for k, vs := range authorization {
-				for _, v := range vs {
-					addAuthorization.Header = append(addAuthorization.Header, &filterapi.HTTPHeaderReplaceValue{
-						Key:   k,
-						Value: v,
-					})
-				}
-			}
-			filterutil.ApplyRequestModification(request, addAuthorization)
-
-			resourceResponse := c.ResourceServer.Filter(ctx, logger, httpClient, discovered, request, sessionData.CurrentAccessToken.Scope)
-			if resourceResponse == nil {
-				// nil means to send the same request+authorization to the upstream service, so tell
-				// Envoy to add the authorization to the request.
-				return addAuthorization
-			} else if resourceResponse, typeOK := resourceResponse.(*filterapi.HTTPResponse); typeOK && resourceResponse.StatusCode == http.StatusUnauthorized {
-				// The upstream Resource Server returns 401 Unauthorized to the Client--the Client does NOT pass
-				// 401 along to the User Agent; the User Agent is NOT using an RFC 7235-compatible
-				// authentication scheme to talk to the Client; 401 would be inappropriate.
-				//
-				// Instead, wrap the 401 response in a 403 Forbidden response.
-				return middleware.NewErrorResponse(ctx, http.StatusForbidden,
-					errors.New("authorization rejected"),
-					map[string]interface{}{
-						"synthesized_upstream_response": resourceResponse,
-					},
-				)
-			} else {
-				// Otherwise, just return the upstream resource server's response
-				return resourceResponse
-			}
+			return sessionInfo.handleAuthenticatedProxyRequest(ctx, logger, httpClient, discovered, request, authorization)
 		} else if err == rfc6749client.ErrNoAccessToken {
 			// This indicates a programming error; we've already checked that there is an access token.
 			panic(err)
@@ -142,16 +113,16 @@ func (c *OAuth2Client) Filter(ctx context.Context, logger types.Logger, httpClie
 	}
 	switch u.Path {
 	case "/callback":
-		if sessionData == nil {
+		if sessionInfo.sessionData == nil {
 			return middleware.NewErrorResponse(ctx, http.StatusForbidden,
 				errors.Errorf("no %q cookie", c.sessionCookieName()), nil)
 		}
-		authorizationCode, err := oauthClient.ParseAuthorizationResponse(sessionData, u)
+		authorizationCode, err := oauthClient.ParseAuthorizationResponse(sessionInfo.sessionData, u)
 		if err != nil {
 			return middleware.NewErrorResponse(ctx, http.StatusBadRequest,
 				err, nil)
 		}
-		originalURL, err := checkState(sessionData.Request.State, c.PublicKey)
+		originalURL, err := checkState(sessionInfo.sessionData.Request.State, c.PublicKey)
 		if err != nil {
 			// This should never happen--the state matched
 			// what we stored in Redis.  For this to
@@ -163,7 +134,7 @@ func (c *OAuth2Client) Filter(ctx context.Context, logger types.Logger, httpClie
 			return middleware.NewErrorResponse(ctx, http.StatusInternalServerError,
 				errors.Wrapf(err, "invalid state"), nil)
 		}
-		if err := oauthClient.AccessToken(sessionData, authorizationCode); err != nil {
+		if err := oauthClient.AccessToken(sessionInfo.sessionData, authorizationCode); err != nil {
 			return middleware.NewErrorResponse(ctx, http.StatusBadGateway,
 				err, nil)
 		}
@@ -176,84 +147,129 @@ func (c *OAuth2Client) Filter(ctx context.Context, logger types.Logger, httpClie
 			Body: "",
 		}
 	default:
-		// Use X-Forwarded-Proto instead of .GetScheme() to build the URL.
-		// https://github.com/datawire/ambassador/issues/1581
-		originalURL, err := url.ParseRequestURI(filterutil.GetHeader(request).Get("X-Forwarded-Proto") + "://" + request.GetRequest().GetHttp().GetHost() + request.GetRequest().GetHttp().GetPath())
-		if err != nil {
-			return middleware.NewErrorResponse(ctx, http.StatusInternalServerError,
-				errors.Wrap(err, "failed to construct URL"), nil)
+		return sessionInfo.handleUnauthenticatedProxyRequest(ctx, logger, httpClient, oauthClient, discovered, request)
+	}
+}
+
+type SessionInfo struct {
+	c           *OAuth2Client
+	sessionID   string
+	sessionData *rfc6749client.AuthorizationCodeClientSessionData
+}
+
+func (sessionInfo *SessionInfo) handleAuthenticatedProxyRequest(ctx context.Context, logger types.Logger, httpClient *http.Client, discovered *discovery.Discovered, request *filterapi.FilterRequest, authorization http.Header) filterapi.FilterResponse {
+	addAuthorization := &filterapi.HTTPRequestModification{}
+	for k, vs := range authorization {
+		for _, v := range vs {
+			addAuthorization.Header = append(addAuthorization.Header, &filterapi.HTTPHeaderReplaceValue{
+				Key:   k,
+				Value: v,
+			})
 		}
+	}
+	filterutil.ApplyRequestModification(request, addAuthorization)
 
-		// Build the scope
-		scope := make(rfc6749client.Scope, len(c.Arguments.Scopes))
-		for _, s := range c.Arguments.Scopes {
-			scope[s] = struct{}{}
-		}
-
-		scope["openid"] = struct{}{} // TODO(lukeshu): More carefully consider always asserting OIDC
-
-		// You'd be tempted to include "offline_access" here, but if offline access isn't
-		// allowed, some Authorization Servers (including Google, Keycloak, and UAA) will
-		// reject the entire authorization request, instead of simply issuing an Access
-		// Token without a Refresh Token.
+	resourceResponse := sessionInfo.c.ResourceServer.Filter(ctx, logger, httpClient, discovered, request, sessionInfo.sessionData.CurrentAccessToken.Scope)
+	if resourceResponse == nil {
+		// nil means to send the same request+authorization to the upstream service, so tell
+		// Envoy to add the authorization to the request.
+		return addAuthorization
+	} else if resourceResponse, typeOK := resourceResponse.(*filterapi.HTTPResponse); typeOK && resourceResponse.StatusCode == http.StatusUnauthorized {
+		// The upstream Resource Server returns 401 Unauthorized to the Client--the Client does NOT pass
+		// 401 along to the User Agent; the User Agent is NOT using an RFC 7235-compatible
+		// authentication scheme to talk to the Client; 401 would be inappropriate.
 		//
-		//scope["offline_access"] = struct{}{}
-
-		// Build the sessionID and the associated cookie
-		sessionID, err = randomString(256) // NB: Do NOT re-declare sessionID
-		if err != nil {
-			return middleware.NewErrorResponse(ctx, http.StatusInternalServerError,
-				errors.Wrap(err, "failed to generate session ID"), nil)
-		}
-		cookie := &http.Cookie{
-			Name:  c.sessionCookieName(),
-			Value: sessionID,
-
-			// Expose the cookie to all paths on this host, not just directories of {{originalURL.Path}}.
-			// This is important, because `/callback` is probably not a subdirectory of originalURL.Path.
-			Path: "/",
-
-			// Strictly match {{originalURL.Hostname}}.  Explicitly setting it to originalURL.Hostname()
-			// would instead also "*.{{originalURL.Hostname}}".
-			Domain: "",
-
-			// How long should the User-Agent retain the cookie?  If unset, it will expire at the end of the
-			// "session" (when they close their browser).
-			Expires: time.Time{},                                // as a time (low precedence)
-			MaxAge:  int((10 * 365 * 24 * time.Hour).Seconds()), // as a duration (high precedence)
-
-			// Whether to send the cookie for non-TLS requests.
-			// TODO(lukeshu): consider using originalURL.Scheme
-			Secure: c.Spec.TLS(),
-
-			// Don't expose the cookie to JavaScript.
-			HttpOnly: true,
-		}
-
-		// Build the full request
-		var authorizationRequestURI *url.URL
-		authorizationRequestURI, sessionData, err = oauthClient.AuthorizationRequest( // NB: Do NOT re-declare sessionData
-			c.Spec.CallbackURL(),
-			scope,
-			c.signState(originalURL, logger),
-		)
-		if err != nil {
-			return middleware.NewErrorResponse(ctx, http.StatusInternalServerError,
-				err, nil)
-		}
-
-		return &filterapi.HTTPResponse{
-			// A 302 "Found" may or may not convert POST->GET.  We want
-			// the UA to GET the Authorization URI, so we shouldn't use
-			// 302 which may or may not do the right thing, but use 303
-			// "See Other" which MUST convert to GET.
-			StatusCode: http.StatusSeeOther,
-			Header: http.Header{
-				"Set-Cookie": {cookie.String()},
-				"Location":   {authorizationRequestURI.String()},
+		// Instead, wrap the 401 response in a 403 Forbidden response.
+		return middleware.NewErrorResponse(ctx, http.StatusForbidden,
+			errors.New("authorization rejected"),
+			map[string]interface{}{
+				"synthesized_upstream_response": resourceResponse,
 			},
-			Body: "",
-		}
+		)
+	} else {
+		// Otherwise, just return the upstream resource server's response
+		return resourceResponse
+	}
+}
+
+func (sessionInfo *SessionInfo) handleUnauthenticatedProxyRequest(ctx context.Context, logger types.Logger, httpClient *http.Client, oauthClient *rfc6749client.AuthorizationCodeClient, discovered *discovery.Discovered, request *filterapi.FilterRequest) filterapi.FilterResponse {
+	// Use X-Forwarded-Proto instead of .GetScheme() to build the URL.
+	// https://github.com/datawire/ambassador/issues/1581
+	originalURL, err := url.ParseRequestURI(filterutil.GetHeader(request).Get("X-Forwarded-Proto") + "://" + request.GetRequest().GetHttp().GetHost() + request.GetRequest().GetHttp().GetPath())
+	if err != nil {
+		return middleware.NewErrorResponse(ctx, http.StatusInternalServerError,
+			errors.Wrap(err, "failed to construct URL"), nil)
+	}
+
+	// Build the scope
+	scope := make(rfc6749client.Scope, len(sessionInfo.c.Arguments.Scopes))
+	for _, s := range sessionInfo.c.Arguments.Scopes {
+		scope[s] = struct{}{}
+	}
+
+	scope["openid"] = struct{}{} // TODO(lukeshu): More carefully consider always asserting OIDC
+
+	// You'd be tempted to include "offline_access" here, but if offline access isn't
+	// allowed, some Authorization Servers (including Google, Keycloak, and UAA) will
+	// reject the entire authorization request, instead of simply issuing an Access
+	// Token without a Refresh Token.
+	//
+	//scope["offline_access"] = struct{}{}
+
+	// Build the sessionID and the associated cookie
+	sessionInfo.sessionID, err = randomString(256)
+	if err != nil {
+		return middleware.NewErrorResponse(ctx, http.StatusInternalServerError,
+			errors.Wrap(err, "failed to generate session ID"), nil)
+	}
+	cookie := &http.Cookie{
+		Name:  sessionInfo.c.sessionCookieName(),
+		Value: sessionInfo.sessionID,
+
+		// Expose the cookie to all paths on this host, not just directories of {{originalURL.Path}}.
+		// This is important, because `/callback` is probably not a subdirectory of originalURL.Path.
+		Path: "/",
+
+		// Strictly match {{originalURL.Hostname}}.  Explicitly setting it to originalURL.Hostname()
+		// would instead also "*.{{originalURL.Hostname}}".
+		Domain: "",
+
+		// How long should the User-Agent retain the cookie?  If unset, it will expire at the end of the
+		// "session" (when they close their browser).
+		Expires: time.Time{},                                // as a time (low precedence)
+		MaxAge:  int((10 * 365 * 24 * time.Hour).Seconds()), // as a duration (high precedence)
+
+		// Whether to send the cookie for non-TLS requests.
+		// TODO(lukeshu): consider using originalURL.Scheme
+		Secure: sessionInfo.c.Spec.TLS(),
+
+		// Don't expose the cookie to JavaScript.
+		HttpOnly: true,
+	}
+
+	// Build the full request
+	var authorizationRequestURI *url.URL
+	authorizationRequestURI, sessionInfo.sessionData, err = oauthClient.AuthorizationRequest(
+		sessionInfo.c.Spec.CallbackURL(),
+		scope,
+		sessionInfo.c.signState(originalURL, logger),
+	)
+	if err != nil {
+		return middleware.NewErrorResponse(ctx, http.StatusInternalServerError,
+			err, nil)
+	}
+
+	return &filterapi.HTTPResponse{
+		// A 302 "Found" may or may not convert POST->GET.  We want
+		// the UA to GET the Authorization URI, so we shouldn't use
+		// 302 which may or may not do the right thing, but use 303
+		// "See Other" which MUST convert to GET.
+		StatusCode: http.StatusSeeOther,
+		Header: http.Header{
+			"Set-Cookie": {cookie.String()},
+			"Location":   {authorizationRequestURI.String()},
+		},
+		Body: "",
 	}
 }
 
