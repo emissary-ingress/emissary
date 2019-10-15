8cdd2ed3b9d0e8d8a81a80061907391c745e0d7a
Luke Shumaker <lukeshu@datawire.io>
2019-10-15 16:29:55 -0400

    builder: Use docker.mk to tag and push

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
 OSS_HOME:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
-include $(OSS_HOME)/builder/builder.mk
+# We'll set REGISTRY_ERR in builder.mk
+docker.tag.dev = $(if $(DEV_REGISTRY),$(DEV_REGISTRY)/$*:$(patsubst sha256:%,%,$(shell cat $<)),$(REGISTRY_ERR))
+
 include $(OSS_HOME)/build-aux/prelude.mk
+include $(OSS_HOME)/build-aux/docker.mk
+include $(OSS_HOME)/builder/builder.mk
 include $(OSS_HOME)/build-aux-local/version.mk
 
 $(call module,ambassador,$(OSS_HOME))
 
 compile: python/ambassador/VERSION.py
+
+.SECONDARY:
--- builder/builder.mk
+++ builder/builder.mk
@@ -50,34 +50,25 @@ commit:
 	@$(BUILDER) commit snapshot
 .PHONY: commit
 
-images:
+external-images = $(shell sed -n '/\#external/{N;s/.* as  *//p;}' < $(BUILDER_HOME)/Dockerfile)
+images: $(addsuffix .docker.tag.dev,$(external-images))
+.PHONY: images
+snapshot.docker.stamp:
 	@$(MAKE) --no-print-directory compile
 	@$(MAKE) --no-print-directory commit
-	@printf "$(WHT)==$(GRN)Building $(BLU)ambassador$(GRN) image$(WHT)==$(END)\n"
-	@$(DBUILD) $(BUILDER_HOME) --build-arg artifacts=snapshot --target ambassador -t ambassador
-	@printf "$(WHT)==$(GRN)Building $(BLU)kat-client$(GRN) image$(WHT)==$(END)\n"
-	@$(DBUILD) $(BUILDER_HOME) --build-arg artifacts=snapshot --target kat-client -t kat-client
-	@printf "$(WHT)==$(GRN)Building $(BLU)kat-server$(GRN) image$(WHT)==$(END)\n"
-	@$(DBUILD) $(BUILDER_HOME) --build-arg artifacts=snapshot --target kat-server -t kat-server
-.PHONY: images
+	@docker image inspect snapshot --format='{{.Id}}' > $@
+$(addsuffix .docker.stamp,$(external-images)): %.docker.stamp: snapshot.docker
+	@printf "$(WHT)==$(GRN)Building $(BLU)$*$(GRN) image$(WHT)==$(END)\n"
+	@$(DBUILD) $(BUILDER_HOME) --iidfile $@ --build-arg artifacts=$$(cat snapshot.docker) --target $*
+%.docker: %.docker.stamp $(COPY_IFCHANGED)
+	@$(COPY_IFCHANGED) $< $@
 
-AMB_IMAGE=$(DEV_REGISTRY)/ambassador:$(shell docker images -q ambassador:latest)
-KAT_CLI_IMAGE=$(DEV_REGISTRY)/kat-client:$(shell docker images -q kat-client:latest)
-KAT_SRV_IMAGE=$(DEV_REGISTRY)/kat-server:$(shell docker images -q kat-server:latest)
+define REGISTRY_ERR
+$(shell printf '$(RED)ERROR: please set the DEV_REGISTRY make/env variable to the docker registry\n       you would like to use for development$(END)\n' >&2)
+$(error error)
+endef
 
-export REGISTRY_ERR=$(RED)ERROR: please set the DEV_REGISTRY make/env variable to the docker registry\n       you would like to use for development$(END)
-
-push: images
-	@test -n "$(DEV_REGISTRY)" || (printf "$${REGISTRY_ERR}\n"; exit 1)
-	@printf "$(WHT)==$(GRN)Pushing $(BLU)ambassador$(GRN) image$(WHT)==$(END)\n"
-	docker tag ambassador $(AMB_IMAGE)
-	docker push $(AMB_IMAGE)
-	@printf "$(WHT)==$(GRN)Pushing $(BLU)kat-client$(GRN) image$(WHT)==$(END)\n"
-	docker tag kat-client $(KAT_CLI_IMAGE)
-	docker push $(KAT_CLI_IMAGE)
-	@printf "$(WHT)==$(GRN)Pushing $(BLU)kat-server$(GRN) image$(WHT)==$(END)\n"
-	docker tag kat-server $(KAT_SRV_IMAGE)
-	docker push $(KAT_SRV_IMAGE)
+push: $(addsuffix .docker.push.dev,$(external-images))
 .PHONY: push
 
 export KUBECONFIG_ERR=$(RED)ERROR: please set the $(YEL)DEV_KUBECONFIG$(RED) make/env variable to the docker registry\n       you would like to use for development. Note this cluster must have access\n       to $(YEL)DEV_REGISTRY$(RED) ($(WHT)$(DEV_REGISTRY)$(RED))$(END)
@@ -101,9 +92,9 @@ PYTEST_ARGS ?=
 pytest: test-ready
 	@printf "$(WHT)==$(GRN)Running $(BLU)py$(GRN) tests$(WHT)==$(END)\n"
 	docker exec \
-		-e AMBASSADOR_DOCKER_IMAGE=$(AMB_IMAGE) \
-		-e KAT_CLIENT_DOCKER_IMAGE=$(KAT_CLI_IMAGE) \
-		-e KAT_SERVER_DOCKER_IMAGE=$(KAT_SRV_IMAGE) \
+		-e AMBASSADOR_DOCKER_IMAGE=$$(sed -n 2p ambassador.docker.push.dev) \
+		-e KAT_CLIENT_DOCKER_IMAGE=$$(sed -n 2p kat-client.docker.push.dev) \
+		-e KAT_SERVER_DOCKER_IMAGE=$$(sed -n 2p kat-server.docker.push.dev) \
 		-e KAT_IMAGE_PULL_POLICY=Always \
 		-e KAT_REQ_LIMIT \
 		-it $(shell $(BUILDER)) pytest $(PYTEST_ARGS)
@@ -125,7 +116,7 @@ shell:
 	@$(BUILDER) shell
 .PHONY: shell
 
-clean:
+clean: $(addsuffix .docker.clean,$(external-images) snapshot)
 	@$(BUILDER) clean
 .PHONY: clean
 
