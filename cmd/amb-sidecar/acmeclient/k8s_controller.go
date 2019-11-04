4b71ce69bc24c4495eac048cecd6c483d2d0895d
Luke Shumaker <lukeshu@datawire.io>
2019-11-04 11:14:58 -0500

    my race-condition fix broke getting a cert for multiple hosts

--- cmd/amb-sidecar/acmeclient/k8s_controller.go
+++ cmd/amb-sidecar/acmeclient/k8s_controller.go
@@ -206,9 +206,13 @@ func (c *Controller) rectify(logger types.Logger) {
 		FillDefaults(host)
 		if !proto.Equal(host.Spec, _host.Spec) {
 			logger.Debugln("saving defaults")
+			nextPhase := ambassadorTypesV2.HostPhase_NA
+			if host.Status.TlsCertificateSource == ambassadorTypesV2.HostTLSCertificateSource_ACME {
+				nextPhase = ambassadorTypesV2.HostPhase_ACMEUserPrivateKeyCreated
+			}
 			c.recordHostPending(logger, host,
 				ambassadorTypesV2.HostPhase_DefaultsFilled,
-				ambassadorTypesV2.HostPhase_NA)
+				nextPhase)
 			continue
 		}
 
@@ -234,20 +238,15 @@ func (c *Controller) rectify(logger types.Logger) {
 					c.recordHostError(logger, host,
 						ambassadorTypesV2.HostPhase_ACMEUserPrivateKeyCreated,
 						err)
-				} else {
-					c.recordHostPending(logger, host,
-						ambassadorTypesV2.HostPhase_ACMEUserPrivateKeyCreated,
-						ambassadorTypesV2.HostPhase_ACMEUserRegistered)
 				}
-				continue
-			} else if host.Status.PhaseCompleted == ambassadorTypesV2.HostPhase_DefaultsFilled {
-				// this can happen if we end up with a snapshot of the world that was created in-between
-				// the call to `createUserPrivateKey()` and the call to `c.recordHostPending()`.
-				logger.Debugln("rectify: deferring host until next snapshot")
-				continue
+			} else if host.Status.State == ambassadorTypesV2.HostState_Pending && host.Status.PhaseCompleted < ambassadorTypesV2.HostPhase_ACMEUserPrivateKeyCreated {
+				c.recordHostPending(logger, host,
+					ambassadorTypesV2.HostPhase_ACMEUserPrivateKeyCreated,
+					ambassadorTypesV2.HostPhase_ACMEUserRegistered)
+			} else {
+				logger.Debugln("rectify: accepting host for next phase")
+				acmeHosts = append(acmeHosts, host)
 			}
-			logger.Debugln("rectify: accepting host for next phase")
-			acmeHosts = append(acmeHosts, host)
 		}
 	}
 
