e7069c5dad808e3954a43aacdef75cf1fb05acc9
Flynn <flynn@datawire.io>
2019-11-05 17:08:42 -0500

    Include the fallback mapping and the ACME challenge mapping using init-config, and bump AMBASSADOR_COMMIT to make sure OSS lines up with that.

--- post-compile.sh
+++ post-compile.sh
@@ -1,2 +1,32 @@
+set -e
+
 sudo cp /buildroot/bin/amb-sidecar /ambassador/sidecars
 sudo touch /ambassador/.edge_stack
+
+if [ ! -d /ambassador/init-config ]; then
+	sudo mkdir /ambassador/init-config
+fi
+
+cat > /tmp/edge-stack-mappings.yaml <<EOF
+---
+apiVersion: getambassador.io/v1
+kind: Mapping
+metadata:
+  name: edgestack-fallback-mapping
+spec:
+  prefix: /
+  rewrite: /edge_stack_ui/
+  service: 127.0.0.1:8500
+  precedence: -1000000
+---
+apiVersion: getambassador.io/v1
+kind: Mapping
+metadata:
+  name: edgestack-acme-mapping
+spec:
+  prefix: /.well-known/acme-challenge/
+  rewrite: /.well-known/acme-challenge/
+  service: 127.0.0.1:8500
+EOF
+
+sudo mv /tmp/edge-stack-mappings.yaml /ambassador/init-config
