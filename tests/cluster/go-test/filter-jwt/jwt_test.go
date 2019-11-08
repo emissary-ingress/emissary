69d238b9c3475c8e655b6fc49244a31cdcae9f02
Luke Shumaker <lukeshu@datawire.io>
2019-11-08 11:46:28 -0500

    Add some tests for JWT Filter errorResponse

--- tests/cluster/go-test/filter-jwt/jwt_test.go
+++ tests/cluster/go-test/filter-jwt/jwt_test.go
@@ -96,3 +96,89 @@ func TestJWTInjectHeaders(t *testing.T) {
 		}
 	}
 }
+
+func TestJWTErrorResponse(t *testing.T) {
+	assert := &testutil.Assert{T: t}
+
+	// build the test-case /////////////////////////////////////////////////
+	tokenStruct := jwt.NewWithClaims(jwt.GetSigningMethod("none"), jwt.MapClaims{
+		"sub":  "1234567890",
+		"name": "John Doe",
+		"iat":  1516239022,
+		"exp":  1516239023,
+	})
+	tokenStruct.Header["extra"] = "so much"
+	tokenString, err := tokenStruct.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	assert.NotError(err)
+
+	testHeaders := map[string]string{
+		"X-Correlation-ID": "foobar",
+		"Content-Type":     "application/json",
+	}
+	testBody := struct {
+		ErrorMessage    string `json:"errorMessage"`
+		AltErrorMessage string `json:"altErrorMessage"`
+		ErrorCode       int    `json:"errorCode"`
+		HTTPStatus      string `json:"httpStatus"`
+		//RequestID       string `json:"requestId"`
+	}{
+		ErrorMessage:    "Token validation error: token is invalid: errorFlags=0x00000010=(ValidationErrorExpired) wrappedError=(Token is expired)",
+		AltErrorMessage: "expired",
+		ErrorCode:       16,
+		HTTPStatus:      "401",
+		//"requestId": "7167523368329307446"
+	}
+
+	// run the filter //////////////////////////////////////////////////////
+	u, err := url.Parse("https://ambassador.standalone.svc.cluster.local/jwt/headers")
+	assert.NotError(err)
+	client := &http.Client{
+		Transport: &http.Transport{
+			// #nosec G402
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+	resp, err := client.Do(&http.Request{
+		Method: "GET",
+		URL:    u,
+		Header: http.Header{
+			"Authorization":    {"Bearer " + tokenString},
+			"X-Override":       {"before"},
+			"X-Correlation-ID": {"foobar"},
+		},
+	})
+	assert.NotError(err)
+	assert.Bool(resp != nil)
+	defer resp.Body.Close()
+
+	// inspect the result //////////////////////////////////////////////////
+
+	assert.HTTPResponseStatusEQ(resp, http.StatusUnauthorized)
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	assert.NotError(err)
+	t.Logf("Body: %s", bodyBytes)
+	var body struct {
+		ErrorMessage    string `json:"errorMessage"`
+		AltErrorMessage string `json:"altErrorMessage"`
+		ErrorCode       int    `json:"errorCode"`
+		HTTPStatus      string `json:"httpStatus"`
+		//RequestID       string `json:"requestId"`
+	}
+	assert.NotError(json.Unmarshal(bodyBytes, &body))
+
+	for key, val := range testHeaders {
+		if val != resp.Header.Get(key) {
+			t.Errorf("Wrong header[%q]:\n"+
+				"\texpected: %q\n"+
+				"\treceived: %q\n",
+				key, val, resp.Header.Get(key))
+		}
+	}
+
+	if testBody != body {
+		t.Errorf("Wrong body:\n"+
+			"\texpected: %#v\n"+
+			"\treceived: %#v\n",
+			testBody, body)
+	}
+}
