2384dccef8d832010863b7e4ef0ce71a98939b20
Rafael Schloming <rhs@datawire.io>
2019-11-20 13:41:10 -0500

    make proper host widgets

--- cmd/amb-sidecar/webui/bindata/edge_stack/components/host.js
+++ cmd/amb-sidecar/webui/bindata/edge_stack/components/host.js
@@ -1,85 +1,138 @@
 import { LitElement, html, css } from 'https://cdn.pika.dev/-/lit-element/2.2.1/dist-es2019/lit-element.min.js'
-import { repeat } from '/edge_stack/components/repeat.js';
-import {registerContextChangeHandler, useContext} from '/edge_stack/components/context.js';
+import {registerContextChangeHandler, useContext} from '/edge_stack/components/context.js'
 
-export class HostAdd extends LitElement {
+// holds the UI state of a host widget, this can be merged with Host when we have repeat
+class HostUIState {
 
-  static get properties() {
-    return {
-      state: { type: String },
-      tos: { type: String },
-      messages: { type: Array }
+  constructor() {
+    this.mode = "list" // one of add, edit, list, off
+    this.messages = []
+    this.show_tos = false
+  }
+
+  renderErrors() {
+    if (this.messages.length > 0) {
+      return html`
+<div class="both">
+  <ul>
+    ${this.messages.map(m=>html`<li><span class="error">${m}</span></li>`)}
+  </ul>
+</div>`
+    } else {
+      return html``
     }
   }
+}
+
+export class Host extends LitElement {
 
   static get styles() {
     return css`
-#form.start { display: none; }
-#add.entry { display: none; }
+.error {
+  color: red;
+}
 
 div {
-  margin: 0.5em;
+  margin: 0.4em;
 }
 
-.error {
-  color: red;  
+div.frame {
+  display: grid;
+  grid-template-columns: max-content;
+  border: 2px solid #ede7f3;
+  border-radius: 0.4em;
 }
+
+div.title {
+  grid-column: 1 / 3;
+  background: #ede7f3;
+  margin: 0;
+  padding: 0.5em;
+}
+
+div.left {
+  grid-column: 1 / 2;
+}
+
+div.right {
+  grid-column: 2 / 3;
+}
+
+div.both {
+  grid-column: 1 / 3;
+}
+
+.off { display: none; }
 `
+  }
+
+  static get properties() {
+    return {
+      host: {type: Map},
+      state: {type: Object},
+    }
   }
 
   constructor() {
     super()
-    this.state = 'start'
+    this.host = {}
+    this.state = {}
     this.tos = html`...`
-    this.messages = []
   }
 
-  firstUpdated(e) {
-    this.providerChanged()
+  onAdd() {
+    this.state.mode = "add"
+    this.requestUpdate()
   }
 
-  providerChanged() {
-    let value = this.provider().value
-    let url = new URL('/edge_stack/api/tos-url', window.location)
-    url.searchParams.set('ca-url', value)
-    fetch(url, {
-      headers: new Headers({
-        'Authorization': 'Bearer ' + window.location.hash.slice(1)
-      })
-    })
-      .then(r=>{
-        r.text().then(t=>{
-          if (r.ok) {
-            this.tos = html`<a href="${t}">${t}</a>`
-          } else {
-            this.tos = html`...`
-          }
-        })
-      })
+  onEdit(host) {
+    if (this.state.mode != "edit") {
+      this.state.mode = "edit"
+    } else {
+      this.state.mode = "list"
+    }
+    this.requestUpdate()
   }
 
-  handleStart() {
-    this.state = "entry"
+  onDelete(host) {
+    alert("TODO")
   }
 
-  handleCancel() {
-    this.state = "start"
+  onCancel(host) {
+    this.requestUpdate()
+
+    if (this.state.mode == "add") {
+      this.state.mode = "off"
+    } else {
+      this.state.mode = "list"
+    }
+
+    this.reset()
   }
 
-  handleAdd() {
-    this.messages = []
+  reset() {
+    this.state.messages.length = 0
+    let fields = [this.provider(), this.email(), this.hostname()]
+    fields.forEach(x=>x.value = x.defaultValue)
+    this.tos_agree().checked = false
+    this.state.show_tos = false
+  }
 
+  onSave(host) {
+    this.requestUpdate()
+
+    this.state.messages.length = 0
     if (!this.tos_agree().checked) {
-      this.messages.push("you must agree to terms of service")
+      this.state.messages.push("you must agree to terms of service")
     }
 
     var emailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/
 
     if (!this.email().value.match(emailFormat)) {
-      this.messages.push("you must supply a valid email address")
+      this.state.messages.push("you must supply a valid email address")
     }
 
-    if (this.messages.length > 0) {
+    if (this.state.messages.length > 0) {
       return
     }
 
@@ -96,6 +149,7 @@ spec:
     authority: "${this.provider().value}"
     email: "${this.email().value}"
 `
+
     fetch('/edge_stack/api/apply',
           {
             method: "POST",
@@ -111,7 +165,12 @@ spec:
           } else {
             alert("BAD\n\n" + yaml + "\n\n" + t)
           }
-          this.state = "start"
+          if (this.state.mode == "add") {
+            this.state.mode = "off"
+          } else {
+            this.state.mode = "list"
+          }
+          this.reset()
         })
       })
   }
@@ -132,64 +191,118 @@ spec:
     return this.shadowRoot.querySelector('input[name="email"]')
   }
 
-  renderErrors() {
-    if (this.messages.length > 0) {
-      return html`<label>
-  <ul>
-    ${this.messages.map(m=>html`<li><span class="error">${m}</span></li>`)}
-  </ul>
-</label>`
-    } else {
-      return html``
+  firstUpdated(e) {
+    this.providerChanged(false)
+  }
+
+  providerChanged(userEdit) {
+    this.requestUpdate()
+    if (userEdit) {
+      this.state.show_tos = true
     }
+    let value = this.provider().value
+    let url = new URL('/edge_stack/api/tos-url', window.location)
+    url.searchParams.set('ca-url', value)
+    fetch(url, {
+      headers: new Headers({
+        'Authorization': 'Bearer ' + window.location.hash.slice(1)
+      })
+    })
+      .then(r=>{
+        r.text().then(t=>{
+          if (r.ok) {
+            this.tos = html`<a href="${t}">${t}</a>`
+          } else {
+            this.tos = html`...`
+          }
+        })
+      })
   }
 
   render() {
+    let host = this.host
+    let spec = host.spec
+    let status = host.status.state
+    let reason = status == "Error" ? `(${host.status.reason})` : ''
+
+    let state = this.state
+
+    let add = state.mode == "add" ? "" : "off"
+    let list = state.mode == "list" ? "" : "off"
+    let edit = state.mode == "edit" || state.mode == "add" ? "" : "off"
+    let list_or_edit = state.mode == "list" || state.mode == "edit" ? "" : "off"
+    let tos = this.state.show_tos || state.mode == "add" ? "right" : "off"
+
     return html`
-<slot class="${this.state}" id="add" @click=${this.handleStart}></slot>
-<div class="${this.state}" id="form">
-  <fieldset>
-    <legend>Add a Host</legend>
+<slot class="${state.mode == "off" ? "" : "off"}" @click=${this.onAdd.bind(this)}></slot>
+<div class="${state.mode == "off" ? "off" : "frame"}">
+  <div class="title">
+    Host: <span class="${list_or_edit}">${host.metadata.name}</span>
+          <input class="${add}" type="text" value="${host.metadata.name}"/>
+
+
+      (<span class="${list_or_edit}">${host.metadata.namespace}</span>
+       <input class="${add}" type="text" value="${host.metadata.namespace}"/>)</div>
+
+  <div class="left">Hostname:</div>
+  <div class="right">
+    <span class="${list}">${spec.hostname}</span>
+    <input class="${edit}" type="text" name="hostname"  value="${spec.hostname}" />
+  </div>
+
+  <div class="left">ACME Provider:</div>
+  <div class="right">
+    <span class="${list}">${spec.acmeProvider.authority}</span>
+    <input
+      class="${edit}"
+      type="url"
+      size="60"
+      name="provider"
+      value="${spec.acmeProvider.authority}"
+      @change=${()=>this.providerChanged(true)}
+    />
+  </div>
+
+  <div class="${tos}">
+    <input type="checkbox" name="tos_agree" />
+    <span>I have agreed to to the Terms of Service at: ${this.tos}</span>
+  </div>
+
+  <div class="left">Email:</div>
+  <div class="right">
+    <span class="${list}">${spec.acmeProvider.email}</span>
+    <input class="${edit}" type="email" name="email" value="${spec.acmeProvider.email}" />
+  </div>
+
+  <div class="left ${list_or_edit}">Status:</div>
+  <div class="right ${list_or_edit}">
+    <span>${status} ${reason}</span>
+  </div>
+
+  <div class="both">
     <label>
-      <span>Hostname:</span>
-      <input type="text" name="hostname"  value="${window.location.hostname}" />
+      <button class="${list}" @click=${()=>this.onEdit(host)}>Edit</button>
+      <button class="${list}" @click=${()=>this.onDelete(host)}>Delete</button>
+      <button class="${edit}" @click=${()=>this.onCancel(host)}>Cancel</button>
+      <button class="${edit}" @click=${(e)=>this.onSave(host, e)}>Save</button>
     </label>
-    <label>
-      <span>ACME provider:</span>
-      <input type="url" name="provider" @change=${this.providerChanged} value="https://acme-v02.api.letsencrypt.org/directory" />
-    </label>
-    <div>
-      <label>
-        <input type="checkbox" name="tos_agree" />
-        <span>I have agreed to to the Terms of Service at: ${this.tos}</span>
-      </label>
-    </div>
-    <label>
-      <span>Email:</span>
-      <input type="email" name="email" />
-    </label>
-    <div>
-      <label>
-        <button @click=${this.handleCancel}>Cancel</button>
-      </label>
-      <label>
-        <button @click=${this.handleAdd}>Add</button>
-      </label>
-    </div>
-    ${this.renderErrors()}
-  </fieldset>
+  </div>
+
+  ${this.state.renderErrors()}
 </div>`
   }
+
 }
 
-customElements.define('dw-host-add', HostAdd)
-
-// XXX: dw-hosts is not used yet!
+customElements.define('dw-host', Host)
 
 export default class Hosts extends LitElement {
+
   static get properties() {
     return {
-      hosts: Array
+      hosts: {type: Array},
+      _states: {type: Map},
+      addState: {type: Object}
     };
   }
 
@@ -198,29 +311,50 @@ export default class Hosts extends LitElement {
 
     const arr = useContext('aes-api-snapshot', null);
     if (arr[0] != null) {
-      this.hosts = arr[0]['Host'] || [];
+      this.hosts = arr[0]['Host'] || []
     } else {
-      this.hosts = [];
+      this.hosts = []
     }
-    registerContextChangeHandler('aes-api-snapshot', this.onSnapshotChange.bind(this));
+    this._states = {}
+    this.addState = new HostUIState()
+    this.addState.mode = "off"
+    registerContextChangeHandler('aes-api-snapshot', this.onSnapshotChange.bind(this))
   }
 
   onSnapshotChange(snapshot) {
     let kube = snapshot['Kubernetes'] || {'Host': []}
-    this.hosts = kube['Host'] || [];
+    this.hosts = kube['Host'] || []
   }
 
-  renderHost(host) {
-    let name = host.spec.hostname
-    let state = host.status.state
-    let reason = state == "Error" ? `(${host.status.reason})` : ''
-    return html`<div>${name}: ${state} ${reason}</div>`
+  state(host) {
+    let key = host.metadata.namespace + ":" + host.metadata.name
+    if (this._states[key] == undefined) {
+      this._states[key] = new HostUIState()
+    }
+    return this._states[key]
   }
 
   render() {
-    return html`<div>${this.hosts.map(h => this.renderHost(h))}</div>`
+    let addHost = {
+      metadata: {
+        namespace: "default",
+        name: window.location.hostname
+      },
+      spec: {
+        hostname: window.location.hostname,
+        acmeProvider: {
+          authority: "https://acme-v02.api.letsencrypt.org/directory",
+          email: ""
+        }
+      },
+      status: {}}
+    return html`
+<dw-host .host=${addHost} .state=${this.addState}><button>Add</button></dw-host>
+<div>
+  ${this.hosts.map(h => html`<dw-host .host=${h} .state=${this.state(h)}></dw-host>`)}
+</div>`
   }
 
 }
 
-customElements.define('dw-hosts', Hosts);
+customElements.define('dw-hosts', Hosts)
