9450263509b752c25a9d11e0689e8e7202279be3
Alvaro Saurin <alvaro@datawire.io>
2019-11-21 16:40:02 +0100

    Try to re-watch CRs after a new CRD is installed
    Signed-off-by: Alvaro Saurin <alvaro@datawire.io>

--- pkg/k8s/watcher.go
+++ pkg/k8s/watcher.go
@@ -37,6 +37,7 @@ func (lw listWatchAdapter) Watch(options v1.ListOptions) (pwatch.Interface, erro
 	return lw.resource.Watch(options)
 }
 
+// Watcher is a kubernetes watcher that can watch multiple queries simultaneously
 type Watcher struct {
 	Client  *Client
 	watches map[ResourceType]watch
@@ -92,6 +93,10 @@ func (w *Watcher) Watch(resources string, listener func(*Watcher)) error {
 
 func (w *Watcher) WatchNamespace(namespace, resources string, listener func(*Watcher)) error {
 	return w.SelectiveWatch(namespace, resources, "", "", listener)
+}
+
+func (w *Watcher) Refresh() error {
+	return w.Client.Refresh()
 }
 
 func (w *Watcher) SelectiveWatch(namespace, resources, fieldSelector, labelSelector string,
@@ -190,6 +195,7 @@ func (w *Watcher) WatchQuery(query Query, listener func(*Watcher)) error {
 	return nil
 }
 
+// Start starts the watcher
 func (w *Watcher) Start() {
 	w.mutex.Lock()
 	if w.started {
@@ -229,6 +235,7 @@ func (w *Watcher) sync(kind ResourceType) {
 	}
 }
 
+// List lists all the resources with kind `kind`
 func (w *Watcher) List(kind string) []Resource {
 	ri, err := w.Client.ResolveResourceType(kind)
 	if err != nil {
@@ -247,6 +254,7 @@ func (w *Watcher) List(kind string) []Resource {
 	}
 }
 
+// UpdateStatus updates the status of the `resource` provided
 func (w *Watcher) UpdateStatus(resource Resource) (Resource, error) {
 	ri, err := w.Client.ResolveResourceType(resource.QKind())
 	if err != nil {
@@ -276,6 +284,7 @@ func (w *Watcher) UpdateStatus(resource Resource) (Resource, error) {
 	}
 }
 
+// Get gets the `qname` resource (of kind `kind`)
 func (w *Watcher) Get(kind, qname string) Resource {
 	resources := w.List(kind)
 	for _, res := range resources {
@@ -286,6 +295,7 @@ func (w *Watcher) Get(kind, qname string) Resource {
 	return Resource{}
 }
 
+// Exists returns true if the `qname` resource (of kind `kind`) exists
 func (w *Watcher) Exists(kind, qname string) bool {
 	return w.Get(kind, qname).Name() != ""
 }
