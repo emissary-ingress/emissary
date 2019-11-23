f7cd24d0b7f382cf02fd7a52d310dcb03473f997
Bruce Horn <bruce@consilientlabs.com>
2019-11-23 14:53:56 -0800

    checkpoint

--- cmd/amb-sidecar/webui/bindata/edge_stack/components/dashboard.js
+++ cmd/amb-sidecar/webui/bindata/edge_stack/components/dashboard.js
@@ -1,9 +1,12 @@
-import {html} from 'https://cdn.pika.dev/-/lit-element/2.2.1/dist-es2019/lit-element.min.js'
-import {getCookie} from '/edge_stack/components/cookies.js';
+
+  /* Dashboard and dashboard element classes. */
+
+import { LitElement, html, css } from "https://cdn.pika.dev/-/lit-element/2.2.1/dist-es2019/lit-element.min.js";
+/* import { Chart } from "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"; */
 
 export class Dashboard extends LitElement {
 
-  /* styles() returns the styules for frames, triangles, etc. copied from resources.js.
+  /* styles() returns the styles for frames, triangles, etc. copied from resources.js.
      this should really be in a superclass that is shared by all Admin pages. */
   static get styles() {
     return css`
@@ -24,6 +27,14 @@ export class Dashboard extends LitElement {
         background: #ede7f3;
         margin: 0;
         padding: 0.5em;
+      }
+
+      div.dash-element {
+        background-color: lightgrey;
+        width: 300px;
+        border: 15px solid green;
+        padding: 50px;
+        margin: 20px;
       }
 
       /*
@@ -73,42 +84,47 @@ export class Dashboard extends LitElement {
       }
       .off { display: none; }
       span.code { font-family: Monaco, monospace; }`
-}
+  };
 
   /* The constructor doesn't do anything at the moment...*/
-  }
   constructor() {
     super()
-  }
+  };
 
   /* Initialize the dashboard. */
   init() {
-  }
+    super.init()
+  };
 
   /* Reset the dashboard. */
   reset() {
     super.reset()
-  }
+  };
+
+  /* Update, will eventually call render() */
+  update() {
+    super.update()
+  };
 
   /* Validate the dashboard.  Not sure why this would be called.*/
   validate() {
     this.state.messages.push("validating dashboard...why?")
-  }
+  };
 
   /* Render the component by returning a TemplateResult, using the html helper function. */
   render() {
-    return html`<div class="left">Dashboard </div>`
-  }
+    return html`<p>Hello World from the Dashboard</p>`
+  };
 
   /* Returns a single graph item in a box. */
   renderGraphItem() {
-    return html`<div class="left">Graph Item</div>`
+    return html`<div class="dash-element">Graph Item</div>`
   }
 
   /* Returns a single summary item in a box. */
   renderSummaryItem() {
-    return html`<div class="left">Summary </div>`
+    return html`<div class="dash-element">Summary Item</div>`
   }
-}
+};
 
-customElements.define('dw-dashboard', Dashboard)
+customElements.define('dw-dashboard', Dashboard);
