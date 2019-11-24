e2b0c5faec6aefa6295f129f57d33cc121005f9d
Bruce Horn <bruce@consilientlabs.com>
2019-11-24 12:07:50 -0800

    checkpoint

--- cmd/amb-sidecar/webui/bindata/edge_stack/components/dashboard.js
+++ cmd/amb-sidecar/webui/bindata/edge_stack/components/dashboard.js
@@ -1,10 +1,16 @@
 
-  /* Dashboard and dashboard element classes. */
+  /* Dashboard and dashboard element classes.
+   * Assumes that ChartJS has been loaded into the html page.
+   */
 
 import { LitElement, html, css } from "https://cdn.pika.dev/-/lit-element/2.2.1/dist-es2019/lit-element.min.js";
-/* import { Chart } from "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"; */
 
+   // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.setOnLoadCallback(chartsLoaded);
+
+/* The Dashboard class, drawing dashboard elements in a matrix of dash-element div's. */
 export class Dashboard extends LitElement {
+  var chartElementsToUpdate = new Set();
 
   /* styles() returns the styles for frames, triangles, etc. copied from resources.js.
      this should really be in a superclass that is shared by all Admin pages. */
@@ -13,76 +19,28 @@ export class Dashboard extends LitElement {
       .error {
         color: red;
       }
-      div {
-        margin: 0.4em;
-      }
-      div.frame {
-        display: grid;
-        grid-template-columns: 50% 50%;
-        border: 2px solid #ede7f3;
-        border-radius: 0.4em;
-      }
-      div.title {
-        grid-column: 1 / 3;
-        background: #ede7f3;
-        margin: 0;
-        padding: 0.5em;
-      }
-
-      div.dash-element {
-        background-color: lightgrey;
-        width: 300px;
-        border: 15px solid green;
-        padding: 50px;
-        margin: 20px;
-      }
-
-      /*
-      * These styles are used in mappings.js
-      */
-      div.frame-no-grid {
-        border: 2px solid #ede7f3;
-        border-radius: 0.4em;
-      }
-      .collapsed div.up-down-triangle {
-        float: left;
-        margin-left: 0;
-        margin-top: 0.25em;
-        cursor: pointer;
-      }
-      .collapsed div.up-down-triangle::before {
-        content: "\\25B7"
-      }
-      .expanded div.up-down-triangle {
-        float: left;
-        margin-left: 0;
-        margin-top: 0.25em;
-        cursor: pointer; 
-      }
-      .expanded div.up-down-triangle::before {
-        content: "\\25BD"
-      }
-      div.grid {
-        display: grid;
-        grid-template-columns: 50% 50%;
-      }
-      div.grid div {
-        margin: 0.1em;
-      }
-      /*
-      * End of styles for mappings.js
-      */
       
-      div.left {
-        grid-column: 1 / 2;
+      div.element-titlebar {
+        font-weight: bold;
+        background-color: lightgray;
+        width: 200px;
+        height: 16px;
+        border: 2px solid lightgray;
+        padding: 8px;
+        left-margin: 20px;
+        bottom-margin: 0px;
       }
-      div.right {
-        grid-column: 2 / 3;
-      }
-      div.both {
-        grid-column: 1 / 3;
-      }
-      .off { display: none; }
+
+      div.element-content {
+        background-color: whitesmoke;
+        width: 200px;
+        height: 200px;
+        border: 2px solid lightgray;
+        padding: 8px;
+        top-margin: 0px;
+        left-margin: 20px;
+       }
+       
       span.code { font-family: Monaco, monospace; }`
   };
 
@@ -113,18 +71,80 @@ export class Dashboard extends LitElement {
 
   /* Render the component by returning a TemplateResult, using the html helper function. */
   render() {
-    return html`<p>Hello World from the Dashboard</p>`
+    return html`
+        $(this.renderGraphItem("Graph1"))
+        $(this.renderSummaryItem("Summary1"))
+        $(this.renderGraphItem("Graph2"))
+        $(this.renderSummaryItem("Summary2"))
+        $(this.renderGraphItem("Graph3"))
+        $(this.renderSummaryItem("Summary3"))
+    `
+    // return html`Hello World from Dashboard`
   };
 
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      console.log(`${propName} changed. oldValue: ${oldValue}`);
+    });
+  }
+
+  firstUpdated() {
+    this.drawChart("chart-div");
+  }
+
+  /* Draw a chart in the given element. */
+  drawChart(element_id) {
+    if (true) {
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Create the data table.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['Mushrooms', 3],
+        ['Onions', 1],
+        ['Olives', 1],
+        ['Zucchini', 1],
+        ['Pepperoni', 2]
+      ]);
+
+      // Set chart options
+      var options = {'title':'How Much Pizza I Ate Last Night',
+                     'width':400,
+                     'height':300};
+
+      // Instantiate and draw our chart, passing in some options.
+      var element  = this.shadowRoot.getElementById(element_id);
+      var chart    = new google.visualization.PieChart(element);
+      chart.draw(data, options);
+    }
+
+  }
+
   /* Returns a single graph item in a box. */
-  renderGraphItem() {
-    return html`<div class="dash-element">Graph Item</div>`
+  renderGraphItem(title) {
+    return html`
+        <div float: right>
+        <div class="element-titlebar">Graph $(title)</div>
+        <div class="element-content">Graph Content Goes Here</div>
+        </div>`
   }
 
   /* Returns a single summary item in a box. */
-  renderSummaryItem() {
-    return html`<div class="dash-element">Summary Item</div>`
+  renderSummaryItem(title) {
+    return html`
+        <div float: right>
+        <div class="element-titlebar">Summary $(title)</div>
+        <div class="element-content">Summary Content Goes Here</div>
+        </div>`
   }
 };
+
+// Callback to recognize when the Google Charts are loaded.
+function chartsLoaded() {
+  googleChartsLoaded = true;
+  console.log("Google Charts Loaded")
+}
 
 customElements.define('dw-dashboard', Dashboard);
