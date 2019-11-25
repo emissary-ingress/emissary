a4bff9e2887c2db18cb2bad44003ee5c8de150f3
Luke Shumaker <lukeshu@datawire.io>
2019-11-25 12:55:57 -0500

    Move FilterSpec validation from cmd/…/filters/controller to apis/…

--- apis/getambassador.io/v1beta2/filter.go
+++ apis/getambassador.io/v1beta2/filter.go
@@ -1,5 +1,14 @@
 package v1
 
+import (
+	"fmt"
+	"sort"
+
+	"github.com/pkg/errors"
+
+	coreV1client "k8s.io/client-go/kubernetes/typed/core/v1"
+)
+
 type FilterSpec struct {
 	AmbassadorID AmbassadorID `json:"ambassador_id"`
 
@@ -8,4 +17,93 @@ type FilterSpec struct {
 	JWT      *FilterJWT      `json:",omitempty"`
 	External *FilterExternal `json:",omitempty"`
 	Internal *FilterInternal `json:",omitempty"`
+}
+
+func kindCount(isKind map[string]bool) uint {
+	var cnt uint
+	for _, is := range isKind {
+		if is {
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func kindNames(isKind map[string]bool) []string {
+	ret := make([]string, 0, len(isKind))
+	for kind := range isKind {
+		ret = append(ret, kind)
+	}
+	sort.Strings(ret)
+	return ret
+}
+
+type FilterInfo struct {
+	Spec interface{}
+	Desc string
+	Err  error
+}
+
+func (spec *FilterSpec) Validate(namespace string, secretsGetter coreV1client.SecretsGetter, haveRedis bool) FilterInfo {
+	var ret FilterInfo
+	if spec == nil {
+		ret.Err = errors.New("spec must be set")
+		return ret
+	}
+
+	isKind := map[string]bool{
+		"OAuth2":   spec.OAuth2 != nil,
+		"Plugin":   spec.Plugin != nil,
+		"JWT":      spec.JWT != nil,
+		"External": spec.External != nil,
+		"Internal": spec.Internal != nil,
+	}
+	if kindCount(isKind) != 1 {
+		ret.Err = errors.Errorf("must specify exactly 1 of: %v", kindNames(isKind))
+		return ret
+	}
+
+	switch {
+	case spec.OAuth2 != nil:
+		ret.Err = spec.OAuth2.Validate(namespace, secretsGetter)
+		ret.Spec = *spec.OAuth2
+		if ret.Err == nil && !haveRedis {
+			ret.Err = errors.Errorf("filter disabled because Redis does not seem to be available")
+		}
+		if ret.Err == nil {
+			switch spec.OAuth2.GrantType {
+			case GrantType_AuthorizationCode:
+				ret.Desc = fmt.Sprintf("oauth2_domain=%s, oauth2_client_id=%s", spec.OAuth2.Domain(), spec.OAuth2.ClientID)
+			case GrantType_ClientCredentials:
+				ret.Desc = fmt.Sprintf("oauth2_client_credentials=%s", spec.OAuth2.AuthorizationURL)
+			default:
+				panic("should not happen")
+			}
+		}
+	case spec.Plugin != nil:
+		ret.Err = spec.Plugin.Validate()
+		ret.Spec = *spec.Plugin
+		if ret.Err == nil {
+			ret.Desc = fmt.Sprintf("plugin=%s", spec.Plugin.Name)
+		}
+	case spec.JWT != nil:
+		ret.Err = spec.JWT.Validate()
+		ret.Spec = *spec.JWT
+		if ret.Err == nil {
+			ret.Desc = "jwt"
+		}
+	case spec.External != nil:
+		ret.Err = spec.External.Validate()
+		ret.Spec = *spec.External
+		if ret.Err == nil {
+			ret.Desc = fmt.Sprintf("external=%s", spec.External.AuthService)
+		}
+	case spec.Internal != nil:
+		ret.Spec = *spec.Internal
+		ret.Desc = "internal"
+	default:
+		panic("should not happen")
+	}
+
+	return ret
 }
--- cmd/amb-sidecar/filters/controller/controller.go
+++ cmd/amb-sidecar/filters/controller/controller.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"context"
 	"fmt"
-	"sort"
 	"strings"
 	"sync/atomic"
 
@@ -43,6 +42,8 @@ func (c *Controller) LoadRules() []crd.Rule {
 	return typed
 }
 
+type FilterInfo = crd.FilterInfo
+
 func (c *Controller) storeFilters(filters map[string]FilterInfo) {
 	c.filters.Store(filters)
 }
@@ -57,31 +58,6 @@ func (c *Controller) LoadFilters() map[string]FilterInfo {
 		return nil
 	}
 	return typed
-}
-
-func kindCount(isKind map[string]bool) uint {
-	var cnt uint
-	for _, is := range isKind {
-		if is {
-			cnt++
-		}
-	}
-	return cnt
-}
-
-func kindNames(isKind map[string]bool) []string {
-	ret := make([]string, 0, len(isKind))
-	for kind := range isKind {
-		ret = append(ret, kind)
-	}
-	sort.Strings(ret)
-	return ret
-}
-
-type FilterInfo struct {
-	Spec interface{}
-	Desc string
-	Err  error
 }
 
 type NotThisAmbassadorError struct {
@@ -107,70 +83,12 @@ func processFilterSpec(
 	if err := mapstructure.Convert(filter.Spec(), &spec); err != nil {
 		return FilterInfo{Err: errors.Wrap(err, "malformed filter resource spec")}
 	}
-	ret := FilterInfo{
-		Spec: spec,
-	}
 	if !spec.AmbassadorID.Matches(cfg.AmbassadorID) {
 		return FilterInfo{Err: &NotThisAmbassadorError{
 			Message: fmt.Sprintf("AMBASSADOR_ID: .spec.ambassador_id=%v not contains AMBASSADOR_ID=%q", spec.AmbassadorID, cfg.AmbassadorID),
 		}}
 	}
-
-	isKind := map[string]bool{
-		"OAuth2":   spec.OAuth2 != nil,
-		"Plugin":   spec.Plugin != nil,
-		"JWT":      spec.JWT != nil,
-		"External": spec.External != nil,
-		"Internal": spec.Internal != nil,
-	}
-	if kindCount(isKind) != 1 {
-		ret.Err = errors.Errorf("must specify exactly 1 of: %v", kindNames(isKind))
-		return ret
-	}
-
-	switch {
-	case spec.OAuth2 != nil:
-		ret.Err = spec.OAuth2.Validate(filter.Namespace(), coreClient)
-		ret.Spec = *spec.OAuth2
-		if ret.Err == nil && !haveRedis {
-			ret.Err = errors.Errorf("filter disabled because Redis does not seem to be available")
-		}
-		if ret.Err == nil {
-			switch spec.OAuth2.GrantType {
-			case crd.GrantType_AuthorizationCode:
-				ret.Desc = fmt.Sprintf("oauth2_domain=%s, oauth2_client_id=%s", spec.OAuth2.Domain(), spec.OAuth2.ClientID)
-			case crd.GrantType_ClientCredentials:
-				ret.Desc = fmt.Sprintf("oauth2_client_credentials=%s", spec.OAuth2.AuthorizationURL)
-			default:
-				panic("should not happen")
-			}
-		}
-	case spec.Plugin != nil:
-		ret.Err = spec.Plugin.Validate()
-		ret.Spec = *spec.Plugin
-		if ret.Err == nil {
-			ret.Desc = fmt.Sprintf("plugin=%s", spec.Plugin.Name)
-		}
-	case spec.JWT != nil:
-		ret.Err = spec.JWT.Validate()
-		ret.Spec = *spec.JWT
-		if ret.Err == nil {
-			ret.Desc = "jwt"
-		}
-	case spec.External != nil:
-		ret.Err = spec.External.Validate()
-		ret.Spec = *spec.External
-		if ret.Err == nil {
-			ret.Desc = fmt.Sprintf("external=%s", spec.External.AuthService)
-		}
-	case spec.Internal != nil:
-		ret.Spec = *spec.Internal
-		ret.Desc = "internal"
-	default:
-		panic("should not happen")
-	}
-
-	return ret
+	return spec.Validate(filter.Namespace(), coreClient, haveRedis)
 }
 
 // Watch monitor changes in k8s cluster and updates rules
