a2c2faa4646a85cb772e8e118d105ba764615e64
alex <alex@datawire.io>
2019-11-30 10:31:46 -0500

    Silencing external deps warnings

--- cmd/amb-sidecar/webui/bindata/rollup.config.js
+++ cmd/amb-sidecar/webui/bindata/rollup.config.js
@@ -9,5 +9,13 @@ export default [{
     terser({
       include: [/^.+\.js$/],
     })
+  ],
+  external: [ // Listing dependencies here will reduce the number of warnings
+    '/edge_stack/vendor/lit-element.min.js',
+    '/edge_stack/components/snapshot.js',
+    '/edge_stack/components/resources.js',
+    '/edge_stack/components/cookies.js',
+    '/edge_stack/components/context.js',
+    '/edge_stack/components/request-labels.js'
   ]
 }];
