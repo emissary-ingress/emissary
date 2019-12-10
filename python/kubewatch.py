kphelps/ambassador

Simple ridethrough if the LogService CRD isn't present
Signed-off-by: Alvaro Saurin <alvaro@datawire.io>

python/kubewatch.py
@@ -208,7 +208,13 @@ def main(debug):
                 [
                     'hosts.getambassador.io'
                 ]
-            )            
+            ),
+            (
+                '.ambassador_ignore_crds_4', 'LogService CRDs',
+                [
+                    'logservice.getambassador.io'
+                ]
+            )
         ]
 
         # Flynn would say "Ew.", but we need to patch this till https://github.com/kubernetes-client/python/issues/376

emissary-ingress/emissary

python/kubewatch.py
@@ -202,6 +202,12 @@ def main(debug):
                     'kubernetesendpointresolvers.getambassador.io',
                     'kubernetesserviceresolvers.getambassador.io'
                 ]
+            ),
+            (
+                '.ambassador_ignore_crds_4', 'LogService CRDs',
+                [
+                    'logservices.getambassador.io'
+                ]
             )
         ]
 