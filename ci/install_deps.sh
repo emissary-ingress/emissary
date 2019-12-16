159743148de7f71fa8817c0398c31c9aa965a73e
Noah Krause <krausenoah@gmail.com>
2019-12-16 14:31:50 -0500

    move kubectl install to script
    Signed-off-by: Noah Krause <krausenoah@gmail.com>

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -11,4 +11,8 @@ tar -xzf /tmp/helm.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
 rm -rf linux-amd64
 
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 
