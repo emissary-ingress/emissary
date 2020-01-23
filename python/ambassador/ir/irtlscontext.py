6dda6f1aa660c08e098c85515a23628903fe0e6a
Flynn <flynn@datawire.io>
2020-01-23 13:19:12 -0500

    Don't create the fallback context unless we have no termination contexts _and_ no Hosts (and we're running in Edge Stack)

--- python/ambassador/ir/irtlscontext.py
+++ python/ambassador/ir/irtlscontext.py
@@ -372,7 +372,7 @@ class TLSContextFactory:
         # are just ACResources; they need to be turned into IRTLSContexts.
         tls_contexts = aconf.get_config('tls_contexts')
 
-        found_termination_context = False
+        # found_termination_context = False
         if tls_contexts is not None:
             for config in tls_contexts.values():
                 ctx = IRTLSContext(ir, aconf, **config)
@@ -382,29 +382,38 @@ class TLSContextFactory:
                     ctx.sourced_by(config)
 
                     ir.save_tls_context(ctx)
+                    #
+                    # if ctx.get('hosts'):  # not None and not the empty list
+                    #     found_termination_context = True
 
-                    if ctx.get('hosts'):  # not None and not the empty list
-                        found_termination_context = True
-
-        if ir.edge_stack_allowed and not found_termination_context:
-            # Edge Stack always wants a termination context
-            ctx_name = "fallback-self-signed-context"
-            tls_name = "fallback-self-signed-cert"
-
-            new_ctx = dict(
-                rkey=f"{ctx_name}.99999",
-                name=ctx_name,
-                location="-internal-",
-                hosts=["*"],
-                secret=tls_name,
-                is_fallback=True
-            )
-
-            if not os.environ.get('AMBASSADOR_NO_TLS_REDIRECT', None):
-                new_ctx['redirect_cleartext_from'] = 8080
-
-            ctx = IRTLSContext(ir, aconf, **new_ctx)
-
-            assert ctx.is_active()
-            if ctx.resolve_secret(tls_name):
-                ir.save_tls_context(ctx)
+        # if ir.edge_stack_allowed:
+        #     # We're running Edge Stack. If we have no termination contexts and we
+        #     # have no Hosts...
+        #     host_count = len(ir.get_hosts() or [])
+        #
+        #     ir.logger.info(f"FTC {found_termination_context}, host_count {host_count}")
+        #
+        #     if (host_count == 0) and not found_termination_context:
+        #         ir.logger.info("Creating fallback context")
+        #         # ...then we know that this is an unconfigured installation, so let's set
+        #         # up the fallback TLSContext so we can redirect people to the UI.
+        #         ctx_name = "fallback-self-signed-context"
+        #         tls_name = "fallback-self-signed-cert"
+        #
+        #         new_ctx = dict(
+        #             rkey=f"{ctx_name}.99999",
+        #             name=ctx_name,
+        #             location="-internal-",
+        #             hosts=["*"],
+        #             secret=tls_name,
+        #             is_fallback=True
+        #         )
+        #
+        #         if not os.environ.get('AMBASSADOR_NO_TLS_REDIRECT', None):
+        #             new_ctx['redirect_cleartext_from'] = 8080
+        #
+        #         ctx = IRTLSContext(ir, aconf, **new_ctx)
+        #
+        #         assert ctx.is_active()
+        #         if ctx.resolve_secret(tls_name):
+        #             ir.save_tls_context(ctx)
