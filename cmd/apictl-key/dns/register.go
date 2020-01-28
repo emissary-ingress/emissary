598ccb6dfa3a5482475f503e52344e8db466148d
alex <alex@datawire.io>
2020-01-28 10:22:08 -0500

    A bit of refactoring into functions

--- cmd/apictl-key/dns/register.go
+++ cmd/apictl-key/dns/register.go
@@ -19,6 +19,11 @@ type dnsclient struct {
 	dnsRegistrationTLD string
 }
 
+type registration struct {
+	Email string
+	Ip    string
+}
+
 func init() {
 	// Make sure our generator is truly random
 	rand.Seed(time.Now().UnixNano())
@@ -34,21 +39,43 @@ func NewController(l *logrus.Logger, hostedZoneId string, dnsRegistrationTLD str
 
 func (c *dnsclient) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
-	var registration struct {
-		Email string
-		Ip    string
-	}
 
 	// Decode the registration request:
 	//   {"email":"alex@datawire.io","ip":"34.94.127.81"}
+	var registration registration
 	err := decoder.Decode(&registration)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// TODO: "ping-back" mechanism where we check the ambassador installation is publicly accessible.
+	// Check the IP is public and is serving an AES install
+	err = c.verifyIPIsReady(registration.Ip)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusPreconditionFailed)
+		return
+	}
 
+	// Do register a DNS entry
+	domainName, err := c.doRegister(registration)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// TODO: Keep track of this DNS and IP registration: save this info in a database somewhere
+
+	// If all is good, return 200OK and the generated domain name in plain text.
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(domainName))
+}
+
+func (c *dnsclient) verifyIPIsReady(ip string) error {
+	// TODO: "ping-back" mechanism where we check the ambassador installation is publicly accessible.
+	return nil
+}
+
+func (c *dnsclient) doRegister(registration registration) (string, error) {
 	// Generate a random domain name
 	domainName := fmt.Sprintf("%s%s", generateRandomName(), c.dnsRegistrationTLD)
 
@@ -56,8 +83,7 @@ func (c *dnsclient) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	sess, err := session.NewSession()
 	if err != nil {
 		c.l.WithError(err).Error("error creating aws route53 session")
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return "", nil
 	}
 	r53 := route53.New(sess)
 
@@ -88,14 +114,9 @@ func (c *dnsclient) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	result, err := r53.ChangeResourceRecordSets(input)
 	if err != nil {
 		c.l.WithError(err).Error("error creating dns entry")
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return "", err
 	}
 	c.l.Infof(result.String())
 
-	// TODO: Keep track of this DNS and IP registration: save this info in a database somewhere
-
-	// If all is good, return 200OK and the generated domain name in plain text.
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(domainName))
+	return domainName, nil
 }
