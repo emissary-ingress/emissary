8798070779344937d184c66f4536f89c3384fea0
Flynn <flynn@datawire.io>
2020-01-30 12:34:20 -0500

    Comments requested by Abhay.

--- python/ambassador_cli/grab_snapshots.py
+++ python/ambassador_cli/grab_snapshots.py
@@ -1,5 +1,25 @@
 #!python
 
+# Copyright 2019-2020 Datawire. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+########
+# This is a debugging tool that can grab snapshots and Envoy configs from
+# Ambassador's configuration directory, sanitize secrets out of the snapshots,
+# and hand back a compressed tarfile that the user can hand back to Datawire.
+########
+
 import sys
 
 import functools
