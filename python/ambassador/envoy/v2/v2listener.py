3308ddee0db41f0db4a2d9a18a50a673cb96f800
Luke Shumaker <lukeshu@datawire.io>
2020-02-11 17:33:52 -0500

    v2listener.py: generate(): Track all irlisteners_by_port instead of just first_irlistener_by_port
    For simplicity.

--- python/ambassador/envoy/v2/v2listener.py
+++ python/ambassador/envoy/v2/v2listener.py
@@ -1222,16 +1222,14 @@ class V2Listener(dict):
         # with that.
 
         v2listener_by_port = V2ListenerCollection(config)
+        irlisteners_by_port: Dict[int, List[IRListener]] = {}
 
         # Also, in Edge Stack, the magic extremely-low-precedence / Mapping is always routed,
         # rather than being redirected. If a user doesn't want this behavior, they can override
         # the Mapping.
 
-        first_irlistener_by_port: Dict[int, IRListener] = {}
-
         for irlistener in config.ir.listeners:
-            if irlistener.service_port not in first_irlistener_by_port:
-                first_irlistener_by_port[irlistener.service_port] = irlistener
+            irlisteners_by_port[irlistener.service_port] = irlisteners_by_port.get(irlistener.service_port, []) + [ irlistener ]
 
             if log_debug:
                 logger.debug(f"V2Listeners: working on {irlistener.pretty()}")
@@ -1254,8 +1252,7 @@ class V2Listener(dict):
                 # Make sure we have a v2listener on the right port for this.
                 v2listener = v2listener_by_port.get(irlistener.insecure_addl_port, irlistener.use_proxy_proto)
 
-                if irlistener.insecure_addl_port not in first_irlistener_by_port:
-                    first_irlistener_by_port[irlistener.insecure_addl_port] = irlistener
+                irlisteners_by_port[irlistener.insecure_addl_port] = irlisteners_by_port.get(irlistener.insecure_addl_port, []) + [ irlistener ]
 
                 # Do we already have a VHost for this hostname?
                 if vhostname not in v2listener.vhosts:
@@ -1289,7 +1286,7 @@ class V2Listener(dict):
             if 8080 not in v2listener_by_port:
                 # Check for a v2listener on the main service port to see if the proxy proto
                 # is enabled.
-                main_listener = first_irlistener_by_port.get(config.ir.ambassador_module.service_port, None)
+                main_listener: Optional[IRListener] = irlisteners_by_port.get(config.ir.ambassador_module.service_port, typecast(List[IRListener], [None]))[0]
                 use_proxy_proto = main_listener.use_proxy_proto if main_listener else False
 
                 # Force a v2listener on 8080 with a VHost for '*' that rejects everything. The ACME
