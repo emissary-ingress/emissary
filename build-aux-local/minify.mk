bc0420310d8ceb0f14ff05e2b82d04e3a67d4183
Rafael Schloming <rhs@datawire.io>
2020-02-18 19:30:12 -0500

    actually install vendors files

--- build-aux-local/minify.mk
+++ build-aux-local/minify.mk
@@ -5,20 +5,17 @@ INSTALL_ROOT=/ambassador
 
 ROLLUP_CONFIG_JS=$(SRC_ROOT)/cmd/amb-sidecar/webui/bindata/rollup.config.js
 
-SRC_FILES:=$(filter-out %/rollup.config.js,$(shell find $(SRC_ROOT)/cmd/amb-sidecar/webui/bindata/ -type f -not -path $(SRC_ROOT)/cmd/amb-sidecar/webui/bindata/edge_stack/vendor/\*))
+SRC_FILES:=$(filter-out %/rollup.config.js,$(shell find $(SRC_ROOT)/cmd/amb-sidecar/webui/bindata/ -type f))
+INSTALLED=$(SRC_FILES:$(SRC_ROOT)/cmd/amb-sidecar/%=$(INSTALL_ROOT)/%)
 
-SRC_FILES_JS:=$(filter %.js,$(SRC_FILES))
-SRC_FILES_OTHER:=$(filter-out $(SRC_FILES_JS),$(SRC_FILES))
-
-INSTALLED_JS=$(SRC_FILES_JS:$(SRC_ROOT)/cmd/amb-sidecar/%=$(INSTALL_ROOT)/%)
-INSTALLED_OTHER=$(SRC_FILES_OTHER:$(SRC_ROOT)/cmd/amb-sidecar/%=$(INSTALL_ROOT)/%)
-INSTALLED=$(INSTALLED_JS) $(INSTALLED_OTHER)
+$(INSTALL_ROOT)/webui/bindata/edge_stack/vendor/%: $(SRC_ROOT)/cmd/amb-sidecar/webui/bindata/edge_stack/vendor/%
+	install -m644 -D $< $@
 
 $(INSTALL_ROOT)/%.js: $(SRC_ROOT)/cmd/amb-sidecar/%.js $(ROLLUP_CONFIG_JS)
 	NODE_PATH="$$(npm root -g)" rollup -c $(ROLLUP_CONFIG_JS) -i $< -o $@
 
 $(INSTALL_ROOT)/%: $(SRC_ROOT)/cmd/amb-sidecar/%
-	install -D $< $@
+	install -m644 -D $< $@
 
 all: $(INSTALLED)
 	rm -f -- $(filter-out $(INSTALLED),$(shell find $(INSTALL_ROOT)/webui/bindata -type f))
