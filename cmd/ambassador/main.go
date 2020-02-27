3cee783dac0d19e32fff6a7a1d37d8611f37eb18
Abhay Saxena <ark3@datawire.io>
2020-02-27 16:46:21 -0500

    Combine all go binaries into a single ambassador binary
    - Modify the original programs so they can be embedded this way
    - Modify build stuff so things still work as they did

---
+++ cmd/ambassador/main.go
@@ -0,0 +1,39 @@
+// Ambassador combines the various Golang binaries used in the Ambassador
+// container, dispatching on os.Args[0] like BusyBox.
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/datawire/ambassador/cmd/ambex"
+	"github.com/datawire/ambassador/cmd/kubestatus"
+	"github.com/datawire/ambassador/cmd/watt"
+)
+
+func main() {
+	name := filepath.Base(os.Args[0])
+	if name == "ambassador" && len(os.Args) > 1 {
+		name = os.Args[1]
+		os.Args = os.Args[1:]
+	}
+	switch name {
+	case "ambex":
+		ambex.Main()
+	case "watt":
+		watt.Main()
+	case "kubestatus":
+		kubestatus.Main()
+	default:
+		fmt.Println("The Ambassador main program is a multi-call binary that combines various")
+		fmt.Println("support programs into one executable.")
+		fmt.Println()
+		fmt.Println("Usage: ambassador [program] [arguments]...")
+		fmt.Println("   or: [program] [arguments]...")
+		fmt.Println()
+		fmt.Println("Available programs: ambex kubestatus watt")
+		fmt.Println()
+		fmt.Printf("Unknown name %q\n", name)
+	}
+}
--- post-compile.sh
+++ post-compile.sh
@@ -1,8 +1,15 @@
 #!/hint/bash
 set -e
 
-sudo install -D -t /opt/ambassador/bin/ \
-     /buildroot/bin/ambex \
-     /buildroot/bin/watt \
-     /buildroot/bin/kubestatus
+# Create symlinks to the multi-call binary
+ln -sf /buildroot/bin/ambassador /buildroot/bin/ambex
+ln -sf /buildroot/bin/ambassador /buildroot/bin/kubestatus
+ln -sf /buildroot/bin/ambassador /buildroot/bin/watt
+
+# Stuff in /opt/ambassador/bin in the builder winds up in /usr/local/bin in the
+# production image.
+sudo install -D -t /opt/ambassador/bin/ /buildroot/bin/ambassador
+sudo ln -sf /opt/ambassador/bin/ambassador /opt/ambassador/bin/ambex
+sudo ln -sf /opt/ambassador/bin/ambassador /opt/ambassador/bin/kubestatus
+sudo ln -sf /opt/ambassador/bin/ambassador /opt/ambassador/bin/watt
 sudo install /buildroot/bin/capabilities_wrapper /opt/ambassador/bin/wrapper
