7ecc8152d7dd0f22d46a7de1cfdadb4d45becb24
Noah Krause <nkrause@datawire.io>
2020-03-06 12:42:04 -0500

    make it a one liner
    Signed-off-by: Noah Krause <nkrause@datawire.io>

--- ci/push_chart.sh
+++ ci/push_chart.sh
@@ -12,10 +12,7 @@ source "$CURR_DIR/common.sh"
 
 # Check for update to version of Chart.yaml
 
-version_changed=$(git diff $TRAVIS_COMMIT_RANGE Chart.yaml | grep +version)
-
-if [[ -z $version_changed ]]
-then
+if ! git diff $TRAVIS_COMMIT_RANGE Chart.yaml | grep -q +version; then
   info "The version was not changed in Chart.yaml: the chart will not be pushed..."
   exit 0
 fi
