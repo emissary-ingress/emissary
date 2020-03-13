5574c3e30c67b0b02ff0fe450e1e2d1e75758d7d
Flynn <flynn@datawire.io>
2020-03-13 01:35:14 -0400

    Fix syntax errors in oauth2_test.go (and fine, let the !*@&#!*& Go formatter take away some whitespace, grrr).

--- tests/cluster/go-test/filter-oauth2/oauth2_test.go
+++ tests/cluster/go-test/filter-oauth2/oauth2_test.go
@@ -130,13 +130,13 @@ func TestHeaderCredentials(t *testing.T) {
 	u := urlMust(url.Parse("https://ambassador.ambassador.svc.cluster.local/azure-header-credentials/httpbin/headers"))
 
 	// Lifted from idp_azure.js
-	username := "testuser@aprotesting.onmicrosoft.com",
-	password := "6qak5GgDMgd/6iNFfuw5jA==",
+	username := "testuser@aprotesting.onmicrosoft.com"
+	password := "6qak5GgDMgd/6iNFfuw5jA=="
 
 	testcases := map[string]testcase{
 		"empty":   {URL: u, Header: nil, ExpectedStatus: http.StatusForbidden},
-		"valid":   {URL: u, Header: http.Header{"X-Ambassador-Username": { username }, "X-Ambassador-Password": { password }}, ExpectedStatus: http.StatusOK},
-		"invalid": {URL: u, Header: http.Header{"X-Ambassador-Username": { username }, "X-Ambassador-Password": { "bogus" }}, ExpectedStatus: http.StatusForbidden},
+		"valid":   {URL: u, Header: http.Header{"X-Ambassador-Username": {username}, "X-Ambassador-Password": {password}}, ExpectedStatus: http.StatusOK},
+		"invalid": {URL: u, Header: http.Header{"X-Ambassador-Username": {username}, "X-Ambassador-Password": {"bogus"}}, ExpectedStatus: http.StatusForbidden},
 	}
 
 	for tcName, tc := range testcases {
