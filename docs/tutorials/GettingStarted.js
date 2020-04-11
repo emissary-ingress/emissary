83b182451c2d76389761ca5ff77352ecbbdc2ea8
Cindy Mullins <cindymullins@datawire.io>
2020-04-11 14:42:58 -0700

    fix Windows detection

--- docs/tutorials/GettingStarted.js
+++ docs/tutorials/GettingStarted.js
@@ -7,7 +7,7 @@ class GettingStarted extends Component {
     var os = "other";
     if (/Mac(intosh|Intel|PPC|68K)/.test(window.navigator.platform)) {
       os = 'mac';
-    } else if (/Win(dows|36|64|CE)/.test(window.navigator.platform)) {
+    } else if (/Win(dows|32|64|CE)/.test(window.navigator.platform)) {
       os = 'windows';
     } else if (/Linux/.test(window.navigator.platform)) {
       os = 'linux';
