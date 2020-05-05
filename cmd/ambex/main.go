e965245e70036782a5b98f743ce7ed2290183c2f
Shahriar Rostami <srdsecond@gmail.com>
2020-05-05 13:31:53 -0400

    Make changes to be compatible with Envoy 1.14.1.
    Ambassador OSS and Ambassador Edge Stack gold files.

--- cmd/ambex/main.go
+++ cmd/ambex/main.go
@@ -8,7 +8,7 @@ package ambex
  * go-control-plane, several different classes manage this stuff:
  *
  * - The root of the world is a SnapshotCache.
- *   - import github.com/datawire/ambassador/pkg/envoy-control-plane/cache, then refer
+ *   - import github.com/datawire/ambassador/pkg/envoy-control-plane/cache/v2, then refer
  *     to cache.SnapshotCache.
  *   - A collection of internally consistent configuration objects is a
  *     Snapshot (cache.Snapshot).
@@ -61,8 +61,9 @@ import (
 	"github.com/gogo/protobuf/types"
 
 	// envoy control plane
-	"github.com/datawire/ambassador/pkg/envoy-control-plane/cache"
-	"github.com/datawire/ambassador/pkg/envoy-control-plane/server"
+	ctypes "github.com/datawire/ambassador/pkg/envoy-control-plane/cache/types"
+	"github.com/datawire/ambassador/pkg/envoy-control-plane/cache/v2"
+	"github.com/datawire/ambassador/pkg/envoy-control-plane/server/v2"
 
 	// envoy protobuf -- Be sure to import the package of any types that the Python
 	// emits a "@type" of in the generated config, even if that package is otherwise
@@ -226,11 +227,11 @@ func Clone(src proto.Message) proto.Message {
 }
 
 func update(config cache.SnapshotCache, generation *int, dirs []string) {
-	clusters := []cache.Resource{}  // v2.Cluster
-	endpoints := []cache.Resource{} // v2.ClusterLoadAssignment
-	routes := []cache.Resource{}    // v2.RouteConfiguration
-	listeners := []cache.Resource{} // v2.Listener
-	runtimes := []cache.Resource{}  // discovery.Runtime
+	clusters := []ctypes.Resource{}  // v2.Cluster
+	endpoints := []ctypes.Resource{} // v2.ClusterLoadAssignment
+	routes := []ctypes.Resource{}    // v2.RouteConfiguration
+	listeners := []ctypes.Resource{} // v2.Listener
+	runtimes := []ctypes.Resource{}  // discovery.Runtime
 
 	var filenames []string
 
@@ -254,7 +255,7 @@ func update(config cache.SnapshotCache, generation *int, dirs []string) {
 			log.Warnf("%s: %v", name, e)
 			continue
 		}
-		var dst *[]cache.Resource
+		var dst *[]ctypes.Resource
 		switch m.(type) {
 		case *v2.Cluster:
 			dst = &clusters
@@ -270,17 +271,17 @@ func update(config cache.SnapshotCache, generation *int, dirs []string) {
 			bs := m.(*bootstrap.Bootstrap)
 			sr := bs.StaticResources
 			for _, lst := range sr.Listeners {
-				listeners = append(listeners, Clone(lst).(cache.Resource))
+				listeners = append(listeners, Clone(lst).(ctypes.Resource))
 			}
 			for _, cls := range sr.Clusters {
-				clusters = append(clusters, Clone(cls).(cache.Resource))
+				clusters = append(clusters, Clone(cls).(ctypes.Resource))
 			}
 			continue
 		default:
 			log.Warnf("Unrecognized resource %s: %v", name, e)
 			continue
 		}
-		*dst = append(*dst, m.(cache.Resource))
+		*dst = append(*dst, m.(ctypes.Resource))
 	}
 
 	version := fmt.Sprintf("v%d", *generation)
