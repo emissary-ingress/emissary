f564078f538d9e3a4bde1e8b7702a7e094b066cf
Noah Fontes <noah.fontes@puppet.com>
2020-05-06 12:48:31 -0700

    Split fetcher/processor into its own module

---
+++ python/ambassador/fetch/ambassador.py
@@ -0,0 +1,36 @@
+from typing import FrozenSet
+
+import itertools
+
+from .k8sobject import KubernetesGVK, KubernetesObject
+from .k8sprocessor import ManagedKubernetesProcessor
+from .resource import NormalizedResource
+
+
+class AmbassadorProcessor (ManagedKubernetesProcessor):
+    """
+    A Kubernetes object processor that emits direct IR from an Ambassador CRD.
+    """
+
+    def kinds(self) -> FrozenSet[KubernetesGVK]:
+        kinds = [
+            'AuthService',
+            'ConsulResolver',
+            'Host',
+            'KubernetesEndpointResolver',
+            'KubernetesServiceResolver',
+            'LogService',
+            'Mapping',
+            'Module',
+            'RateLimitService',
+            'TCPMapping',
+            'TLSContext',
+            'TracingService',
+        ]
+
+        return frozenset([
+            KubernetesGVK.for_ambassador(kind, version=version) for (kind, version) in itertools.product(kinds, ['v1', 'v2'])
+        ])
+
+    def _process(self, obj: KubernetesObject) -> None:
+        self.manager.emit(NormalizedResource.from_kubernetes_object(obj))
