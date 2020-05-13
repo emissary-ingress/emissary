72a2c3c557f5c988d03199d3e35fdf00cfa636e1
Kenn Hussey <kennhussey@datawire.io>
2020-05-13 13:45:43 -0400

    Pointing latest docs at master branch.

--- docs/js/doc-page.js
+++ docs/js/doc-page.js
@@ -52,7 +52,7 @@ export default ({ data, location }) => {
             </div>
           </main>
         </div>
-        <DocFooter page={page} branch="release/v1.4" />
+        <DocFooter page={page} branch="master" />
       </Layout>
     </React.Fragment>
   );
