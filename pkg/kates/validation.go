5002eeb0e0f785ebd169f18713b444fe2034f590
Rafael Schloming <rhs@datawire.io>
2020-06-30 16:50:58 -0400

    (from AES) added docs to validation.go

--- pkg/kates/validation.go
+++ pkg/kates/validation.go
@@ -13,12 +13,18 @@ import (
 	"github.com/go-openapi/validate"
 )
 
+// A Validator may be used in concert with a Client to perform
+// validate of freeform jsonish data structures as kubernetes CRDs.
 type Validator struct {
 	client     *Client
 	mutex      sync.Mutex
 	validators map[string]*validate.SchemaValidator
 }
 
+// The NewValidator constructor returns a *Validator that uses the
+// provided *Client to fetch CustomResourceDefinitions from kubernetes
+// on demand as needed to validate data passed to the Validator.Validate()
+// method.
 func NewValidator(client *Client) *Validator {
 	return &Validator{client: client, validators: make(map[string]*validate.SchemaValidator)}
 }
@@ -62,6 +68,21 @@ func (v *Validator) getValidator(ctx context.Context, crd string) (*validate.Sch
 	return validator, nil
 }
 
+// The Validate method validates the supplied jsonish object as a
+// kubernetes CRD instance.
+//
+// If the supplied object is *not* a CRD instance but instead a
+// regular kubernetes instance, the Validate method will assume that
+// the supplied object is valid.
+//
+// If the supplied object is not a valid kubernetes resource at all,
+// the Validate method will return an error.
+//
+// Typically the Validate method will perform only local operations,
+// however the first time an instance of a given Kind is supplied, the
+// Validator needs to query the cluster to figure out if it is a CRD
+// and if so to fetch the schema needed to perform validation. All
+// subsequent Validate() calls for that Kind will be local.
 func (v *Validator) Validate(ctx context.Context, resource interface{}) error {
 	var tm TypeMeta
 	err := convert(resource, &tm)
