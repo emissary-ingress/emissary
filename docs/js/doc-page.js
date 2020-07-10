be00310dc843265905063ff084e093ff8ddb8686
alex <alex@datawire.io>
2020-07-10 09:30:06 -0400

    (from AES) Fixed doc-page.js syntax error

--- docs/js/doc-page.js
+++ docs/js/doc-page.js
@@ -91,7 +91,7 @@ export default ({ data, location }) => {
             </div>
           </main>
         </div>
-        <DocFooter page={page} branch="release/v1.6 />
+        <DocFooter page={page} branch="release/v1.6" />
       </Layout>
     </React.Fragment>
   );
