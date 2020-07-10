dcb76b8ecfeff2ef6d6a01aea99f47e61c337c4a
Rafael Schloming <rhs@datawire.io>
2020-07-10 16:53:23 -0400

    (from AES) Added accumulator docs

--- pkg/kates/accumulator.go
+++ pkg/kates/accumulator.go
@@ -13,6 +13,44 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 )
 
+// The Accumulator struct is used to efficiently maintain an in-memory copy of kubernetes resources
+// present in a cluster in a form that is easy for business logic to process. It functions as a
+// bridge between delta-based kubernetes watches on individual Kinds and the complete/consistent set
+// of objects on which business logic needs to operate. In that sense it accumulates both multiple
+// kinds of kubernetes resources into a single snapshot, as well as accumulating deltas on
+// individual objects into relevant sets of objects.
+//
+// The Goals/Requirements below are based heavily on the needs of Ambassador as they have evolved
+// over the years. A lot of this comes down to the fact that unlike the exemplary
+// deployment/replicaset controller examples which typically operate on a single resource and render
+// it into another (deployment -> N replicasets, replicaset -> N pods), Ambassador's controller
+// logic has some additional requirements:
+//
+//  1. Complete knowledge of resources in a cluster. Because many thousands of Mappings are
+//     ultimately assembled into a single envoy configuration responsible for ingress into the
+//     cluster, the consequences of producing an envoy configuration when you e.g. know about only
+//     half of those Mappings is catastrophic (you are black-holing half your traffic).
+//
+//  2. Complete knowledge of multiple resources. Instead of having one self contained input like a
+//     deployment or a replicaset, Ambassador's business logic has many inputs, and the consequence
+//     of producing an envoy without knowledge of *all* of those inputs is equally catastrophic,
+//     e.g. it's no use knowing about all the Mappings if you don't know about any of the Hosts yet.
+//
+// Goals/Requirements:
+//
+//  1. Bootstrap of a single Kind: the Accumulator will ensure that all pre-existing resources of
+//     that Kind have been loaded into memory prior to triggering any notifications. This guarantees
+//     we will never trigger business logic on an outdated view of the cluster (e.g. when 500 out of
+//     100 Mappings have been loaded) and makes it safe for the business logic to assume complete
+//     knowledge of the cluster.
+//
+//  2. When multiple Kinds are needed by a controller, the Accumulator will not notify the
+//     controller until all the Kinds have been fully bootstrapped.
+//
+//  3. Graceful load shedding: When the rate of change of resources is very fast, the API and
+//     implementation are structured so that individual object deltas get coalesced into a single
+//     snapshot update. This prevents excessively triggering business logic to process an entire
+//     snapshot for each individual object change that occurs.
 type Accumulator struct {
 	client  *Client
 	fields  map[string]*field
