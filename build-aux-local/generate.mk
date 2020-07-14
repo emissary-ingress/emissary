f992fe9068789437cef1e103284c6d94cfc7cf66
Luke Shumaker <lukeshu@datawire.io>
2020-07-14 14:12:02 -0600

    (from AES) fix-crds: Fuss with the labels and creationTimestamp, to match the Helm chart

--- build-aux-local/generate.mk
+++ build-aux-local/generate.mk
@@ -361,15 +361,15 @@ _generate_controller_gen: $(tools/controller-gen) $(tools/fix-crds) update-yaml-
 	  $(foreach varname,$(filter controller-gen/options/%,$(.VARIABLES)), $(patsubst controller-gen/options/%,%,$(varname))$(if $(strip $($(varname))),:$(call joinlist,$(comma),$($(varname)))) ) \
 	  $(foreach varname,$(filter controller-gen/output/%,$(.VARIABLES)), $(call joinlist,:,output $(patsubst controller-gen/output/%,%,$(varname)) $($(varname))) ) \
 	  paths="./pkg/api/getambassador.io/..."
-	@PS4=; set -ex; for file in $(crds_yaml_dir)/*.yaml; do $(tools/fix-crds) '' 1.11 "$$file" > "$$file.tmp"; mv "$$file.tmp" "$$file"; done
+	@PS4=; set -ex; for file in $(crds_yaml_dir)/getambassador.io_*.yaml; do $(tools/fix-crds) helm 1.11 "$$file" > "$$file.tmp"; mv "$$file.tmp" "$$file"; done
 .PHONY: _generate_controller_gen
 
 $(OSS_HOME)/docs/yaml/ambassador/ambassador-crds.yaml: _generate_controller_gen $(tools/fix-crds) update-yaml-preflight
 	@printf '  $(CYN)$@$(END)\n'
-	$(tools/fix-crds) '' 1.11 $(sort $(wildcard $(crds_yaml_dir)/*.yaml)) > $@
+	$(tools/fix-crds) oss 1.11 $(sort $(wildcard $(crds_yaml_dir)/*.yaml)) > $@
 $(OSS_HOME)/python/tests/manifests/crds.yaml: $(OSS_HOME)/docs/yaml/ambassador/ambassador-crds.yaml $(tools/fix-crds) update-yaml-preflight
 	@printf '  $(CYN)$@$(END)\n'
-	$(tools/fix-crds) '' 1.10 $< > $@
+	$(tools/fix-crds) oss 1.10 $< > $@
 $(OSS_HOME)/docs/yaml/ambassador/%.yaml: $(OSS_HOME)/docs/yaml/ambassador/%.yaml.m4 $(OSS_HOME)/docs/yaml/ambassador/ambassador-crds.yaml update-yaml-preflight
 	@printf '  $(CYN)$@$(END)\n'
 	cd $(@D) && m4 < $(<F) > $(@F)
