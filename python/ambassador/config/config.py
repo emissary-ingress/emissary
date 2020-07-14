57b18b8647b4ecc952da9dea9ae2387b567b6e92
Luke Shumaker <lukeshu@datawire.io>
2020-07-14 14:24:10 -0600

    (from AES) Improve the MyPy annotations around kubestatus

--- python/ambassador/config/config.py
+++ python/ambassador/config/config.py
@@ -142,7 +142,7 @@ class Config:
         self.schema_dir_path = schema_dir_path
 
         self.logger.debug("SCHEMA DIR    %s" % os.path.abspath(self.schema_dir_path))
-        self.k8s_status_updates: Dict[str, Tuple] = {}
+        self.k8s_status_updates: Dict[str, Tuple[str, str, Optional[Dict[str, Any]]]] = {}  # Tuple is (name, namespace, status_json)
         self.k8s_ingresses: Dict[str, Any] = {}
         self.k8s_ingress_classes: Dict[str, Any] = {}
         self.pod_labels: Dict[str, str] = {}
