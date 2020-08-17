8c6695117a3543a49f192b763cde0d80ee992f40
Luke Shumaker <lukeshu@datawire.io>
2020-08-17 14:20:36 -0600

    (from AES) pkg/supervisor: Process.DoClean: Fix data-race

--- pkg/supervisor/process.go
+++ pkg/supervisor/process.go
@@ -120,10 +120,11 @@ func (p *Process) Do(fn func() error) (err error) {
 
 // DoClean is the same as Process.Do() but executes the supplied clean
 // function on abort.
-func (p *Process) DoClean(fn, clean func() error) (err error) {
+func (p *Process) DoClean(fn, clean func() error) error {
 	sup := p.Supervisor()
 	done := make(chan struct{})
 	go func() {
+		var err error
 		defer func() {
 			if r := recover(); r != nil {
 				stack := string(debug.Stack())
@@ -132,6 +133,10 @@ func (p *Process) DoClean(fn, clean func() error) (err error) {
 				sup.errors = append(sup.errors, err)
 				sup.wantsShutdown = true
 				sup.mutex.Unlock()
+			}
+			select {
+			case done <- err:
+			default: // don't block if p.Shutdown() caused us to return early
 			}
 			close(done)
 		}()
@@ -142,7 +147,7 @@ func (p *Process) DoClean(fn, clean func() error) (err error) {
 	select {
 	case <-p.Shutdown():
 		return clean()
-	case <-done:
-		return
+	case err := <-done:
+		return err
 	}
 }
