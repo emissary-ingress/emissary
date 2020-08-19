790dfb536131920ce089c1394bc4620eead925f3
Flynn <flynn@datawire.io>
2020-08-19 10:44:24 -0400

    (from AES) Our updated gunicorn doesn't include iteritems, because Python 3 items() is equivalent.

--- python/ambassador_diag/diagd.py
+++ python/ambassador_diag/diagd.py
@@ -45,7 +45,6 @@ from clize import Parameter
 from flask import Flask, render_template, send_from_directory, request, jsonify, Response
 from flask import json as flask_json
 import gunicorn.app.base
-from gunicorn.six import iteritems
 
 from ambassador import Config, IR, EnvoyConfig, Diagnostics, Scout, Version
 from ambassador.utils import SystemInfo, Timer, PeriodicTrigger, SavedSecret, load_url_contents
@@ -1706,9 +1705,10 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
         self.application.logger.info(f'Ambassador {__version__} booted')
 
     def load_config(self):
-        config = dict([(key, value) for key, value in iteritems(self.options)
-                       if key in self.cfg.settings and value is not None])
-        for key, value in iteritems(config):
+        config = dict([ (key, value) for key, value in self.options.items()
+                        if key in self.cfg.settings and value is not None ])
+
+        for key, value in config.items():
             self.cfg.set(key.lower(), value)
 
     def load(self):
