a2daa8969d044aea89645993eb5677bb34175a07
Rafael Schloming <rhs@datawire.io>
2020-08-20 14:51:11 -0400

    (from AES) [entrypoint.sh] wire golang entrypoint into entrypoint.sh (when AMBASSADOR_FAST_RECONFIGURE is set)

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
+if [ -n "${AMBASSADOR_FAST_RECONFIGURE}" ]; then
+  exec ambassador entrypoint
+fi
+
 ENTRYPOINT_DEBUG=
 
 log () {
