9f0ac5e915fdaf40a2073034a3f50afea00e8e7d
Luke Shumaker <lukeshu@datawire.io>
2020-09-03 11:50:02 -0600

    (from AES) Invent a composable hard/soft shutdown mechanism, called dcontext

---
+++ pkg/dcontext/hardsoft.go
@@ -0,0 +1,49 @@
+// Package dcontext provides tools for dealing with separate hard/soft
+// cancellation of Contexts.
+package dcontext
+
+import (
+	"context"
+	"time"
+)
+
+type parentHardContextKey struct{}
+
+// WithSoftness returns a copy of the parent "hard" context with a way
+// of getting the parent's Done channel.  This allows the child to
+// have an earlier cancellation, triggering a "soft" shutdown, while
+// allowing hard/soft-aware functions to use HardContext() to get the
+// parent's Done channel, for a "hard" shutdown.
+func WithSoftness(hardCtx context.Context) (softCtx context.Context) {
+	return context.WithValue(hardCtx, parentHardContextKey{}, hardCtx)
+}
+
+type childHardContext struct {
+	hardCtx context.Context
+	softCtx context.Context
+}
+
+func (c childHardContext) Deadline() (deadline time.Time, ok bool) { return c.hardCtx.Deadline() }
+func (c childHardContext) Done() <-chan struct{}                   { return c.hardCtx.Done() }
+func (c childHardContext) Err() error                              { return c.hardCtx.Err() }
+func (c childHardContext) Value(key interface{}) interface{}       { return c.softCtx.Value(key) }
+
+// HardContext takes a child Context that is canceled sooner (a
+// "soft" cancellation) and returns a Context with the same values, but
+// with the cancellation of a parent Context that is canceled later
+// (a "hard" cancellation).
+//
+// Such a "soft" cancellation Context is created by
+// WithSoftness(hardCtx).  If the passed-in Context dones't have
+// softness (WithSoftness isn't somewhere in its ancestry), then it is
+// returned unmodified, because it is already hard.
+func HardContext(softCtx context.Context) context.Context {
+	parentHardCtx := softCtx.Value(parentHardContextKey{})
+	if parentHardCtx == nil {
+		return softCtx
+	}
+	return childHardContext{
+		hardCtx: parentHardCtx.(context.Context),
+		softCtx: softCtx,
+	}
+}
---
+++ pkg/dcontext/hardsoft_example_test.go
@@ -0,0 +1,53 @@
+package dcontext_test
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/datawire/ambassador/pkg/dcontext"
+)
+
+func ExampleWithSoftCancel(t *testing.T) {
+	ctx := context.Background()                       // context is hard by default
+	ctx, timeToDie := context.WithCancel(ctx)         // hard context => hard cancel
+	ctx = dcontext.WithSoftness(ctx)                  // make it soft
+	ctx, startShuttingDown := context.WithCancel(ctx) // soft context => soft cancel
+
+	go ListenAndServeHTTPWithContext(ctx, &http.Server{
+		// ...
+	})
+
+	// run for a while
+	time.Sleep(10 * time.Second)
+
+	// shut down
+	startShuttingDown()         // start draining connections
+	time.Sleep(5 * time.Second) // grace period
+	timeToDie()                 // if there are connections that are still in use after the 5-second grace period, kill them forcefully
+}
+
+// ListenAndServeHTTPWithContext runs server.ListenAndServe() on an
+// http.Server, but properly calls server.Shutdown when the Context is
+// canceled.
+//
+// It obeys hard/soft cancellation as implemented by
+// dcontext.WithSoftness; it calls server.Shutdown() when the soft
+// Context is canceled, and the hard Context being canceled causes the
+// .Shutdown() to hurry along and kill any live requests and return,
+// instead of waiting for them to be completed gracefully.
+func ListenAndServeHTTPWithContext(ctx context.Context, server *http.Server) error {
+	server.BaseContext = func(_ net.Listener) context.Context { return dcontext.HardContext(ctx) }
+	serverCh := make(chan error)
+	go func() {
+		serverCh <- server.ListenAndServe()
+	}()
+	select {
+	case err := <-serverCh:
+		return err
+	case <-ctx.Done():
+		return server.Shutdown(dcontext.HardContext(ctx))
+	}
+}
