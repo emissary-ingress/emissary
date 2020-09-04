a42c6518e718e2c9d67fcc7c30efb6b16f5fde8b
Luke Shumaker <lukeshu@datawire.io>
2020-09-04 14:19:55 -0600

    (from AES) dgroup: Be more configurable

--- pkg/dgroup/group.go
+++ pkg/dgroup/group.go
@@ -15,15 +15,15 @@ import (
 
 // Group is a wrapper around
 // github.com/datawire/ambassador/pkg/derrgroup.Group that:
-//  - handles SIGINT and SIGTERM
+//  - (optionally) handles SIGINT and SIGTERM
 //  - manages Context for you
 //  - adds hard/soft cancelation
-//  - does some minimal logging
+//  - (optionally) does some minimal logging
 type Group struct {
-	hardCtx       context.Context
-	softCtx       context.Context
-	loggerFactory func(name string) dlog.Logger
-	inner         *derrgroup.Group
+	cfg     GroupConfig
+	hardCtx context.Context
+	softCtx context.Context
+	inner   *derrgroup.Group
 }
 
 func logGoroutines(ctx context.Context, printf func(ctx context.Context, format string, args ...interface{}), list map[string]derrgroup.GoroutineState) {
@@ -42,54 +42,69 @@ func logGoroutines(ctx context.Context, printf func(ctx context.Context, format 
 	}
 }
 
-// NewGroup returns a new Group.
-func NewGroup(ctx context.Context, loggerFactory func(name string) dlog.Logger) *Group {
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+// GroupConfig is a readable way of setting the configuration options
+// for NewGroup.
+type GroupConfig struct {
+	EnableSignalHandling bool
+	WorkerLogger         func(name string) dlog.Logger
+}
 
+// NewGroup returns a new Group.
+func NewGroup(ctx context.Context, cfg GroupConfig) *Group {
 	hardCtx, hardCancel := context.WithCancel(ctx)
 	softCtx, softCancel := context.WithCancel(hardCtx)
 
-	ret := &Group{
-		hardCtx:       hardCtx,
-		softCtx:       softCtx,
-		loggerFactory: loggerFactory,
-		inner:         derrgroup.NewGroup(softCancel),
+	g := &Group{
+		cfg:     cfg,
+		hardCtx: hardCtx,
+		softCtx: softCtx,
+		inner:   derrgroup.NewGroup(softCancel),
 	}
 
-	ret.Go("supervisor", func(hardCtx, softCtx context.Context) error {
-		<-softCtx.Done()
-		dlog.Infoln(hardCtx, "shutting down...")
-		return nil
-	})
-
-	ret.Go("signal_handler", func(hardCtx, softCtx context.Context) error {
-		defer func() {
-			// If we receive another signal after
-			// graceful-shutdown, we should trigger a
-			// not-so-graceful shutdown.
-			go func() {
-				sig := <-sigs
-				dlog.Errorln(hardCtx, errors.Errorf("received signal %v", sig))
-				logGoroutines(hardCtx, dlog.Errorf, ret.List())
-				hardCancel()
-				// keep logging signals
-				for sig := range sigs {
-					dlog.Errorln(hardCtx, errors.Errorf("received signal %v", sig))
-					logGoroutines(hardCtx, dlog.Errorf, ret.List())
-				}
-			}()
-		}()
-
-		select {
-		case sig := <-sigs:
-			return errors.Errorf("received signal %v", sig)
-		case <-softCtx.Done():
+	if g.cfg.WorkerLogger != nil {
+		g.Go("supervisor", func(hardCtx, softCtx context.Context) error {
+			<-softCtx.Done()
+			dlog.Infoln(hardCtx, "shutting down...")
 			return nil
-		}
-	})
+		})
+	}
 
-	return ret
+	if g.cfg.EnableSignalHandling {
+		g.Go("signal_handler", func(hardCtx, softCtx context.Context) error {
+			sigs := make(chan os.Signal, 1)
+			signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+			defer func() {
+				// If we receive another signal after
+				// graceful-shutdown, we should trigger a
+				// not-so-graceful shutdown.
+				go func() {
+					sig := <-sigs
+					if g.cfg.WorkerLogger != nil {
+						dlog.Errorln(hardCtx, errors.Errorf("received signal %v", sig))
+						logGoroutines(hardCtx, dlog.Errorf, g.List())
+					}
+					hardCancel()
+					// keep logging signals and draining 'sigs'--don't let 'sigs' block
+					for sig := range sigs {
+						if g.cfg.WorkerLogger != nil {
+							dlog.Errorln(hardCtx, errors.Errorf("received signal %v", sig))
+							logGoroutines(hardCtx, dlog.Errorf, g.List())
+						}
+					}
+				}()
+			}()
+
+			select {
+			case sig := <-sigs:
+				return errors.Errorf("received signal %v", sig)
+			case <-softCtx.Done():
+				return nil
+			}
+		})
+	}
+
+	return g
 }
 
 // Go wraps derrgroup.Group.Go().
@@ -98,14 +113,20 @@ func NewGroup(ctx context.Context, loggerFactory func(name string) dlog.Logger) 
 //  - `hardCtx` being canceled should trigger a not-so-graceful shutdown
 func (g *Group) Go(name string, fn func(hardCtx, softCtx context.Context) error) {
 	g.inner.Go(name, func() error {
-		logger := g.loggerFactory(name)
-		hardCtx := dlog.WithLogger(g.hardCtx, logger)
-		softCtx := dlog.WithLogger(g.softCtx, logger)
+		hardCtx, softCtx := g.hardCtx, g.softCtx
+		var logger dlog.Logger
+		if g.cfg.WorkerLogger != nil {
+			logger = g.cfg.WorkerLogger(name)
+			hardCtx = dlog.WithLogger(hardCtx, logger)
+			softCtx = dlog.WithLogger(softCtx, logger)
+		}
 		err := fn(hardCtx, softCtx)
-		if err == nil {
-			logger.Debugln("goroutine exited without error")
-		} else {
-			logger.Errorln("goroutine exited with error:", err)
+		if g.cfg.WorkerLogger != nil {
+			if err == nil {
+				logger.Debugln("goroutine exited without error")
+			} else {
+				logger.Errorln("goroutine exited with error:", err)
+			}
 		}
 		return err
 	})
@@ -114,8 +135,8 @@ func (g *Group) Go(name string, fn func(hardCtx, softCtx context.Context) error)
 // Wait wraps derrgroup.Group.Wait().
 func (g *Group) Wait() error {
 	ret := g.inner.Wait()
-	if ret != nil {
-		ctx := dlog.WithLogger(g.hardCtx, g.loggerFactory("shutdown_status"))
+	if ret != nil && g.cfg.WorkerLogger != nil {
+		ctx := dlog.WithLogger(g.hardCtx, g.cfg.WorkerLogger("shutdown_status"))
 		logGoroutines(ctx, dlog.Infof, g.List())
 	}
 	return ret
