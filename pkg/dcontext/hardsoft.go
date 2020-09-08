ef767adc9c1214a01395b92c3635af263ea9ded4
Luke Shumaker <lukeshu@datawire.io>
2020-09-08 13:48:05 -0600

    (from AES) dcontext: Add lots of docs based on feedback from Rafi

--- pkg/dcontext/hardsoft.go
+++ pkg/dcontext/hardsoft.go
@@ -1,5 +1,73 @@
 // Package dcontext provides tools for dealing with separate hard/soft
 // cancellation of Contexts.
+//
+// Given
+//
+//    softCtx := WithSoftness(hardCtx)
+//
+// then
+//
+//    // The soft Context being done signals the end of "normal
+//    // operation", and the program should initiate a graceful
+//    // shutdown; a "soft shutdown".  In other words, it means, "You
+//    // should start shutting down now."
+//    <-softCtx.Done()
+//
+//    // The hard Context being done signals that the time for a
+//    // graceful shutdown has passed and that the programs should
+//    // terminate *right now*; a "hard shutdown".  In other words, it
+//    // means, "If you haven't finished shutting down yet, then you
+//    // should hurry it up."
+//    <-HardContext(softCtx).Done()
+//
+// It should almost always be the soft Context that is passed around
+// (I recommend that you name it just "ctx", not "softCtx").
+//
+//
+//
+// Interfacing dcontext-aware code with dcontext-unaware code
+//
+// When dcontext-aware code passes the soft Context to
+// dcontext-unaware code, then that callee code will shutdown at the
+// beginning of the shutdown grace period.  This is correct, because
+// the beginning of that grace period means "start shutting down"; if
+// the callee code is dcontext-unaware, then its shutdown is probably
+// more-or-less instantaneous, and so it is OK if it doesn't observe
+// the hard-shutdown signal as it already shut down from the soft
+// shutdown signal.
+//
+// When dcontext-unaware code passes a hard (normal) Context to
+// dcontext-aware code, then that callee code will observe the
+// <-ctx.Done() and <-HardContext(ctx).Done() occurring at the same
+// instant.  This is correct, because the caller code doesn't allow
+// any grace period between "start shutting down" and "you need to
+// finish shutting down now", so both of those are in the same
+// instant.
+//
+// Because of these two properties, it is the correct thing for
+//  - dcontext-aware callee code to just always assume that the
+//    Context it has received is a soft Context, and for
+//  - dcontext-aware caller code to just always pass the soft Context
+//    to things, regardless of whether the code being called it is
+//    dcontext-aware or not.
+//
+//
+//
+// Why this is necessary
+//
+// Why not a separate channel for soft shutdown, instead of a full
+// Context; something like `<-dcontext.GracefulShutdownStarted(ctx)`?
+// Similar to how github.com/datawire/ambassador/pkg/supervisor does?
+// Because, as explained above, the correct thing is for
+// dcontext-unaware code to observe shutdown when the graceful/soft
+// shutdown is initiated.  Using a stand-alone channel instead of a
+// Context means that one has to be very careful about passing the
+// normal (hard) Context to functions that are not dcontext-aware, as
+// to ensure that they don't shutdown too late.
+//
+// So, why not do the opposite, and use a separate channel for hard
+// shutdown?  Because that means you wouldn't have a Context to pass
+// to anything that your shutdown routine calls.
 package dcontext
 
 import (
@@ -34,7 +102,7 @@ func (c childHardContext) Value(key interface{}) interface{}       { return c.so
 // (a "hard" cancellation).
 //
 // Such a "soft" cancellation Context is created by
-// WithSoftness(hardCtx).  If the passed-in Context dones't have
+// WithSoftness(hardCtx).  If the passed-in Context doesn't have
 // softness (WithSoftness isn't somewhere in its ancestry), then it is
 // returned unmodified, because it is already hard.
 func HardContext(softCtx context.Context) context.Context {
--- pkg/dcontext/hardsoft_example_test.go
+++ pkg/dcontext/hardsoft_example_test.go
@@ -39,15 +39,25 @@ func ExampleWithSoftCancel(t *testing.T) {
 // .Shutdown() to hurry along and kill any live requests and return,
 // instead of waiting for them to be completed gracefully.
 func ListenAndServeHTTPWithContext(ctx context.Context, server *http.Server) error {
-	server.BaseContext = func(_ net.Listener) context.Context { return dcontext.HardContext(ctx) }
+	// Regardless of if you use dcontext, you should always set
+	// `.BaseContext` on your `http.Server`s so that your HTTP Handler
+	// receives a request object that has `Request.Context()` set correctly.
+	server.BaseContext = func(_ net.Listener) context.Context {
+		// We use the hard Context here instead of the soft Context so
+		// that in-progress requests don't get interrupted when we enter
+		// the shutdown grace period.
+		return dcontext.HardContext(ctx)
+	}
 	serverCh := make(chan error)
 	go func() {
 		serverCh <- server.ListenAndServe()
 	}()
 	select {
 	case err := <-serverCh:
+		// The server quit on its own.
 		return err
 	case <-ctx.Done():
+		// A soft shutdown has been initiated; call server.Shutdown().
 		return server.Shutdown(dcontext.HardContext(ctx))
 	}
 }
