3d2b6527cb4715cc4e99d7467e9deb5d89dac497
Rafael Schloming <rhs@datawire.io>
2020-09-30 12:42:17 -0400

    (from AES) Use memory helpers to log memory usage every 10 seconds for processes exceeding 50% of their limit, and log memory usage for all processes on exit.

--- cmd/entrypoint/entrypoint.go
+++ cmd/entrypoint/entrypoint.go
@@ -135,6 +135,18 @@ func Main() {
 	group.Go("watcher", func(ctx context.Context) {
 		watcher(ctx, snapshot)
 	})
+	group.Go("memory", func(ctx context.Context) {
+		ticker := time.Tick(10 * time.Second)
+		for {
+			select {
+			case <-ticker:
+				LogMemoryUsage(50)
+			case <-ctx.Done():
+				LogMemoryUsage(0)
+				return
+			}
+		}
+	})
 
 	// Launch every file in the sidecar directory. Note that this is "bug compatible" with
 	// entrypoint.sh for now, e.g. we don't check execute bits or anything like that.
