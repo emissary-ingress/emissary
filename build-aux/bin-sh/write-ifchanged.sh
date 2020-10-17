136f7316937fd1c77f5114bf16cb22c8f4c1b25f
Luke Shumaker <lukeshu@datawire.io>
2020-10-16 20:45:49 -0600

    (from AES) build-aux: *-ifchanged, write-dockertagfile: Better diagnostic output
    If these detect an erroneous change in CI, have them spit out a diff to
    make it easier to debug the issue.

--- build-aux/bin-sh/write-ifchanged.sh
+++ build-aux/bin-sh/write-ifchanged.sh
@@ -24,6 +24,7 @@ if cmp -s "$tmpfile" "$outfile"; then
 else
 	if [[ -n "$CI" && -e "$outfile" ]]; then
 		echo "error: This should not happen in CI: ${outfile} should not change" >&2
+		diff -u "$outfile" "$tmpfile" >&2
 		exit 1
 	fi
 	mv -f "$tmpfile" "$outfile"
