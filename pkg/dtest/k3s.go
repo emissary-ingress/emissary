b04453031f5371729c8878e45bf77bf74791ba82
Rafael Schloming <rhs@datawire.io>
2020-11-13 00:01:40 -0500

    more missing aliases

--- pkg/dtest/k3s.go
+++ pkg/dtest/k3s.go
@@ -11,3 +11,5 @@ var K3sUp = dtest_k3s.K3sUp
 var K3sDown = dtest_k3s.K3sDown
 var RegistryUp = dtest_k3s.RegistryUp
 var RegistryDown = dtest_k3s.RegistryDown
+var WithMachineLock = dtest_k3s.WithMachineLock
+var WithNamedMachineLock = dtest_k3s.WithNamedMachineLock
