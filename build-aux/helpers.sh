95903ddf5e858629e0684f81a56595ac79b1e8b5
Alix Cook <73843761+acookin@users.noreply.github.com>
2020-12-16 08:10:20 -0800

    (from AES) retry kluster creates and fail when we should (#2145)
    * retry kluster creates and fail when we should
    
    * make curl_retry more generic

--- build-aux/helpers.sh
+++ build-aux/helpers.sh
@@ -31,9 +31,45 @@ wait_for_ip() ( # use a subshell so the set +x is local to the function
     echo "$external_ip"
 )
 
-wait_for_url() {
-    wait_for_url_output /dev/null "$@"
-}
+curl_retry() (
+    { set +x; } 2>/dev/null # make the set +x be quiet
+    local tries=0
+    local retry_count=$1
+    echo "retry_count ${retry_count}"
+    shift 1
+    local ok_status_codes=$1
+    echo "ok_status_codes ${ok_status_codes}"
+    shift 1
+    local abort_status_codes=$1
+    echo "abort status codes ${abort_status_codes}"
+    shift 1
+    while [ "${retry_count}" -lt 0 ] || [ "$tries" -lt "${retry_count}" ] ; do
+        local code=$(curl --retry 100 --retry-connrefused -skL -w "%{http_code}" "$@")
+        local curlStatus="$?"
+        echo $curlStatus
+        if [ "$curlStatus" -ne "56" ] && [ "$curlStatus" -ne "18" ] ; then
+            if [ "${ok_status_codes}" == "" ] ; then
+                return 0
+            fi
+            for ok_code in ${ok_status_codes//,/ } ; do
+                if [ "${ok_code}" -eq "${code}" ] ; then
+                    echo "Got $code, ready!" 1>&2
+                    return 0
+                fi
+            done
+            for bad_code in ${abort_status_codes//,/ } ; do
+                if [ "${bad_code}" -eq "${code}" ] ; then
+                    echo "Got $code, aborting" 1>&2
+                    return 1
+                fi
+            done
+        fi
+        echo "Curl exited with code $curlStatus and status code ${code}, sleeping for 10 seconds and then retrying... (tries=$tries)" 1>&2
+        sleep 10
+        tries=$((tries+1))
+    done
+    return 1
+)
 
 wait_for_url_output() ( # use a subshell so the set +x is local to the function
     { set +x; } 2>/dev/null # make the set +x be quiet
@@ -105,16 +141,23 @@ crashLoops() ( # use a subshell so the set +x is local to the function
 )
 
 start_cluster() {
-    local kubeconfig timeout profile
+    local kubeconfig timeout profile retries
     kubeconfig=${1}
     timeout=${2:-3600}
     profile=${3:-default}
+    retries=2
     if [ -e "${kubeconfig}" ]; then
         echo "cannot get cluster, kubeconfig ${kubeconfig} exists" 1>&2
         return 1
     fi
-    curl -s -H "Authorization: bearer ${KUBECEPTION_TOKEN}" "https://sw.bakerstreet.io/kubeception/api/klusters/ci-?generate=true&timeoutSecs=${timeout}&profile=${profile}" -X PUT > "${kubeconfig}"
+    klusterurl="https://sw.bakerstreet.io/kubeception/api/klusters/ci-?generate=true&timeoutSecs=${timeout}&profile=${profile}"
     printf "${BLU}Acquiring cluster:\n==${END}\n" 1>&2
+    curl_retry $retries "200,425" "" -o "${kubeconfig}" -H "Authorization: bearer ${KUBECEPTION_TOKEN}" "${klusterurl}" -X PUT
+    ret="$?"
+    if [ "${ret}" -ne "0" ] ; then
+        echo "Unable to aquire cluster, exiting" 1>&2
+        return ${ret}
+    fi
     cat "${kubeconfig}" 1>&2
     printf "${BLU}==${END}\n" 1>&2
 }
@@ -124,7 +167,12 @@ await_cluster() {
     kubeconfig=${1}
     name="$(head -1 "${kubeconfig}" | cut -c2-)"
     kconfurl="https://sw.bakerstreet.io/kubeception/api/klusters/${name}"
-    wait_for_url_output "${kubeconfig}" "$kconfurl" -H "Authorization: bearer ${KUBECEPTION_TOKEN}"
+	curl_retry -1 "200" "400" -o "${kubeconfig}" "$kconfurl" -H "Authorization: bearer ${KUBECEPTION_TOKEN}"
+	ret=$?
+	if [ "${ret}" -ne "0" ] ; then
+		echo "Failed waiting for cluster to come up" 1>&2
+		return $ret
+	fi
     printf "${BLU}Cluster ${name} acquired:\n==${END}\n" 1>&2
     cat "${kubeconfig}" 1>&2
     printf "${BLU}==${END}\n" 1>&2
@@ -132,6 +180,9 @@ await_cluster() {
 
 get_cluster() {
     start_cluster "$@"
+    if [ "$?" != "0" ] ; then
+        return 1
+    fi
     await_cluster "$@"
 }
 
