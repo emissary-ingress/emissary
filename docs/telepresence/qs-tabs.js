2d16735811ef5fa61fc8066c1af5cb9def972243
mattmcclure-dw <73489204+mattmcclure-dw@users.noreply.github.com>
2021-02-10 16:52:33 -0500

    tabs

--- docs/telepresence/qs-tabs.js
+++ docs/telepresence/qs-tabs.js
@@ -88,7 +88,7 @@ export default function SimpleTabs() {
       1. Download the latest binary(~50 MB):<br/>
       <code>sudo curl -fL https://app.getambassador.io/download/tel2/linux/amd64/latest/telepresence -o /usr/local/bin/telepresence</code><br/>
       2. Make the binary executable:<br/>
-      <code>sudo chmod a+x /usr/local/bin/telepresence</code>  
+      <code>sudo chmod a+x /usr/local/bin/telepresence</code> 
 
         </TabPanel>
 
