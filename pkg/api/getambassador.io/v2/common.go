5101b94261dbe021893bcf02d1a5e65758532d03
Flynn <flynn@datawire.io>
2021-02-11 13:34:28 -0500

    (from AES) Update API design guidelines.

--- pkg/api/getambassador.io/v2/common.go
+++ pkg/api/getambassador.io/v2/common.go
@@ -34,6 +34,26 @@
 //   * Justification: Kubernetes style is to use camelCase. But
 //     historically Ambassador used snake_case for everything.
 //
+// - Give _every_ field a `json:""` struct tag.
+//   * Justification: Marshaling and unmarshaling are key to what we
+//     do, and it's critical to carefully define how it happens.
+//   * Notes: This is not optional. Do it for _every field_. (It's OK
+//     if the tag is literally `json:""` for fields that must never be
+//     exposed during marshaling.)
+//
+// - Prefer `*int`, `*bool`, and `*BoolOrString`, rather than just
+//   `int`, `bool`, and `BoolOrString`.
+//   * Justification: The Ambassador API is rooted in Python, where
+//     it is always possible to tell if a given element was present in
+//     in a CRD, or left unset. This is at odds with Go's `omitempty`
+//     specifier, which really means "omit if empty _or if set to the
+//     default value". For int in particular, this results in a value
+//     of 0 being omitted, and for many Ambassador fields, 0 is not
+//     the correct default value.
+//
+//     This resulted in a lot of bugs in the 1.10 timeframe, so be
+//     careful going forward.
+//
 // - Prefer for object references to not support namespacing
 //   * Exception: If there's a real use-case for it.
 //   * Justification: Most native Kubernetes resources don't support
