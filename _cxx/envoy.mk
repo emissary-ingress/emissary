2c2e24eba3b932cb8f426a87e79becb4e6311c3a
John Esmet <john.esmet@gmail.com>
2021-03-01 20:20:24 +0000

    (from AES) Update Envoy again

--- _cxx/envoy.mk
+++ _cxx/envoy.mk
@@ -10,7 +10,7 @@ IS_PRIVATE ?= $(findstring private,$(_git_remote_urls))
 
 # IF YOU MESS WITH ANY OF THESE VALUES, YOU MUST RUN `make update-base`.
   ENVOY_REPO ?= $(if $(IS_PRIVATE),git@github.com:datawire/envoy-private.git,git://github.com/datawire/envoy.git)
-  ENVOY_COMMIT ?= ff4790c1cc56024c3df4220cdb10437493ff1ddc
+  ENVOY_COMMIT ?= 779911fc06e22e6ffd43d55bf51980a67d7552d9
   ENVOY_COMPILATION_MODE ?= opt
   # Increment BASE_ENVOY_RELVER on changes to `docker/base-envoy/Dockerfile`, or Envoy recipes.
   # You may reset BASE_ENVOY_RELVER when adjusting ENVOY_COMMIT.
