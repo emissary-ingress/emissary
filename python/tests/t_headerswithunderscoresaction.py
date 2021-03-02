8621773b85371d65f4e19efd2767ee7f97ed6825
Noah Krause <krausenoah@gmail.com>
2021-03-01 22:24:34 -0500

    (from AES) Use testing semantics suggested by esmet
    `test_listener_common_http_protocol_options.py` will check the envoy
    config for the correct value being set in the yaml
    
    `t_headerswithunderscoresaction` will check that the right HTTP
    response is sent to the client based off the value of
    `headers_with_underscores_action`
    
    Signed-off-by: Noah Krause <krausenoah@gmail.com>

--- python/tests/t_headerswithunderscoresaction.py
+++ python/tests/t_headerswithunderscoresaction.py
@@ -2,16 +2,39 @@ from kat.harness import Query
 from abstract_tests import AmbassadorTest, ServiceType, HTTP
 import json
 
-class ListenerIdleTimeout(AmbassadorTest):
+class AllowHeadersWithUnderscoresTest(AmbassadorTest):
     target: ServiceType
 
     def init(self):
-        self.target = HTTP()
+        self.target = HTTP(name="target")
 
     def config(self):
         yield self, self.format("""
 ---
-apiVersion: ambassador/v1
+apiVersion: ambassador/v2
+kind:  Mapping
+name:  config__dump
+ambassador_id: {self.ambassador_id}
+prefix: /target/
+service: http://{self.target.path.fqdn}
+""")
+
+    def queries(self):
+        yield Query(self.url("target/"), expected=200, headers={'t_underscore':'foo'})
+
+    def check(self):
+        assert self.results[0].status == 200
+
+class RejectHeadersWithUnderscoresTest(AmbassadorTest):
+    target: ServiceType
+
+    def init(self):
+        self.target = HTTP(name="target")
+
+    def config(self):
+        yield self, self.format("""
+---
+apiVersion: ambassador/v2
 kind: Module
 name: ambassador
 ambassador_id: {self.ambassador_id}
@@ -20,41 +43,16 @@ config:
 """)
         yield self, self.format("""
 ---
-apiVersion: ambassador/v0
+apiVersion: ambassador/v2
 kind:  Mapping
 name:  config__dump
-prefix: /config_dump
-rewrite: /config_dump
-service: http://127.0.0.1:8001
+ambassador_id: {self.ambassador_id}
+prefix: /target/
+service: http://{self.target.path.fqdn}
 """)
 
     def queries(self):
-        yield Query(self.url("config_dump"), phase=2)
-        yield Query(self.url("config_dump"), expected=400, headers={'t_underscore':'foo'})
+        yield Query(self.url("target/"), expected=400, headers={'t_underscore':'foo'})
 
     def check(self):
-        expected_val = 'REJECT_REQUEST'
-        actual_val = ''
-        body = json.loads(self.results[0].body)
-        for config_obj in body.get('configs'):
-          if config_obj.get('@type') == 'type.googleapis.com/envoy.admin.v3.ListenersConfigDump':
-            listeners = config_obj.get('dynamic_listeners')
-            found_headers_with_underscores_action = False
-            for listener_obj in listeners:
-              listener = listener_obj.get('active_state').get('listener')
-              filter_chains = listener.get('filter_chains')
-              for filters in filter_chains:
-                for filter in filters.get('filters'):
-                  if filter.get('name') == 'envoy.filters.network.http_connection_manager':
-                    filter_config = filter.get('typed_config')
-                    common_http_protocol_options = filter_config.get('common_http_protocol_options')
-                    if common_http_protocol_options:
-                      actual_val = common_http_protocol_options.get('headers_with_underscores_action', '')
-                      if actual_val != '':
-                        if actual_val == expected_val:
-                          found_headers_with_underscores_action = True
-                      else:
-                        assert False, "Expected to find common_http_protocol_options.headers_with_underscores_action property on listener"
-                    else:
-                      assert False, "Expected to find common_http_protocol_options property on listener"
-            assert found_headers_with_underscores_action, "Expected common_http_protocol_options.headers_with_underscores_action = {}, Got common_http_protocol_options.headers_with_underscores_action = {}".format(expected_val, actual_val)
+        assert self.results[0].status == 400
