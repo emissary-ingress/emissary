332f5ee31f7e0bdcd31c447793b5996a06c01036
Flynn <flynn@datawire.io>
2021-04-06 14:12:45 -0400

    Build up listener filters for V2Listeners

--- python/ambassador/envoy/v3/v3listener.py
+++ python/ambassador/envoy/v3/v3listener.py
@@ -59,8 +59,25 @@ class V3Listener(dict):
         if self._log_debug:
             self.config.ir.logger.debug(f"V3Listener {self.name} created -- {self._security_model}, l7Depth {self._l7_depth}")
 
-        # Start by building our base HTTP config...
-        self._base_http_config = self.base_http_config()
+        # Build out our listener filters, and figure out if we're an HTTP listener
+        # in the process.
+        for proto in irlistener.protocolStack:
+            if proto == "HTTP":
+                # Start by building our base HTTP config...
+                self._base_http_config = self.base_http_config()
+
+            if proto == "PROXY":
+                self.listener_filters.append({
+                    'name': 'envoy.filters.listener.proxy_protocol'
+                })
+
+            if proto == "TLS":
+                self.listener_filters.append({
+                    'name': 'envoy.filters.listener.tls_inspector'
+                })
+
+            # TCP (and, later, UDP) don't require any specific listener filters.
+            # They're handled exclusively in the filter chains.
 
     # access_log constructs the access_log configuration for this V3Listener
     def access_log(self) -> List[dict]:
@@ -379,4 +396,4 @@ class V3Listener(dict):
             config.listeners.append(v3listener)
 
             if v3listener._log_debug:
-                config.ir.logger.debug(f"V3Listener generated: {v3listener.pretty()}")
+                config.ir.logger.debug(f"V3Listener generated: {v3listener}")
