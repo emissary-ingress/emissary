1aed1067bf7a77be0c92d89162471831fb964ba8
Alix Cook <alixcook@datawire.io>
2021-05-23 14:10:49 -0700

    KEEP: dang it

--- charts/charts.mk
+++ charts/charts.mk
@@ -7,13 +7,11 @@ define _push_chart
 endef
 
 define _set_tag_and_repo
-	$(OSS_HOME)/venv/bin/python $(OSS_HOME)/charts/scripts/update_chart_image_values.py $(1) $(2) $(3) && \
-		$(YQ) w -i $(1) 'appVersion' $(2)
+	$(OSS_HOME)/venv/bin/python $(OSS_HOME)/charts/scripts/update_chart_image_values.py $(1) $(2) $(3)
 endef
 
 define _set_tag
-	$(OSS_HOME)/venv/bin/python $(OSS_HOME)/charts/scripts/update_chart_image_values.py $(1) $(2) docker.io/alixcook11/ambassador && \
-		$(YQ) w -i $(1) 'appVersion' $(2)
+	$(OSS_HOME)/venv/bin/python $(OSS_HOME)/charts/scripts/update_chart_image_values.py $(1) $(2) docker.io/alixcook11/ambassador
 endef
 
 define _docgen
@@ -35,6 +33,7 @@ chart-push-ci: push-preflight
 	for chart in $(AMBASSADOR_CHART) $(EMISSARY_CHART) ; do \
 		sed -i.bak -E "s/version: ([0-9]+\.[0-9]+\.[0-9]+).*/version: \1${CHART_VERSION_SUFFIX}/g" $$chart/Chart.yaml && rm $$chart/Chart.yaml.bak ; \
 		$(call _set_tag_and_repo,$$chart/values.yaml,${IMAGE_TAG},${IMAGE_REPO}) ; \
+		$(YQ) w -i $$chart/Chart.yaml 'appVersion' ${IMAGE_TAG} ; \
 		$(call _push_chart,`basename $$chart`) ; \
 	done ;
 .PHONY: chart-push-ci
@@ -48,6 +47,7 @@ chart-push-ga: push-preflight
 	@for chart in $(AMBASSADOR_CHART) $(EMISSARY_CHART) ; do \
 		sed -i.bak -E "s/version: ([0-9]+\.[0-9]+\.[0-9]+).*/version: \1/g" $$chart/Chart.yaml && rm $$chart/Chart.yaml.bak ; \
 		$(call _set_tag_and_repo,$$chart/values.yaml,${IMAGE_TAG},${IMAGE_REPO}) ; \
+		$(YQ) w -i $$chart/Chart.yaml 'appVersion' ${IMAGE_TAG} ; \
 		$(call _push_chart,`basename $$chart`) ; \
 	done ;
 .PHONY: chart-push-ga
@@ -76,6 +76,7 @@ release/chart/update-images: doc-gen-preflight $(YQ)
 	([[ "${IMAGE_TAG}" =~ .*\.0$$ ]] && $(MAKE) release/chart-bump/minor) || $(MAKE) release/chart-bump/revision
 	for chart in $(AMBASSADOR_CHART) $(EMISSARY_CHART) ; do \
 		$(call _set_tag,$$chart/values.yaml,${IMAGE_TAG}) ; \
+		$(YQ) w -i $$chart/Chart.yaml 'appVersion' ${IMAGE_TAG} ; \
 		IMAGE_TAG="${IMAGE_TAG}" CHART_NAME=`basename $$chart` $(OSS_HOME)/charts/scripts/image_tag_changelog_update.sh ; \
 		CHART_NAME=`basename $$chart` $(OSS_HOME)/charts/scripts/update_chart_changelog.sh ; \
 		$(call _docgen,$$chart) ; \
