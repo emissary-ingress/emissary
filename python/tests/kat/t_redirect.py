f7c0868bab3aefed55354282a927266b3528b0a5
Flynn <flynn@datawire.io>
2021-05-28 14:41:43 -0400

    Update some tests for 2.0

--- python/tests/kat/t_redirect.py
+++ python/tests/kat/t_redirect.py
@@ -220,14 +220,37 @@ class XFPRedirect(AmbassadorTest):
 
         self.target = HTTP()
 
+    def manifests(self):
+        return self.format('''
+---
+apiVersion: getambassador.io/v2
+kind: Listener
+metadata:
+  name: cleartext-only
+spec:
+  ambassador_id: {self.ambassador_id}
+  port: 8080
+  protocol: HTTP
+  securityModel: XFP
+  l7Depth: 1
+---
+apiVersion: getambassador.io/v2
+kind: Host
+metadata:
+  name: weird-xfp-test-host
+spec:
+  ambassador_id: {self.ambassador_id}
+  requestPolicy:
+    insecure:
+      action: Redirect
+''') + super().manifests()
+
+
     def config(self):
         yield self.target, self.format("""
----
-apiVersion: ambassador/v1
 kind: Module
 name: ambassador
 config:
-  x_forwarded_proto_redirect: true
   use_remote_address: false
 ---
 apiVersion: ambassador/v1
