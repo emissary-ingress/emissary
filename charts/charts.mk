10fb4017ffc5fe93018e318c362d55bd5b50fc4f
Alix Cook <alixcook@datawire.io>
2021-06-01 07:01:29 -0700

    remove signed tags

--- charts/charts.mk
+++ charts/charts.mk
@@ -53,7 +53,7 @@ release/chart/tag:
 		fi; \
 		chart_ver=`grep 'version:' $(AMBASSADOR_CHART)/Chart.yaml | awk ' { print $$2 }'` ; \
 		chart_ver=chart-v$${chart_ver} ; \
-		git tag -s -m "Tagging $${chart_ver}" -a $${chart_ver} ; \
+		git tag -m "Tagging $${chart_ver}" -a $${chart_ver} ; \
 		git push origin $${chart_ver} ; \
 	}
 
