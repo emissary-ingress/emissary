2d7976ecc1d896cd757bbe5f15e69ce5bb60304c
Alix Cook <alixcook@datawire.io>
2021-06-01 15:00:29 -0700

    fix bad rebase

--- python/ambassador/envoy/v3/v3listener.py
+++ python/ambassador/envoy/v3/v3listener.py
@@ -322,7 +322,7 @@ class V3Listener(dict):
             base_http_config["max_request_headers_kb"] = max_request_headers_kb
 
         if 'enable_http10' in self.config.ir.ambassador_module:
-            http_options = self.base_http_config.setdefault("http_protocol_options", {})
+            http_options = base_http_config.setdefault("http_protocol_options", {})
             http_options['accept_http_10'] = self.config.ir.ambassador_module.enable_http10
 
         if 'preserve_external_request_id' in self.config.ir.ambassador_module:
