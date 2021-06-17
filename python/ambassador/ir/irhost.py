7568bc82c1525a7275276358a8d6a8806b265349
Rafael Schloming <rhs@datawire.io>
2021-06-17 16:38:54 -0400

    wip
    Signed-off-by: Rafael Schloming <rhs@datawire.io>

--- python/ambassador/ir/irhost.py
+++ python/ambassador/ir/irhost.py
@@ -37,7 +37,7 @@ class IRHost(IRResource):
                  location: str,  # REQUIRED
                  namespace: Optional[str]=None,
                  kind: str="IRHost",
-                 apiVersion: str="getambassador.io/v2",   # Not a typo! See below.
+                 apiVersion: str="x.getambassador.io/v3alpha1",   # Not a typo! See below.
                  **kwargs) -> None:
 
         new_args = {
