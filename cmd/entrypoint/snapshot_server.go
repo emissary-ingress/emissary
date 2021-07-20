455c990b49e08ed524b8945bbbaefd72c6f54b1f
alex <alex@datawire.io>
2021-07-20 14:05:14 -0400

    Testing external snapshot sidecar process-info injection
    Signed-off-by: alex <alex@datawire.io>

--- cmd/entrypoint/snapshot_server.go
+++ cmd/entrypoint/snapshot_server.go
@@ -20,45 +20,12 @@ const ExternalSnapshotPort = 8005
 func externalSnapshotServer(ctx context.Context, snapshot *atomic.Value) error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/snapshot-external", func(w http.ResponseWriter, r *http.Request) {
-		rawSnapshot := snapshot.Load().([]byte)
-		snapDecoded := snapshotTypes.Snapshot{}
-		err := json.Unmarshal(rawSnapshot, &snapDecoded)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		err = snapDecoded.Sanitize()
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if snapDecoded.AmbassadorMeta != nil && IsEdgeStack() {
-			sidecarProcessInfoUrl := fmt.Sprintf("%s/process-info/", GetSidecarHost())
-			dlog.Debugf(ctx, "loading sidecar process-info using [%s]...", sidecarProcessInfoUrl)
-			resp, err := http.DefaultClient.Get(sidecarProcessInfoUrl)
-			if err != nil {
-				dlog.Error(ctx, err.Error())
-			} else {
-				defer resp.Body.Close()
-				if resp.StatusCode == 200 {
-					bodyBytes, err := ioutil.ReadAll(resp.Body)
-					if err != nil {
-						dlog.Warnf(ctx, "error reading response body: %v", err)
-					} else {
-						snapDecoded.AmbassadorMeta.Sidecar = bodyBytes
-					}
-				} else {
-					dlog.Warnf(ctx, "unexpected status code %v", resp.StatusCode)
-				}
-			}
-		}
-		sanitizedSnap, err := json.Marshal(snapDecoded)
+		sanitizedSnap, err := sanitizeExternalSnapshot(ctx, snapshot.Load().([]byte), http.DefaultClient)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		w.Header().Set("content-type", "application/json")
-
 		w.Write(sanitizedSnap)
 	})
 
@@ -80,4 +47,38 @@ func snapshotServer(ctx context.Context, snapshot *atomic.Value) error {
 	}
 
 	return s.ListenAndServe(ctx, "localhost:9696")
+}
+
+func sanitizeExternalSnapshot(ctx context.Context, rawSnapshot []byte, client *http.Client) ([]byte, error) {
+	snapDecoded := snapshotTypes.Snapshot{}
+	err := json.Unmarshal(rawSnapshot, &snapDecoded)
+	if err != nil {
+		return nil, err
+	}
+	err = snapDecoded.Sanitize()
+	if err != nil {
+		return nil, err
+	}
+	if snapDecoded.AmbassadorMeta != nil && IsEdgeStack() {
+		sidecarProcessInfoUrl := fmt.Sprintf("%s/process-info/", GetSidecarHost())
+		dlog.Debugf(ctx, "loading sidecar process-info using [%s]...", sidecarProcessInfoUrl)
+		resp, err := client.Get(sidecarProcessInfoUrl)
+		if err != nil {
+			dlog.Error(ctx, err.Error())
+		} else {
+			defer resp.Body.Close()
+			if resp.StatusCode == 200 {
+				bodyBytes, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					dlog.Warnf(ctx, "error reading response body: %v", err)
+				} else {
+					snapDecoded.AmbassadorMeta.Sidecar = bodyBytes
+				}
+			} else {
+				dlog.Warnf(ctx, "unexpected status code %v", resp.StatusCode)
+			}
+		}
+	}
+
+	return json.Marshal(snapDecoded)
 }
