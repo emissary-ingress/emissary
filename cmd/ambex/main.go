8d344345cb075ce0435e3e2dd7d47c54f62431f5
Flynn <flynn@datawire.io>
2021-07-23 18:10:39 -0400

    Allow disabling the ambex ratelimiter
    Signed-off-by: Flynn <flynn@datawire.io>

--- cmd/ambex/main.go
+++ cmd/ambex/main.go
@@ -601,6 +601,8 @@ func (l loggerv3) OnFetchResponse(req *v3discovery.DiscoveryRequest, res *v3disc
 	l.Infof("V3 Fetch response: %v -> %v", req, res)
 }
 
+// NOTE WELL: this Main() does NOT RUN from entrypoint! This one is only relevant if you
+// explicitly run Ambex by hand.
 func Main(ctx context.Context, Version string, rawArgs ...string) error {
 	usage := memory.GetMemoryUsage()
 	go usage.Watch(ctx)
