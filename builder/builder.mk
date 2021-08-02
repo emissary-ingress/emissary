c175fc0a1924493b955fb7c9408ee9410e82cddc
Flynn <flynn@datawire.io>
2021-08-02 16:44:51 -0400

    Clean up indentation.
    Signed-off-by: Flynn <flynn@datawire.io>
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- builder/builder.mk
+++ builder/builder.mk
@@ -863,23 +863,23 @@ release/promote-oss/pr-to-passed-ci:
 release/promote-oss/to-hotfix:
 	@test -n "$(RELEASE_REGISTRY)" || (printf "$${RELEASE_REGISTRY_ERR}\n"; exit 1)
 	@set -e; { \
-	  docker login -u `keybase fs read /keybase/team/datawireio/secrets/dockerhub.webui.d6eautomaton.username` \
-		-p `keybase fs read /keybase/team/datawireio/secrets/dockerhub.webui.d6eautomaton.password` ; \
-	  hotfix_tag=`git describe --tags --exact --match '*-hf*' | sed 's/^v//g'` ; \
-	  [[ "$(RELEASE_VERSION)" =~ ^[0-9]+\.[0-9]+\.[0-9]+-hf\.\[0-9]+\+[0-9]+$$ ]] || (printf '$(RED)ERROR: tag %s does not look like a hotfix tag\n' "$$hotfix_tag"; exit 1) ;\
-	  commit=$$(git rev-parse HEAD) ;\
-	  $(OSS_HOME)/releng/release-wait-for-commit --commit $$commit --s3-key passed-pr ; \
-	  dev_version=$$(aws s3 cp s3://datawire-static-files/passed-pr/$$commit -) ;\
-	  if [ -z "$$dev_version" ]; then \
-		  printf "$(RED)==> found no passed dev version for $$commit in S3...$(END)\n" ;\
-		  exit 1 ;\
-      fi ;\
- 	  printf "$(CYN)==> $(GRN)found version $(BLU)$$dev_version$(GRN) for $(BLU)$$commit$(GRN) in S3...$(END)\n" ;\
-	  $(MAKE) release/promote-oss/.main \
-	    PROMOTE_FROM_VERSION="$$dev_version" \
-		PROMOTE_FROM_REPO=$(DEV_REGISTRY) \
-	    PROMOTE_TO_VERSION="$$hotfix_tag" \
-	    PROMOTE_CHANNEL=hotfix \
+		docker login -u `keybase fs read /keybase/team/datawireio/secrets/dockerhub.webui.d6eautomaton.username` \
+					 -p `keybase fs read /keybase/team/datawireio/secrets/dockerhub.webui.d6eautomaton.password` ; \
+		hotfix_tag=`git describe --tags --exact --match '*-hf*' | sed 's/^v//g'` ; \
+		[[ "$(RELEASE_VERSION)" =~ ^[0-9]+\.[0-9]+\.[0-9]+-hf\.\[0-9]+\+[0-9]+$$ ]] || (printf '$(RED)ERROR: tag %s does not look like a hotfix tag\n' "$$hotfix_tag"; exit 1) ;\
+		commit=$$(git rev-parse HEAD) ;\
+		$(OSS_HOME)/releng/release-wait-for-commit --commit $$commit --s3-key passed-pr ; \
+		dev_version=$$(aws s3 cp s3://datawire-static-files/passed-pr/$$commit -) ;\
+		if [ -z "$$dev_version" ]; then \
+			printf "$(RED)==> found no passed dev version for $$commit in S3...$(END)\n" ;\
+			exit 1 ;\
+		fi ;\
+		printf "$(CYN)==> $(GRN)found version $(BLU)$$dev_version$(GRN) for $(BLU)$$commit$(GRN) in S3...$(END)\n" ;\
+		$(MAKE) release/promote-oss/.main \
+			PROMOTE_FROM_VERSION="$$dev_version" \
+			PROMOTE_FROM_REPO=$(DEV_REGISTRY) \
+			PROMOTE_TO_VERSION="$$hotfix_tag" \
+			PROMOTE_CHANNEL=hotfix \
 		chartsuffix=$$hotfix_tag ; \
 		chartsuffix=$${chartsuffix#*-} ; \
 		export AWS_ACCESS_KEY_ID=`keybase fs read /keybase/team/datawireio/secrets/aws.s3-bot.cli-credentials | grep 'aws_access_key_id' | sed 's/aws_access_key_id=//g'`; \
