61329b2104ffeb3ea98f00fba3d8035f1b743b3b
AliceProxy <aliceproxy@protonmail.com>
2021-08-10 15:39:22 -0700

    expose chunked length
    Signed-off-by: AliceProxy <aliceproxy@protonmail.com>

---
+++ python/tests/kat/t_chunked_length.py
@@ -0,0 +1,107 @@
+from kat.harness import Query
+from abstract_tests import AmbassadorTest, ServiceType, HTTP
+import json
+
+class AllowChunkedLengthTestTrue(AmbassadorTest):
+    target: ServiceType
+
+    def init(self):
+        self.target = HTTP(name="target")
+
+    def config(self):
+        yield self, self.format("""
+---
+apiVersion: ambassador/v2
+kind:  Module
+name:  ambassador
+config:
+  allow_chunked_length: true
+---
+apiVersion: ambassador/v2
+kind:  Mapping
+name:  {self.target.path.k8s}-foo
+prefix: /foo/
+service: {self.target.path.fqdn}
+""")
+
+    def queries(self):
+        yield Query(self.url("foo/"), 
+            headers={
+                "content-length": "0",
+                "transfer-encoding": "gzip"
+        })
+        yield Query(self.url("ambassador/v0/diag/"), 
+            headers={
+                "content-length": "0",
+                "transfer-encoding": "gzip"
+        })   
+
+    def check(self):
+        # We expect 501 when sending both these headers because it service probably doesnt support gzip transfer encoding
+        # Not getting a 400 bad request is confirmation that this setting works as long as the request can reach the upstream
+        assert self.results[0].status == 501
+        assert self.results[1].status == 501
+
+class AllowChunkedLengthTestFalse(AmbassadorTest):
+    target: ServiceType
+
+    def init(self):
+        self.target = HTTP(name="target")
+
+    def config(self):
+        yield self, self.format("""
+---
+apiVersion: ambassador/v2
+kind:  Module
+name:  ambassador
+config:
+  allow_chunked_length: false
+---
+apiVersion: ambassador/v2
+kind:  Mapping
+name:  {self.target.path.k8s}-foo
+prefix: /foo/
+service: {self.target.path.fqdn}
+""")
+
+    def queries(self):
+        yield Query(self.url("foo/"), 
+            headers={
+                "content-length": "0",
+                "transfer-encoding": "gzip"
+        })
+        yield Query(self.url("ambassador/v0/diag/"), 
+            headers={
+                "content-length": "0",
+                "transfer-encoding": "gzip"
+        })   
+
+    def check(self):
+        # When this setting is disabled, we expect to get a 400 bad requet response whenever these two headers are sent together.
+        assert self.results[0].status == 400
+        assert self.results[1].status == 400
+
+class AllowChunkedLengthTestUnset(AmbassadorTest):
+    target: ServiceType
+
+    def init(self):
+        self.target = HTTP(name="target")
+
+    # Test generating config without the setting to confirm that we can still get 200 request normally
+    def config(self):
+        yield self, self.format("""
+---
+apiVersion: ambassador/v2
+kind:  Mapping
+name:  {self.target.path.k8s}-foo
+prefix: /foo/
+service: {self.target.path.fqdn}
+""")
+
+    def queries(self):
+        yield Query(self.url("foo/"))
+        yield Query(self.url("ambassador/v0/diag/"))   
+
+    def check(self):
+        assert self.results[0].status == 200
+        assert self.results[1].status == 200
--- python/tests/test_listener_http_protocol_options.py
+++ python/tests/test_listener_http_protocol_options.py
@@ -37,3 +37,14 @@ def test_proper_case_true():
 def test_proper_case_and_enable_http_10():
     yaml = module_and_mapping_manifests(["proper_case: true", "enable_http10: true"], [])
     _test_listener_http_protocol_options(yaml, expectations={'accept_http_10': True, 'header_key_format': {'proper_case_words': {}}})
+
+# Compiler tests for allowing chunked length
+@pytest.mark.compilertest
+def test_allow_chunked_length_false():
+    yaml = module_and_mapping_manifests(["allow_chunked_length: false"], [])
+    _test_listener_http_protocol_options(yaml, expectations={'allow_chunked_length': False})
+
+@pytest.mark.compilertest
+def test_allow_chunked_length_true():
+    yaml = module_and_mapping_manifests(["allow_chunked_length: true"], [])
+    _test_listener_http_protocol_options(yaml, expectations={'allow_chunked_length': True})
