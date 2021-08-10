106c71d811a0a916a28ed74860df3e413a181426
AliceProxy <aliceproxy@protonmail.com>
2021-08-10 16:37:32 -0700

    improve test coverage
    Signed-off-by: AliceProxy <aliceproxy@protonmail.com>

--- python/tests/kat/t_chunked_length.py
+++ python/tests/kat/t_chunked_length.py
@@ -25,6 +25,8 @@ service: {self.target.path.fqdn}
 """)
 
     def queries(self):
+        yield Query(self.url("foo/"))
+        yield Query(self.url("ambassador/v0/diag/"))   
         yield Query(self.url("foo/"), 
             headers={
                 "content-length": "0",
@@ -39,8 +41,10 @@ service: {self.target.path.fqdn}
     def check(self):
         # We expect 501 when sending both these headers because it service probably doesnt support gzip transfer encoding
         # Not getting a 400 bad request is confirmation that this setting works as long as the request can reach the upstream
-        assert self.results[0].status == 501
-        assert self.results[1].status == 501
+        assert self.results[0].status == 200
+        assert self.results[1].status == 200
+        assert self.results[2].status == 501
+        assert self.results[3].status == 501
 
 class AllowChunkedLengthTestFalse(AmbassadorTest):
     target: ServiceType
@@ -65,6 +69,8 @@ service: {self.target.path.fqdn}
 """)
 
     def queries(self):
+        yield Query(self.url("foo/"))
+        yield Query(self.url("ambassador/v0/diag/")) 
         yield Query(self.url("foo/"), 
             headers={
                 "content-length": "0",
@@ -78,8 +84,10 @@ service: {self.target.path.fqdn}
 
     def check(self):
         # When this setting is disabled, we expect to get a 400 bad requet response whenever these two headers are sent together.
-        assert self.results[0].status == 400
-        assert self.results[1].status == 400
+        assert self.results[0].status == 200
+        assert self.results[1].status == 200
+        assert self.results[2].status == 400
+        assert self.results[3].status == 400
 
 class AllowChunkedLengthTestUnset(AmbassadorTest):
     target: ServiceType
