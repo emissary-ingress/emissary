5107ff7b76dbe795dc454c914e7a10a6ed67125c
Aidan Hahn <aidanhahn@datawire.io>
2021-08-16 11:36:22 -0700

    update envoy commit
    Signed-off-by: Aidan Hahn <aidanhahn@datawire.io>
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- _cxx/envoy.mk
+++ _cxx/envoy.mk
@@ -9,7 +9,7 @@ IS_PRIVATE ?= $(findstring private,$(_git_remote_urls))
 
 # IF YOU MESS WITH ANY OF THESE VALUES, YOU MUST RUN `make update-base`.
   ENVOY_REPO ?= $(if $(IS_PRIVATE),git@github.com:datawire/envoy-private.git,git://github.com/datawire/envoy.git)
-  ENVOY_COMMIT ?= 32c3f1c15e6ad2bbcdf76b86bc893f19fac7d96d
+  ENVOY_COMMIT ?= d4dd111f1143f413f0ffb6cddbffffc3a863038c
   ENVOY_COMPILATION_MODE ?= opt
   # Increment BASE_ENVOY_RELVER on changes to `docker/base-envoy/Dockerfile`, or Envoy recipes.
   # You may reset BASE_ENVOY_RELVER when adjusting ENVOY_COMMIT.
