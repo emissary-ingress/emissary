b4cdb8e401507b6bb8b3b22bdf23d388b563864e
Alix Cook <alixcook@datawire.io>
2021-08-27 11:32:33 -0400

    boop
    Signed-off-by: Alix Cook <alixcook@datawire.io>

--- releng/lib/start_release_updates.py
+++ releng/lib/start_release_updates.py
@@ -8,7 +8,7 @@ def update_versions_yaml(next_ver):
         if line.startswith("version:"):
             line = f"version: {next_ver}\n"
         sys.stdout.write(line)
-    git_add("docs/yaml/versions.yml")
+    # git_add("docs/yaml/versions.yml")
 
 def update_changelog_date(next_ver):
     changelog_ver_pattern = re.compile(r"^## \[([0-9]+\.[0-9]+\.[0-9]+(-ea)?)\]")
@@ -31,4 +31,4 @@ def update_changelog_date(next_ver):
                     f"[{next_ver}]: https://github.com/emissary-ingress/emissary/compare/v{prev_ver}...v{next_ver}\n")
         else:
             sys.stdout.write(line)
-    git_add("CHANGELOG.md")
+    #git_add("CHANGELOG.md")
