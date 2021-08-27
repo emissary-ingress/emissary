e5faec07e6ee3cd606df04768979b932280e4e7a
Alix Cook <alixcook@datawire.io>
2021-08-27 11:27:21 -0400

    simplify the start of releases
    Signed-off-by: Alix Cook <alixcook@datawire.io>

--- releng/lib/start_release_updates.py
+++ releng/lib/start_release_updates.py
@@ -14,35 +14,21 @@ def update_changelog_date(next_ver):
     changelog_ver_pattern = re.compile(r"^## \[([0-9]+\.[0-9]+\.[0-9]+(-ea)?)\]")
     in_notes = False
     buf = ""
+    found = False
     for line in fileinput.FileInput("CHANGELOG.md", inplace=True):
         if not in_notes:
             sys.stdout.write(line)
             if line.startswith("## RELEASE NOTES"):
                 in_notes = True
             continue
-
-        match = changelog_ver_pattern.match(line)
-        if not match:
-            buf += line
-        elif line.startswith(f"## [{next_ver}]"):
-            # Don't do anything, this changelog already has an entry for the next version
-            sys.stdout.write(buf)
+        if not found and line.startswith('## Next Release'):
+            found = True
             sys.stdout.write(line)
-            in_notes = False
-        else:
-            prev_ver = match[1]
-            # dope let's get the last version first
-            # this is the beginning of the last version line
-            sys.stdout.write("\n")
+            sys.stdout.write('\n\n### Emissary Ingress\n\n')
+            sys.stdout.write('(no changes yet)\n\n')
             sys.stdout.write(f"## [{next_ver}] (TBD)\n")
             sys.stdout.write(
                     f"[{next_ver}]: https://github.com/emissary-ingress/emissary/compare/v{prev_ver}...v{next_ver}\n")
-            sys.stdout.write("\n")
-            sys.stdout.write("### Emissary Ingress\n")
-            sys.stdout.write("\n")
-            sys.stdout.write("(no changes yet)\n")
-            sys.stdout.write(buf)
+        else:
             sys.stdout.write(line)
-            in_notes = False
-
     git_add("CHANGELOG.md")
