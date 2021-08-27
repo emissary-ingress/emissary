b29238d788da0599d4803189a323046fe62d1d62
Alix Cook <alixcook@datawire.io>
2021-08-27 11:45:45 -0400

    boop

--- releng/lib/start_release_updates.py
+++ releng/lib/start_release_updates.py
@@ -11,16 +11,8 @@ def update_versions_yaml(next_ver):
     # git_add("docs/yaml/versions.yml")
 
 def update_changelog_date(next_ver):
-    changelog_ver_pattern = re.compile(r"^## \[([0-9]+\.[0-9]+\.[0-9]+(-ea)?)\]")
-    in_notes = False
-    buf = ""
     found = False
     for line in fileinput.FileInput("CHANGELOG.md", inplace=True):
-        if not in_notes:
-            sys.stdout.write(line)
-            if line.startswith("## RELEASE NOTES"):
-                in_notes = True
-            continue
         if not found and line.startswith('## Next Release'):
             found = True
             sys.stdout.write(line)
