1441859a61c02e2944a0c806c0f3f0b8dfc9bbe7
AliceProxy <aliceproxy@protonmail.com>
2021-09-08 18:41:38 -0700

    Add tests
    Signed-off-by: AliceProxy <aliceproxy@protonmail.com>
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

---
+++ python/tests/test_cluster_options.py
@@ -0,0 +1,70 @@
+from utils import econf_compile, econf_foreach_cluster, module_and_mapping_manifests, SUPPORTED_ENVOY_VERSIONS
+
+import pytest
+
+# Tests if `setting` exists within the cluster config and has `expected` as the value for that setting
+# Use `exists` to test if you expect a setting to not exist
+def _test_cluster_setting(yaml, setting, expected, exists=True, envoy_version="V2"):
+    econf = econf_compile(yaml, envoy_version=envoy_version)
+
+    def check(cluster):
+        if exists:
+            assert setting in cluster
+            assert cluster[setting] == expected
+        else:
+            assert setting not in cluster
+
+    econf_foreach_cluster(econf, check)
+
+# Tests a setting in a cluster that has it's own fields. Example: common_http_protocol_options has multiple subfields
+def _test_cluster_subfields(yaml, setting, expectations={}, exists=True, envoy_version="V2"):
+    econf = econf_compile(yaml, envoy_version=envoy_version)
+
+    def check(cluster):
+        if exists:
+            assert setting in cluster
+        else:
+            assert setting not in cluster
+        for key, expected in expectations.items():
+            print("Checking key: {} for the {} setting in Envoy cluster".format(key, setting))
+            assert key in cluster[setting]
+            assert cluster[setting][key] == expected
+
+    econf_foreach_cluster(econf, check)
+
+# Test dns_type setting in Mapping
+@pytest.mark.compilertest
+def test_logical_dns_type():
+    yaml = module_and_mapping_manifests(None, ["dns_type: logical_dns"])
+    for v in SUPPORTED_ENVOY_VERSIONS:
+        # The dns type is listed as just "type"
+        _test_cluster_setting(yaml, setting="type",
+            expected="LOGICAL_DNS", exists=True, envoy_version=v)
+
+@pytest.mark.compilertest
+def test_logical_dns_type():
+    # Make sure we can configure strict dns as well even though it's the default
+    yaml = module_and_mapping_manifests(None, ["dns_type: strict_dns"])
+    for v in SUPPORTED_ENVOY_VERSIONS:
+        # The dns type is listed as just "type"
+        _test_cluster_setting(yaml, setting="type",
+            expected="STRICT_DNS", exists=True, envoy_version=v)
+
+@pytest.mark.compilertest
+def test_logical_dns_type_wrong():
+    # Ensure we fallback to strict_dns as the setting when an invalid string is passed
+    # This is preferable to invalid config and an error is logged
+    yaml = module_and_mapping_manifests(None, ["dns_type: something_new"])
+    for v in SUPPORTED_ENVOY_VERSIONS:
+        # The dns type is listed as just "type"
+        _test_cluster_setting(yaml, setting="type", 
+            expected="STRICT_DNS", exists=True, envoy_version=v)
+
+@pytest.mark.compilertest
+def test_logical_dns_type_wrong():
+    # Ensure we use endpoint discovery instead of this value when using the endpoint resolver
+    yaml = module_and_mapping_manifests(None, ["dns_type: logical_dns", "resolver: endpoint"])
+    for v in SUPPORTED_ENVOY_VERSIONS:
+        # The dns type is listed as just "type"
+        _test_cluster_setting(yaml, setting="type", 
+            expected="EDS", exists=True, envoy_version=v)
