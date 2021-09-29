1bd176abae67c535bff7924976385c0a96240a7b
Luke Shumaker <lukeshu@datawire.io>
2021-09-29 01:54:48 -0600

    make lint, make format: Check consistent formatting of python/schemas/ [ci-skip]
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>
    (cherry picked from commit 64f696ba28e0e4b6a0acc9b64d838f36f2793435)

--- build-aux-local/lint.mk
+++ build-aux-local/lint.mk
@@ -1,10 +1,6 @@
 # Must be included *after* generate.mk, because we use GOHOSTOS and GOHOSTARCH defined there.
 
-# The version number of golangci-lint is controllers in a go.mod file
-tools/golangci-lint = $(OSS_HOME)/bin_$(GOHOSTOS)_$(GOHOSTARCH)/golangci-lint
-$(tools/golangci-lint): $(OSS_HOME)/build-aux/bin-go/golangci-lint/go.mod
-	mkdir -p $(@D)
-	cd $(<D) && go build -o $@ github.com/golangci/golangci-lint/cmd/golangci-lint
+include build-aux/tools.mk
 
 lint/go-dirs = $(OSS_HOME)
 
@@ -19,10 +15,14 @@ lint:
 		printf "$(CYN)==>$(END) Linting $(BLU)Helm$(END)...\n"; \
 		helm_status=0; $(MAKE) lint-chart || { helm_status=$$?; r=$$helm_status; }; \
 		\
+		printf "$(CYN)==>$(END) Linting $(BLU)Schemas$(END)...\n"; \
+		schema_status=0; $(MAKE) schemalint || { schema_status=$$?; r=$$schema_status; }; \
+		\
 		set +x; \
-		printf "$(CYN)==>$(END) $(BLU)Go$(END)     lint $$(if [[ $$go_status   == 0 ]]; then printf "$(GRN)OK"; else printf "$(RED)FAIL"; fi)$(END)\n"; \
-		printf "$(CYN)==>$(END) $(BLU)Python$(END) lint $$(if [[ $$py_status   == 0 ]]; then printf "$(GRN)OK"; else printf "$(RED)FAIL"; fi)$(END)\n"; \
-		printf "$(CYN)==>$(END) $(BLU)Helm$(END)   lint $$(if [[ $$helm_status == 0 ]]; then printf "$(GRN)OK"; else printf "$(RED)FAIL"; fi)$(END)\n"; \
+		printf "$(CYN)==>$(END) $(BLU)Go$(END)      lint $$(if [[ $$go_status     == 0 ]]; then printf "$(GRN)OK"; else printf "$(RED)FAIL"; fi)$(END)\n"; \
+		printf "$(CYN)==>$(END) $(BLU)Python$(END)  lint $$(if [[ $$py_status     == 0 ]]; then printf "$(GRN)OK"; else printf "$(RED)FAIL"; fi)$(END)\n"; \
+		printf "$(CYN)==>$(END) $(BLU)Helm$(END)    lint $$(if [[ $$helm_status   == 0 ]]; then printf "$(GRN)OK"; else printf "$(RED)FAIL"; fi)$(END)\n"; \
+		printf "$(CYN)==>$(END) $(BLU)Schemas$(END) lint $$(if [[ $$schema_status == 0 ]]; then printf "$(GRN)OK"; else printf "$(RED)FAIL"; fi)$(END)\n"; \
 		set -x; \
 		\
 		exit $$r; \
@@ -38,10 +38,15 @@ golint: $(tools/golangci-lint)
 	}
 .PHONY: golint
 
-format: $(tools/golangci-lint)
+schemalint: $(tools/schema-fmt)
+	$(tools/schema-fmt) -d python/schemas/*/*.schema
+.PHONY: schemalint
+
+format: $(tools/golangci-lint) $(tools/schema-fmt)
 	@PS4=; set -x; { \
 		for dir in $(lint/go-dirs); do \
 			(cd $$dir && $(tools/golangci-lint) run --fix ./...) || true; \
 		done; \
 	}
+	$(tools/schema-fmt) -w python/schemas/*/*.schema
 .PHONY: format
