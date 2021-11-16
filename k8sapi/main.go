d66dff8bcf89f4c371ce0152fc852442548eaab5
Aidan Hahn <aidanhahn@datawire.io>
2021-11-16 15:49:39 -0800

    kubebuilder wants it like this
    Signed-off-by: Aidan Hahn <aidanhahn@datawire.io>

---
+++ k8sapi/main.go
@@ -0,0 +1,27 @@
+package k8sapi
+
+import (
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	getambassadoriov3alpha1 "github.com/emissary-ingress/emissary/api/v3alpha1"
+	// +kubebuilder:scaffold:imports
+)
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(getambassadoriov3alpha1.AddToScheme(scheme))
+	//+kubebuilder:scaffold:scheme
+}
