186c96ed7484a973b7a28ee35cc226674d1e236b
Luke Shumaker <lukeshu@datawire.io>
2022-01-13 12:41:56 -0700

    pkg/snapshot/v1/annotations.go: Re-work to compile in modern Emissary
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- pkg/snapshot/v1/annotations.go
+++ pkg/snapshot/v1/annotations.go
@@ -1,29 +1,49 @@
-package watt
+package snapshot
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
-	"github.com/datawire/ambassador/pkg/k8s"
-	"github.com/datawire/ambassador/pkg/watt"
+	"github.com/datawire/ambassador/v2/pkg/kates"
 )
 
-func parseAnnotationResources(resource k8s.Resource) (annotationResources []k8s.Resource, annotationErrs []watt.Error) {
-	annotationStr, annotationStrOK := resource.Metadata().Annotations()["getambassador.io/config"].(string)
+//nolint:unused // This will be used by a later commit.
+type wattError struct {
+	Source kates.Object
+	Err    error
+}
+
+//nolint:unused // This will be used by a later commit.
+func (e *wattError) Error() string {
+	return fmt.Sprintf("%s/%s.%s: %v",
+		e.Source.GetObjectKind().GroupVersionKind().Kind,
+		e.Source.GetName(), e.Source.GetNamespace(),
+		e.Err)
+}
+
+//nolint:unused // This will be used by a later commit.
+func parseAnnotationResources(resource kates.Object) (annotationResources []kates.Object, annotationErrs []*wattError) {
+	annotationStr, annotationStrOK := resource.GetAnnotations()["getambassador.io/config"]
 	if !annotationStrOK {
 		return
 	}
 	// Parse in to a scratch _annotationResources list instead of the final annotationResources, so that we can more
 	// easily prune invalid entries out before returning it.
-	_annotationResources, err := k8s.ParseResources("", annotationStr)
+	_annotationResources, err := kates.ParseManifestsToUnstructured(annotationStr)
 	if err != nil {
-		annotationErrs = append(annotationErrs, watt.NewError(
-			fmt.Sprintf("%s/%s", resource.QKind(), resource.QName()),
-			"could not read YAML in annotation getambassador.io/config"))
+		annotationErrs = append(annotationErrs, &wattError{
+			Source: resource,
+			Err:    fmt.Errorf("could not read YAML in annotation getambassador.io/config: %w", err),
+		})
 		return
 	}
-	for _, annotationResource := range _annotationResources {
+	for _, _annotationResource := range _annotationResources {
+		annotationResource := _annotationResource.(*kates.Unstructured).Object
 		// Un-fold annotations with collapsed metadata/spec
+		if _, ok := annotationResource["apiVersion"].(string); !ok {
+			annotationResource["apiVersion"] = ""
+		}
 		if dat, ok := annotationResource["metadata"].(map[string]interface{}); !ok || dat == nil {
 			annotationResource["metadata"] = map[string]interface{}{}
 		}
@@ -47,32 +67,34 @@ func parseAnnotationResources(resource k8s.Resource) (annotationResources []k8s.
 		}
 
 		// Default attributes from the parent
-		if annotationResource.Namespace() == "" {
-			annotationResource.Metadata()["namespace"] = resource.Namespace()
+		if ns, ok := annotationResource["metadata"].(map[string]interface{})["namespace"].(string); !ok || ns == "" {
+			annotationResource["metadata"].(map[string]interface{})["namespace"] = resource.GetNamespace()
 		}
-		if annotationResource.Metadata()["labels"] == nil && resource.Metadata()["labels"] != nil {
-			annotationResource.Metadata()["labels"] = resource.Metadata()["labels"]
+		if annotationResource["metadata"].(map[string]interface{})["labels"] == nil && resource.GetLabels() != nil {
+			annotationResource["metadata"].(map[string]interface{})["labels"] = resource.GetLabels()
 		}
 
 		// The Canonical API Version for our resources always starts with "getambassador.io/",
 		// but it used to always start with "ambassador/". Translate as needed for backward
 		// compatibility.
-		if apiVersion := k8s.Map(annotationResource).GetString("apiVersion"); strings.HasPrefix(apiVersion, "ambassador/") {
+		if apiVersion := annotationResource["apiVersion"].(string); strings.HasPrefix(apiVersion, "ambassador/") {
 			annotationResource["apiVersion"] = "getambassador.io/" + strings.TrimPrefix(apiVersion, "ambassador/")
 		}
 
 		// Make sure it's in the right API group
-		if !strings.HasPrefix(k8s.Map(annotationResource).GetString("apiVersion"), "getambassador.io/") {
-			annotationErrs = append(annotationErrs, watt.NewError(
-				fmt.Sprintf("%s/%s: annotation getambassador.io/config: %s/%s",
-					resource.QKind(), resource.QName(),
-					annotationResource.QKind(), annotationResource.QName()),
-				"not in the getambassador.io apiGroup, ignoring"))
+		if !strings.HasPrefix(annotationResource["apiVersion"].(string), "getambassador.io/") {
+			annotationErrs = append(annotationErrs, &wattError{
+				Source: resource,
+				Err: fmt.Errorf("annotation getambassador.io/config: %w", &wattError{
+					Source: &kates.Unstructured{Object: annotationResource},
+					Err:    errors.New("not in the getambassador.io apiGroup, ignoring"),
+				}),
+			})
 			continue
 		}
 
 		// Add it to the snapshot
-		annotationResources = append(annotationResources, annotationResource)
+		annotationResources = append(annotationResources, &kates.Unstructured{Object: annotationResource})
 	}
 	return
 }
