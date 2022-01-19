c1c21814818646dd61ae99b25f758f5e934b32a3
Luke Shumaker <lukeshu@datawire.io>
2022-01-18 17:30:43 -0700

    Rip out the Python validator
    IDK why v2/zz_generated.deepcopy.go changed.
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- build-aux/lint.mk
+++ build-aux/lint.mk
@@ -13,14 +13,10 @@ lint:
 		printf "$(CYN)==>$(END) Linting $(BLU)Helm$(END)...\n"; \
 		helm_status=0; $(MAKE) lint-chart || { helm_status=$$?; r=$$helm_status; }; \
 		\
-		printf "$(CYN)==>$(END) Linting $(BLU)Schemas$(END)...\n"; \
-		schema_status=0; $(MAKE) schemalint || { schema_status=$$?; r=$$schema_status; }; \
-		\
 		set +x; \
 		printf "$(CYN)==>$(END) $(BLU)Go$(END)      lint $$(if [[ $$go_status     == 0 ]]; then printf "$(GRN)OK"; else printf "$(RED)FAIL"; fi)$(END)\n"; \
 		printf "$(CYN)==>$(END) $(BLU)Python$(END)  lint $$(if [[ $$py_status     == 0 ]]; then printf "$(GRN)OK"; else printf "$(RED)FAIL"; fi)$(END)\n"; \
 		printf "$(CYN)==>$(END) $(BLU)Helm$(END)    lint $$(if [[ $$helm_status   == 0 ]]; then printf "$(GRN)OK"; else printf "$(RED)FAIL"; fi)$(END)\n"; \
-		printf "$(CYN)==>$(END) $(BLU)Schemas$(END) lint $$(if [[ $$schema_status == 0 ]]; then printf "$(GRN)OK"; else printf "$(RED)FAIL"; fi)$(END)\n"; \
 		set -x; \
 		\
 		exit $$r; \
@@ -36,15 +32,10 @@ golint: $(tools/golangci-lint)
 	}
 .PHONY: golint
 
-schemalint: $(tools/schema-fmt)
-	$(tools/schema-fmt) -d python/schemas/*/*.schema
-.PHONY: schemalint
-
-format: $(tools/golangci-lint) $(tools/schema-fmt)
+format: $(tools/golangci-lint)
 	@PS4=; set -x; { \
 		for dir in $(lint/go-dirs); do \
 			(cd $$dir && $(tools/golangci-lint) run --fix ./...) || true; \
 		done; \
 	}
-	$(tools/schema-fmt) -w python/schemas/*/*.schema
 .PHONY: format
