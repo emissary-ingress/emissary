c8ea663741e62f2d91cb09add738b36f9b17bd7f
Flynn <flynn@datawire.io>
2022-01-18 19:01:22 -0500

    Hack: logging in apiext
    Signed-off-by: Flynn <flynn@datawire.io>
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- cmd/apiext/serve.go
+++ cmd/apiext/serve.go
@@ -1,17 +1,21 @@
 package apiext
 
 import (
+	"bytes"
 	"context"
 	"crypto/tls"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 
 	// k8s utils
 	k8sRuntime "k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/conversion"
 
 	"github.com/datawire/dlib/dhttp"
+	"github.com/datawire/dlib/dlog"
 )
 
 const (
@@ -19,6 +23,38 @@ const (
 	pathProbesReady        = "/probes/ready"
 	pathProbesLive         = "/probes/live"
 )
+
+func doConversion(wh *conversion.Webhook, w http.ResponseWriter, r *http.Request) {
+	if r.Body == nil {
+		dlog.Errorf(r.Context(), "no conversion request provided?")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	inputBytes, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		dlog.Errorf(r.Context(), "could not read conversion request: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	dlog.Infof(r.Context(), "INPUT: %s", string(inputBytes))
+
+	r.Body = io.NopCloser(bytes.NewBuffer(inputBytes))
+	rec := httptest.NewRecorder()
+	wh.ServeHTTP(rec, r)
+
+	dlog.Infof(r.Context(), "OUTPUT: %s", rec.Body)
+
+	for k, v := range rec.Result().Header {
+		w.Header()[k] = v
+	}
+
+	w.WriteHeader(rec.Code)
+	//nolint:errcheck
+	rec.Body.WriteTo(w)
+}
 
 func ServeHTTPS(ctx context.Context, port int, ca *CA, scheme *k8sRuntime.Scheme) error {
 	webhook := &conversion.Webhook{}
@@ -30,8 +66,12 @@ func ServeHTTPS(ctx context.Context, port int, ca *CA, scheme *k8sRuntime.Scheme
 
 	mux.Handle(pathWebhooksCrdConvert, webhook)
 
+	dlog.Infof(ctx, "Serving HTTPS on port %d", port)
+
 	sc := &dhttp.ServerConfig{
-		Handler: mux,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			doConversion(webhook, w, r)
+		}),
 		TLSConfig: &tls.Config{
 			GetCertificate: func(clientHello *tls.ClientHelloInfo) (*tls.Certificate, error) {
 				return ca.GenServerCert(ctx, clientHello.ServerName)
