d901c47d2566d380b80e56593d64ad32e5274c92
Luke Shumaker <lukeshu@datawire.io>
2022-01-24 17:48:45 -0700

    Avoid importing anything from tests/integration or KAT in tests/unit
    This involves moving some code from python/tests/utils.py to
    python/tests/integration/utils.py.
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- python/tests/kat/t_ingress.py
+++ python/tests/kat/t_ingress.py
@@ -9,7 +9,7 @@ import time
 from kat.harness import Query, is_ingress_class_compatible
 from abstract_tests import AmbassadorTest, HTTP, ServiceType
 from kat.utils import namespace_manifest
-from tests.utils import KUBESTATUS_PATH
+from tests.integration.utils import KUBESTATUS_PATH
 from ambassador.utils import parse_bool
 
 class IngressStatusTest1(AmbassadorTest):
