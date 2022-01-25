2bc73e9f06efe3a342c4f9bc41e3380d6b8074c6
Luke Shumaker <lukeshu@datawire.io>
2022-01-24 17:58:07 -0700

    Makefile: Have the pytests (except unit) depend on the nescessary images
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- build-aux/check.mk
+++ build-aux/check.mk
@@ -1,6 +1,16 @@
 include build-aux/tools.mk
 
-# These are triggered by `python/tests/integration/manifests.py`.
+# Keep this list in-sync with python/tests/integration/manifests.py
+push-pytest-images: docker/emissary.docker.push.remote
+push-pytest-images: docker/test-auth.docker.push.remote
+push-pytest-images: docker/test-ratelimit.docker.push.remote
+push-pytest-images: docker/test-shadow.docker.push.remote
+push-pytest-images: docker/test-stats.docker.push.remote
+push-pytest-images: docker/kat-client.docker.push.remote
+push-pytest-images: docker/kat-server.docker.push.remote
+.PHONY: push-pytest-images
+
+# These are triggered by `python/tests/integration/manifests.py` (or by `push-pytest-images`)
 test_svcs = auth ratelimit shadow stats
 $(foreach svc,$(test_svcs),docker/.test-$(svc).docker.stamp): docker/.%.docker.stamp: docker/%/Dockerfile FORCE
 	docker build --iidfile=$@ $(<D)
