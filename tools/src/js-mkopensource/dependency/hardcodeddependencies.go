2e33add006712092d5b4c1942e9f42dc0b8fe5c2
Andres Osorio <aosorio@datawire.io>
2022-01-28 08:43:26 -0500

    Fix linting issue

--- tools/src/js-mkopensource/dependency/hardcodeddependencies.go
+++ tools/src/js-mkopensource/dependency/hardcodeddependencies.go
@@ -3,7 +3,7 @@ package dependency
 import "github.com/datawire/go-mkopensource/pkg/detectlicense"
 
 var hardcodedDependencies = map[string][]string{
-	"cyclist@0.2.2": []string{detectlicense.MIT.Name},
-	"indexof@0.0.1": []string{detectlicense.MIT.Name},
-	"pako@1.0.10":   []string{detectlicense.MIT.Name},
+	"cyclist@0.2.2": {detectlicense.MIT.Name},
+	"indexof@0.0.1": {detectlicense.MIT.Name},
+	"pako@1.0.10":   {detectlicense.MIT.Name},
 }
