8643bf7cb7ecc5e9ec91c2fa36b70fa6afa0a430
Andres Osorio <aosorio@datawire.io>
2022-01-28 18:38:13 -0500

    Simplify build scripts
    Signed-off-by: Andres Osorio <aosorio@datawire.io>

--- build-aux/license-info/python-deps.sh
+++ build-aux/license-info/python-deps.sh
@@ -1,11 +1,24 @@
 #!/bin/env bash
 set -e
 
-DIR=$(dirname $1)
-pushd "${DIR}" >/dev/null
+function scan_dependencies() {
+  >&2 echo "Scannning file $1"
 
-docker run --rm -i "python-deps-builder" sh -c 'tar xf - && pip3 --disable-pip-version-check install -r requirements.txt >/dev/null && pip3 --disable-pip-version-check freeze --exclude-editable  | cut -d= -f1 | xargs pip show' \
-       < <(tar cf - requirements.txt) \
-       >pip-deps.txt
-echo '---' >>pip-deps.txt
-popd >/dev/null
+  DIR=$(dirname $1)
+  pushd "${DIR}" >/dev/null
+
+  docker run --rm -i "python-deps-builder" sh -c 'tar xf - && pip3 --disable-pip-version-check install -r requirements.txt >/dev/null && pip3 --disable-pip-version-check freeze --exclude-editable  | cut -d= -f1 | xargs pip show' \
+    < <(tar cf - requirements.txt) \
+
+  echo '---'
+  popd >/dev/null
+}
+
+export -f scan_dependencies
+
+find . \( -path "./_cxx/envoy/*" \
+  -o -path "./docker/test-auth/*" \
+  -o -path "./docker/test-shadow/*" \
+  -o -path "./docker/test-stats/*" \
+  -o -path "./_generate.tmp/*" \
+  \) -prune -o -name requirements.txt -type f -exec bash -c 'scan_dependencies "{}"' \;
