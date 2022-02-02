970f048d0d3d3de70f5dc0e41ac7994e58628ce6
Aidan Hahn <aidanhahn@datawire.io>
2022-02-01 16:01:26 -0800

    remove superfluous changes to extauth and ratelimit tests
    Signed-off-by: Aidan Hahn <aidanhahn@datawire.io>

--- python/tests/kat/abstract_tests.py
+++ python/tests/kat/abstract_tests.py
@@ -450,7 +450,7 @@ class AHTTP(ServiceType):
 class AGRPC(ServiceType):
     skip_variant: ClassVar[bool] = True
 
-    def __init__(self, protocol_version: str="v3", *args, **kwargs) -> None:
+    def __init__(self, protocol_version: str="v2", *args, **kwargs) -> None:
         self.protocol_version = protocol_version
 
         # Do this unconditionally, because that's the point of this class.
@@ -463,7 +463,7 @@ class AGRPC(ServiceType):
 class RLSGRPC(ServiceType):
     skip_variant: ClassVar[bool] = True
 
-    def __init__(self, protocol_version: str="v3", *args, **kwargs) -> None:
+    def __init__(self, protocol_version: str="v2", *args, **kwargs) -> None:
         self.protocol_version = protocol_version
 
         # Do this unconditionally, because that's the point of this class.
