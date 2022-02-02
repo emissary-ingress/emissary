3ac6117def422bf87625d4cf353f42f9bd1e69ad
Aidan Hahn <aidanhahn@datawire.io>
2022-02-02 14:17:31 -0800

    fix filters and remove outdated tests
    * dont set transport api protocol to V2
    * remove tests for unsupported configuration apis
    
    Signed-off-by: Aidan Hahn <aidanhahn@datawire.io>

--- python/tests/kat/t_ratelimit.py
+++ python/tests/kat/t_ratelimit.py
@@ -8,109 +8,7 @@ from tests.selfsigned import TLSCerts
 from ambassador import Config
 
 
-class RateLimitV0Test(AmbassadorTest):
-    # debug = True
-    target: ServiceType
-    rls: ServiceType
-
-    def init(self):
-        self.target = HTTP()
-        self.rls = RLSGRPC()
-
-    def config(self) -> Generator[Union[str, Tuple[Node, str]], None, None]:
-        # Use self.target here, because we want this mapping to be annotated
-        # on the service, not the Ambassador.
-        # ambassador_id: [ {self.with_tracing.ambassador_id}, {self.no_tracing.ambassador_id} ]
-        yield self.target, self.format("""
----
-apiVersion: getambassador.io/v3alpha1
-kind: Mapping
-name:  ratelimit_target_mapping
-hostname: "*"
-prefix: /target/
-service: {self.target.path.fqdn}
-labels:
-  ambassador:
-    - request_label_group:
-      - request_headers:
-          key: x-ambassador-test-allow
-          header_name: "x-ambassador-test-allow"
-          omit_if_not_present: true
-      - request_headers:
-          key: x-ambassador-test-headers-append
-          header_name: "x-ambassador-test-headers-append"
-          omit_if_not_present: true
----
-apiVersion: getambassador.io/v3alpha1
-kind: Mapping
-name:  ratelimit_label_mapping
-hostname: "*"
-prefix: /labels/
-service: {self.target.path.fqdn}
-labels:
-  ambassador:
-    - host_and_user:
-      - request_headers:
-          key: custom-label
-          header_name: ":authority"
-          omit_if_not_present: true
-      - request_headers:
-          key: user
-          header_name: "x-user"
-          omit_if_not_present: true
-
-    - omg_header:
-      - request_headers:
-          key: custom-label
-          header_name: "x-omg"
-          default: "OMFG!"
-""")
-
-        # For self.with_tracing, we want to configure the TracingService.
-        yield self, self.format("""
----
-apiVersion: getambassador.io/v3alpha1
-kind: RateLimitService
-name: {self.rls.path.k8s}
-service: "{self.rls.path.fqdn}"
-timeout_ms: 500
-""")
-
-    def queries(self):
-        # Speak through each Ambassador to the traced service...
-        # yield Query(self.with_tracing.url("target/"))
-        # yield Query(self.no_tracing.url("target/"))
-
-        # [0]
-        # No matching headers, won't even go through ratelimit-service filter
-        yield Query(self.url("target/"))
-
-        # [1]
-        # Header instructing dummy ratelimit-service to allow request
-        yield Query(self.url("target/"), expected=200, headers={
-            'x-ambassador-test-allow': 'true',
-            'x-ambassador-test-headers-append': 'no header',
-        })
-
-        # [2]
-        # Header instructing dummy ratelimit-service to reject request with
-        # a custom response body
-        yield Query(self.url("target/"), expected=429, headers={
-            'x-ambassador-test-allow': 'over my dead body',
-            'x-ambassador-test-headers-append': 'Hello=Foo; Hi=Baz',
-        })
-
-    def check(self):
-        # [2] Verifies the 429 response and the proper content-type.
-        # The kat-server gRPC ratelimit implementation explicitly overrides
-        # the content-type to json, because the response is in fact json
-        # and we need to verify that this override is possible/correct.
-        assert self.results[2].headers["Hello"] == [ "Foo" ]
-        assert self.results[2].headers["Hi"] == [ "Baz" ]
-        assert self.results[2].headers["Content-Type"] == [ "application/json" ]
-        assert self.results[2].headers["X-Grpc-Service-Protocol-Version"] == [ "v2" ]
-
-class RateLimitV1Test(AmbassadorTest):
+class RateLimitV3Test(AmbassadorTest):
     # debug = True
     target: ServiceType
 
@@ -149,6 +47,7 @@ kind: RateLimitService
 name: {self.rls.path.k8s}
 service: "{self.rls.path.fqdn}"
 timeout_ms: 500
+protocol_version: "v3"
 """)
 
     def queries(self):
