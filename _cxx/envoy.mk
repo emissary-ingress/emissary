2fb7c7320cae7d69d7a41727a45a82c8ce461578
Aidan Hahn <aidanhahn@datawire.io>
2022-02-14 12:48:30 -0800

    bump envoy
    Signed-off-by: Aidan Hahn <aidanhahn@datawire.io>
    (cherry picked from commit 2cf149d3ab1e3a7e8843bd61f48f6983bd63b5bb)
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- _cxx/envoy.mk
+++ _cxx/envoy.mk
@@ -6,7 +6,7 @@ ENVOY_TEST_LABEL ?= //test/...
 
 # IF YOU MESS WITH ANY OF THESE VALUES, YOU MUST RUN `make update-base`.
   ENVOY_REPO ?= $(if $(IS_PRIVATE),git@github.com:datawire/envoy-private.git,https://github.com/datawire/envoy.git)
-  ENVOY_COMMIT ?= 049f125c1c9a6cd7e49bd4a660cdeccd9f6ec383
+  ENVOY_COMMIT ?= 4ce93dc3ace00ae9108b179d0afaceac13f4602a
   ENVOY_COMPILATION_MODE ?= opt
   # Increment BASE_ENVOY_RELVER on changes to `docker/base-envoy/Dockerfile`, or Envoy recipes.
   # You may reset BASE_ENVOY_RELVER when adjusting ENVOY_COMMIT.
