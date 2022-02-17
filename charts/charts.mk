0107767698d55f08974ace8a443d8e53d6999e1b
Luke Shumaker <lukeshu@datawire.io>
2022-02-17 12:55:41 -0700

    fix: Don't try to push to the unused 'chart-builds' or 'passed-builds' S3 prefixes
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- charts/charts.mk
+++ charts/charts.mk
@@ -9,11 +9,3 @@ else
 	CHART_NAME=$(notdir $(EMISSARY_CHART)) $(OSS_HOME)/charts/scripts/push_chart.sh
 endif
 .PHONY: release/push-chart
-
-release/promote-chart-passed:
-	@set -ex; { \
-	  commit=$$(git rev-parse HEAD); \
-	  printf "$(CYN)==> $(GRN)Promoting $(BLU)$$commit$(GRN) in S3...$(END)\n"; \
-	  echo "PASSED" | aws s3 cp - s3://$(AWS_S3_BUCKET)/chart-builds/$$commit; \
-	}
-.PHONY: release/promote-chart-passed
