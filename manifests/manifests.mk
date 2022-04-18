ee780d8bac894b75c1b90b7f81f8485c9ffa47c0
Luke Shumaker <lukeshu@datawire.io>
2022-04-18 13:02:08 -0600

    misc cleanup: manifests/manifests.mk: Clean up
    This isn't really changing anything, just cleaning it up so I can read
    what's there.
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- manifests/manifests.mk
+++ manifests/manifests.mk
@@ -1,7 +1,8 @@
 push-manifests:
-	if [ ! -z $(IS_PRIVATE) ]; then \
-		echo "Private repo, not pushing chart" ;\
-		exit 1 ;\
-	fi;
-	$(OSS_HOME)/manifests/push_manifests.sh
+ifneq ($(IS_PRIVATE),)
+	@echo "Private repo, not pushing chart" >&2
+	@exit 1
+else
+	manifests/push_manifests.sh
+endif
 .PHONY: push-manifests
