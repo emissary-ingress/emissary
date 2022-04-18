a2cf61f44aee46b78b24bee60e503dfa7690e760
Luke Shumaker <lukeshu@datawire.io>
2022-04-18 12:08:58 -0600

    misc cleanup: manifests/push_manifests.sh: Clean up; get it to pass shellcheck
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- manifests/manifests.mk
+++ manifests/manifests.mk
@@ -3,6 +3,6 @@ ifneq ($(IS_PRIVATE),)
 	@echo "Private repo, not pushing chart" >&2
 	@exit 1
 else
-	manifests/push_manifests.sh
+	manifests/push_manifests.sh manifests/emissary
 endif
 .PHONY: push-manifests
