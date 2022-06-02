a80d5190d303ac049dbc1939ea37d40ec534592a
Luke Shumaker <lukeshu@datawire.io>
2022-06-01 18:38:12 -0600

    LogService: Document that .spec.grpc should go away
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- pkg/api/getambassador.io/v3alpha1/crd_logservice.go
+++ pkg/api/getambassador.io/v3alpha1/crd_logservice.go
@@ -44,8 +44,13 @@ type LogServiceSpec struct {
 	DriverConfig          *DriverConfig   `json:"driver_config,omitempty"`
 	FlushIntervalTime     *SecondDuration `json:"flush_interval_time,omitempty"`
 	FlushIntervalByteSize *int            `json:"flush_interval_byte_size,omitempty"`
-	GRPC                  *bool           `json:"grpc,omitempty"`
-	StatsName             string          `json:"stats_name,omitempty"`
+
+	// TODO(lukeshu): In v3alpha2, drop this LogService.spec.grpc.  Due to sloppy implementation
+	// it is required to be present, and required to be 'true'.  It is silly to have a required
+	// field with only one valid value, we should just remove the thing.
+	GRPC *bool `json:"grpc,omitempty"`
+
+	StatsName string `json:"stats_name,omitempty"`
 }
 
 // LogService is the Schema for the logservices API
