7116c7d5e288b7c0642c33cf149b194a3c981d16
AliceProxy <alicewasko@datawire.io>
2022-06-02 09:03:49 -0700

    filter-secrets: fix style errors
    Signed-off-by: AliceProxy <alicewasko@datawire.io>

--- cmd/entrypoint/testutil_fake_filtersecrets_test.go
+++ cmd/entrypoint/testutil_fake_filtersecrets_test.go
@@ -3,9 +3,10 @@ package entrypoint
 import (
 	"testing"
 
-	"github.com/datawire/ambassador/v2/pkg/snapshot/v1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/datawire/ambassador/v2/pkg/snapshot/v1"
 )
 
 type subtest struct {
