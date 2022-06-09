emissary-ingress/emissary

commit c014b4ecbc6579ff449f9d2b17a52b9f238b88e5

Begone with release-wait-for-commit in favor of devversion
Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

builder/builder.mk
@@ -266,13 +266,6 @@ push-dev: docker/$(LCNAME).docker.tag.local
 	@printf '$(CYN)==> $(GRN)pushing $(BLU)%s$(GRN) as $(BLU)$(GRN)...$(END)\n' '$(LCNAME)' '$(DEV_REGISTRY)/$(LCNAME):$(patsubst v%,%,$(VERSION))'
 	docker tag $$(cat docker/$(LCNAME).docker) '$(DEV_REGISTRY)/$(LCNAME):$(patsubst v%,%,$(VERSION))'
 	docker push '$(DEV_REGISTRY)/$(LCNAME):$(patsubst v%,%,$(VERSION))'
-
-ifneq ($(IS_PRIVATE),)
-	@echo '$@: not pushing to S3 because this is a private repo'
-else
-	@printf '$(CYN)==> $(GRN)recording $(BLU)%s$(GRN) => $(BLU)%s$(GRN) in S3...$(END)\n' "$$(git rev-parse HEAD)" $(patsubst v%,%,$(VERSION))
-	echo '$(patsubst v%,%,$(VERSION))' | aws s3 cp - 's3://$(AWS_S3_BUCKET)/dev-builds/'"$$(git rev-parse HEAD)"
-endif
 .PHONY: push-dev
 
 export KUBECONFIG_ERR=$(RED)ERROR: please set the $(BLU)DEV_KUBECONFIG$(RED) make/env variable to the cluster\n       you would like to use for development. Note this cluster must have access\n       to $(BLU)DEV_REGISTRY$(RED) (currently $(BLD)$(DEV_REGISTRY)$(END)$(RED))$(END)
@@ -511,18 +504,12 @@ release/promote-oss/.main: $(tools/docker-promote)
 	printf '{"application":"emissary","latest_version":"%s","notices":[]}' "$(patsubst v%,%,$(VERSION))" | aws s3 cp - s3://scout-datawire-io/emissary-ingress/$(PROMOTE_CHANNEL)app.json
 .PHONY: release/promote-oss/.main
 
-release/promote-oss/to-rc:
+release/promote-oss/to-rc: $(tools/devversion)
 	@test -n "$(RELEASE_REGISTRY)" || (printf "$${RELEASE_REGISTRY_ERR}\n"; exit 1)
 	@[[ "$(VERSION)" =~ ^v[0-9]+\.[0-9]+\.[0-9]+-rc\.[0-9]+$$ ]] || (printf '$(RED)ERROR: VERSION=%s does not look like an RC tag\n' "$(VERSION)"; exit 1)
 	@set -e; { \
-	  commit=$$(git rev-parse HEAD) ;\
-	  $(OSS_HOME)/releng/release-wait-for-commit --commit $$commit --s3-key dev-builds ;\
-	  dev_version=$$(aws s3 cp s3://$(AWS_S3_BUCKET)/dev-builds/$$commit -) ;\
-	  if [ -z "$$dev_version" ]; then \
-	    printf "$(RED)==> found no dev version for $$commit in S3...$(END)\n" ;\
-	    exit 1 ;\
-	  fi ;\
-	  printf "$(CYN)==> $(GRN)found version $(BLU)$$dev_version$(GRN) for $(BLU)$$commit$(GRN) in S3...$(END)\n" ;\
+	  dev_version=$$($(tools/devversion)); \
+	  printf "$(CYN)==> $(GRN)found version $(BLU)$$dev_version$(GRN).$(END)\n"; \
 	  $(MAKE) release/promote-oss/.main \
 	    PROMOTE_FROM_VERSION="$$dev_version" \
 	    PROMOTE_FROM_REPO=$(DEV_REGISTRY) \
@@ -540,18 +527,12 @@ endif
 
 # To be run from a checkout at the tag you are promoting _from_.
 # This is normally run from CI by creating the GA tag.
-release/promote-oss/to-ga:
+release/promote-oss/to-ga: $(tools/devversion)
 	@test -n "$(RELEASE_REGISTRY)" || (printf "$${RELEASE_REGISTRY_ERR}\n"; exit 1)
 	@[[ "$(VERSION)" =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-ea)?$$ ]] || (printf '$(RED)ERROR: VERSION=%s does not look like a GA tag\n' "$(VERSION)"; exit 1)
 	@set -e; { \
-	  commit=$$(git rev-parse HEAD) ;\
-	  $(OSS_HOME)/releng/release-wait-for-commit --commit $$commit --s3-key dev-builds ; \
-	  dev_version=$$(aws s3 cp s3://$(AWS_S3_BUCKET)/dev-builds/$$commit -) ;\
-	  if [ -z "$$dev_version" ]; then \
-	    printf "$(RED)==> found no passed dev version for $$commit in S3...$(END)\n" ;\
-	    exit 1 ;\
-	  fi ;\
-	  printf "$(CYN)==> $(GRN)found version $(BLU)$$dev_version$(GRN) for $(BLU)$$commit$(GRN) in S3...$(END)\n" ;\
+	  dev_version=$$($(tools/devversion)); \
+	  printf "$(CYN)==> $(GRN)found version $(BLU)$$dev_version$(GRN).$(END)\n"; \
 	  $(MAKE) release/promote-oss/.main \
 	    PROMOTE_FROM_VERSION="$$dev_version" \
 	    PROMOTE_FROM_REPO=$(DEV_REGISTRY) \

releng/release-wait-for-commit
@@ -1,49 +1,0 @@
-#!/usr/bin/env python3
-"""Waits for commit to show up in s3
-"""
-
-import argparse
-import http.client
-import os
-import sys
-import time
-from lib import ansiterm
-from datetime import datetime, timedelta
-
-
-def main(commit: str, s3_key: str, timeout: int) -> int:
-    s3_bucket = os.getenv("AWS_S3_BUCKET")
-
-    if s3_bucket == '':
-        s3_bucket = 'datawire-static-files'
-    print(f"{ansiterm.sgr.bold.fg_cyn}==> Waiting for commit {commit} to show up in s3://{s3_bucket}/{s3_key}/")
-    print(f"    Check Circle CI if you would like to check progress:")
-    print(f"        https://app.circleci.com/pipelines/github/emissary-ingress/emissary{ansiterm.sgr.reset}")
-    s3_url = f"{s3_bucket}.s3.amazonaws.com"
-    started = datetime.now()
-    stop_time = started + timedelta(minutes=timeout)
-    while datetime.now() < stop_time:
-        conn = http.client.HTTPSConnection(s3_url)
-        conn.request("GET", f"/{s3_key}/{commit}")
-        r1 = conn.getresponse()
-        if r1.status == 200:
-            print(f"{ansiterm.sgr.bold.fg_grn}==> Commit {commit} found in S3!{ansiterm.sgr}")
-            return 0
-        print(f"    {ansiterm.sgr.fg_red}Commit {commit} still not in S3, sleeping before trying again.{ansiterm.sgr}")
-        time.sleep(30)
-    print('Timed out waiting for commit')
-    return 1
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Wait for commit to show up in s3')
-    parser.add_argument('--commit', required=True)
-    parser.add_argument('--s3-key', default="dev-builds")
-    parser.add_argument('--timeout-minutes', type=int, default=10)
-    args = parser.parse_args()
-
-    sys.exit(main(
-        commit=args.commit,
-        s3_key=args.s3_key,
-        timeout=args.timeout_minutes,
-    ))

commit 1c90674b374fadc7869b6aab77f44b23462270b2

builder/builder.mk: release/promote-oss/.main: Re-think the input variables
 - Have PROMOTE_FROM_VERSION and PROMOTE_TO_VERSION include a leading 'v',
   actually check for this.
 - Have PROMOTE_FROM_REPO be a repo, not a registry
 - Have PROMOTE_FROM_REPO be required, rather than falling back to
   RELEASE_REGISTRY.
 - Add a PROMOTE_TO_REPO instead of using RELEASE_REGISTRY.
 - Don't use VERSION, explicitly use PROMOTE_TO_VERSION

And while we're at it, add a 'pushing...' verb to those CYN @printf lines
so it's clear what's being indicated.

This fixes an issue introduced in c014b4ecb (Begone with
release-wait-for-commit in favor of devversion, 2022-02-28) where
PROMOTE_FROM_VERSION needed to *not* have a leading 'v', but it was being
set *with* a leading 'v', and then was failing mysteriously instead of
failing early and clearly.  Now it needs the 'v', and also does an early
check for it.

Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

builder/builder.mk
@@ -475,26 +475,26 @@ AMB_IMAGE_RELEASE=$(RELEASE_REGISTRY)/$(REPO):$(patsubst v%,%,$(VERSION))
 export RELEASE_REGISTRY_ERR=$(RED)ERROR: please set the RELEASE_REGISTRY make/env variable to the docker registry\n       you would like to use for release$(END)
 
 release/promote-oss/.main: $(tools/docker-promote)
-	@[[ "$(VERSION)"              =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-.*)?$$ ]] || (echo "Must set VERSION to a vSEMVER value"; exit 1)
-	@[[ -n "$(PROMOTE_FROM_VERSION)" ]] || (echo "MUST SET PROMOTE_FROM_VERSION"; exit 1)
-	@[[ '$(PROMOTE_TO_VERSION)'   =~ ^[0-9]+\.[0-9]+\.[0-9]+(-.*)?$$ ]] || (echo "MUST SET PROMOTE_TO_VERSION" ; exit 1)
-	@case "$(PROMOTE_CHANNEL)" in \
-	  ""|wip|early|test|hotfix) true ;; \
-	  *) echo "Unknown PROMOTE_CHANNEL $(PROMOTE_CHANNEL)" >&2 ; exit 1;; \
+	@[[ '$(PROMOTE_FROM_VERSION)' =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-.*)?$$ ]] || (echo >&2 'Must set PROMOTE_FROM_VERSION to a vSEMVER value'; exit 1)
+	@[[ '$(PROMOTE_TO_VERSION)'   =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-.*)?$$ ]] || (echo >&2 'Must set PROMOTE_TO_VERSION to a vSEMVER value' ; exit 1)
+	@[[ -n '$(PROMOTE_FROM_REPO)'                                     ]] || (echo >&2 'Must set PROMOTE_FROM_REPO' ; exit 1)
+	@[[ -n '$(PROMOTE_TO_REPO)'                                       ]] || (echo >&2 'Must set PROMOTE_TO_REPO' ; exit 1)
+	@case '$(PROMOTE_CHANNEL)' in \
+	  ''|wip|early|test|hotfix) true;; \
+	  *) echo >&2 'Unknown PROMOTE_CHANNEL $(PROMOTE_CHANNEL)'; exit 1;; \
 	esac
-	@[[ -n '$(or $(PROMOTE_FROM_REPO),$(RELEASE_REGISTRY))' ]] || (echo "Must set PROMOTE_FROM_REPO or RELEASE_REGISTRY" ; exit 1)
 
-	@printf "$(CYN)==> $(GRN)Promoting $(BLU)%s$(GRN) to $(BLU)%s$(GRN) (channel=$(BLU)%s$(GRN))$(END)\n" '$(PROMOTE_FROM_VERSION)' '$(PROMOTE_TO_VERSION)' '$(PROMOTE_CHANNEL)'
+	@printf "$(CYN)==> $(GRN)Promoting $(BLU)%s$(GRN) to $(BLU)%s$(GRN) (channel='$(BLU)%s$(GRN)')$(END)\n" '$(PROMOTE_FROM_VERSION)' '$(PROMOTE_TO_VERSION)' '$(PROMOTE_CHANNEL)'
 
-	@printf '  $(CYN)$(or $(PROMOTE_FROM_REPO),$(RELEASE_REGISTRY))/$(REPO):$(PROMOTE_FROM_VERSION)$(END)\n'
-	$(tools/docker-promote) $(or $(PROMOTE_FROM_REPO),$(RELEASE_REGISTRY))/$(REPO):$(PROMOTE_FROM_VERSION) $(RELEASE_REGISTRY)/$(REPO):$(PROMOTE_TO_VERSION)
-	docker push $(RELEASE_REGISTRY)/$(REPO):$(PROMOTE_TO_VERSION)
+	@printf '  pushing $(CYN)$(PROMOTE_TO_REPO):$(PROMOTE_FROM_VERSION)$(END)...\n'
+	$(tools/docker-promote) $(PROMOTE_FROM_REPO):$(PROMOTE_FROM_VERSION) $(PROMOTE_TO_REPO):$(PROMOTE_TO_VERSION)
+	docker push $(PROMOTE_TO_REPO):$(PROMOTE_TO_VERSION)
 
-	@printf '  $(CYN)https://s3.amazonaws.com/$(AWS_S3_BUCKET)/emissary-ingress/$(PROMOTE_CHANNEL)stable.txt$(END)\n'
-	printf '%s' "$(patsubst v%,%,$(VERSION))" | aws s3 cp - s3://$(AWS_S3_BUCKET)/emissary-ingress/$(PROMOTE_CHANNEL)stable.txt
+	@printf '  pushing $(CYN)https://s3.amazonaws.com/$(AWS_S3_BUCKET)/emissary-ingress/$(PROMOTE_CHANNEL)stable.txt$(END)...\n'
+	printf '%s' "$(patsubst v%,%,$(PROMOTE_TO_VERSION))" | aws s3 cp - s3://$(AWS_S3_BUCKET)/emissary-ingress/$(PROMOTE_CHANNEL)stable.txt
 
-	@printf '  $(CYN)s3://scout-datawire-io/emissary-ingress/$(PROMOTE_CHANNEL)app.json$(END)\n'
-	printf '{"application":"emissary","latest_version":"%s","notices":[]}' "$(patsubst v%,%,$(VERSION))" | aws s3 cp - s3://scout-datawire-io/emissary-ingress/$(PROMOTE_CHANNEL)app.json
+	@printf '  pushing $(CYN)s3://scout-datawire-io/emissary-ingress/$(PROMOTE_CHANNEL)app.json$(END)...\n'
+	printf '{"application":"emissary","latest_version":"%s","notices":[]}' "$(patsubst v%,%,$(PROMOTE_TO_VERSION))" | aws s3 cp - s3://scout-datawire-io/emissary-ingress/$(PROMOTE_CHANNEL)app.json
 .PHONY: release/promote-oss/.main
 
 release/promote-oss/to-rc: $(tools/devversion)
@@ -505,9 +505,10 @@ release/promote-oss/to-rc: $(tools/devversion)
 	  printf "$(CYN)==> $(GRN)found version $(BLU)$$dev_version$(GRN).$(END)\n"; \
 	  $(MAKE) release/promote-oss/.main \
 	    PROMOTE_FROM_VERSION="$$dev_version" \
-	    PROMOTE_FROM_REPO=$(DEV_REGISTRY) \
-	    PROMOTE_TO_VERSION="$(patsubst v%,%,$(VERSION))" \
-	    PROMOTE_CHANNEL=test ; \
+	    PROMOTE_TO_VERSION='$(VERSION)' \
+	    PROMOTE_FROM_REPO='$(DEV_REGISTRY)/$(REPO)' \
+	    PROMOTE_TO_REPO='$(RELEASE_REGISTRY)/$(REPO)' \
+	    PROMOTE_CHANNEL='test'; \
 	}
 ifneq ($(IS_PRIVATE),)
 	echo "Not publishing charts or manifests because in a private repo" >&2
@@ -528,10 +529,10 @@ release/promote-oss/to-ga: $(tools/devversion)
 	  printf "$(CYN)==> $(GRN)found version $(BLU)$$dev_version$(GRN).$(END)\n"; \
 	  $(MAKE) release/promote-oss/.main \
 	    PROMOTE_FROM_VERSION="$$dev_version" \
-	    PROMOTE_FROM_REPO=$(DEV_REGISTRY) \
-	    PROMOTE_TO_VERSION="$(patsubst v%,%,$(VERSION))" \
-	    PROMOTE_CHANNEL= \
-	    ; \
+	    PROMOTE_TO_VERSION='$(VERSION)' \
+	    PROMOTE_FROM_REPO='$(DEV_REGISTRY)/$(REPO)' \
+	    PROMOTE_TO_REPO='$(RELEASE_REGISTRY)/$(REPO)' \
+	    PROMOTE_CHANNEL=''; \
 	}
 ifneq ($(IS_PRIVATE),)
 	echo "Not publishing charts or manifests because in a private repo" >&2