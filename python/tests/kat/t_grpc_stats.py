6f14b47e417ee478914d9d972bdf31c279fc9811
Luke Shumaker <lukeshu@datawire.io>
2022-06-21 22:01:21 -0600

    t_grpc_stats.py: Consolidate
    This shouldn't be changing anything (except for the exact naming of the
    tests); it's just consolidating a lot of duplication to make the tests
    easier to understand.
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- python/tests/kat/t_grpc_stats.py
+++ python/tests/kat/t_grpc_stats.py
@@ -1,24 +1,112 @@
-from typing import Generator, Tuple, Union
+import json
+from typing import Any, Dict, Generator, List, Literal, Tuple, Union
 
 from kat.harness import Query
+from abstract_tests import AmbassadorTest, ServiceType, EGRPC, Node
 
-from abstract_tests import AmbassadorTest, EGRPC, Node
 
-class AcceptanceGrpcStatsTest(AmbassadorTest):
-    def init(self):
+class GRPCStatsTest(AmbassadorTest):
+    target: ServiceType
+
+    variant_name: str
+    cfg: Dict[str, Any]
+    present_metrics: List[str]
+    absent_metrics: List[str]
+
+    @classmethod
+    def variants(cls) -> Generator[Node, None, None]:
+        for cfgname in ['everything', 'onlyselectedservices', 'noupstreamallmethodsfalseinvalidkeys']:
+            yield cls(cfgname=cfgname, name="{self.variant_name}")
+
+    def init(self, cfgname: Literal['everything', 'onlyselectedservices', 'noupstreamallmethodsfalseinvalidkeys']):
         self.target = EGRPC()
+        self.variant_name = cfgname
+        if cfgname == 'everything':
+            self.cfg = {
+                'all_methods': True,
+                'upstream_stats': True,
+            }
+            self.present_metrics = [
+                'envoy_cluster_grpc_EchoService_0',
+                'envoy_cluster_grpc_EchoService_13',
+                'envoy_cluster_grpc_EchoService_request_message_count',
+                'envoy_cluster_grpc_EchoService_response_message_count',
+                'envoy_cluster_grpc_EchoService_success',
+                'envoy_cluster_grpc_EchoService_total',
+                # present only when enable_upstream_stats is true
+                'envoy_cluster_grpc_EchoService_upstream_rq_time',
+            ]
+            self.absent_metrics = [
+                # since all_methods is true, we should not see the generic metrics
+                'envoy_cluster_grpc_0',
+                'envoy_cluster_grpc_13',
+                'envoy_cluster_grpc_request_message_count',
+                'envoy_cluster_grpc_response_message_count',
+                'envoy_cluster_grpc_success',
+                'envoy_cluster_grpc_total',
+            ]
+        elif cfgname == 'onlyselectedservices':
+            self.cfg = {
+                'all_methods': True,  # this will be ignored
+                'services': [
+                    {
+                        'name': 'echo.EchoService',
+                        'method_names': ['Echo'],
+                    },
+                ],
+            }
+            self.present_metrics = [
+                'envoy_cluster_grpc_EchoService_0',
+                'envoy_cluster_grpc_EchoService_13',
+                'envoy_cluster_grpc_EchoService_request_message_count',
+                'envoy_cluster_grpc_EchoService_response_message_count',
+                'envoy_cluster_grpc_EchoService_success',
+                'envoy_cluster_grpc_EchoService_total',
+            ]
+            self.absent_metrics = [
+                # upstream stats is disabled
+                'envoy_cluster_grpc_upstream_rq_time',
+                # the generic metrics shouldn't be present since all the methods being called are on
+                # the allowed list
+                'envoy_cluster_grpc_0',
+                'envoy_cluster_grpc_13',
+                'envoy_cluster_grpc_request_message_count',
+                'envoy_cluster_grpc_response_message_count',
+                'envoy_cluster_grpc_success',
+                'envoy_cluster_grpc_total',
+            ]
+        elif cfgname == 'noupstreamallmethodsfalseinvalidkeys':
+            self.cfg = {
+                'all_methods': False,
+                'upstream_stats': False,
+                'i_will_not_break_envoy': True
+            }
+            # stat_all_methods is disabled and the list of services is empty, so we should only see
+            # generic metrics
+            self.present_metrics = [
+                'envoy_cluster_grpc_0',
+                'envoy_cluster_grpc_13',
+                'envoy_cluster_grpc_request_message_count',
+                'envoy_cluster_grpc_response_message_count',
+                'envoy_cluster_grpc_success',
+                'envoy_cluster_grpc_total',
+            ]
+            self.absent_metrics = [
+                'envoy_cluster_grpc_upstream_rq_time',
+                'envoy_cluster_grpc_EchoService_0'
+            ]
+        else:
+            assert False, f"invalid cfgname={repr(cfgname)}"
 
     def config(self) -> Generator[Union[str, Tuple[Node, str]], None, None]:
-        yield self, self.format("""
+        yield self, f"""
 ---
 apiVersion: getambassador.io/v3alpha1
 kind: Module
 name: ambassador
 config:
-    grpc_stats:
-        all_methods: true
-        upstream_stats: true
-""")
+    grpc_stats: {json.dumps(self.cfg)}
+"""
 
         yield self, self.format("""
 ---
@@ -51,228 +139,22 @@ service: http://127.0.0.1:8877
                         grpc_type="real",
                         phase=1)
 
+        # [1] through [10]
         for i in range(10):
             yield Query(self.url("echo.EchoService/Echo"),
                         headers={ "content-type": "application/grpc", "kat-req-echo-requested-status": "13" },
                         grpc_type="real",
                         phase=1)
 
-        # [1]
+        # [-1]
         yield Query(self.url("metrics"), phase=2)
 
 
     def check(self):
-        # [0]
         stats = self.results[-1].text
 
-        metrics = [
-            'envoy_cluster_grpc_EchoService_0',
-            'envoy_cluster_grpc_EchoService_13',
-            'envoy_cluster_grpc_EchoService_request_message_count',
-            'envoy_cluster_grpc_EchoService_response_message_count',
-            'envoy_cluster_grpc_EchoService_success',
-            'envoy_cluster_grpc_EchoService_total',
-            # present only when enable_upstream_stats is true
-            'envoy_cluster_grpc_EchoService_upstream_rq_time'
-        ]
-
-        # these metrics SHOULD NOT be there based on the filter config
-        absent_metrics = [
-            # since all_methods is true, we should not see the generic metrics
-            'envoy_cluster_grpc_0'
-            'envoy_cluster_grpc_13',
-            'envoy_cluster_grpc_request_message_count',
-            'envoy_cluster_grpc_response_message_count',
-            'envoy_cluster_grpc_success',
-            'envoy_cluster_grpc_total'
-        ]
-
-        # check if the metrics are there
-        for metric in metrics:
+        for metric in self.present_metrics:
             assert metric in stats, f'coult not find metric: {metric}'
 
-        for absent_metric in absent_metrics:
-            assert absent_metric not in stats, f'metric {metric} should not be present'
-
-        # check if the metrics are there
-        for metric in metrics:
-            assert metric in stats, f'coult not find metric: {metric}'
-
-
-class GrpcStatsTestOnlySelectedServices(AmbassadorTest):
-    def init(self):
-        self.target = EGRPC()
-
-    def config(self) -> Generator[Union[str, Tuple[Node, str]], None, None]:
-        yield self, self.format("""
----
-apiVersion: getambassador.io/v3alpha1
-kind: Module
-name: ambassador
-config:
-    grpc_stats:
-        # this will be ignored
-        all_methods: true
-        services:
-            - name: echo.EchoService
-              method_names: [Echo]
-""")
-
-        yield self, self.format("""
----
-apiVersion: getambassador.io/v3alpha1
-kind: Mapping
-grpc: True
-hostname: "*"
-prefix: /echo.EchoService/
-rewrite: /echo.EchoService/
-name:  {self.target.path.k8s}
-service: {self.target.path.k8s}
-""")
-
-        yield self, self.format("""
-apiVersion: getambassador.io/v3alpha1
-kind: Mapping
-name:  metrics
-hostname: "*"
-prefix: /metrics
-rewrite: /metrics
-service: http://127.0.0.1:8877
-""")
-
-
-    def queries(self):
-        # [0]
-        for i in range(10):
-            yield Query(self.url("echo.EchoService/Echo"),
-                        headers={ "content-type": "application/grpc", "kat-req-echo-requested-status": "0" },
-                        grpc_type="real",
-                        phase=1)
-
-        for i in range(10):
-            yield Query(self.url("echo.EchoService/Echo"),
-                        headers={ "content-type": "application/grpc", "kat-req-echo-requested-status": "13" },
-                        grpc_type="real",
-                        phase=1)
-
-        # [1]
-        yield Query(self.url("metrics"), phase=2)
-
-
-    def check(self):
-        # [0]
-        stats = self.results[-1].text
-
-        metrics = [
-            'envoy_cluster_grpc_EchoService_0',
-            'envoy_cluster_grpc_EchoService_13',
-            'envoy_cluster_grpc_EchoService_request_message_count',
-            'envoy_cluster_grpc_EchoService_response_message_count',
-            'envoy_cluster_grpc_EchoService_success',
-            'envoy_cluster_grpc_EchoService_total',
-        ]
-
-        # these metrics SHOULD NOT be there based on the filter config
-        absent_metrics = [
-            # upstream stats is disabled
-            'envoy_cluster_grpc_upstream_rq_time',
-            # the generic metrics shouldn't be present since all the methods being called are on the allowed list
-            'envoy_cluster_grpc_0'
-            'envoy_cluster_grpc_13',
-            'envoy_cluster_grpc_request_message_count',
-            'envoy_cluster_grpc_response_message_count',
-            'envoy_cluster_grpc_success',
-            'envoy_cluster_grpc_total'
-        ]
-
-        # check if the metrics are there
-        for metric in metrics:
-            assert metric in stats, f'coult not find metric: {metric}'
-
-        for absent_metric in absent_metrics:
-            assert absent_metric not in stats, f'metric {metric} should not be present'
-
-class GrpcStatsTestNoUpstreamAllMethodsFalseInvalidKeys(AmbassadorTest):
-    def init(self):
-        self.target = EGRPC()
-
-    def config(self) -> Generator[Union[str, Tuple[Node, str]], None, None]:
-        yield self, self.format("""
----
-apiVersion: getambassador.io/v3alpha1
-kind: Module
-name: ambassador
-config:
-    grpc_stats:
-        all_methods: false
-        upstream_stats: false
-        i_will_not_break_envoy: true
-""")
-
-        yield self, self.format("""
----
-apiVersion: getambassador.io/v3alpha1
-kind: Mapping
-grpc: True
-hostname: "*"
-prefix: /echo.EchoService/
-rewrite: /echo.EchoService/
-name:  {self.target.path.k8s}
-service: {self.target.path.k8s}
-""")
-
-        yield self, self.format("""
-apiVersion: getambassador.io/v3alpha1
-kind: Mapping
-name:  metrics
-hostname: "*"
-prefix: /metrics
-rewrite: /metrics
-service: http://127.0.0.1:8877
-""")
-
-
-    def queries(self):
-        # [0]
-        for i in range(10):
-            yield Query(self.url("echo.EchoService/Echo"),
-                        headers={ "content-type": "application/grpc", "kat-req-echo-requested-status": "0" },
-                        grpc_type="real",
-                        phase=1)
-
-        for i in range(10):
-            yield Query(self.url("echo.EchoService/Echo"),
-                        headers={ "content-type": "application/grpc", "kat-req-echo-requested-status": "13" },
-                        grpc_type="real",
-                        phase=1)
-
-        # [1]
-        yield Query(self.url("metrics"), phase=2)
-
-
-    def check(self):
-        # [0]
-        stats = self.results[-1].text
-
-        # stat_all_methods is disabled and the list of services is empty, so we should only see generic metrics
-        metrics = [
-            'envoy_cluster_grpc_0',
-            'envoy_cluster_grpc_13',
-            'envoy_cluster_grpc_request_message_count',
-            'envoy_cluster_grpc_response_message_count',
-            'envoy_cluster_grpc_success',
-            'envoy_cluster_grpc_total',
-        ]
-
-        # these metrics SHOULD NOT be there based on the filter config
-        absent_metrics = [
-            'envoy_cluster_grpc_upstream_rq_time',
-            'envoy_cluster_grpc_EchoService_0'
-        ]
-
-        # check if the metrics are there
-        for metric in metrics:
-            assert metric in stats, f'coult not find metric: {metric}'
-
-        for absent_metric in absent_metrics:
-            assert absent_metric not in stats, f'metric {metric} should not be present'
+        for metric in self.absent_metrics:
+            assert metric not in stats, f'metric {metric} should not be present'
