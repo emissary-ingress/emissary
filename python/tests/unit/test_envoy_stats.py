2f2794c2a2f9e1a55f3fc35e931de34547b7ecab
Luke Shumaker <lukeshu@datawire.io>
2022-06-27 13:15:30 -0600

    python/tests/unit: Consolidate testdata files
    That's just too many differently-named directories for me to exclude from
    `git grep`.  Also, fixup how test_envoy_stats.py determines the test
    directory.
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>
    (cherry picked from commit cc3e2c30d1bff1755e70f30270c36b0ecf4ab65a)

--- python/tests/unit/test_envoy_stats.py
+++ python/tests/unit/test_envoy_stats.py
@@ -20,12 +20,9 @@ from ambassador.diagnostics import EnvoyStats, EnvoyStatsMgr
 
 class EnvoyStatsMocker:
     def __init__(self) -> None:
-        current_test = os.environ.get("PYTEST_CURRENT_TEST")
-
-        assert current_test is not None, "PYTEST_CURRENT_TEST is not set??"
-
         self.test_dir = os.path.join(
-            os.path.dirname(current_test.split("::")[0]), "test_envoy_stats_data"
+            os.path.dirname(os.path.abspath(__file__)),
+            "testdata",
         )
 
         self.log_idx = 0
