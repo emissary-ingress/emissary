54f2f840cad6e2b74a6d883a1101e44b65d0ae55
Kévin LAMBERT <kevin.lambert.ca@gmail.com>
2022-07-21 16:20:54 -0400

    Add a secret sync directive to the agent (#4325)
    * Add a secret sync directive to the agent
    
    Signed-off-by: Kévin Lambert <kevin.lambert.ca@gmail.com>
    
    * Update changelog
    
    Signed-off-by: Kévin Lambert <kevin.lambert.ca@gmail.com>
    
    * Remove unnecessary command interface
    
    Signed-off-by: Kévin Lambert <kevin.lambert.ca@gmail.com>
    
    * Reorganize functions from secrets files in the agent package
    
    Signed-off-by: Kévin Lambert <kevin.lambert.ca@gmail.com>
    
    * Secret sync command panics when the action is unkown
    
    Signed-off-by: Kévin Lambert <kevin.lambert.ca@gmail.com>
    
    * Improved test coverage for secrets
    
    Signed-off-by: Kévin Lambert <kevin.lambert.ca@gmail.com>
    
    * Fix test & linting
    
    Signed-off-by: Kévin Lambert <kevin.lambert.ca@gmail.com>
    
    * Remove panic in secret directive operations generator & add docstrings
    
    Signed-off-by: Kévin Lambert <kevin.lambert.ca@gmail.com>
    
    * Fix typo in changelog
    
    Signed-off-by: Kévin Lambert <kevin.lambert.ca@gmail.com>
    
    * Allow nil value for secret
    
    Signed-off-by: Kévin Lambert <kevin.lambert.ca@gmail.com>
    
    * Allow empty secret creation as a placeholder
    
    Signed-off-by: Kévin Lambert <kevin.lambert.ca@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -95,6 +95,9 @@ it will be removed; but as it won't be user-visible this isn't considered a brea
   from emissary ingress before sending them to Ambassador cloud. This issue has been resolved to
   ensure that all the nodes composing the emissary ingress cluster are reporting properly.
 
+- Feature: Adds a new command to the agent directive service to manage secrets. This allows a third
+  party product to manage CRDs that depend upon a secret.
+
 ## [3.0.0] June 27, 2022
 [3.0.0]: https://github.com/emissary-ingress/emissary/compare/v2.3.2...v3.0.0
 
--- pkg/agent/directive_handler_test.go
+++ pkg/agent/directive_handler_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/datawire/dlib/dlog"
 	"github.com/emissary-ingress/emissary/v3/pkg/agent"
 	agentTypes "github.com/emissary-ingress/emissary/v3/pkg/api/agent"
+	agentapi "github.com/emissary-ingress/emissary/v3/pkg/api/agent"
 )
 
 func TestHandleDirective(t *testing.T) {
@@ -16,5 +17,33 @@ func TestHandleDirective(t *testing.T) {
 
 	d := &agentTypes.Directive{ID: "one"}
 
+	dh.HandleDirective(ctx, a, d)
+}
+
+func TestHandleSecretSyncDirective(t *testing.T) {
+	// given
+	ctx := dlog.NewTestContext(t, false)
+
+	a := &agent.Agent{}
+	dh := &agent.BasicDirectiveHandler{}
+
+	d := &agentTypes.Directive{
+		ID: "one",
+		Commands: []*agentapi.Command{
+			{
+				SecretSyncCommand: &agentapi.SecretSyncCommand{
+					Name:      "my-secret",
+					Namespace: "my-namespace",
+					CommandId: "1234",
+					Action:    agentapi.SecretSyncCommand_SET,
+					Secret: map[string][]byte{
+						"my-key": []byte("abcd"),
+					},
+				},
+			},
+		},
+	}
+
+	// when
 	dh.HandleDirective(ctx, a, d)
 }
