1e268efd04af1056d53aa556c1aac026945ebc08
njayp <nickjaypowell@gmail.com>
2022-08-24 10:44:33 -0700

    import agent from new repo
    Signed-off-by: njayp <nickjaypowell@gmail.com>

--- cmd/busyambassador/main.go
+++ cmd/busyambassador/main.go
@@ -13,11 +13,11 @@ import (
 	"strings"
 
 	// 1st-party libs
+	aa "github.com/datawire/ambassador-agent"
 	"github.com/emissary-ingress/emissary/v3/pkg/busy"
 	"github.com/emissary-ingress/emissary/v3/pkg/environment"
 
 	// commands
-	"github.com/emissary-ingress/emissary/v3/cmd/agent"
 	"github.com/emissary-ingress/emissary/v3/cmd/apiext"
 	"github.com/emissary-ingress/emissary/v3/cmd/entrypoint"
 	"github.com/emissary-ingress/emissary/v3/cmd/kubestatus"
@@ -57,7 +57,7 @@ func main() {
 		"kubestatus": {Setup: environment.EnvironmentSetupEntrypoint, Run: kubestatus.Main},
 		"entrypoint": {Setup: noop, Run: entrypoint.Main},
 		"reproducer": {Setup: noop, Run: reproducer.Main},
-		"agent":      {Setup: environment.EnvironmentSetupEntrypoint, Run: agent.Main},
+		"agent":      {Setup: environment.EnvironmentSetupEntrypoint, Run: aa.Main},
 		"version":    {Setup: noop, Run: showVersion},
 		"apiext":     {Setup: noop, Run: apiext.Main},
 	})
