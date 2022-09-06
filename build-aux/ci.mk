185f701eaba18a31005d2aa0a08c663f1404a003
Luke Shumaker <lukeshu@datawire.io>
2022-09-06 11:28:39 -0600

    make ci/setup-k3d: Fix K3D_CLUSTER_NAME
    The prior commit stops CI from using this toggle, but it's still useful
    for local dev; so fix it.
    
    Signed-off-by: Luke Shumaker <lukeshu@datawire.io>

--- build-aux/ci.mk
+++ build-aux/ci.mk
@@ -7,7 +7,7 @@ K3D_ARGS         = --k3s-server-arg=--no-deploy=traefik --k3s-server-arg=--kubel
 # https://github.com/nolar/setup-k3d-k3s/blob/v1.0.7/action.sh#L70-L77 and
 # https://github.com/nolar/setup-k3d-k3s/blob/v1.0.7/action.yaml#L34-L46
 ci/setup-k3d: $(tools/k3d) $(tools/kubectl)
-	$(tools/k3d) cluster create --wait --image=docker.io/rancher/k3s:v$(subst +,-,$(K3S_VERSION)) $(K3D_ARGS)
+	$(tools/k3d) cluster create --wait --image=docker.io/rancher/k3s:v$(subst +,-,$(K3S_VERSION)) $(K3D_ARGS) $(K3D_CLUSTER_NAME)
 	while ! $(tools/kubectl) get serviceaccount default >/dev/null; do sleep 1; done
 	$(tools/kubectl) version
 .PHONY: ci/setup-k3d
