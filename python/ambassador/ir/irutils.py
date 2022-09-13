4ab60c4a5e79a2c38bc2c662b8e44c26f37e1caf
AliceProxy <alicewasko@datawire.io>
2022-09-13 16:33:05 -0700

    label selectors: add strict match opt-out env variable
    Signed-off-by: AliceProxy <alicewasko@datawire.io>

--- python/ambassador/ir/irutils.py
+++ python/ambassador/ir/irutils.py
@@ -12,9 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
+import logging
+import os
 from typing import Any, Dict
 
-import logging
+from ambassador.utils import parse_bool
 
 ######
 # Utilities for hostglob_matches
@@ -176,11 +178,19 @@ def selector_matches(logger: logging.Logger, selector: Dict[str, Any], labels: D
         logger.debug("    no incoming labels => False")
         return False
 
+    # Ambassador (2.0-2.3) & (3.0-3.1) consider a match on a single label as a "good enough" match.
+    # In versions 2.4+ and 3.2+ _ALL_ labels in a selector must be present for it to be considered a match.
+    # DISABLE_STRICT_LABEL_SELECTORS provides a way to restore the old unintended loose matching behaviour
+    # in the event that it is desired. The ability to disable strict label matching will be removed in a future version.
+    disable_strict_selectors = parse_bool(os.environ.get("DISABLE_STRICT_LABEL_SELECTORS", "false"))
+
     # For every label in mappingSelector, there must be a label with same value in Mapping itself.
     for k, v in match.items():
         if labels.get(k) == v:
             logger.debug("    selector match for %s=%s => True", k, v)
-        else:
+            if disable_strict_selectors:
+                return True
+        elif not disable_strict_selectors:
             logger.debug("    selector miss for %s=%s => False", k, v)
             return False
 
