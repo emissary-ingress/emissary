e139053983c86abf078f477136e77b9e1ef8178f
Alexis <alexis.munoz@unosquare.com>
2022-09-16 12:56:53 -0500

    Resolves mismatched cluster names on envoy_stats
    Signed-off-by: Alexis <alexis.munoz@unosquare.com>

--- python/ambassador/diagnostics/diagnostics.py
+++ python/ambassador/diagnostics/diagnostics.py
@@ -118,8 +118,10 @@ class DiagResult:
 
         # Go ahead and grab Envoy cluster stats for all possible clusters.
         # XXX This might be a bit silly.
-        self.cluster_names = [cluster.envoy_name for cluster in self.diag.clusters.values()]
-        self.cstats = {name: self.estat.cluster_stats(name) for name in self.cluster_names}
+        self.cstats = {
+            cluster.envoy_name: self.estat.cluster_stats(cluster.stats_name)
+            for cluster in self.diag.clusters.values()
+        }
 
         # Save the request host and scheme. We'll need them later.
         self.request_host = request.headers.get("Host", "*")
