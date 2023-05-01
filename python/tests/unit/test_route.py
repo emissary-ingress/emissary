a183e94d38f14b8082d2e0bf53f177c2daaf3c1e
Lance Austin <laustin@datawire.io>
2023-05-01 16:38:16 -0500

    envoy: add tests for headers_to_add
    The current Envoy config is generated to use the deprecated `append`
    field on the `config.core.v3.HeaderValueOption`.  This adds a test to
    the generation function to verify current behavior.
    
    A follow-up commit will switch to using the recommend `append_action`
    field and will maintain backwards compatibility.
    
    Signed-off-by: Lance Austin <laustin@datawire.io>

--- python/tests/unit/test_route.py
+++ python/tests/unit/test_route.py
@@ -1,5 +1,6 @@
 import pytest
 
+from ambassador.envoy.v3.v3route import V3Route
 from tests.utils import econf_compile, econf_foreach_hcm, module_and_mapping_manifests
 
 
@@ -60,3 +61,40 @@ def test_timeout_ms_both():
     # If we set a default on the Module, it should override the usual default of 3000ms.
     yaml = module_and_mapping_manifests(["cluster_request_timeout_ms: 9000"], ["timeout_ms: 5001"])
     _test_route(yaml, expectations={"timeout": "5.001s"})
+
+
+@pytest.mark.compilertest
+def test_generate_headers_to_add():
+    input_headers = {
+        "x-test-proto": "%PROTOCOL%",
+        "x-test-ip": "%DOWNSTREAM_REMOTE_ADDRESS_WITHOUT_PORT%",
+        "x-test-static": "This is a test header",
+        "x-test-append": {"append": False, "value": "this will not append header if already exist"},
+        "x-test-no-append": {"append": True, "value": "this will allow appending if header exist"},
+    }
+
+    expected = [
+        {"header": {"key": "x-test-proto", "value": "%PROTOCOL%"}, "append": True},
+        {
+            "header": {"key": "x-test-ip", "value": "%DOWNSTREAM_REMOTE_ADDRESS_WITHOUT_PORT%"},
+            "append": True,
+        },
+        {"header": {"key": "x-test-static", "value": "This is a test header"}, "append": True},
+        {
+            "header": {
+                "key": "x-test-append",
+                "value": "this will not append header if already exist",
+            },
+            "append": False,
+        },
+        {
+            "header": {
+                "key": "x-test-no-append",
+                "value": "this will allow appending if header exist",
+            },
+            "append": True,
+        },
+    ]
+
+    result = V3Route.generate_headers_to_add(input_headers)
+    assert expected == result
