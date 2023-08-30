2c1293e8995ad43755052064853824ba95ec53c4
Tenshin Higashi <thigashi@datawire.io>
2023-08-30 13:17:18 -0400

    Adding test
    Signed-off-by: Tenshin Higashi <thigashi@datawire.io>

--- python/tests/kat/t_envoy_logs.py
+++ python/tests/kat/t_envoy_logs.py
@@ -83,3 +83,41 @@ config:
             assert access_log_entry_regex.match(
                 line
             ), f"{line} does not match {access_log_entry_regex}"
+
+
+class EnvoyLogTypeJSONTest(AmbassadorTest):
+    target: ServiceType
+    log_path: str
+
+    def init(self):
+        self.target = HTTP()
+        self.log_path = "/tmp/ambassador/ambassador.log"
+
+    def config(self) -> Generator[Union[str, Tuple[Node, str]], None, None]:
+        yield self, self.format(
+            """
+---
+apiVersion: getambassador.io/v3alpha1
+kind: Module
+name: ambassador
+ambassador_id: [{self.ambassador_id}]
+config:
+  envoy_log_path: {self.log_path}
+  envoy_log_format:
+    protocol: "%PROTOCOL%"
+    duration: "%DURATION%"
+  envoy_log_type: typed_json
+"""
+        )
+
+    def check(self):
+        access_log_entry_regex = re.compile('^({"duration":|{"protocol":)')
+
+        cmd = ShellCommand("tools/bin/kubectl", "exec", self.path.k8s, "cat", self.log_path)
+        if not cmd.check("check envoy access log"):
+            pytest.exit("envoy access log does not exist")
+
+        for line in cmd.stdout.splitlines():
+            assert access_log_entry_regex.match(
+                line
+            ), f"{line} does not match {access_log_entry_regex}"
