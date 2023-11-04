7e23a9b9248e7e858b25f9e5afb53fdc8945132d
Alice Wasko <alicewasko@datawire.io>
2023-11-03 17:32:42 -0700

    add module support for envoy runtime flags
    Signed-off-by: Alice Wasko <alicewasko@datawire.io>

--- python/ambassador/envoy/v3/v3bootstrap.py
+++ python/ambassador/envoy/v3/v3bootstrap.py
@@ -32,22 +32,7 @@ class V3Bootstrap(dict):
                     "lds_config": {"ads": {}, "resource_api_version": api_version},
                 },
                 "admin": dict(config.admin),
-                "layered_runtime": {
-                    "layers": [
-                        {
-                            "name": "static_layer",
-                            "static_layer": {
-                                "re2.max_program_size.error_level": 200,
-                                # TODO(lance):
-                                # the new default is that all filters are looked up using the @type which currently we exclude on a lot of
-                                # our filters. This will ensure we do not break current config. We can migrate over
-                                # in a minor release. see here: https://www.envoyproxy.io/docs/envoy/v1.22.0/version_history/current#minor-behavior-changes
-                                # The biggest impact of this is ensuring that ambex imports all the types because we will need to import many more
-                                "envoy.reloadable_features.no_extension_lookup_by_name": False,
-                            },
-                        }
-                    ]
-                },
+                "layered_runtime": dict(config.layered_runtime),
             }
         )
 
