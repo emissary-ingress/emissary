3df8aefb58eaaf5f63183b89d6c3935a3d922693
Tenshin Higashi <thigashi@datawire.io>
2023-11-16 11:44:14 -0500

    Adding collision testing
    Signed-off-by: Tenshin Higashi <thigashi@datawire.io>

--- pkg/ambex/transforms_test.go
+++ pkg/ambex/transforms_test.go
@@ -1,6 +1,7 @@
 package ambex
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -17,7 +18,6 @@ import (
 )
 
 func TestV3ListenerToRdsListener(t *testing.T) {
-
 	testRoute := &v3Route.Route_Route{
 		Route: &v3Route.RouteAction{
 			ClusterSpecifier: &v3Route.RouteAction_Cluster{
@@ -50,12 +50,19 @@ func TestV3ListenerToRdsListener(t *testing.T) {
 	anyTestHcm, err := anypb.New(testHcm)
 	require.NoError(t, err)
 
+	//Create a second identical Hcm
+	anyTestHcm2, err := anypb.New(testHcm)
+	require.NoError(t, err)
+
 	testListener := &v3Listener.Listener{
 		Name: "emissary-ingress-listener-8080",
 		FilterChains: []*v3Listener.FilterChain{{
 			Filters: []*v3Listener.Filter{{
 				Name:       v3Wellknown.HTTPConnectionManager,
 				ConfigType: &v3Listener.Filter_TypedConfig{TypedConfig: anyTestHcm},
+			}, {
+				Name:       v3Wellknown.HTTPConnectionManager,
+				ConfigType: &v3Listener.Filter_TypedConfig{TypedConfig: anyTestHcm2},
 			}},
 			FilterChainMatch: &v3Listener.FilterChainMatch{
 				DestinationPort: &wrapperspb.UInt32Value{Value: uint32(8080)},
@@ -66,17 +73,17 @@ func TestV3ListenerToRdsListener(t *testing.T) {
 	_, routes, err := V3ListenerToRdsListener(testListener)
 	require.NoError(t, err)
 
-	assert.Equal(t, 1, len(routes))
+	//Should have 2 routes
+	assert.Equal(t, 2, len(routes))
 
-	for _, rc := range routes {
+	for i, rc := range routes {
 		// Confirm that the route name was transformed to the hashed version
-		assert.Equal(t, "emissary-ingress-listener-8080-routeconfig-8c82e45fa3f94ab4e879543e0a1a30ac-0", rc.GetName())
+		assert.Equal(t, fmt.Sprintf("emissary-ingress-listener-8080-routeconfig-8c82e45fa3f94ab4e879543e0a1a30ac-%d", i), rc.GetName())
 
 		// Make sure the virtual hosts are unmodified
 		virtualHosts := rc.GetVirtualHosts()
 		assert.Equal(t, 1, len(virtualHosts))
 		assert.Equal(t, "emissary-ingress-listener-8080-*", virtualHosts[0].GetName())
 		assert.Equal(t, []string{"*"}, virtualHosts[0].GetDomains())
-
 	}
 }
