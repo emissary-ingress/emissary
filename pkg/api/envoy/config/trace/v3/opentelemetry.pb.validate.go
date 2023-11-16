7b7be436c59cf12f23690a5b9732416d8a5bdbff
Lance Austin <laustin@dataiwre.io>
2023-11-16 19:29:09 +0000

    deps: update to envoy 1.28.0
    Bumps to Envoy 1.28.0 and regenerates compiled protos.
    
    steps:
    1. update envoy.mk to v1.28 commit with custom commits
    2. ran `make update-base`
    
    Signed-off-by: Lance Austin <laustin@dataiwre.io>

--- pkg/api/envoy/config/trace/v3/opentelemetry.pb.validate.go
+++ pkg/api/envoy/config/trace/v3/opentelemetry.pb.validate.go
@@ -86,6 +86,35 @@ func (m *OpenTelemetryConfig) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetHttpService()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, OpenTelemetryConfigValidationError{
+					field:  "HttpService",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, OpenTelemetryConfigValidationError{
+					field:  "HttpService",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetHttpService()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return OpenTelemetryConfigValidationError{
+				field:  "HttpService",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	// no validation rules for ServiceName
 
 	if len(errors) > 0 {
