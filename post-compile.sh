43aab8470dfedcb26a173821933b7afe41b12d06
Lance Austin <laustin@datawire.io>
2023-11-17 15:04:07 -0600

    cleanup: remove post installers and no longer used files
    This cleans up the optimized docker image to no longer
    run installers. These are not used at all and are around from
    pre emissary-ingress days.
    
    Simplified optimized image to reduce footprint.
    
    Signed-off-by: Lance Austin <laustin@datawire.io>

--- post-compile.sh
+++ post-compile.sh
@@ -3,7 +3,6 @@ set -e
 
 busyprograms=(
     kubestatus
-    watt
     apiext
 )
 sudo install -D -t /opt/ambassador/bin/ /buildroot/bin/busyambassador
@@ -17,18 +16,3 @@ for busyprogram in "${busyprograms[@]}"; do
 done
 
 sudo install /buildroot/bin/capabilities_wrapper /opt/ambassador/bin/wrapper
-
-# Copy installer support into /opt/image-build to be run at docker build for the
-# production image. Then run the installers for the builder container.
-# Note: When this (ambassador's) post-compile runs, it always runs first, and
-# every other post-compile runs as well. So this is the place to recreate the
-# /opt/image-build tree from scratch so the builder container stays valid.
-sudo rm -rf /opt/image-build
-sudo install -D -t /opt/image-build /buildroot/ambassador/build-aux/install.sh
-sudo cp -a /buildroot/ambassador/build-aux/installers /opt/image-build/
-sudo /opt/image-build/install.sh
-
-# run any extra, local post-compile task
-if [ -f post-compile.local.sh ] ; then
-    bash post-compile.local.sh
-fi
