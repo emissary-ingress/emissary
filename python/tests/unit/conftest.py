5d99d3b0925963ad8436af373062c85998605250
Lance Austin <laustin@datawire.io>
2024-01-03 16:30:21 -0600

    python: cleanup noop env-vars
    This cleans up some env-vars that are not used and are effectively no-ops.
    
    Signed-off-by: Lance Austin <laustin@datawire.io>

--- python/tests/unit/conftest.py
+++ python/tests/unit/conftest.py
@@ -9,17 +9,3 @@ def go_library():
     with patch("ambassador.ir.irgofilter.go_library_exists") as go_library_exists:
         go_library_exists.return_value = True
         yield go_library_exists
-
-
-@pytest.fixture(params=["true", "yes", "1", "True", "YES"])
-def disable_go_filter(request):
-    with patch("ambassador.ir.irgofilter.go_filter_disabled") as go_filter_disabled:
-        go_filter_disabled.return_value = request.param
-        yield go_filter_disabled
-
-
-@pytest.fixture(params=["false", "no", "0"])
-def enable_go_filter(request):
-    with patch("ambassador.ir.irgofilter.go_filter_disabled") as go_filter_disabled:
-        go_filter_disabled.return_value = request.param
-        yield go_filter_disabled
