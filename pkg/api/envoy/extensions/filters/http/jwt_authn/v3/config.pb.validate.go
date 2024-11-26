b545a46ff31565829406066f2c7066125449ea9a
Flynn <flynn+github@kodachi.com>
2024-11-26 15:46:38 -0500

    gmake compile-envoy-protos
    Signed-off-by: Flynn <flynn+github@kodachi.com>

--- pkg/api/envoy/extensions/filters/http/jwt_authn/v3/config.pb.validate.go
+++ pkg/api/envoy/extensions/filters/http/jwt_authn/v3/config.pb.validate.go
@@ -575,6 +575,17 @@ func (m *RemoteJwks) validate(all bool) error {
 
 	var errors []error
 
+	if m.GetHttpUri() == nil {
+		err := RemoteJwksValidationError{
+			field:  "HttpUri",
+			reason: "value is required",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
 	if all {
 		switch v := interface{}(m.GetHttpUri()).(type) {
 		case interface{ ValidateAll() error }:
@@ -2301,6 +2312,8 @@ func (m *JwtAuthentication) validate(all bool) error {
 
 		}
 	}
+
+	// no validation rules for StripFailureResponse
 
 	if len(errors) > 0 {
 		return JwtAuthenticationMultiError(errors)
